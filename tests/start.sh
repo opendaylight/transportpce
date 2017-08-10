c1be1540990ac166e53743eb679d9243e79e9f80
Cédric Ollivier <cedric.ollivier@orange.com>
2017-08-10 21:52:49 +0200

    Add submodules and scripts to run netconf-testtool
    Change-Id: Idb44b8da8caa2b927c0f82e021b8b589e5d677a5
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>

---
+++ tests/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Please see https://wiki.opendaylight.org/view/OpenDaylight_Controller:Netconf:Testtool
+# It's configured to run 1 node listening to 17830 (ssh)
+# You may put your configuration in config.xml
+
+# opts="${opts} --device-count 2"
+# opts="${opts} --debug true"
+# opts="${opts} --ssh tcp"
+# opts="${opts} --notification-file notif.xml"
+
+java -jar ./netconf/netconf/tools/netconf-testtool/target/netconf-testtool-1.3.0-SNAPSHOT-executable.jar \
+     --schemas-dir schemas \
+     --initial-config-xml-file config.xml \
+     ${opts}
+
+exit $?
