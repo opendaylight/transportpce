8dca05db31d3fa182a3119f5d35755a84e3c1a0c
Ahmed Triki <ahmed.triki@orange.com>
2018-06-28 11:22:34 +0200

    Functional tests for the topology init
    - connects successively ROADMA, XPDRA ROADMB and ROADMC and then
    disconnect them
    - checks that the clli-network, the openroadm-network and the
    openroadm-topology are well updated and the appropriate links/nodes
    are well created/deleted after each creation/deletion operation
    - checks that a degree is not used by more than one link at the same
    time
    - tests 9 and 17 currently fail beause 4 TPs appear (XPDR1-CLIENT1,
    XPDR1-NETWORK1, XPDR1-CLIENT2 and XPDR1-NETWORK2) while only 2 TPs are
    defined in the sample-config-XPDRA.xml
    - tests 24, 33 and 37 currently fail because the link ROADM-to-ROADM
    is not automatically deleted.
    A new version of the code will be proposed by ATT where these links will
    be marked as up or down:
        Up to indicate the pce that the path is available for service
        Down to indicate that the path is not available
    
    JIRA: TRNSPRTPCE-4
    Change-Id: Iba78869ad884fadd766f11c4c321ae1b4bdfcd64
    Signed-off-by: Ahmed Triki <ahmed.triki@orange.com>

---
+++ tests/buildTesttool.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# It may require to call git submodule update --init
+
+set -e
+
+yang="\
+mdsal/model/iana/iana-afn-safi/src/main/yang/iana-afn-safi@2013-07-04.yang \
+mdsal/model/ietf/ietf-inet-types-2013-07-15/src/main/yang/ietf-inet-types@2013-07-15.yang \
+mdsal/model/ietf/ietf-yang-types-20130715/src/main/yang/ietf-yang-types@2013-07-15.yang \
+netconf/netconf/models/ietf-netconf/src/main/yang/ietf-netconf@2011-06-01.yang \
+netconf/netconf/models/ietf-netconf-notifications/src/main/yang/ietf-netconf-notifications@2012-02-06.yang \
+netconf/netconf/models/ietf-netconf-notifications/src/main/yang/notifications@2008-07-14.yang"
+
+rm -rf netconf mdsal && git submodule update --init
+(cd netconf && patch -p1 < ../netconf.patch && patch -p1 < ../get_connection_port_trail.patch)
+(cd netconf/netconf/tools/netconf-testtool && mvn clean install -DskipTests)
+rm -rf schemas && mkdir -p schemas
+cp ordmodels_1.2.1/org-openroadm-* schemas
+cp ${yang} schemas
+
+exit $?
