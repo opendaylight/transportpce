d65920b0d9ce9ec805ae9dc6c074bbe0704f05b6
doha.khaled <dkhaled.ext@orange.com>
2018-11-22 15:17:32 +0200

    OLM unit testing update
    Change-Id: Ic513dd8d5e36fd35b436cd4d397d7f2eb80c8572

---
+++ olm/src/test/java/org/opendaylight/transportpce/olm/util/RoadmLinksTest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright © 2018 Orange Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.olm.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RoadmLinksTest {
+
+    @Test
+    public void test() {
+        RoadmLinks roadmlinks0 = new RoadmLinks();
+        roadmlinks0.setDestNodeId("dest node");
+        roadmlinks0.setDestTpid("tp");
+        roadmlinks0.setSrcNodeId("src node");
+        roadmlinks0.setSrcTpId("tp");
+
+        Assert.assertEquals("dest node", roadmlinks0.getDestNodeId());
+        Assert.assertEquals("tp", roadmlinks0.getDestTpid());
+        Assert.assertEquals("src node", roadmlinks0.getSrcNodeId());
+        Assert.assertEquals("tp", roadmlinks0.getSrcTpId());
+    }
+}
