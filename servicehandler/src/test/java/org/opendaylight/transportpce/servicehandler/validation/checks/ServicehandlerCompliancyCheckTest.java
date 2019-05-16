60a34046775bcff8272c14ac209f27dce472b8f3
Rami <rmohamed.ext@orange.com>
2019-05-16 09:19:56 +0200

    Increase ServiceHandler Junit coverage
    Increase SH UT coverage for
    - ModelMappingUtils
    - MappingConstraint
    - ServiceDataStoreOperation
    - ServiceHandlerCompliancyCheck
    - ServiceHandlerTxRxcheck
    
    Change-Id: I37449064357888ea60b4c9bc7be62054da2c9c24
    Signed-off-by: Rami <rmohamed.ext@orange.com>

--- servicehandler/src/test/java/org/opendaylight/transportpce/servicehandler/validation/checks/ServicehandlerCompliancyCheckTest.java
+++ servicehandler/src/test/java/org/opendaylight/transportpce/servicehandler/validation/checks/ServicehandlerCompliancyCheckTest.java
@@ -23,9 +23,69 @@ public class ServicehandlerCompliancyCheckTest {
         ComplianceCheckResult result = ServicehandlerCompliancyCheck.check("service 1",
             new SdncRequestHeaderBuilder().setRequestId("1").setRequestSystemId("1").setNotificationUrl("1")
             .setRpcAction(RpcActions.ServiceCreate).build(),
-            ConnectionType.Service,RpcActions.ServiceCreate, true, false);
+            ConnectionType.Service,RpcActions.ServiceCreate, false, false);
 
         Assert.assertEquals("", result.getMessage());
-        Assert.assertEquals(true, result.hasPassed());
+        Assert.assertTrue(result.hasPassed());
+    }
+
+    @Test
+    public void checkServiceNameNull() {
+        ComplianceCheckResult result = ServicehandlerCompliancyCheck.check(null, null,
+                ConnectionType.Service,null, false, false);
+
+        Assert.assertEquals("Service Name (common-id for Temp service) is not set", result.getMessage());
+        Assert.assertFalse(result.hasPassed());
+    }
+
+    @Test
+    public void checkConTypeFalseAndNull() {
+        ComplianceCheckResult result = ServicehandlerCompliancyCheck.check("service 1", null,
+                null,null, true, false);
+        Assert.assertEquals("Service ConnectionType is not set", result.getMessage());
+        Assert.assertFalse(result.hasPassed());
+    }
+
+    @Test
+    public void checkSdncRequestHeaderNull() {
+        ComplianceCheckResult result = ServicehandlerCompliancyCheck.check("service 1", null,
+                ConnectionType.Service,null, true, true);
+
+        Assert.assertEquals("Service sndc-request-header is not set ", result.getMessage());
+        Assert.assertFalse(result.hasPassed());
+    }
+
+    @Test
+    public void checkRequestIdEmptyString() {
+        ComplianceCheckResult result = ServicehandlerCompliancyCheck.check("service 1",
+                new SdncRequestHeaderBuilder().setRequestId("")
+                        .setRpcAction(RpcActions.ServiceCreate).build(),
+                ConnectionType.Service, RpcActions.ServiceCreate, true, true);
+
+        Assert.assertEquals("Service sdncRequestHeader 'request-id' is not set", result.getMessage());
+        Assert.assertFalse(result.hasPassed());
+    }
+
+    @Test
+    public void checkDifferentAction() {
+        ComplianceCheckResult result = ServicehandlerCompliancyCheck.check("service 1",
+                new SdncRequestHeaderBuilder().setRequestId("1")
+                        .setRpcAction(RpcActions.ServiceCreate).build(),
+                ConnectionType.Service, RpcActions.NetworkReOptimization, true, true);
+
+        Assert.assertEquals("Service sdncRequestHeader rpc-action '" + RpcActions.ServiceCreate.name()
+                + "' not equal to '"
+                + RpcActions.NetworkReOptimization.name() + "'", result.getMessage());
+        Assert.assertFalse(result.hasPassed());
+    }
+
+    @Test
+    public void checkServiceActionNull() {
+        ComplianceCheckResult result = ServicehandlerCompliancyCheck.check("service 1",
+                new SdncRequestHeaderBuilder().setRequestId("1").build(),
+                ConnectionType.Service, RpcActions.NetworkReOptimization, true, true);
+
+        Assert.assertEquals("Service sndc-request-header 'rpc-action' is not set ", result.getMessage());
+        Assert.assertFalse(result.hasPassed());
     }
 }
---
+++ servicehandler/src/test/java/org/opendaylight/transportpce/servicehandler/validation/checks/ServicehandlerTxRxCheckTest.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright © 2019 Orange, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.servicehandler.validation.checks;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.opendaylight.transportpce.servicehandler.ServiceEndpointType;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.common.service.types.rev161014.ServiceFormat;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.common.service.types.rev161014.service.ServiceAEndBuilder;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.common.service.types.rev161014.service.endpoint.RxDirectionBuilder;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.common.service.types.rev161014.service.endpoint.TxDirectionBuilder;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.common.service.types.rev161014.service.lgx.LgxBuilder;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.common.service.types.rev161014.service.port.PortBuilder;
+
+public class ServicehandlerTxRxCheckTest {
+
+    @Test
+    public void checkPortShouldBeFalseForNullPort() {
+        Assert.assertFalse(ServicehandlerTxRxCheck.checkPort(null));
+    }
+
+    @Test
+    public void checkLgxShouldBeFalseForNullLgx() {
+        Assert.assertFalse(ServicehandlerTxRxCheck.checkLgx(null));
+    }
+
+    @Test
+    public void checkTxOrRxInfoForNullTx() {
+        ComplianceCheckResult result = ServicehandlerTxRxCheck.checkTxOrRxInfo(null, null);
+
+        Assert.assertFalse(result.hasPassed());
+        Assert.assertEquals("Service TxDirection is not correctly set", result.getMessage());
+    }
+
+    @Test
+    public void checkTxOrRxInfoForNullTxPort() {
+        ComplianceCheckResult result = ServicehandlerTxRxCheck
+                .checkTxOrRxInfo(new TxDirectionBuilder().build(), null);
+
+        Assert.assertFalse(result.hasPassed());
+        Assert.assertEquals("Service TxDirection Port is not correctly set", result.getMessage());
+    }
+
+    @Test
+    public void checkTxOrRxInfoForNullTxLgx() {
+        ComplianceCheckResult result = ServicehandlerTxRxCheck
+            .checkTxOrRxInfo(new TxDirectionBuilder()
+                .setPort(new PortBuilder().setPortDeviceName("q")
+                    .setPortName("n").setPortRack("r").setPortShelf("s").setPortType("t").build()).build(), null);
+
+        Assert.assertFalse(result.hasPassed());
+        Assert.assertEquals("Service TxDirection Lgx is not correctly set", result.getMessage());
+    }
+
+    @Test
+    public void checkTxOrRxInfoForNullRx() {
+        ComplianceCheckResult result = ServicehandlerTxRxCheck
+                .checkTxOrRxInfo(new TxDirectionBuilder()
+                        .setPort(new PortBuilder().setPortDeviceName("q")
+                                .setPortName("n").setPortRack("r").setPortShelf("s").setPortType("t").build())
+                        .setLgx(new LgxBuilder().setLgxDeviceName("l")
+                                .setLgxPortName("p").setLgxPortRack("r").setLgxPortShelf("s").build()).build(), null);
+
+        Assert.assertFalse(result.hasPassed());
+        Assert.assertEquals("Service RxDirection is not correctly set", result.getMessage());
+    }
+
+    @Test
+    public void checkTxOrRxInfoForNullRxPort() {
+        ComplianceCheckResult result = ServicehandlerTxRxCheck
+            .checkTxOrRxInfo(new TxDirectionBuilder()
+                .setPort(new PortBuilder().setPortDeviceName("q")
+                .setPortName("n").setPortRack("r").setPortShelf("s").setPortType("t").build())
+                .setLgx(new LgxBuilder().setLgxDeviceName("l")
+                .setLgxPortName("p").setLgxPortRack("r").setLgxPortShelf("s").build()).build(),
+            new RxDirectionBuilder().build());
+
+        Assert.assertFalse(result.hasPassed());
+        Assert.assertEquals("Service RxDirection Port is not correctly set", result.getMessage());
+    }
+
+    @Test
+    public void checkTxOrRxInfoForNullRxLgx() {
+        ComplianceCheckResult result = ServicehandlerTxRxCheck
+                .checkTxOrRxInfo(new TxDirectionBuilder()
+                        .setPort(new PortBuilder().setPortDeviceName("q")
+                                .setPortName("n").setPortRack("r").setPortShelf("s").setPortType("t").build())
+                        .setLgx(new LgxBuilder().setLgxDeviceName("l")
+                                .setLgxPortName("p").setLgxPortRack("r").setLgxPortShelf("s").build()).build(),
+                        new RxDirectionBuilder().setPort(new PortBuilder().setPortDeviceName("q")
+                                .setPortName("n").setPortRack("r").setPortShelf("s").setPortType("t").build()).build());
+
+        Assert.assertFalse(result.hasPassed());
+        Assert.assertEquals("Service RxDirection Lgx is not correctly set", result.getMessage());
+    }
+
+    @Test
+    public void checkForServiceEndNull() {
+        ComplianceCheckResult result = ServicehandlerTxRxCheck.check(null, ServiceEndpointType.SERVICEAEND);
+
+        Assert.assertFalse(result.hasPassed());
+        Assert.assertEquals(ServiceEndpointType.SERVICEAEND + " is not set", result.getMessage());
+    }
+
+    @Test
+    public void checkForServiceRateNull() {
+        ComplianceCheckResult result =
+            ServicehandlerTxRxCheck.check(new ServiceAEndBuilder().build(), ServiceEndpointType.SERVICEAEND);
+
+        Assert.assertFalse(result.hasPassed());
+        Assert.assertEquals("Service " + ServiceEndpointType.SERVICEAEND + " rate is not set", result.getMessage());
+    }
+
+    @Test
+    public void checkForServiceRateEquals0() {
+        ComplianceCheckResult result = ServicehandlerTxRxCheck.check(
+            new ServiceAEndBuilder().setServiceRate(0L).build(), ServiceEndpointType.SERVICEAEND);
+
+        Assert.assertFalse(result.hasPassed());
+        Assert.assertEquals("Service " + ServiceEndpointType.SERVICEAEND + " rate is not set", result.getMessage());
+    }
+
+    @Test
+    public void checkForServiceFormatNull() {
+        ComplianceCheckResult result = ServicehandlerTxRxCheck.check(
+            new ServiceAEndBuilder().setServiceRate(3L).build(), ServiceEndpointType.SERVICEAEND);
+
+        Assert.assertFalse(result.hasPassed());
+        Assert.assertEquals("Service " + ServiceEndpointType.SERVICEAEND + " format is not set", result.getMessage());
+    }
+
+    @Test
+    public void checkForClliEmpty() {
+        ComplianceCheckResult result = ServicehandlerTxRxCheck.check(new ServiceAEndBuilder()
+            .setServiceRate(3L).setClli("").setServiceFormat(ServiceFormat.Ethernet).build(),
+            ServiceEndpointType.SERVICEAEND);
+
+        Assert.assertFalse(result.hasPassed());
+        Assert.assertEquals(
+            "Service" + ServiceEndpointType.SERVICEAEND + " clli format is not set", result.getMessage());
+    }
+
+    @Test
+    public void checkForFailTxOrRx() {
+        ComplianceCheckResult result = ServicehandlerTxRxCheck.check(new ServiceAEndBuilder()
+            .setServiceRate(3L).setClli("cc").setServiceFormat(ServiceFormat.Ethernet).build(),
+            ServiceEndpointType.SERVICEAEND);
+
+        Assert.assertFalse(result.hasPassed());
+        Assert.assertEquals("Service TxDirection is not correctly set", result.getMessage());
+    }
+}
