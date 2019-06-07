c4aff381c1e715e0bb0ce09297b503cc9bbf0a00
guillaume.lambert <guillaume.lambert@orange.com>
2019-06-07 11:55:13 +0200

    automate lighty.io build process
    in the lighty folder
    - create a patch file to remove deviations from ordm 4.1 network models:
        org-openroadm-otn-network-topology@2018-11-30.yang
        org-openroadm-network-topology@2018-11-30.yang
        org-openroadm-network@2018-11-30.yang
    - script the lighty.io build process described in the README file
        - patch ordm 4.1 network models mentionned above
        - build transportPCE with karaf
        - download and build lighty-core 10.x.x
        - build transportPCE with lighty.io
        - comment out patch and lighty-core folder clean-up
    - mention the script in the README file
    
    JIRA: TRNSPRTPCE-126
    Change-Id: I6bfedb2f9424304d4f86f8ede8bc49521c3cb185
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>

---
+++ lighty/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ..
+patch -p0 <lighty/patch_ordm41_deviations.diff
+mvn clean install -DskipTests
+#git checkout -- ordmodels/network/src/main/yang/org-openroadm-network-topology@2018-11-30.yang ordmodels/network/src/main/yang/org-openroadm-network@2018-11-30.yang ordmodels/network/src/main/yang/org-openroadm-otn-network-topology@2018-11-30.yang
+git clone https://github.com/PantheonTechnologies/lighty-core.git
+cd lighty-core
+git checkout 10.0.x
+mvn clean install -DskipTests
+cd ../lighty
+mvn clean install
+cd  target
+unzip lighty-transportpce-10.0.1-SNAPSHOT-bin.zip
+cd ..
+# rm -rf ../lighty-core
