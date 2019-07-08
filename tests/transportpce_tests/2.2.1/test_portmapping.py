7190ca70d60bb35dab94bca1ec9a25a1c7be2d5e
Gilles Thouenon <gilles.thouenon@orange.com>
2019-07-08 16:04:58 +0200

    Update portmapping functional tests
    Tests the new portmapping hierarchy with node-info
    
    JIRA: TRNSPRTPCE-109
    
    Change-Id: I6dcf4da32a55f2af7d3ea0768757a8ad52e3dc22
    Signed-off-by: Gilles Thouenon <gilles.thouenon@orange.com>
    Co-authored-by: Christophe Betoule <christophe.betoule@orange.com>

--- tests/transportpce_tests/2.2.1/test_portmapping.py
+++ tests/transportpce_tests/2.2.1/test_portmapping.py
@@ -119,7 +119,25 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
             'connected')
         time.sleep(10)
 
-    def test_03_rdm_portmapping_DEG1_TTP_TXRX(self):
+    def test_03_rdm_portmapping_info(self):
+        url = ("{}/config/transportpce-portmapping:network/"
+               "nodes/ROADM-A1/node-info"
+               .format(self.restconf_baseurl))
+        headers = {'content-type': 'application/json'}
+        response = requests.request(
+            "GET", url, headers=headers, auth=('admin', 'admin'))
+        self.assertEqual(response.status_code, requests.codes.ok)
+        res = response.json()
+        self.assertEqual(
+            {u'node-info': {u'node-type': u'rdm',
+             u'node-ip-address': u'127.0.0.11',
+             u'node-clli': u'NodeA',
+             u'openroadm-version': u'2.2.1', u'node-vendor': u'vendorA',
+             u'node-model': u'model2'}},
+            res)
+        time.sleep(3)
+
+    def test_04_rdm_portmapping_DEG1_TTP_TXRX(self):
         url = ("{}/config/transportpce-portmapping:network/"
                "nodes/ROADM-A1/mapping/DEG1-TTP-TXRX"
                .format(self.restconf_baseurl))
@@ -130,10 +148,10 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
         res = response.json()
         self.assertIn(
             {'supporting-port': 'L1', 'supporting-circuit-pack-name': '1/0',
-             'logical-connection-point': 'DEG1-TTP-TXRX'},
+             'logical-connection-point': 'DEG1-TTP-TXRX', 'port-direction': 'bidirectional'},
             res['mapping'])
 
-    def test_04_rdm_portmapping_DEG2_TTP_TXRX_with_ots_oms(self):
+    def test_05_rdm_portmapping_DEG2_TTP_TXRX_with_ots_oms(self):
         url = ("{}/config/transportpce-portmapping:network/"
                "nodes/ROADM-A1/mapping/DEG2-TTP-TXRX"
                .format(self.restconf_baseurl))
@@ -145,10 +163,11 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
         self.assertIn(
             {'supporting-port': 'L1', 'supporting-circuit-pack-name': '2/0',
              'logical-connection-point': 'DEG2-TTP-TXRX',
-             'supporting-oms': 'OMS-DEG2-TTP-TXRX', 'supporting-ots': 'OTS-DEG2-TTP-TXRX'},
+             'supporting-oms': 'OMS-DEG2-TTP-TXRX', 'supporting-ots': 'OTS-DEG2-TTP-TXRX',
+             'port-direction': 'bidirectional'},
             res['mapping'])
 
-    def test_05_rdm_portmapping_SRG1_PP3_TXRX(self):
+    def test_06_rdm_portmapping_SRG1_PP3_TXRX(self):
         url = ("{}/config/transportpce-portmapping:network/"
                "nodes/ROADM-A1/mapping/SRG1-PP3-TXRX"
                .format(self.restconf_baseurl))
@@ -159,10 +178,10 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
         res = response.json()
         self.assertIn(
             {'supporting-port': 'C3', 'supporting-circuit-pack-name': '3/0',
-             'logical-connection-point': 'SRG1-PP3-TXRX'},
+             'logical-connection-point': 'SRG1-PP3-TXRX', 'port-direction': 'bidirectional'},
             res['mapping'])
 
-    def test_06_rdm_portmapping_SRG3_PP1_TXRX(self):
+    def test_07_rdm_portmapping_SRG3_PP1_TXRX(self):
         url = ("{}/config/transportpce-portmapping:network/"
                "nodes/ROADM-A1/mapping/SRG3-PP1-TXRX"
                .format(self.restconf_baseurl))
@@ -173,10 +192,10 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
         res = response.json()
         self.assertIn(
             {'supporting-port': 'C1', 'supporting-circuit-pack-name': '5/0',
-             'logical-connection-point': 'SRG3-PP1-TXRX'},
+             'logical-connection-point': 'SRG3-PP1-TXRX', 'port-direction': 'bidirectional'},
             res['mapping'])
 
-    def test_07_xpdr_device_connected(self):
+    def test_08_xpdr_device_connected(self):
         url = ("{}/config/network-topology:"
                "network-topology/topology/topology-netconf/node/XPDR-A1"
               .format(self.restconf_baseurl))
@@ -195,7 +214,7 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
         self.assertEqual(response.status_code, requests.codes.created)
         time.sleep(20)
 
-    def test_08_xpdr_device_connected(self):
+    def test_09_xpdr_device_connected(self):
         url = ("{}/operational/network-topology:"
                "network-topology/topology/topology-netconf/node/XPDR-A1"
                .format(self.restconf_baseurl))
@@ -209,7 +228,25 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
             'connected')
         time.sleep(10)
 
-    def test_09_xpdr_portmapping_NETWORK1(self):
+    def test_10_xpdr_portmapping_info(self):
+        url = ("{}/config/transportpce-portmapping:network/"
+               "nodes/XPDR-A1/node-info"
+               .format(self.restconf_baseurl))
+        headers = {'content-type': 'application/json'}
+        response = requests.request(
+            "GET", url, headers=headers, auth=('admin', 'admin'))
+        self.assertEqual(response.status_code, requests.codes.ok)
+        res = response.json()
+        self.assertEqual(
+            {u'node-info': {u'node-type': u'xpdr',
+             u'node-ip-address': u'1.2.3.4',
+             u'node-clli': u'NodeA',
+             u'openroadm-version': u'2.2.1', u'node-vendor': u'vendorA',
+             u'node-model': u'model2'}},
+            res)
+        time.sleep(3)
+
+    def test_11_xpdr_portmapping_NETWORK1(self):
         url = ("{}/config/transportpce-portmapping:network/"
                "nodes/XPDR-A1/mapping/XPDR1-NETWORK1"
                .format(self.restconf_baseurl))
@@ -220,10 +257,11 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
         res = response.json()
         self.assertIn(
             {'supporting-port': '1', 'supporting-circuit-pack-name': '1/0/1-PLUG-NET',
-             'logical-connection-point': 'XPDR1-NETWORK1'},
+             'logical-connection-point': 'XPDR1-NETWORK1', 'port-qual': 'xpdr-network',
+             'port-direction': 'bidirectional', 'associated-lcp': 'XPDR1-CLIENT1'},
             res['mapping'])
 
-    def test_10_xpdr_portmapping_NETWORK2(self):
+    def test_12_xpdr_portmapping_NETWORK2(self):
         url = ("{}/config/transportpce-portmapping:network/"
                "nodes/XPDR-A1/mapping/XPDR1-NETWORK2"
                .format(self.restconf_baseurl))
@@ -234,10 +272,11 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
         res = response.json()
         self.assertIn(
             {'supporting-port': '1', 'supporting-circuit-pack-name': '1/0/2-PLUG-NET',
-             'logical-connection-point': 'XPDR1-NETWORK2'},
+             'logical-connection-point': 'XPDR1-NETWORK2', 'port-direction': 'bidirectional',
+             'associated-lcp': 'XPDR1-CLIENT2', 'port-qual': 'xpdr-network'},
             res['mapping'])
 
-    def test_11_xpdr_portmapping_CLIENT1(self):
+    def test_13_xpdr_portmapping_CLIENT1(self):
         url = ("{}/config/transportpce-portmapping:network/"
                "nodes/XPDR-A1/mapping/XPDR1-CLIENT1"
                .format(self.restconf_baseurl))
@@ -249,10 +288,11 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
         self.assertIn(
             {'supporting-port': 'C1',
              'supporting-circuit-pack-name': '1/0/1-PLUG-CLIENT',
-             'logical-connection-point': 'XPDR1-CLIENT1'},
+             'logical-connection-point': 'XPDR1-CLIENT1', 'port-direction': 'bidirectional',
+             'associated-lcp': 'XPDR1-NETWORK1', 'port-qual': 'xpdr-client'},
             res['mapping'])
 
-    def test_12_xpdr_portmapping_CLIENT2(self):
+    def test_14_xpdr_portmapping_CLIENT2(self):
         url = ("{}/config/transportpce-portmapping:network/"
                "nodes/XPDR-A1/mapping/XPDR1-CLIENT2"
                .format(self.restconf_baseurl))
@@ -264,10 +304,11 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
         self.assertIn(
             {'supporting-port': 'C1',
                  'supporting-circuit-pack-name': '1/0/2-PLUG-CLIENT',
-                 'logical-connection-point': 'XPDR1-CLIENT2'},
+                 'logical-connection-point': 'XPDR1-CLIENT2', 'port-direction': 'bidirectional',
+                 'associated-lcp': 'XPDR1-NETWORK2', 'port-qual': 'xpdr-client'},
             res['mapping'])
 
-    def test_13_xpdr_device_disconnected(self):
+    def test_15_xpdr_device_disconnected(self):
         url = ("{}/config/network-topology:"
                 "network-topology/topology/topology-netconf/node/XPDR-A1"
                .format(self.restconf_baseurl))
@@ -278,7 +319,7 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
         self.assertEqual(response.status_code, requests.codes.ok)
         time.sleep(20)
 
-    def test_14_xpdr_device_disconnected(self):
+    def test_16_xpdr_device_disconnected(self):
         url = ("{}/operational/network-topology:network-topology/topology/"
                "topology-netconf/node/XPDR-A1".format(self.restconf_baseurl))
         headers = {'content-type': 'application/json'}
@@ -291,7 +332,7 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
              "error-message":"Request could not be completed because the relevant data model content does not exist"},
             res['errors']['error'])
 
-    def test_15_xpdr_device_disconnected(self):
+    def test_17_xpdr_device_disconnected(self):
         url = ("{}/config/transportpce-portmapping:network/nodes/XPDR-A1".format(self.restconf_baseurl))
         headers = {'content-type': 'application/json'}
         response = requests.request(
@@ -303,7 +344,7 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
              "error-message":"Request could not be completed because the relevant data model content does not exist"},
             res['errors']['error'])
 
-    def test_16_rdm_device_disconnected(self):
+    def test_18_rdm_device_disconnected(self):
         url = ("{}/config/network-topology:network-topology/topology/topology-netconf/node/ROADM-A1"
                .format(self.restconf_baseurl))
         headers = {'content-type': 'application/json'}
@@ -313,7 +354,7 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
         self.assertEqual(response.status_code, requests.codes.ok)
         time.sleep(20)
 
-    def test_17_rdm_device_disconnected(self):
+    def test_19_rdm_device_disconnected(self):
         url = ("{}/operational/network-topology:network-topology/topology/topology-netconf/node/ROADM-A1"
                .format(self.restconf_baseurl))
         headers = {'content-type': 'application/json'}
@@ -326,7 +367,7 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
              "error-message":"Request could not be completed because the relevant data model content does not exist"},
             res['errors']['error'])
 
-    def test_18_rdm_device_disconnected(self):
+    def test_20_rdm_device_disconnected(self):
         url = ("{}/config/transportpce-portmapping:network/nodes/ROADM-A1".format(self.restconf_baseurl))
         headers = {'content-type': 'application/json'}
         response = requests.request(
