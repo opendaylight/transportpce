5df9a6b6fac66dad8ccf73e4fc5d545a3215b31d
Ahmed Abbas <ahmad.helmy@orange.com>
2019-07-30 16:12:49 +0200

    add unit tests for serviceHandlerProvider class
    - test that init method registers ServiceHandler to rpcRegistery
    
    Change-Id: I29d8aacb1e539bd0e951f18f74b5b94b5e430f55
    Signed-off-by: Ahmed Abbas <ahmad.helmy@orange.com>

---
+++ servicehandler/src/test/java/org/opendaylight/transportpce/servicehandler/impl/ServicehandlerProviderTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright © 2019 Orange, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.transportpce.servicehandler.impl;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.opendaylight.controller.md.sal.binding.api.NotificationPublishService;
+import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
+import org.opendaylight.transportpce.pce.service.PathComputationService;
+import org.opendaylight.transportpce.renderer.NetworkModelWavelengthService;
+import org.opendaylight.transportpce.renderer.provisiondevice.RendererServiceOperations;
+import org.opendaylight.transportpce.test.AbstractTest;
+
+
+public class ServicehandlerProviderTest  extends AbstractTest {
+
+    @Mock
+    private PathComputationService pathComputationService;
+
+    @Mock
+    private RendererServiceOperations rendererServiceOperations;
+
+    @Mock
+    private NotificationPublishService notificationPublishService;
+
+    @Mock
+    RpcProviderRegistry rpcProviderRegistry;
+
+    @Mock
+    NetworkModelWavelengthService networkModelWavelengthService;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+
+    }
+
+    @Test
+    public void testInitRegisterServiceHandlerToRpcRegistry() {
+        ServicehandlerProvider provider =  new ServicehandlerProvider(
+                getDataBroker(), rpcProviderRegistry,
+                getNotificationService() , pathComputationService,
+                rendererServiceOperations, networkModelWavelengthService,
+                notificationPublishService);
+
+        provider.init();
+
+        verify(rpcProviderRegistry, times(1))
+                .addRpcImplementation(any(), any(ServicehandlerImpl.class));
+    }
+
+
+
+
+
+}
