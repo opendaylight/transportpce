f127ad85915f3601aa98c6c358a715d35f3f743c
Samuel Kontris <samuel.kontris@pantheon.tech>
2019-09-17 13:55:42 +0200

    Bump lighty.io version
    lighty.io 10.0.1-SNAPSHOT to 11.0.0-SNAPSHOT
    
    Signed-off-by: Samuel Kontris <samuel.kontris@pantheon.tech>
    Change-Id: I8ee4f92fc830e201de731285b2b40e5661a2bdec

--- lighty/build.sh
+++ lighty/build.sh
@@ -6,11 +6,11 @@ mvn clean install -DskipTests
 #git checkout -- ordmodels/network/src/main/yang/org-openroadm-network-topology@2018-11-30.yang ordmodels/network/src/main/yang/org-openroadm-network@2018-11-30.yang ordmodels/network/src/main/yang/org-openroadm-otn-network-topology@2018-11-30.yang
 git clone https://github.com/PantheonTechnologies/lighty-core.git
 cd lighty-core
-git checkout 10.0.x
+git checkout master
 mvn clean install -DskipTests
 cd ../lighty
 mvn clean install
 cd  target
-unzip lighty-transportpce-10.0.1-SNAPSHOT-bin.zip
+unzip lighty-transportpce-11.0.0-SNAPSHOT-bin.zip
 cd ..
 # rm -rf ../lighty-core
