2fb3efccb6a5f65056111c952eddf15ca30af35a
Balagangadhar Bathula <bb4341@att.com>
2020-01-22 14:25:42 -0500

    Adding SAPI/DAPI to Renderer
    - Refactor code for codestyle warnings
    - Removed deprecated warnings
    - Removed hardcorded lines
    - updated code to have hash value for SAPI and DAPI
    - introduce a FNV1 128 bit private hash function
      to abide by the 16 Bytes limitation on these parameters length.
    - clean some parts of the code
    - modified test cases, both 1.2.1 and 2.2.1
    - Added expected sapi/dapi
    - Removed some deprecated methods in test suite
    
    JIRA: TRNSPRTPCE-183
    Co-authored-by: Shweta Vachhani <sv111y@att.com>
    Co-authored-by: Guillaume Lambert <guillaume.lambert@orange.com>
    Change-Id: Iefb56ec9e2bcf99c120143e2816a0653c48be248
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>

--- tests/transportpce_tests/2.2.1/test_portmapping.py
+++ tests/transportpce_tests/2.2.1/test_portmapping.py
@@ -239,7 +239,8 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
             {'supported-interface-capability': ['org-openroadm-port-types:if-OCH'],
              'supporting-port': '1', 'supporting-circuit-pack-name': '1/0/1-PLUG-NET',
              'logical-connection-point': 'XPDR1-NETWORK1', 'port-qual': 'xpdr-network',
-             'port-direction': 'bidirectional', 'connection-map-lcp': 'XPDR1-CLIENT1'},
+             'port-direction': 'bidirectional', 'connection-map-lcp': 'XPDR1-CLIENT1',
+             'lcp-hash-val': '8e128ba57560403cfd4ffafae38cd941'},
             res['mapping'])
 
     def test_12_xpdr_portmapping_NETWORK2(self):
@@ -255,7 +256,8 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
             {'supported-interface-capability': ['org-openroadm-port-types:if-OCH'],
              'supporting-port': '1', 'supporting-circuit-pack-name': '1/0/2-PLUG-NET',
              'logical-connection-point': 'XPDR1-NETWORK2', 'port-direction': 'bidirectional',
-             'connection-map-lcp': 'XPDR1-CLIENT2', 'port-qual': 'xpdr-network'},
+             'connection-map-lcp': 'XPDR1-CLIENT2', 'port-qual': 'xpdr-network',
+             'lcp-hash-val': '8e128ba57560403cfd4ffafae38cd942'},
             res['mapping'])
 
     def test_13_xpdr_portmapping_CLIENT1(self):
@@ -272,7 +274,8 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
              'supporting-port': 'C1',
              'supporting-circuit-pack-name': '1/0/1-PLUG-CLIENT',
              'logical-connection-point': 'XPDR1-CLIENT1', 'port-direction': 'bidirectional',
-             'connection-map-lcp': 'XPDR1-NETWORK1', 'port-qual': 'xpdr-client'},
+             'connection-map-lcp': 'XPDR1-NETWORK1', 'port-qual': 'xpdr-client',
+             'lcp-hash-val': '3ed8ed1336784ac7c2f66c22f2f03d8'},
             res['mapping'])
 
     def test_14_xpdr_portmapping_CLIENT2(self):
@@ -289,7 +292,8 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
              'supporting-port': 'C1',
              'supporting-circuit-pack-name': '1/0/2-PLUG-CLIENT',
              'logical-connection-point': 'XPDR1-CLIENT2', 'port-direction': 'bidirectional',
-             'connection-map-lcp': 'XPDR1-NETWORK2', 'port-qual': 'xpdr-client'},
+             'connection-map-lcp': 'XPDR1-NETWORK2', 'port-qual': 'xpdr-client',
+             'lcp-hash-val': '3ed8ed1336784ac7c2f66c22f2f03db'},
             res['mapping'])
 
     def test_15_xpdr_device_disconnected(self):
