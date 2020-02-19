8c05eb77a6e6f413a1377205923941759da1ff84
Emmanuelle Delfour <emmanuelle.delfour@gmail.com>
2020-02-19 17:20:19 +0100

    HoneyNode Java 11 support for 121 devices
    - honeycomb 1.19.04 upgrade
    - rename 2.1 folder into 1.2.1 to reflect the openroadm device version
      rather than the openroadm common version
    - update resconf honeynode config to allow remote access
    - samples config files update
    - delete old testtool 1.2.1 sample configs
    - added support for openconfig models
    - remove JDK8 gate support from tox and associated scripts
    
    NB: script installJDKCentos.sh contains a workaround to have the right
        JDK.
    
    JIRA: TRNSPRTPCE-192
    Signed-off-by: Emmanuelle Delfour <emmanuelle.delfour@gmail.com>
    Change-Id: Ia30ceb05c927937d9b4fc81d42d91b7558cd1dfd

---
+++ tests/honeynode/1.2.1/minimal-distribution-core/src/main/java/io/fd/honeycomb/infra/distro/data/HoneycombDOMRpcServiceProvider.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2016 Cisco and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.fd.honeycomb.infra.distro.data;
+
+import com.google.inject.Inject;
+import io.fd.honeycomb.binding.init.ProviderTrait;
+import io.fd.honeycomb.rpc.HoneycombDOMRpcService;
+import io.fd.honeycomb.rpc.RpcRegistry;
+import org.opendaylight.mdsal.binding.dom.adapter.BindingToNormalizedNodeCodec;
+import org.opendaylight.mdsal.dom.api.DOMRpcService;
+
+public final class HoneycombDOMRpcServiceProvider extends ProviderTrait<DOMRpcService> {
+
+    @Inject
+    private BindingToNormalizedNodeCodec serializer;
+
+    @Inject
+    private RpcRegistry rpcRegistry;
+
+    @Override
+    protected DOMRpcService create() {
+        return new HoneycombDOMRpcService(serializer, rpcRegistry);
+    }
+}
