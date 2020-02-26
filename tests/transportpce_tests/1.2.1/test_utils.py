1778b5eef08e83d6acabc97a52a52ccbd734631a
guillaume.lambert <guillaume.lambert@orange.com>
2020-02-26 16:58:47 +0100

    improve functional tests
    - make scripts with "sed" compatible with BSD and macOS
    
    - remove illegal reflective access warnings introduced by JDK11
      - create a file tests/reflectwarn.sh with the right JDK_JAVA_OPTIONS
        variable content.
        sourcing this file at build or runtime removes the warnings.
        But instead, it generates a long note in stderr that still pollutes
        nosetests results.
    - in functional tests, move sims and controller stderr to a file to
      prevent that note from polluting nosetests results.
    
    https://dev.to/erichelgeson/removing-illegal-reflective-access-warnings-in-grails-4-393o
    
    JIRA: TRNSPRTPCE-192 TRNSPRTPCE-197
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    Change-Id: I04156ab104d7420d1eb2d180579593c67293175b

--- tests/transportpce_tests/1.2.1/test_utils.py
+++ tests/transportpce_tests/1.2.1/test_utils.py
@@ -8,7 +8,7 @@ def start_xpdra_honeynode():
         with open('honeynode1.log', 'w') as outfile:
             return subprocess.Popen(
                 [executable, "17830", "sample_configs/openroadm/1.2.1/oper-XPDRA.xml"],
-                stdout=outfile)
+                stdout=outfile, stderr=outfile)
 
 def start_roadma_full_honeynode():
     executable = ("./honeynode/1.2.1/honeynode-distribution/target/honeynode-distribution-1.19.04-hc"
@@ -17,7 +17,7 @@ def start_roadma_full_honeynode():
         with open('honeynode2.log', 'w') as outfile:
             return subprocess.Popen(
                 [executable, "17821", "sample_configs/openroadm/1.2.1/oper-ROADMA-full.xml"],
-                stdout=outfile)
+                stdout=outfile, stderr=outfile)
 
 def start_roadma_honeynode():
     executable = ("./honeynode/1.2.1/honeynode-distribution/target/honeynode-distribution-1.19.04-hc"
@@ -26,7 +26,7 @@ def start_roadma_honeynode():
         with open('honeynode2.log', 'w') as outfile:
             return subprocess.Popen(
                 [executable, "17831", "sample_configs/openroadm/1.2.1/oper-ROADMA.xml"],
-                stdout=outfile)
+                stdout=outfile, stderr=outfile)
 
 def start_roadmb_honeynode():
     executable = ("./honeynode/1.2.1/honeynode-distribution/target/honeynode-distribution-1.19.04-hc"
@@ -35,7 +35,7 @@ def start_roadmb_honeynode():
         with open('honeynode3.log', 'w') as outfile:
             return subprocess.Popen(
                 [executable, "17832", "sample_configs/openroadm/1.2.1/oper-ROADMB.xml"],
-                stdout=outfile)
+                stdout=outfile, stderr=outfile)
 
 def start_roadmc_full_honeynode():
     executable = ("./honeynode/1.2.1/honeynode-distribution/target/honeynode-distribution-1.19.04-hc"
@@ -44,7 +44,7 @@ def start_roadmc_full_honeynode():
         with open('honeynode3.log', 'w') as outfile:
             return subprocess.Popen(
                 [executable, "17823", "sample_configs/openroadm/1.2.1/oper-ROADMC-full.xml"],
-                stdout=outfile)
+                stdout=outfile, stderr=outfile)
 
 def start_roadmc_honeynode():
     executable = ("./honeynode/1.2.1/honeynode-distribution/target/honeynode-distribution-1.19.04-hc"
@@ -53,7 +53,7 @@ def start_roadmc_honeynode():
         with open('honeynode4.log', 'w') as outfile:
             return subprocess.Popen(
                 [executable, "17833", "sample_configs/openroadm/1.2.1/oper-ROADMC.xml"],
-                stdout=outfile)
+                stdout=outfile, stderr=outfile)
 
 def start_xpdrc_honeynode():
     executable = ("./honeynode/1.2.1/honeynode-distribution/target/honeynode-distribution-1.19.04-hc"
@@ -62,7 +62,7 @@ def start_xpdrc_honeynode():
         with open('honeynode4.log', 'w') as outfile:
             return subprocess.Popen(
                 [executable, "17834", "sample_configs/openroadm/1.2.1/oper-XPDRC.xml"],
-                stdout=outfile)
+                stdout=outfile, stderr=outfile)
 
 def start_tpce():
     if "USE_LIGHTY" in os.environ and os.environ['USE_LIGHTY'] == 'True':
@@ -70,12 +70,12 @@ def start_tpce():
         executable = "../lighty/target/lighty-transportpce-12.0.0-SNAPSHOT/clean-start-controller.sh"
         with open('odl.log', 'w') as outfile:
             return subprocess.Popen(
-                ["bash", executable], stdout=outfile,
+                ["bash", executable], stdout=outfile, stderr=outfile,
                 stdin=open(os.devnull))
     else:
         print ("starting KARAF TransportPCE build...")
         executable = "../karaf/target/assembly/bin/karaf"
         with open('odl.log', 'w') as outfile:
             return subprocess.Popen(
-                ["bash", executable, "server"], stdout=outfile,
+                ["bash", executable, "server"], stdout=outfile, stderr=outfile,
                 stdin=open(os.devnull))
--- tests/transportpce_tests/2.2.1/test_utils.py
+++ tests/transportpce_tests/2.2.1/test_utils.py
@@ -8,7 +8,7 @@ def start_xpdra_honeynode():
         with open('honeynode1.log', 'w') as outfile:
             return subprocess.Popen(
                 [executable, "17840", "sample_configs/openroadm/2.2.1/oper-XPDRA.xml"],
-                stdout=outfile)
+                stdout=outfile, stderr=outfile)
 
 def start_roadma_honeynode():
     executable = ("./honeynode/2.2.1/honeynode-distribution/target/honeynode-distribution-1.19.04-hc"
@@ -17,7 +17,7 @@ def start_roadma_honeynode():
         with open('honeynode2.log', 'w') as outfile:
             return subprocess.Popen(
                 [executable, "17841", "sample_configs/openroadm/2.2.1/oper-ROADMA.xml"],
-                stdout=outfile)
+                stdout=outfile, stderr=outfile)
 
 def start_roadmb_honeynode():
     executable = ("./honeynode/2.2.1/honeynode-distribution/target/honeynode-distribution-1.19.04-hc"
@@ -26,7 +26,7 @@ def start_roadmb_honeynode():
         with open('honeynode5.log', 'w') as outfile:
             return subprocess.Popen(
                 [executable, "17842", "sample_configs/openroadm/2.2.1/oper-ROADMB.xml"],
-                stdout=outfile)
+                stdout=outfile, stderr=outfile)
 
 def start_roadmc_honeynode():
     executable = ("./honeynode/2.2.1/honeynode-distribution/target/honeynode-distribution-1.19.04-hc"
@@ -35,7 +35,7 @@ def start_roadmc_honeynode():
         with open('honeynode3.log', 'w') as outfile:
             return subprocess.Popen(
                 [executable, "17843", "sample_configs/openroadm/2.2.1/oper-ROADMC.xml"],
-                stdout=outfile)
+                stdout=outfile, stderr=outfile)
 
 def start_xpdrc_honeynode():
     executable = ("./honeynode/2.2.1/honeynode-distribution/target/honeynode-distribution-1.19.04-hc"
@@ -44,7 +44,7 @@ def start_xpdrc_honeynode():
         with open('honeynode4.log', 'w') as outfile:
             return subprocess.Popen(
                 [executable, "17844", "sample_configs/openroadm/2.2.1/oper-XPDRC.xml"],
-                stdout=outfile)
+                stdout=outfile, stderr=outfile)
 
 def start_spdra_honeynode():
     executable = ("./honeynode/2.2.1/honeynode-distribution/target/honeynode-distribution-1.19.04-hc"
@@ -53,7 +53,7 @@ def start_spdra_honeynode():
         with open('honeynode6.log', 'w') as outfile:
             return subprocess.Popen(
                 [executable, "17845", "sample_configs/openroadm/2.2.1/oper-SPDRAv2.xml"],
-                stdout=outfile)
+                stdout=outfile, stderr=outfile)
 
 def start_tpce():
     if "USE_LIGHTY" in os.environ and os.environ['USE_LIGHTY'] == 'True':
@@ -61,12 +61,12 @@ def start_tpce():
         executable = "../lighty/target/lighty-transportpce-12.0.0-SNAPSHOT/clean-start-controller.sh"
         with open('odl.log', 'w') as outfile:
             return subprocess.Popen(
-                ["bash", executable], stdout=outfile,
+                ["bash", executable], stdout=outfile, stderr=outfile,
                 stdin=open(os.devnull))
     else:
         print ("starting KARAF TransportPCE build...")
         executable = "../karaf/target/assembly/bin/karaf"
         with open('odl.log', 'w') as outfile:
             return subprocess.Popen(
-                ["bash", executable, "server"], stdout=outfile,
+                ["bash", executable, "server"], stdout=outfile, stderr=outfile,
                 stdin=open(os.devnull))
