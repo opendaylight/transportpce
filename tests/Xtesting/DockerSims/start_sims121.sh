271d6d16618003679550a4bf4fbccc92c37dba00
Christophe Betoule <christophe.betoule@orange.com>
2020-03-16 12:59:11 +0100

    Docker refactoring for Java11
    - add multi-stage docker file for build and run
    - applicative user in Dockerfile
    - one image
    - one container per node configuration
    - decrease image size
    - use of docker option --name to set name of container
    - use of docker command to stop and delete container
    - new tag name for image based on supported jdk
    - sudo managment for docker command (check if user is in sudoers)
    - add healthcheck
    - expose netconf and restconf port
    
    Signed-off-by: Christophe Betoule <christophe.betoule@orange.com>
    Change-Id: I86b47cac0f795c2ac9d1615efa703d8ca87b34e5

---
+++ tests/Xtesting/DockerSims/start_sims121.sh
@@ -0,0 +1,30 @@
+#!/bin/sh 
+GROUP=docker
+DOCKER_CMD=docker
+RESTCONF_USER=admin
+RESTCONF_PASSWORD=admin
+if id -nG "$USER" | grep -qw "$GROUP"; then
+    echo $USER belongs to $GROUP group
+else
+    echo $USER does not belong to $GROUP group
+    DOCKER_CMD="sudo docker"
+fi
+
+for docker_image in xpdra-1.2.1  roadma-1.2.1 roadmb-1.2.1  roadmc-1.2.1 xpdrc-1.2.1 roadma-full-1.2.1 roadmc-full-1.2.1;do
+    if [ ! "$(${DOCKER_CMD} ps -q -f name=${docker_image})" ];then
+       if [  "$(${DOCKER_CMD} ps -aq -f status=exited -f name=${docker_image})" ];then
+          ${DOCKER_CMD} rm ${docker_image}
+       fi
+    elif [  "$(${DOCKER_CMD} ps -q -f status=running -f name=${docker_image})" ];then
+          ${DOCKER_CMD} stop ${docker_image} 
+    fi
+done
+
+echo `${DOCKER_CMD} run --rm  -p 17830:8080 -p 18830:8180 -e USER=${RESTCONF_USER} -e PASSWORD=${RESTCONF_PASSWORD} --name xpdra-1.2.1 -e DEVICE_VERSION=1.2.1 -e DEVICE_FILE=oper-XPDRA.xml  -dit  honeynode:alpine-jre11`
+echo `${DOCKER_CMD} run --rm  -p 17821:8080 -p 18821:8180 -e USER=${RESTCONF_USER} -e PASSWORD=${RESTCONF_PASSWORD} --name roadma-1.2.1 -e DEVICE_VERSION=1.2.1 -e DEVICE_FILE=oper-ROADMA.xml -dit honeynode:alpine-jre11`
+echo `${DOCKER_CMD} run --rm  -p 17822:8080 -p 18822:8180 -e USER=${RESTCONF_USER} -e PASSWORD=${RESTCONF_PASSWORD} --name roadmb-1.2.1 -e DEVICE_VERSION=1.2.1 -e DEVICE_FILE=oper-ROADMB.xml  -dit honeynode:alpine-jre11`
+echo `${DOCKER_CMD} run --rm  -p 17823:8080 -p 18823:8180 -e USER=${RESTCONF_USER} -e PASSWORD=${RESTCONF_PASSWORD} --name roadmc-1.2.1 -e DEVICE_VERSION=1.2.1 -e DEVICE_FILE=oper-ROADMC.xml -dit honeynode:alpine-jre11`
+echo `${DOCKER_CMD} run --rm  -p 17834:8080 -p 18834:8180 -e USER=${RESTCONF_USER} -e PASSWORD=${RESTCONF_PASSWORD} --name xpdrc-1.2.1 -e DEVICE_VERSION=1.2.1 -e DEVICE_FILE=oper-XPDRC.xml  -dit honeynode:alpine-jre11`
+echo `${DOCKER_CMD} run --rm  -p 17831:8080 -p 18831:8180 -e USER=${RESTCONF_USER} -e PASSWORD=${RESTCONF_PASSWORD} --name roadma-full-1.2.1 -e DEVICE_VERSION=1.2.1 -e DEVICE_FILE=oper-ROADMA-full.xml -dit honeynode:alpine-jre11`
+echo `${DOCKER_CMD} run --rm  -p 17833:8080 -p 18833:8180 -e USER=${RESTCONF_USER} -e PASSWORD=${RESTCONF_PASSWORD} --name roadmc-full-1.2.1 -e DEVICE_VERSION=1.2.1 -e DEVICE_FILE=oper-ROADMC-full.xml -dit honeynode:alpine-jre11`
+
