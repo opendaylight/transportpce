8e312621f80d4c091cef37aa50ab550ba7ef8e14
Christophe Betoule <christophe.betoule@orange.com>
2020-03-16 12:59:11 +0100

    Honeynode Docker refactoring for Java11
    Upgrade Docker image generation process to use new Java11 simulators.
    Both OpenROADM versions 1.2.1 and 2.2.1 simulators are now using a
    same docker image and the same scripts.
    
    This brings the following improvements:
    - create a multi-stage docker file to isolate build and run processes
    - decrease image size
    - use an applicative user in Dockerfile rather than root
    - add healthcheck
    - expose both netconf and restconf ports
    - use the docker option --name to set the name of the container
    - rootless container and sudo management for docker command
    - centralize most scripts parameters in dockercmd_profile.sh
    
    JIRA: TRNSPRTPCE-239
    Signed-off-by: Christophe Betoule <christophe.betoule@orange.com>
    Co-authored-by: Emmanuelle Delfour <emmanuelle.delfour@orange.com>
    Co-authored-by: Guillaume Lambert <guillaume.lambert@orange.com>
    Change-Id: I86b47cac0f795c2ac9d1615efa703d8ca87b34e5

---
+++ tests/Xtesting/DockerSims/dockercmd_profile.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+DEVICE_VERSION=${1:-1.2.1}
+
+# this file centralizes inb one place parameters needed by docker scripts,
+# more especially the image lists for 1.2.1 and 2.2.1 devices.
+# device versions supported as scripts argument are 1.2.1 and 2.2.1 and "all"
+
+#IMAGE_LIST format is suffix_port : container/image name : config file to fill netconf operational datastore
+
+IMAGE121_LIST="30:xpdra-1.2.1:oper-XPDRA.xml
+31:roadma-1.2.1:oper-ROADMA.xml
+32:roadmb-1.2.1:oper-ROADMB.xml
+33:roadmc-1.2.1:oper-ROADMC.xml
+34:xpdrc-1.2.1:oper-XPDRC.xml
+21:roadma-full-1.2.1:oper-ROADMA-full.xml
+23:roadmc-full-1.2.1:oper-ROADMC-full.xml
+"
+
+IMAGE221_LIST="40:xpdra-2.2.1:oper-XPDRA.xml
+41:roadma-2.2.1:oper-ROADMA.xml
+42:roadmb-2.2.1:oper-ROADMB.xml
+43:roadmc-2.2.1:oper-ROADMC.xml
+44:xpdrc-2.2.1:oper-XPDRC.xml
+45:spdrav2-2.2.1:oper-SPDRAv2.xml
+46:spdrav1-2.2.1:oper-SPDRAv1.xml
+"
+
+case "$DEVICE_VERSION" in
+    "1.2.1") IMAGE_LIST=$IMAGE121_LIST
+    ;;
+    "2.2.1") IMAGE_LIST=$IMAGE221_LIST
+    ;;
+    *) echo "unsupported device version"
+    exit 1
+    ;;
+esac
+
+DOCKER_CMD=docker
+DOCKER_IMAGE=honeynode:alpine-jre11
+RESTCONF_USER=admin
+RESTCONF_PASSWORD=admin
+if [ `which podman` ]
+then DOCKER_CMD=podman
+else
+    if [ ! `which $DOCKER_CMD` ]
+    then  echo "no docker command available" >&2
+        exit 1
+    fi
+    #if "docker ps" cannot be run without error, prepend sudo
+    if ( ! $DOCKER_CMD ps >/dev/null 2>&1 );then
+        echo "docker command only usable as root, using sudo" >&2
+        DOCKER_CMD="sudo docker"
+    fi
+fi
+
+
