b35e8257cd3ccd57e15916f3a39a3aacee6eede9
guillaume.lambert <guillaume.lambert@orange.com>
2020-03-18 13:39:16 +0100

    make Honeynode and tox scripts BSD compatible
    - use a sed syntax compatible with both BSD and GNU versions
    - replace /bin/bash by /bin/sh (regular bourne shell)
      make necessary adaptations for compatibility
    
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    Change-Id: I6d390cede8abbbd44f92554e7b0f164ae5c0b173

--- tests/buildHoneynode.sh
+++ tests/buildHoneynode.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 ORDMVERSION=${1:-1.2.1}
 
 set -e
-. reflectwarn.sh
+. $(dirname $0)/reflectwarn.sh
 cd honeynode/$ORDMVERSION
 mvn clean install -DskipTests -Dcheckstyle.skip -Dmaven.javadoc.skip=true -s ../fd_io_honeycomb_settings.xml -q -V
 chmod +x ./honeynode-distribution/target/honeynode-distribution-1.19.04-hc/honeynode-distribution-1.19.04/honeycomb-tpce
