a7b0c33bfbafd1026b01226f9472af2b4a8df06a
errea <errea@eurecom.fr>
2020-03-23 19:42:02 +0100

    device listener implementation, OnNotificationChange
    Change-Id: I610b32e243abc6ce5d31f2b754f47a3a71a0a995
    
    Trying to handle device change notifications. When I execute an
    <edit-congif> operation to the device datastore from an external
    netconf session (not through ODL controller) I try to read the datastore
    of the device who notified a change using the function getDataFromDevice.
    However, I get an RPC error when reading the datastore.
    
    I am using the emulated honeynode 1.2.1 with ROADMA sample configuration
    
    Change-Id: I610b32e243abc6ce5d31f2b754f47a3a71a0a995

--- networkmodel/src/main/java/org/opendaylight/transportpce/networkmodel/listeners/DeviceListener.java
+++ networkmodel/src/main/java/org/opendaylight/transportpce/networkmodel/listeners/DeviceListener.java
@@ -8,15 +8,32 @@
 
 package org.opendaylight.transportpce.networkmodel.listeners;
 
+import java.util.Optional;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.transportpce.common.Timeouts;
+import org.opendaylight.transportpce.common.device.DeviceTransactionManager;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.device.rev170206.ChangeNotification;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.device.rev170206.OrgOpenroadmDeviceListener;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.device.rev170206.OtdrScanResult;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.device.rev170206.org.openroadm.device.container.OrgOpenroadmDevice;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.device.rev170206.org.openroadm.device.container.org.openroadm.device.Info;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DeviceListener implements OrgOpenroadmDeviceListener {
 
     private static final Logger LOG = LoggerFactory.getLogger(DeviceListener.class);
+    private final DataBroker databroker;
+    private final String deviceId;
+    private final DeviceTransactionManager deviceTransactionManager;
+
+    public DeviceListener(String deviceId, DeviceTransactionManager deviceTransactionManager, DataBroker dataBroker) {
+        this.databroker = dataBroker;
+        this.deviceId = deviceId;
+        this.deviceTransactionManager = deviceTransactionManager;
+    }
 
     /**
      * Callback for change-notification.
@@ -26,7 +43,28 @@ public class DeviceListener implements OrgOpenroadmDeviceListener {
     @Override
     public void onChangeNotification(ChangeNotification notification) {
 
-        LOG.info("Notification {} received {}", ChangeNotification.QNAME, notification);
+        LOG.info("Notification {} on node {} received {}", ChangeNotification.QNAME, this.deviceId ,notification);
+        if (this.deviceTransactionManager.isDeviceMounted(deviceId)) {
+            InstanceIdentifier<Info> infoIID = InstanceIdentifier.create(OrgOpenroadmDevice.class).child(Info.class);
+            LOG.info("Path arguments of infoIID: {}", infoIID.getPathArguments().toString());
+            LOG.info("Target type of infoIID: {}", infoIID.getTargetType().toString());
+            Optional<Info> infoOpt =
+                    this.deviceTransactionManager.getDataFromDevice(this.deviceId,
+                                                                    LogicalDatastoreType.OPERATIONAL,
+                                                                    infoIID, Timeouts.DEVICE_READ_TIMEOUT,
+                                                                    Timeouts.DEVICE_READ_TIMEOUT_UNIT);
+            if (infoOpt.isPresent()) {
+                LOG.info("Info of device {} from datastore: {}", this.deviceId, infoOpt.get().toString());
+            } else {
+                LOG.warn("Could not get info from device");
+            }
+        } else {
+            LOG.warn("Device {} not mounted", this.deviceId);
+        }
+
+
+
+
     }
 
     /**
