342b6957e10aa01c6b93e44098976c3eb79308b9
guillaume.lambert <guillaume.lambert@orange.com>
2020-03-27 22:32:57 +0100

    Improve Honeynode Docker scripts
    to allow 1.2.1 and 2.2.1 containers simultaneous use
    and make it the default behaviour
    
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    Change-Id: I200ebfe108c358181d9f28a96109758f33da4fb9

--- tests/Xtesting/DockerSims/dockercmd_profile.sh
+++ tests/Xtesting/DockerSims/dockercmd_profile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DEVICE_VERSION=${1:-1.2.1}
+DEVICE_VERSION=${1:-all}
 
 # this file centralizes inb one place parameters needed by docker scripts,
 # more especially the image lists for 1.2.1 and 2.2.1 devices.
@@ -8,28 +8,27 @@ DEVICE_VERSION=${1:-1.2.1}
 
 #IMAGE_LIST format is suffix_port : container/image name : config file to fill netconf operational datastore
 
-IMAGE121_LIST="30:xpdra-1.2.1:oper-XPDRA.xml
-31:roadma-1.2.1:oper-ROADMA.xml
-32:roadmb-1.2.1:oper-ROADMB.xml
-33:roadmc-1.2.1:oper-ROADMC.xml
-34:xpdrc-1.2.1:oper-XPDRC.xml
-21:roadma-full-1.2.1:oper-ROADMA-full.xml
-23:roadmc-full-1.2.1:oper-ROADMC-full.xml
+IMAGE121_LIST="30:xpdra-1.2.1:1.2.1/oper-XPDRA.xml
+31:roadma-1.2.1:1.2.1/oper-ROADMA-full.xml
+32:roadmb-1.2.1:1.2.1/oper-ROADMB.xml
+33:roadmc-1.2.1:1.2.1/oper-ROADMC-full.xml
+34:xpdrc-1.2.1:1.2.1/oper-XPDRC.xml
 "
 
-IMAGE221_LIST="40:xpdra-2.2.1:oper-XPDRA.xml
-41:roadma-2.2.1:oper-ROADMA.xml
-42:roadmb-2.2.1:oper-ROADMB.xml
-43:roadmc-2.2.1:oper-ROADMC.xml
-44:xpdrc-2.2.1:oper-XPDRC.xml
-45:spdrav2-2.2.1:oper-SPDRAv2.xml
-46:spdrav1-2.2.1:oper-SPDRAv1.xml
+IMAGE221_LIST="40:xpdra-2.2.1:2.2.1/oper-XPDRA.xml
+41:roadma-2.2.1:2.2.1/oper-ROADMA.xml
+42:roadmb-2.2.1:2.2.1/oper-ROADMB.xml
+43:roadmc-2.2.1:2.2.1/oper-ROADMC.xml
+44:xpdrc-2.2.1:2.2.1/oper-XPDRC.xml
+45:spdrav1-2.2.1:2.2.1/oper-SPDRAv1.xml
 "
 
 case "$DEVICE_VERSION" in
     "1.2.1") IMAGE_LIST=$IMAGE121_LIST
     ;;
     "2.2.1") IMAGE_LIST=$IMAGE221_LIST
+    ;;
+    "all") IMAGE_LIST=$IMAGE121_LIST" "$IMAGE221_LIST
     ;;
     *) echo "unsupported device version"
     exit 1
