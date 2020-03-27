1e0427a5cc2e97e64610a9f00b56270be279d725
guillaume.lambert <guillaume.lambert@orange.com>
2020-03-27 22:32:57 +0100

    Improve Honeynode Docker scripts
    to allow 1.2.1 and 2.2.1 containers simultaneous use
    and make it the default behaviour
    
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    Change-Id: I200ebfe108c358181d9f28a96109758f33da4fb9

--- tests/Xtesting/DockerSims/start_sims.sh
+++ tests/Xtesting/DockerSims/start_sims.sh
@@ -2,13 +2,17 @@
 
 . $(dirname $0)/dockercmd_profile.sh
 
-DOCKER_OPTIONS="-e USER=${RESTCONF_USER} -e PASSWORD=${RESTCONF_PASSWORD} -e DEVICE_VERSION=${DEVICE_VERSION} -dit ${DOCKER_IMAGE}"
+DOCKER_OPTIONS="-e USER=${RESTCONF_USER} -e PASSWORD=${RESTCONF_PASSWORD} -dit ${DOCKER_IMAGE}"
 
 for image in $IMAGE_LIST;do
     suffix_port=`echo -n $image| cut -d: -f1`
     image_name=`echo -n $image| cut -d: -f2`
     device_file=`echo -n $image| cut -d: -f3`
-    #echo "$image_name $suffix_port $device_file"
+    if [ "${DEVICE_VERSION}" = "all" ]
+        then deviceversion=`echo -n $device_file| cut -d'/' -f1`
+    else
+        deviceversion=${DEVICE_VERSION}
+    fi
     if [ ! "$(${DOCKER_CMD} ps -q -f name=${image_name})" ];then
        if [  "$(${DOCKER_CMD} ps -aq -f status=exited -f name=${image_name})" ];then
           ${DOCKER_CMD} rm ${image_name}
@@ -17,7 +21,7 @@ for image in $IMAGE_LIST;do
           ${DOCKER_CMD} stop ${image_name}
     fi
     echo ${image_name}
-    ${DOCKER_CMD} run --rm -p 178$suffix_port:1830 -p 81$suffix_port:8130 --name ${image_name} -e DEVICE_FILE=${device_file} ${DOCKER_OPTIONS}
+    ${DOCKER_CMD} run --rm -p 178$suffix_port:1830 -p 81$suffix_port:8130 --name ${image_name} -e DEVICE_VERSION=${deviceversion} -e DEVICE_FILE=${device_file} ${DOCKER_OPTIONS}
 done
 
 exit
