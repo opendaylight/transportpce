03d947bb16b0b3450d692de86eea7ca3fc0abe91
shaaban Altanany <shaaban.eltanany.ext@orange.com>
2020-03-31 10:45:40 +0200

    adding some pce unit tests and remove ignore from olm class
    Change-Id: Id6325fdf47100b4bdc75c5cacd826f18cda12eca
    Signed-off-by: ShaabanEltanany <shaaban.eltanany.ext@orange.com>

--- olm/src/test/java/org/opendaylight/transportpce/olm/service/OlmPowerServiceImplSpanLossBaseTest.java
+++ olm/src/test/java/org/opendaylight/transportpce/olm/service/OlmPowerServiceImplSpanLossBaseTest.java
@@ -13,7 +13,6 @@ import java.util.Optional;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
@@ -59,7 +58,7 @@ import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Ignore
+
 public class OlmPowerServiceImplSpanLossBaseTest extends AbstractTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(OlmPowerServiceImplSpanLossBaseTest.class);
