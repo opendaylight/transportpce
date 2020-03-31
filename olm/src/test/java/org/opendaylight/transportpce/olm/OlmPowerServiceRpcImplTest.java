80ab1596be06083d4ff5e32d997db2c8ff9560eb
shaaban Altanany <shaaban.eltanany.ext@orange.com>
2020-03-31 10:45:40 +0200

    adding some pce unit tests and remove ignore from olm class adding PCE unit tests
    Change-Id: Id6325fdf47100b4bdc75c5cacd826f18cda12eca
    Signed-off-by: ShaabanEltanany <shaaban.eltanany.ext@orange.com>

--- olm/src/test/java/org/opendaylight/transportpce/olm/OlmPowerServiceRpcImplTest.java
+++ olm/src/test/java/org/opendaylight/transportpce/olm/OlmPowerServiceRpcImplTest.java
@@ -15,7 +15,6 @@ import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.opendaylight.mdsal.binding.api.MountPoint;
@@ -74,7 +73,7 @@ import org.opendaylight.yangtools.yang.common.RpcResult;
 import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
 import org.opendaylight.yangtools.yang.common.Uint64;
 
-@Ignore
+
 public class OlmPowerServiceRpcImplTest extends AbstractTest {
 
     private MountPoint mountPoint;
