feff9d51ec35bbf37ab7705f75cc6f8b024701b9
Gilles Thouenon <gilles.thouenon@orange.com>
2020-04-02 09:53:30 +0200

    Update otn-topology with otn-links
    Adds and implements new methods in NetworkModelService and
    OpenRoadmOtnTopology to create, update and delete otn links. These
    methods are intended to be used by renderer module after otn interfaces
    and odu connections have been correctly configured in otn devices.
    
    JIRA: TRNSPRTPCE-199
    Signed-off-by: Gilles Thouenon <gilles.thouenon@orange.com>
    Change-Id: Ieba5a6491b4fe0c8617e63a816254c26b051640f

--- networkmodel/src/main/java/org/opendaylight/transportpce/networkmodel/service/NetworkModelService.java
+++ networkmodel/src/main/java/org/opendaylight/transportpce/networkmodel/service/NetworkModelService.java
@@ -7,6 +7,8 @@
  */
 package org.opendaylight.transportpce.networkmodel.service;
 
+import java.util.List;
+import org.opendaylight.yang.gen.v1.http.transportpce.topology.rev200129.OtnLinkType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.NetconfNodeConnectionStatus;
 
 /**
@@ -14,9 +16,9 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev15
  */
 public interface NetworkModelService {
 
-
     /**
-        * Create new OpenROADM node in all OpenROADM topologies.
+     * Create new OpenROADM node in all OpenROADM topologies.
+     *
      * @param nodeId
      *   unique node ID of new OpenROADM node
      * @param nodeVersion
@@ -27,7 +29,8 @@ public interface NetworkModelService {
     /**
      * Delete OpenROADM node mapping and topologies.
      *
-     * @param nodeId unique node ID of OpenROADM node.
+     * @param nodeId
+     *   unique node ID of OpenROADM node.
      *
      */
     void deleteOpenRoadmnode(String nodeId);
@@ -41,5 +44,55 @@ public interface NetworkModelService {
      *   connection status of the node
      */
     void setOpenRoadmNodeStatus(String nodeId, NetconfNodeConnectionStatus.ConnectionStatus connectionStatus);
+
+    /**
+     * create new otn link in otn-topology.
+     *
+     * @param nodeA
+     *   OpenROADM node ID for link termination point A
+     * @param tpA
+     *   OpenROADM tp id on nodeA for link termination point A
+     * @param nodeZ
+     *   OpenROADM node ID for link termination point Z
+     * @param tpZ
+     *   OpenROADM tp id on nodeZ for link termination point Z
+     * @param linkType
+     *   OtnLinkType, as OTU4, ODTU, etc
+     */
+    void createOtnLinks(String nodeA, String tpA, String nodeZ, String tpZ, OtnLinkType linkType);
+
+    /**
+     * delete otn links from otn-topology.
+     *
+     * @param nodeA
+     *   OpenROADM node ID for link termination point A
+     * @param tpA
+     *   OpenROADM tp id on nodeA for link termination point A
+     * @param nodeZ
+     *   OpenROADM node ID for link termination point Z
+     * @param tpZ
+     *   OpenROADM tp id on nodeZ for link termination point Z
+     * @param linkType
+     *   OtnLinkType, as OTU4, ODTU, etc
+     */
+    void deleteOtnLinks(String nodeA, String tpA, String nodeZ, String tpZ, OtnLinkType linkType);
+
+    /**
+     * delete otn links from otn-topology.
+     *
+     * @param nodeTps
+     *   List containing a string composed of the netconf nodeId , and the
+     *       termination point supporting the service
+     * @param serviceRate
+     *   Service rate may be 1G, 10G or 100G
+     * @param tribPortNb
+     *   Trib port number allocated by the service
+     * @param tribSoltNb
+     *   First trib slot number allocated by the service
+     * @param isDeletion
+     *   True indicates if the low-order otn service must be deleted
+     */
+    void updateOtnLinks(List<String> nodeTps, String serviceRate, Short tribPortNb, Short tribSoltNb,
+        boolean isDeletion);
 
 }
