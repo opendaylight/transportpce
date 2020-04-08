79ca6444584ab411b5e2ad7683ca5de2c98c77de
Guillaume Lambert <guillaume.lambert@orange.com>
2020-04-08 09:45:54 +0200

    update lighty.io build for Magnesium support
    - update lighy-core branch to 12.1.x
    - update dependencies version to 12.1.0
    - update functional tests to use lighty executable version 12.1.0
    - JAVA 11 support: disable javadoc to allow building directly
      without setting the JAVA_HOME variable
    - add transportpcer-networkmodel new dependency
    - fix tpce java Main class and utils class
    
    TODO: update lighty documentation
          add tapi and inventory support to lighty
          fix functional tests with lighty
    
    JIRA: TRNSPRTPCE-237
    Signed-off-by: Guillaume Lambert <guillaume.lambert@orange.com>
    Co-authored-by: Balagangadhar Bathula <bb4341@att.com>
    Change-Id: Icec9547f5ead4e0abc10cdb67e2e8d8e6055a4ec

--- olm/src/main/java/org/opendaylight/transportpce/olm/util/OlmUtils.java
+++ olm/src/main/java/org/opendaylight/transportpce/olm/util/OlmUtils.java
@@ -29,8 +29,8 @@ public final class OlmUtils {
 
     private static final Logger LOG = LoggerFactory.getLogger(OlmUtils.class);
     private static long DATABROKER_READ_TIMEOUT_SECONDS = 120;
-    public static final long OLM_TIMER_1 = 120000; //#FUNCTESTVAL= 3000;
-    public static final long OLM_TIMER_2 = 20000;  //#FUNCTESTVAL= 2000;
+    public static final long OLM_TIMER_1 = 3000;
+    public static final long OLM_TIMER_2 = 2000;
 
     /**
      * This static method returns the port mapping {@link Nodes} for node.
