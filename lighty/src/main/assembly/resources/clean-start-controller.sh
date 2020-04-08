fe47d16151bf83bc81c19e0033e2386603188b2b
Guillaume Lambert <guillaume.lambert@orange.com>
2020-04-08 09:45:54 +0200

    update lighty.io build script for Mg SR0 support
    - update lighy-core branch to 12.0.x
    - update dependencies version to 12.0.1
    - JAVA 11 support: disable javadoc to allow building directly
      without setting the JAVA_HOME variable
    
    TODO: fix tpce java Main class
    
    JIRA: TRNSPRTPCE-237
    Signed-off-by: Guillaume Lambert <guillaume.lambert@orange.com>
    Change-Id: Icec9547f5ead4e0abc10cdb67e2e8d8e6055a4ec

--- lighty/src/main/assembly/resources/clean-start-controller.sh
+++ lighty/src/main/assembly/resources/clean-start-controller.sh
@@ -8,4 +8,4 @@ rm -rf cache
 rm -rf target
 
 #start controller
-java -ms128m -mx128m -XX:MaxMetaspaceSize=128m -jar lighty-transportpce-12.0.0-SNAPSHOT.jar
+java -ms128m -mx128m -XX:MaxMetaspaceSize=128m -jar lighty-transportpce-12.0.1-SNAPSHOT.jar
