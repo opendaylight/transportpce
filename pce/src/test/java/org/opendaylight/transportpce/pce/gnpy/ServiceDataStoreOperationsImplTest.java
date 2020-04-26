3df227d90375aa0b3151917c40359a8e43437d74
AhmedEldeeb50 <ahmed.eldeeb.ext@orange.com>
2020-04-26 16:07:59 +0200

    update PceLink test object and NetworkService
    JIRA: TRNSPRTPCE-209
    Signed-off-by: AhmedEldeeb50 <ahmed.eldeeb.ext@orange.com>
    Change-Id: If3d8ba209f1f4e1ca91f8b19d3e6e9f5a4ecf212
    (cherry picked from commit a77937222db28970e716507baa1d221679d4e0d0)

---
+++ pce/src/test/java/org/opendaylight/transportpce/pce/gnpy/ServiceDataStoreOperationsImplTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright © 2020 Orange Labs, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.pce.gnpy;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.opendaylight.transportpce.common.network.NetworkTransactionImpl;
+import org.opendaylight.transportpce.test.AbstractTest;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.device.rev170206.org.openroadm.device.container.OrgOpenroadmDevice;
+
+public class ServiceDataStoreOperationsImplTest extends AbstractTest {
+
+    ServiceDataStoreOperationsImpl serviceDataStoreOperations;
+    NetworkTransactionImpl networkTransaction = Mockito.mock(NetworkTransactionImpl.class);
+    OrgOpenroadmDevice orgOpenroadmDevice = Mockito.mock(OrgOpenroadmDevice.class);
+
+    @Before
+    public void setUp() {
+        serviceDataStoreOperations = new ServiceDataStoreOperationsImpl(networkTransaction);
+    }
+
+    // TODO: fix augmentation issue
+    @Test
+    public void createXMLFromDeviceTest() throws GnpyException {
+        serviceDataStoreOperations.createXMLFromDevice(this.getDataStoreContextUtil(),
+                orgOpenroadmDevice, "some-output");
+    }
+}
