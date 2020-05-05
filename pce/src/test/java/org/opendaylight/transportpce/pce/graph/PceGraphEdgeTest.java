1c9b3146fec684ab28eeedc54ffd11f25c649c12
AhmedEldeeb50 <ahmed.eldeeb.ext@orange.com>
2020-05-05 11:37:50 +0200

    add unit test for graph package
    Signed-off-by: AhmedEldeeb50 <ahmed.eldeeb.ext@orange.com>
    Change-Id: Ice69da80f9ff1d8adedda73e70f9aa3c6db4c595

--- pce/src/test/java/org/opendaylight/transportpce/pce/graph/PceGraphEdgeTest.java
+++ pce/src/test/java/org/opendaylight/transportpce/pce/graph/PceGraphEdgeTest.java
@@ -6,41 +6,28 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
-package org.opendaylight.transportpce.pce;
+package org.opendaylight.transportpce.pce.graph;
 
-import java.util.List;
-import java.util.Map;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.opendaylight.transportpce.common.network.NetworkTransactionImpl;
-import org.opendaylight.transportpce.common.network.RequestProcessor;
-import org.opendaylight.transportpce.pce.constraints.PceConstraints;
-import org.opendaylight.transportpce.pce.constraints.PceConstraintsCalc;
-import org.opendaylight.transportpce.pce.networkanalyzer.MapUtils;
 import org.opendaylight.transportpce.pce.networkanalyzer.PceLink;
 import org.opendaylight.transportpce.pce.networkanalyzer.PceOpticalNode;
-import org.opendaylight.transportpce.pce.networkanalyzer.PceResult;
 import org.opendaylight.transportpce.pce.utils.NodeUtils;
-import org.opendaylight.transportpce.pce.utils.PceTestData;
-import org.opendaylight.transportpce.test.AbstractTest;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.network.types.rev181130.OpenroadmNodeType;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.service.format.rev190531.ServiceFormat;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.rev180226.NodeId;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.rev180226.networks.network.Node;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.rev180226.networks.network.NodeBuilder;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.topology.rev180226.LinkId;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.topology.rev180226.networks.network.Link;
 
+public class PceGraphEdgeTest {
 
-public class PcePathDescriptionTests extends AbstractTest {
-
-    private PcePathDescription pcePathDescription;
-    private PceResult pceResult;
-    private static final Long WAVE_LENGTH = 20L;
-    private PceLink pceLink = null;
     private Link link = null;
     private Node node = null;
+    private PceLink pceLink = null;
+    private PceGraphEdge pceGraphEdge = null;
+
 
     @Before
     public void setUp() {
@@ -64,32 +51,14 @@ public class PcePathDescriptionTests extends AbstractTest {
         pceLink = new PceLink(link, pceOpticalNode, pceOpticalNode2);
         pceLink.setClient("XPONDER-CLIENT");
 
-        pceResult = new PceResult();
-        pceResult.setRC("200");
-        pceResult.setRate(Long.valueOf(1));
-        Map<LinkId, PceLink> map = Map.of(new LinkId("OpenROADM-3-1-DEG1-to-OpenROADM-3-2-DEG1"), pceLink);
-        pcePathDescription = new PcePathDescription(List.of(pceLink),
-                map, pceResult);
+        pceLink = new PceLink(link, pceOpticalNode, pceOpticalNode2);
 
-    }
-
-    // TODO fix opposite link
-    @Test(expected = Exception.class)
-    public void buildDescriptionsTest() {
-
-        pcePathDescription.buildDescriptions();
-        Assert.assertEquals(pcePathDescription.getReturnStructure().getMessage(), "No path available by PCE");
+        pceGraphEdge = new PceGraphEdge(pceLink);
     }
 
     @Test
-    public void mapUtil() {
-        PceConstraints pceConstraintsCalc = new PceConstraintsCalc(PceTestData
-                .getPCERequest(), new NetworkTransactionImpl(new RequestProcessor(this.getDataBroker())))
-                .getPceHardConstraints();
-        MapUtils.mapDiversityConstraints(List.of(node), List.of(link), pceConstraintsCalc);
-        MapUtils.getSupLink(link);
-        MapUtils.getAllSupNode(node);
-        MapUtils.getSRLGfromLink(link);
+    public void getLink() {
+        Assert.assertEquals(pceGraphEdge.link().getLinkId(),
+                pceLink.getLinkId());
     }
-
 }
--- pce/src/test/java/org/opendaylight/transportpce/pce/graph/PceGraphTest.java
+++ pce/src/test/java/org/opendaylight/transportpce/pce/graph/PceGraphTest.java
@@ -6,41 +6,40 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
-package org.opendaylight.transportpce.pce;
+package org.opendaylight.transportpce.pce.graph;
 
-import java.util.List;
 import java.util.Map;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.opendaylight.transportpce.common.network.NetworkTransactionImpl;
-import org.opendaylight.transportpce.common.network.RequestProcessor;
 import org.opendaylight.transportpce.pce.constraints.PceConstraints;
-import org.opendaylight.transportpce.pce.constraints.PceConstraintsCalc;
-import org.opendaylight.transportpce.pce.networkanalyzer.MapUtils;
 import org.opendaylight.transportpce.pce.networkanalyzer.PceLink;
+import org.opendaylight.transportpce.pce.networkanalyzer.PceNode;
 import org.opendaylight.transportpce.pce.networkanalyzer.PceOpticalNode;
 import org.opendaylight.transportpce.pce.networkanalyzer.PceResult;
 import org.opendaylight.transportpce.pce.utils.NodeUtils;
 import org.opendaylight.transportpce.pce.utils.PceTestData;
-import org.opendaylight.transportpce.test.AbstractTest;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.pce.rev200128.PathComputationRequestInput;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.network.types.rev181130.OpenroadmNodeType;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.service.format.rev190531.ServiceFormat;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.rev180226.NodeId;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.rev180226.networks.network.Node;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.rev180226.networks.network.NodeBuilder;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.topology.rev180226.LinkId;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.topology.rev180226.networks.network.Link;
 
 
-public class PcePathDescriptionTests extends AbstractTest {
+public class PceGraphTest {
 
-    private PcePathDescription pcePathDescription;
-    private PceResult pceResult;
-    private static final Long WAVE_LENGTH = 20L;
-    private PceLink pceLink = null;
     private Link link = null;
     private Node node = null;
+    private PceLink pceLink = null;
+    private PceGraph pceGraph = null;
+    private PathComputationRequestInput requestInput = PceTestData.getPCE_test2_request_54();
+    private PceConstraints pceHardConstraints = null;
+    private PceResult rc = null;
+    private PceOpticalNode pceOpticalNode = null;
+    private PceOpticalNode pceOpticalNode2 = null;
+    private Map<NodeId, PceNode> allPceNodes = null;
 
     @Before
     public void setUp() {
@@ -49,47 +48,61 @@ public class PcePathDescriptionTests extends AbstractTest {
                 "OpenROADM-3-1-DEG1",
                 "DEG1-TTP-TX", "DEG1-TTP-RX").build();
 
-        //  Link link=genereateLinkBuilder();
-
         NodeBuilder node1Builder = NodeUtils.getNodeBuilder(NodeUtils.geSupportingNodes());
         node = node1Builder.build();
 
-        PceOpticalNode pceOpticalNode = new PceOpticalNode(node,
+        pceOpticalNode = new PceOpticalNode(node,
                 OpenroadmNodeType.SRG, new NodeId("OpenROADM-3-2-DEG1"), ServiceFormat.Ethernet,
                 "DEGREE");
-        PceOpticalNode pceOpticalNode2 = new PceOpticalNode(node,
+
+        pceOpticalNode2 = new PceOpticalNode(node,
                 OpenroadmNodeType.SRG, new NodeId("OpenROADM-3-1-DEG1"), ServiceFormat.Ethernet,
                 "DEGREE");
 
         pceLink = new PceLink(link, pceOpticalNode, pceOpticalNode2);
         pceLink.setClient("XPONDER-CLIENT");
 
-        pceResult = new PceResult();
-        pceResult.setRC("200");
-        pceResult.setRate(Long.valueOf(1));
-        Map<LinkId, PceLink> map = Map.of(new LinkId("OpenROADM-3-1-DEG1-to-OpenROADM-3-2-DEG1"), pceLink);
-        pcePathDescription = new PcePathDescription(List.of(pceLink),
-                map, pceResult);
+        pceLink.getDestId();
+        pceOpticalNode.addOutgoingLink(pceLink);
 
-    }
+        pceLink = new PceLink(link, pceOpticalNode, pceOpticalNode2);
 
-    // TODO fix opposite link
-    @Test(expected = Exception.class)
-    public void buildDescriptionsTest() {
-
-        pcePathDescription.buildDescriptions();
-        Assert.assertEquals(pcePathDescription.getReturnStructure().getMessage(), "No path available by PCE");
+        // init PceHardContraints
+        pceHardConstraints = new PceConstraints();
+        // pceHardConstraints.setp
+        allPceNodes = Map.of(new NodeId("OpenROADM-3-2-DEG1"), pceOpticalNode,
+                new NodeId("OpenROADM-3-1-DEG1"), pceOpticalNode2);
+        rc = new PceResult();
+        pceGraph = new PceGraph(pceOpticalNode, pceOpticalNode2, allPceNodes,
+                pceHardConstraints,
+                null, rc,
+                "ODU4");
     }
 
     @Test
-    public void mapUtil() {
-        PceConstraints pceConstraintsCalc = new PceConstraintsCalc(PceTestData
-                .getPCERequest(), new NetworkTransactionImpl(new RequestProcessor(this.getDataBroker())))
-                .getPceHardConstraints();
-        MapUtils.mapDiversityConstraints(List.of(node), List.of(link), pceConstraintsCalc);
-        MapUtils.getSupLink(link);
-        MapUtils.getAllSupNode(node);
-        MapUtils.getSRLGfromLink(link);
+    public void clacPath() {
+
+        Assert.assertEquals(pceGraph.calcPath(), true);
     }
 
+    @Test
+    public void clacPathFalse() {
+        pceGraph = new PceGraph(pceOpticalNode, pceOpticalNode2, allPceNodes,
+                pceHardConstraints,
+                null, rc,
+                "100GE");
+
+        Assert.assertEquals(pceGraph.calcPath(), false);
+    }
+
+    @Test(expected = Exception.class)
+    public void clacPath1GE() {
+        pceGraph = new PceGraph(pceOpticalNode, pceOpticalNode2, allPceNodes,
+                pceHardConstraints,
+                null, rc,
+                "10GE");
+
+        Assert.assertEquals(pceGraph.calcPath(), false);
+    }
 }
+
