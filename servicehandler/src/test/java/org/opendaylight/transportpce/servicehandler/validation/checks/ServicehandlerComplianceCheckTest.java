0d79ddbe94c0bff532992e852b4aab1c238e2ba9
guillaume.lambert <guillaume.lambert@orange.com>
2020-05-06 12:41:13 +0200

    fix ServiceHandler SLF4J SpotBugs issues
    and by the way,
    - rename ServiceHandlerCompliancyCheck class for coherence.
    - review how messages and logs are handled there
    
    JIRA: TRNSPRTPCE-214 TRNSPRTPCE-246 TRNSPRTPCE-265
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    Change-Id: I82e1ac784bb66acbdcf62004da6da6eb3701434b

--- servicehandler/src/test/java/org/opendaylight/transportpce/servicehandler/validation/checks/ServicehandlerComplianceCheckTest.java
+++ servicehandler/src/test/java/org/opendaylight/transportpce/servicehandler/validation/checks/ServicehandlerComplianceCheckTest.java
@@ -7,20 +7,22 @@
  */
 package org.opendaylight.transportpce.servicehandler.validation.checks;
 
+import static org.opendaylight.transportpce.servicehandler.validation.checks.ServicehandlerComplianceCheck.LogMessages;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.common.service.types.rev190531.ConnectionType;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.common.service.types.rev190531.RpcActions;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.common.service.types.rev190531.sdnc.request.header.SdncRequestHeaderBuilder;
 
-public class ServicehandlerCompliancyCheckTest {
+public class ServicehandlerComplianceCheckTest {
 
-    public ServicehandlerCompliancyCheckTest(){
+    public ServicehandlerComplianceCheckTest(){
     }
 
     @Test
     public void checkFalseSdncRequest() {
-        ComplianceCheckResult result = ServicehandlerCompliancyCheck.check("service 1",
+        ComplianceCheckResult result = ServicehandlerComplianceCheck.check("service 1",
             new SdncRequestHeaderBuilder().setRequestId("1").setRequestSystemId("1").setNotificationUrl("1")
             .setRpcAction(RpcActions.ServiceCreate).build(),
             ConnectionType.Service,RpcActions.ServiceCreate, false, false);
@@ -31,61 +33,61 @@ public class ServicehandlerCompliancyCheckTest {
 
     @Test
     public void checkServiceNameNull() {
-        ComplianceCheckResult result = ServicehandlerCompliancyCheck.check(null, null,
+        ComplianceCheckResult result = ServicehandlerComplianceCheck.check(null, null,
                 ConnectionType.Service,null, false, false);
 
-        Assert.assertEquals("Service Name (common-id for Temp service) is not set", result.getMessage());
+        Assert.assertEquals(LogMessages.SERVICENAME_NOT_SET, result.getMessage());
         Assert.assertFalse(result.hasPassed());
     }
 
     @Test
     public void checkConTypeFalseAndNull() {
-        ComplianceCheckResult result = ServicehandlerCompliancyCheck.check("service 1", null,
+        ComplianceCheckResult result = ServicehandlerComplianceCheck.check("service 1", null,
                 null,null, true, false);
-        Assert.assertEquals("Service ConnectionType is not set", result.getMessage());
+        Assert.assertEquals(LogMessages.CONNECTIONTYPE_NOT_SET, result.getMessage());
         Assert.assertFalse(result.hasPassed());
     }
 
     @Test
     public void checkSdncRequestHeaderNull() {
-        ComplianceCheckResult result = ServicehandlerCompliancyCheck.check("service 1", null,
+        ComplianceCheckResult result = ServicehandlerComplianceCheck.check("service 1", null,
                 ConnectionType.Service,null, true, true);
 
-        Assert.assertEquals("Service sndc-request-header is not set ", result.getMessage());
+        Assert.assertEquals(LogMessages.HEADER_NOT_SET, result.getMessage());
         Assert.assertFalse(result.hasPassed());
     }
 
     @Test
     public void checkRequestIdEmptyString() {
-        ComplianceCheckResult result = ServicehandlerCompliancyCheck.check("service 1",
+        ComplianceCheckResult result = ServicehandlerComplianceCheck.check("service 1",
                 new SdncRequestHeaderBuilder().setRequestId("")
                         .setRpcAction(RpcActions.ServiceCreate).build(),
                 ConnectionType.Service, RpcActions.ServiceCreate, true, true);
 
-        Assert.assertEquals("Service sdncRequestHeader 'request-id' is not set", result.getMessage());
+        Assert.assertEquals(LogMessages.REQUESTID_NOT_SET, result.getMessage());
         Assert.assertFalse(result.hasPassed());
     }
 
     @Test
     public void checkDifferentAction() {
-        ComplianceCheckResult result = ServicehandlerCompliancyCheck.check("service 1",
+        ComplianceCheckResult result = ServicehandlerComplianceCheck.check("service 1",
                 new SdncRequestHeaderBuilder().setRequestId("1")
                         .setRpcAction(RpcActions.ServiceCreate).build(),
                 ConnectionType.Service, RpcActions.NetworkReOptimization, true, true);
 
-        Assert.assertEquals("Service sdncRequestHeader rpc-action '" + RpcActions.ServiceCreate.name()
-                + "' not equal to '"
-                + RpcActions.NetworkReOptimization.name() + "'", result.getMessage());
+        Assert.assertEquals(
+                LogMessages.rpcactionsDiffers(RpcActions.ServiceCreate, RpcActions.NetworkReOptimization),
+                result.getMessage());
         Assert.assertFalse(result.hasPassed());
     }
 
     @Test
     public void checkServiceActionNull() {
-        ComplianceCheckResult result = ServicehandlerCompliancyCheck.check("service 1",
+        ComplianceCheckResult result = ServicehandlerComplianceCheck.check("service 1",
                 new SdncRequestHeaderBuilder().setRequestId("1").build(),
                 ConnectionType.Service, RpcActions.NetworkReOptimization, true, true);
 
-        Assert.assertEquals("Service sndc-request-header 'rpc-action' is not set ", result.getMessage());
+        Assert.assertEquals(LogMessages.RPCACTION_NOT_SET, result.getMessage());
         Assert.assertFalse(result.hasPassed());
     }
 }
