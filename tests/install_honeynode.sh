3d3512dfbeb832fe583aa456593fd1c759e72c94
manuedelf <emmanuelle.delfour@gmail.com>
2020-05-18 12:21:08 +0200

    Remove honeynode source code
    JIRA: TRNSPRTPCE-238
    Change-Id: I87282c61db9bd57e08941095e2dd615c1b7b89b5

---
+++ tests/install_honeynode.sh
@@ -0,0 +1,35 @@
+#/bin/bash
+
+#set -x
+
+#check if curl exists
+if ! [ -x "$(command -v curl)" ];then
+    echo "curl is not installed." >&2
+    exit 1
+fi
+#check if unzip exists
+if ! [ -x "$(command -v unzip)" ];then
+    echo "unzip is not installed." >&2
+    exit 1
+fi
+
+#clean honeynode directories
+
+if [ -d "$(dirname $0)/honeynode/1.2.1/honeynode-simulator" ];then
+    echo "Removing $(dirname $0)/honeynode/1.2.1/honeynode-simulator directory"
+    rm -rf $(dirname $0)/honeynode/1.2.1/honeynode-simulator
+fi
+if [ -d "$(dirname $0)/honeynode/2.2.1/honeynode-simulator" ];then
+    echo "Removing $(dirname $0)/honeynode/2.2.1/honeynode-simulator directory"
+    rm -rf $(dirname $0)/honeynode/2.2.1/honeynode-simulator
+fi
+#download honeynode for 1.2.1 devices and install it
+echo "Installing honeynode for 1.2.1 devices to $(dirname $0)/honeynode/1.2.1/honeynode-simulator directory "
+curl --location --request GET "https://gitlab.com/api/v4/projects/17518226/jobs/artifacts/honeynode-plugin-aggregator-1.0.0/download?job=mvn-build" -o $(dirname $0)/honeynode/1.2.1/artifact.zip
+unzip $(dirname $0)/honeynode/1.2.1/artifact.zip -d $(dirname $0)/honeynode/1.2.1
+rm -f $(dirname $0)/honeynode/1.2.1/artifact.zip
+#download honeynode for 2.2.1 devices and install it
+echo "Installing honeynode for 2.2.1 devices to $(dirname $0)/honeynode/1.2.1/honeynode-simulator directory "
+curl --location --request GET "https://gitlab.com/api/v4/projects/17518226/jobs/artifacts/honeynode-plugin-aggregator-2.0.0/download?job=mvn-build" -o $(dirname $0)/honeynode/2.2.1/artifact.zip
+unzip $(dirname $0)/honeynode/2.2.1/artifact.zip -d $(dirname $0)/honeynode/2.2.1
+rm -f $(dirname $0)/honeynode/2.2.1/artifact.zip
