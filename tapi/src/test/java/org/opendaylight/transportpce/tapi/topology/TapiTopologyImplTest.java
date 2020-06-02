1849a0ee665443ceb69c97499d8ffa7294753000
manuedelf <emmanuelle.delfour@gmail.com>
2020-06-03 00:11:07 +0200

    Fix tapi execution
    - Fix NPE in TapiTopologyImpl
    - Fix Junit for TapiTopoogyImpl
    
    JIRA: TRNSPRTPCE-275
    Change-Id: Ia2323068a5d39ca91fcbd05d19659a6f748e0759

--- tapi/src/test/java/org/opendaylight/transportpce/tapi/topology/TapiTopologyImplTest.java
+++ tapi/src/test/java/org/opendaylight/transportpce/tapi/topology/TapiTopologyImplTest.java
@@ -7,12 +7,16 @@
  */
 package org.opendaylight.transportpce.tapi.topology;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
+import org.eclipse.jdt.annotation.Nullable;
 import org.junit.Before;
 import org.junit.Test;
 import org.opendaylight.transportpce.common.DataStoreContext;
@@ -21,9 +25,11 @@ import org.opendaylight.transportpce.tapi.utils.TopologyDataUtils;
 import org.opendaylight.transportpce.test.AbstractTest;
 import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.topology.rev181210.GetTopologyDetailsInput;
 import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.topology.rev181210.GetTopologyDetailsOutput;
+import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.topology.rev181210.get.topology.details.output.Topology;
 import org.opendaylight.yangtools.yang.common.RpcResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 
 public class TapiTopologyImplTest extends AbstractTest {
     private static final Logger LOG = LoggerFactory.getLogger(TapiTopologyImplTest.class);
@@ -31,40 +37,40 @@ public class TapiTopologyImplTest extends AbstractTest {
     private ListeningExecutorService executorService;
     private CountDownLatch endSignal;
     private static final int NUM_THREADS = 3;
-    private boolean callbackRan;
     private DataStoreContext dataStoreContextUtil;
 
     @Before
     public void setUp() throws InterruptedException {
         executorService = MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(NUM_THREADS));
         endSignal = new CountDownLatch(1);
-        callbackRan = false;
         dataStoreContextUtil = new DataStoreContextImpl();
         TopologyDataUtils.writeTopologyFromFileToDatastore(dataStoreContextUtil);
         TopologyDataUtils.writePortmappingFromFileToDatastore(dataStoreContextUtil);
         LOG.info("setup done");
-        Thread.sleep(1000);
     }
 
     @Test
     public void getTopologyDetailsWhenSuccessful() throws ExecutionException, InterruptedException {
         GetTopologyDetailsInput input = TopologyDataUtils.buildGetTopologyDetailsInput();
         TapiTopologyImpl tapiTopoImpl = new TapiTopologyImpl(dataStoreContextUtil.getDataBroker());
-
         ListenableFuture<RpcResult<GetTopologyDetailsOutput>> result = tapiTopoImpl.getTopologyDetails(input);
-
         result.addListener(new Runnable() {
             @Override
             public void run() {
-                callbackRan = true;
                 endSignal.countDown();
             }
         }, executorService);
-
         endSignal.await();
-
         RpcResult<GetTopologyDetailsOutput> rpcResult = result.get();
-        LOG.info("topo TAPI returned = {}", rpcResult.getResult().getTopology());
+        @Nullable
+        Topology topology = rpcResult.getResult().getTopology();
+        LOG.info("topo TAPI returned = {}", topology);
+
+        assertNotNull("Topology should not be null", topology);
+        assertEquals("Nodes list size should be 1",
+                1, topology.getNode().size());
+        assertEquals("Node name should be TapiNode1",
+                "TapiNode1", topology.getNode().get(0).getName().get(0).getValue());
     }
 
 }
