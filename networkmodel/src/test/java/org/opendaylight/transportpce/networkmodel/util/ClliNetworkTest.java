89ba1fa9b390d05a5e0d4a33c8a8ce1526eaf29f
Gilles Thouenon <gilles.thouenon@orange.com>
2020-06-04 10:15:22 +0200

    Junit tests for network model
    Add new Junit unitary tests for package
    org.opendaylight.transportpce.networkmodel.util
    
    JIRA: TRNSPRTPCE-270
    Signed-off-by: Gilles Thouenon <gilles.thouenon@orange.com>
    Change-Id: Ia93aa3b37bc5a2df8981e862ebffd164bcd5d852

---
+++ networkmodel/src/test/java/org/opendaylight/transportpce/networkmodel/util/ClliNetworkTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright © 2020 Orange Labs, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.transportpce.networkmodel.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev200429.network.nodes.NodeInfo;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev200429.network.nodes.NodeInfo.OpenroadmVersion;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev200429.network.nodes.NodeInfoBuilder;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.clli.network.rev181130.Node1;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.clli.network.rev181130.Node1Builder;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.common.types.rev181019.NodeTypes;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4Address;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.rev180226.NodeId;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.rev180226.networks.network.Node;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.rev180226.networks.network.NodeKey;
+
+public class ClliNetworkTest {
+
+    @Test
+    public void createNodeTest() {
+        //prepare data test
+        NodeInfoBuilder nodeBldr = computeNodeInfoBuilder();
+        NodeInfo nodeInfo = nodeBldr.build();
+        Node1 clliAugmentation = new Node1Builder()
+                .setClli(nodeInfo .getNodeClli())
+                .build();
+
+        //run test
+        Node createdNode = ClliNetwork.createNode("XPDRA01", nodeInfo);
+        assertNull("SupportingNode should be null", createdNode.getSupportingNode());
+        assertEquals("Node key should be equals to NodeClliId ", new NodeKey(new NodeId("NodeClliId")),
+            createdNode.key());
+        assertEquals("Expect org.opendaylight.yang.gen.v1.http.org.openroadm.clli.network.rev181130.Node1 augmentation",
+            clliAugmentation, createdNode.augmentation(Node1.class));
+    }
+
+    @Test
+    public void createNodeWithNullDeviceIdTest() {
+        NodeInfoBuilder nodeBldr = computeNodeInfoBuilder();
+        Node createdNode = ClliNetwork.createNode(null, nodeBldr.build());
+        assertNull("SupportingNode should be null", createdNode.getSupportingNode());
+        assertEquals("Node id should be equals to NodeClliId ", new NodeId("NodeClliId"), createdNode.getNodeId());
+    }
+
+    @Test
+    public void createNodeWithNullClliTest() {
+        NodeInfoBuilder nodeBldr = computeNodeInfoBuilderWithoutClli();
+        Node createdNode = ClliNetwork.createNode("XPDRA01", nodeBldr.build());
+        assertNull("Node id should be null", createdNode.getNodeId());
+    }
+
+    private NodeInfoBuilder computeNodeInfoBuilder() {
+        NodeInfoBuilder nodeBldr = new NodeInfoBuilder();
+        nodeBldr.setNodeClli("NodeClliId");
+        nodeBldr.setNodeModel("NodeModel");
+        nodeBldr.setNodeIpAddress(new IpAddress(new Ipv4Address("127.0.0.1")));
+        nodeBldr.setNodeType(NodeTypes.Xpdr);
+        nodeBldr.setNodeVendor("VendorA");
+        nodeBldr.setOpenroadmVersion(OpenroadmVersion._121);
+        return nodeBldr;
+    }
+
+    private NodeInfoBuilder computeNodeInfoBuilderWithoutClli() {
+        NodeInfoBuilder nodeBldr = new NodeInfoBuilder();
+        nodeBldr.setNodeModel("NodeModel");
+        nodeBldr.setNodeIpAddress(new IpAddress(new Ipv4Address("127.0.0.1")));
+        nodeBldr.setNodeType(NodeTypes.Xpdr);
+        nodeBldr.setNodeVendor("VendorA");
+        nodeBldr.setOpenroadmVersion(OpenroadmVersion._121);
+        return nodeBldr;
+    }
+}
---
+++ networkmodel/src/test/java/org/opendaylight/transportpce/networkmodel/util/TpceNetworkTest.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright © 2020 Orange, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.transportpce.networkmodel.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.util.concurrent.ExecutionException;
+import org.junit.Test;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.transportpce.common.network.NetworkTransactionImpl;
+import org.opendaylight.transportpce.common.network.RequestProcessor;
+import org.opendaylight.transportpce.test.AbstractTest;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.clli.network.rev181130.NetworkTypes1;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.clli.network.rev181130.NetworkTypes1Builder;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.clli.network.rev181130.networks.network.network.types.ClliNetworkBuilder;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.common.network.rev181130.networks.network.network.types.OpenroadmCommonNetworkBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.rev180226.NetworkId;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.rev180226.Networks;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.rev180226.networks.Network;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.rev180226.networks.NetworkKey;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.rev180226.networks.network.NetworkTypes;
+import org.opendaylight.yangtools.yang.binding.Augmentation;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+
+public class TpceNetworkTest extends AbstractTest {
+    TpceNetwork tpceNetwork = new TpceNetwork(new NetworkTransactionImpl(new RequestProcessor(getDataBroker())));
+
+    @Test
+    public void createLayerClliTest() throws InterruptedException, ExecutionException {
+        tpceNetwork.createLayer("clli-network");
+        InstanceIdentifier<Network> nwIID = InstanceIdentifier.create(Networks.class)
+            .child(Network.class, new NetworkKey(new NetworkId("clli-network")));
+        Network createdClli = getDataBroker().newReadOnlyTransaction()
+            .read(LogicalDatastoreType.CONFIGURATION, nwIID).get().get();
+        assertNotNull("Clli layer should be created and not null", createdClli);
+
+        Augmentation<NetworkTypes> ordClli = new NetworkTypes1Builder()
+            .setClliNetwork(new ClliNetworkBuilder().build())
+            .build();
+        assertNotNull("clli augmentation should not be null", createdClli.getNetworkTypes()
+            .augmentation(NetworkTypes1.class));
+        assertEquals("bad clli augmentation for network-types", ordClli,
+            createdClli.getNetworkTypes().augmentation(NetworkTypes1.class));
+    }
+
+    @Test
+    public void createLayerNetworkTest() throws InterruptedException, ExecutionException {
+        tpceNetwork.createLayer("openroadm-network");
+        InstanceIdentifier<Network> nwIID = InstanceIdentifier.create(Networks.class)
+            .child(Network.class, new NetworkKey(new NetworkId("openroadm-network")));
+        Network createdOrdNetwork = getDataBroker().newReadOnlyTransaction()
+            .read(LogicalDatastoreType.CONFIGURATION, nwIID).get().get();
+        assertNotNull("openroadm-network layer should be created and not null", createdOrdNetwork);
+        commonNetworkAugmentationTest(createdOrdNetwork);
+    }
+
+    @Test
+    public void createLayerTopologyTest() throws InterruptedException, ExecutionException {
+        tpceNetwork.createLayer("openroadm-topology");
+        InstanceIdentifier<Network> nwIID = InstanceIdentifier.create(Networks.class)
+            .child(Network.class, new NetworkKey(new NetworkId("openroadm-topology")));
+        Network createdOrdNetwork = getDataBroker().newReadOnlyTransaction()
+            .read(LogicalDatastoreType.CONFIGURATION, nwIID).get().get();
+        assertNotNull("openroadm-logpology layer should be created and not null", createdOrdNetwork);
+        commonNetworkAugmentationTest(createdOrdNetwork);
+    }
+
+    @Test
+    public void createLayerOtnTest() throws InterruptedException, ExecutionException {
+        tpceNetwork.createLayer("otn-topology");
+        InstanceIdentifier<Network> nwIID = InstanceIdentifier.create(Networks.class)
+            .child(Network.class, new NetworkKey(new NetworkId("otn-topology")));
+        Network createdOrdNetwork = getDataBroker().newReadOnlyTransaction()
+            .read(LogicalDatastoreType.CONFIGURATION, nwIID).get().get();
+        assertNotNull("otn-logpology layer should be created and not null", createdOrdNetwork);
+        commonNetworkAugmentationTest(createdOrdNetwork);
+    }
+
+    @Test
+    public void createBadLayerTest() throws InterruptedException, ExecutionException {
+        tpceNetwork.createLayer("toto");
+        InstanceIdentifier<Network> nwIID = InstanceIdentifier.create(Networks.class)
+            .child(Network.class, new NetworkKey(new NetworkId("toto")));
+        Network createdOrdNetwork = getDataBroker().newReadOnlyTransaction()
+            .read(LogicalDatastoreType.CONFIGURATION, nwIID).get().get();
+        assertNotNull("toto layer should be created and not null", createdOrdNetwork);
+        assertNull("toto layer should not have any network-type augmentation", createdOrdNetwork.getNetworkTypes()
+            .augmentation(NetworkTypes1.class));
+        assertNull("toto layer should not have any network-type augmentation", createdOrdNetwork.getNetworkTypes()
+            .augmentation(org.opendaylight.yang.gen.v1.http.org.openroadm.common.network.rev181130
+            .NetworkTypes1.class));
+    }
+
+    private void commonNetworkAugmentationTest(Network createdOrdNetwork) {
+        Augmentation<NetworkTypes> ordComNet = new org.opendaylight.yang.gen.v1.http.org.openroadm.common.network
+            .rev181130.NetworkTypes1Builder().setOpenroadmCommonNetwork(new OpenroadmCommonNetworkBuilder().build())
+            .build();
+        assertNotNull("common-network augmentation should not be null", createdOrdNetwork.getNetworkTypes()
+            .augmentation(org.opendaylight.yang.gen.v1.http.org.openroadm.common.network.rev181130
+            .NetworkTypes1.class));
+        assertEquals("bad common-network augmentation for network-types", ordComNet,
+            createdOrdNetwork.getNetworkTypes().augmentation(org.opendaylight.yang.gen.v1.http.org.openroadm.common
+            .network.rev181130.NetworkTypes1.class));
+    }
+}
