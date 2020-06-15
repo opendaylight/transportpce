25b30678e5d6c4aa67b0e3b783539790f643f0eb
Susmitha Avula <sa396f@att.com>
2020-06-15 16:22:00 -0400

    Enabled flex-grid support for 2.2.1 Roadms
    - Added FlexGridImpl class
    - Made changes in transportpce-portmapping and
    transportpce-common-types yang files
    
    JIRA: TRNSPRTPCE-253
    Change-Id: I6d3546d35d7dc80c4b3fb60a0a8916416d28658a

--- common/src/main/java/org/opendaylight/transportpce/common/mapping/PortMappingVersion221.java
+++ common/src/main/java/org/opendaylight/transportpce/common/mapping/PortMappingVersion221.java
@@ -10,6 +10,7 @@ package org.opendaylight.transportpce.common.mapping;
 
 import com.google.common.util.concurrent.FluentFuture;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
@@ -45,6 +46,7 @@ import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmappi
 import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev200429.network.nodes.MappingBuilder;
 import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev200429.network.nodes.MappingKey;
 import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev200429.network.nodes.NodeInfo;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev200429.network.nodes.NodeInfo.GridImplementation;
 import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev200429.network.nodes.NodeInfo.OpenroadmVersion;
 import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev200429.network.nodes.NodeInfoBuilder;
 import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev200429.network.nodes.SwitchingPoolLcp;
@@ -52,6 +54,7 @@ import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmappi
 import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev200429.network.nodes.switching.pool.lcp.NonBlockingList;
 import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev200429.network.nodes.switching.pool.lcp.NonBlockingListBuilder;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.common.types.rev181019.Direction;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.common.types.rev181019.FrequencyGHz;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.common.types.rev181019.PortQual;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.common.types.rev181019.XpdrNodeTypes;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.device.rev181019.CircuitPack;
@@ -118,7 +121,7 @@ public class PortMappingVersion221 {
             return false;
         }
         deviceInfo = deviceInfoOptional.get();
-        nodeInfo = createNodeInfo(deviceInfo);
+        nodeInfo = createNodeInfo(deviceInfo, nodeId);
         if (nodeInfo == null) {
             return false;
         }
@@ -992,7 +995,7 @@ public class PortMappingVersion221 {
         return true;
     }
 
-    private NodeInfo createNodeInfo(Info deviceInfo) {
+    private NodeInfo createNodeInfo(Info deviceInfo, String nodeId) {
         NodeInfoBuilder nodeInfoBldr = new NodeInfoBuilder();
         if (deviceInfo.getNodeType() != null) {
             nodeInfoBldr.setOpenroadmVersion(OpenroadmVersion._221).setNodeType(deviceInfo.getNodeType());
@@ -1009,6 +1012,17 @@ public class PortMappingVersion221 {
             }
             if (deviceInfo.getIpAddress() != null) {
                 nodeInfoBldr.setNodeIpAddress(deviceInfo.getIpAddress());
+            }
+            //Grid-implementation
+            List<Degree> degrees = getDegrees(nodeId, deviceInfo);
+            for (Degree degree : degrees) {
+                FrequencyGHz centerFreq = degree.getMcCapabilities().getCenterFreqGranularity();
+                if (centerFreq.getValue().equals(BigDecimal.valueOf(50.0))) {
+                    nodeInfoBldr.setGridImplementation(GridImplementation.FixedGrid);
+                }
+                else {
+                    nodeInfoBldr.setGridImplementation(GridImplementation.FlexGrid);
+                }
             }
         } else {
          // TODO make mandatory in yang
