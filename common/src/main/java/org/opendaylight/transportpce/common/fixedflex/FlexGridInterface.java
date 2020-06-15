6fdd59e7b458e48d472493a6d701b5d77e278a32
Susmitha Avula <sa396f@att.com>
2020-06-15 16:22:00 -0400

    Enabled flex-grid support for 2.2.1 Roadms
    - Added FlexGridImpl class
    - Made changes in transportpce-portmapping to
      support flex-grid implementation
    
    JIRA: TRNSPRTPCE-253
    Change-Id: I6d3546d35d7dc80c4b3fb60a0a8916416d28658a

---
+++ common/src/main/java/org/opendaylight/transportpce/common/fixedflex/FlexGridInterface.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright © 2020 AT&T, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.transportpce.common.fixedflex;
+
+public interface FlexGridInterface {
+
+    FlexGridImpl getFlexWaveMapping(float centerFrequency, float slotwidth);
+
+    double getStart();
+
+    double getStop();
+}
