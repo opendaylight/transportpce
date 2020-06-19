139219f22b246e2813e5d6288ce7bd906951b7b5
manuedelf <emmanuelle.delfour@gmail.com>
2020-06-19 22:23:44 +0200

    Update honeynode simulator version
    New version following bux fix "Make honeycomb-tpce script BSD compatible"
    
    Change-Id: If259f0d24924b86a55da0aeb655ce6b13ffb0685

--- tests/install_honeynode.sh
+++ tests/install_honeynode.sh
@@ -26,11 +26,11 @@ fi
 #download honeynode for 1.2.1 devices and install it
 #complete source code can be found at https://gitlab.com/Orange-OpenSource/lfn/odl/honeynode-simulator.git
 echo "Installing honeynode for 1.2.1 devices to $(dirname $0)/honeynode/1.2.1/honeynode-simulator directory "
-curl --location --request GET "https://gitlab.com/api/v4/projects/17518226/jobs/artifacts/honeynode-plugin-aggregator-1.0.2/download?job=mvn-build" -o $(dirname $0)/honeynode/1.2.1/artifact.zip
+curl --location --request GET "https://gitlab.com/api/v4/projects/17518226/jobs/artifacts/honeynode-plugin-aggregator-1.0.3/download?job=mvn-build" -o $(dirname $0)/honeynode/1.2.1/artifact.zip
 unzip $(dirname $0)/honeynode/1.2.1/artifact.zip -d $(dirname $0)/honeynode/1.2.1
 rm -f $(dirname $0)/honeynode/1.2.1/artifact.zip
 #download honeynode for 2.2.1 devices and install it
 echo "Installing honeynode for 2.2.1 devices to $(dirname $0)/honeynode/2.2.1/honeynode-simulator directory "
-curl --location --request GET "https://gitlab.com/api/v4/projects/17518226/jobs/artifacts/honeynode-plugin-aggregator-2.0.2/download?job=mvn-build" -o $(dirname $0)/honeynode/2.2.1/artifact.zip
+curl --location --request GET "https://gitlab.com/api/v4/projects/17518226/jobs/artifacts/honeynode-plugin-aggregator-2.0.3/download?job=mvn-build" -o $(dirname $0)/honeynode/2.2.1/artifact.zip
 unzip $(dirname $0)/honeynode/2.2.1/artifact.zip -d $(dirname $0)/honeynode/2.2.1
 rm -f $(dirname $0)/honeynode/2.2.1/artifact.zip
