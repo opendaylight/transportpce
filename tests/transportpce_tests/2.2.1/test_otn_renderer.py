5b59c1a67e32dbe232ce4d4bd7ef424fd4674103
guillaume.lambert <guillaume.lambert@orange.com>
2020-06-23 16:14:31 +0200

    replace portmapping FNV1-128 by FNV1-64 + Base64
    FNV1 128bit hash produces 16 bytes results but they cannot easily be
    represented as a UTF-8 readable string of 16 characters as required
    by SAPI/DAPI OpenROADM specification.
    Base64 conversion allows to fix this but generates an overhead.
    By using FNV1 64bit hash + Base64, we can guarantee the results
    will not exceed 16 characters (usually around 13).
    
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    Change-Id: I559b5439f83cffc619a1db4e29b9d78c8401ed06

--- tests/transportpce_tests/2.2.1/test_otn_renderer.py
+++ tests/transportpce_tests/2.2.1/test_otn_renderer.py
@@ -101,7 +101,7 @@ class TransportPCEtesting(unittest.TestCase):
              'logical-connection-point': 'XPDR1-CLIENT1',
              'port-direction': 'bidirectional',
              'port-qual': 'xpdr-client',
-             'lcp-hash-val': '8b3efff522736722500b5e68fb6e696e'},
+             'lcp-hash-val': 'AIg960BwzGKe'},
             res['mapping'])
 
     def test_03_get_portmapping_NETWORK1(self):
@@ -123,7 +123,7 @@ class TransportPCEtesting(unittest.TestCase):
                 "port-qual": "xpdr-network",
                 "supporting-circuit-pack-name": "CP1-CFP0",
                 "xponder-type": "mpdr",
-             'lcp-hash-val': '1021db8d2affe7386705c438c67ea21f'},
+             'lcp-hash-val': 'AM7VlW5NpPm3'},
             res['mapping'])
 
     def test_04_service_path_create_OCH_OTU4(self):
@@ -169,7 +169,7 @@ class TransportPCEtesting(unittest.TestCase):
                 "port-qual": "xpdr-network",
                 "supporting-circuit-pack-name": "CP1-CFP0",
                 "xponder-type": "mpdr",
-                "lcp-hash-val": "1021db8d2affe7386705c438c67ea21f"},
+                "lcp-hash-val": "AM7VlW5NpPm3"},
             res['mapping'])
 
     def test_06_check_interface_och(self):
@@ -281,7 +281,7 @@ class TransportPCEtesting(unittest.TestCase):
                 "supporting-circuit-pack-name": "CP1-CFP0",
                 "xponder-type": "mpdr",
                 "supporting-odu4": "XPDR1-NETWORK1-ODU4",
-                "lcp-hash-val": "1021db8d2affe7386705c438c67ea21f"
+                "lcp-hash-val": "AM7VlW5NpPm3"
              },
             res['mapping'])
 
