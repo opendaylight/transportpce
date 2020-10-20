73848270bf601494ec25c0de438f9e23c91b7d56
Gilles Thouenon <gilles.thouenon@orange.com>
2020-10-20 18:07:49 +0200

    TAPI topology consolidation
    Modify TAPI topology implementation to give an abstraction of
    otn-topology for otn nodes whose network ports are connected to ROADM
    through xponder-input/output links. Moreover, abstracts also the ROADM
    infrastructure through a Photonic TAPI node. As for xponder-input/output
    links, they are represented in by OMS links in the T0 multilayer TAPI
    topology.
    - add new method in ConvertORTopoToTapiTopo to create the ROADM
      infrastructure abstraction
    - add new method in ConvertORTopoToTapiTopo to create OMS links
    - create and manage a TapiTopologyException
    - refacto TapiTopologyImpl to improve datastore readings
    - refacto and improve UT for TapiTopologyImpl and
      ConvertORTopoToTapiTopo classes
    - new samples of xml files to adapt portmapping and topologies to new
      TAPI topology code and test cases
    - disable TAPI functional tests in tox
    
    JIRA: TRNSPRTPCE-341
    Signed-off-by: Gilles Thouenon <gilles.thouenon@orange.com>
    Co-authored-by: Christophe Betoule <christophe.betoule@orange.com>
    Change-Id: I38904051cc3e183eda2586de7a104e46d56e39c8

---
+++ tapi/src/main/java/org/opendaylight/transportpce/tapi/topology/TapiTopologyException.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright © 2020 Orange, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.transportpce.tapi.topology;
+
+
+public class TapiTopologyException extends Exception {
+
+    public TapiTopologyException(String message) {
+        super(message);
+    }
+
+    public TapiTopologyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
