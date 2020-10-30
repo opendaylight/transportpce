f639861b2b1375318906720d0ac2aa1d3a137889
Balagangadhar Bathula <bb4341@att.com>
2020-10-30 19:17:43 -0400

    Port-mapping changes to support 7.1.0 models
    - upgrade portmapping API YANG files to use 7.1.0 models
    - add in API pom.xml a dependency to ordm devices
      the attributes 'node-types' and 'xpdr-node-types' have moved to there.
    - update java code to use the new portmapping model revision
    
    JIRA: TRNSPRTPCE-256
    Change-Id: I5ed81415cc80800b1fadf40ab4ec5ea204e12f49

--- tapi/src/test/java/org/opendaylight/transportpce/tapi/utils/TopologyDataUtils.java
+++ tapi/src/test/java/org/opendaylight/transportpce/tapi/utils/TopologyDataUtils.java
@@ -20,7 +20,7 @@ import org.opendaylight.mdsal.common.api.CommitInfo;
 import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.transportpce.test.DataStoreContext;
 import org.opendaylight.transportpce.test.converter.XMLDataObjectConverter;
-import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev200827.Network;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev201012.Network;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.rev180226.Networks;
 import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.topology.rev181210.GetTopologyDetailsInput;
 import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.topology.rev181210.GetTopologyDetailsInputBuilder;
