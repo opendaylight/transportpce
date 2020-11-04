e4f9ab04c67c3e08e60ee1d24f5e6a19d981a308
guillaume.lambert <guillaume.lambert@orange.com>
2020-11-04 09:27:53 +0100

    ligthy support Silicon migration
    - Bump lighty pom to 14.0.0-SNAPSHOT
    - create a scrip to build lighty core master
      since the package is not available on maven central yet
    - adapt tox.ini accordingly
    - fix README.md
    - force lighty use in tox
    
    TODO: unforce lighty use inside tox or use a separate commit
    
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    Change-Id: If7124a7b93e4606037ffa25d4faff79deac023d9

---
+++ lighty/build_lighty_core.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd $(dirname "$0")
+cd ..
+git clone https://github.com/PantheonTechnologies/lighty-core.git
+cd lighty-core
+git checkout master
+mvn clean install -DskipTests -Dmaven.javadoc.skip=true
