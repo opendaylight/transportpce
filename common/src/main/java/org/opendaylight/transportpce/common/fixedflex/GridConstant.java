3e966d8a3851687e878984c5e05dcc00919344bd
manuedelf <emmanuelle.delfour@orange.com>
2020-11-04 09:17:44 +0100

    Common changes to support 7.1.0 models
    JIRA: TRNSPRTPCE-259
    Signed-off-by: manuedelf <emmanuelle.delfour@orange.com>
    Change-Id: Iadd8e1bd43b253ca52d9ee17f76d2f86a5d4bafc

---
+++ common/src/main/java/org/opendaylight/transportpce/common/fixedflex/GridConstant.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright © 2020 Orange, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.common.fixedflex;
+
+/**
+ * Constant class common to fixed grid and flex grid.
+ * @author Orange
+ *
+ */
+public final class GridConstant {
+
+    public static final int AVAILABLE_SLOT_VALUE = 1;
+    public static final int USED_SLOT_VALUE = 0;
+
+    private GridConstant() {
+    }
+
+    public static final String C_BAND = "cband";
+
+}
