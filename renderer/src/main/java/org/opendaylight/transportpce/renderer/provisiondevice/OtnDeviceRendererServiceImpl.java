84bbe621585fc3eff26f972caee2116a27bc2a7e
manuedelf <emmanuelle.delfour@orange.com>
2020-11-04 09:10:37 +0100

    Upgrade to OpenROADM 7.1.0 network models
    - Replace existing OpenROADM network models with version 7.1.0
    - New version of transportpce-topology model to suppport 7.1.0 models
    - common module: add constants to manage fixed and flex grid,
      add GridUtils class to init frequencies map
    - update networkmodel module: update import version, update unit tests
    - update renderer module: update import version, replace wavelength
      with frequencies map, update unit tests
    - update olm module: update import version
    - update pce module: update import version, replace wavelength with
      frequencies map (from 7.1 models), update unit tests and data test
      files
    - update tapi module: update import version and data test file
    - update lighty module: update import version
    - update functional tests: update response check, add constants and
      functions to compute freq map, update data test files
    
    Note that in pce, tapi modules and functional tests, there is
    a major update in test data files: replacement of
    available-wavelength
    with avail-freq-maps which contains a base64 encoded 96 bytes
    representing available frequency
    
    JIRA: TRNSPRTPCE-259
    Signed-off-by: manuedelf <emmanuelle.delfour@orange.com>
    Change-Id: Iaf95e04232283f6adf08ef41b394c1cd0fca2354

--- renderer/src/main/java/org/opendaylight/transportpce/renderer/provisiondevice/OtnDeviceRendererServiceImpl.java
+++ renderer/src/main/java/org/opendaylight/transportpce/renderer/provisiondevice/OtnDeviceRendererServiceImpl.java
@@ -34,7 +34,7 @@ import org.opendaylight.yang.gen.v1.http.org.transportpce.common.types.rev200615
 import org.opendaylight.yang.gen.v1.http.org.transportpce.common.types.rev200615.node.interfaces.NodeInterfaceBuilder;
 import org.opendaylight.yang.gen.v1.http.org.transportpce.common.types.rev200615.node.interfaces.NodeInterfaceKey;
 import org.opendaylight.yang.gen.v1.http.org.transportpce.common.types.rev200615.otn.renderer.input.Nodes;
-import org.opendaylight.yang.gen.v1.http.transportpce.topology.rev200129.OtnLinkType;
+import org.opendaylight.yang.gen.v1.http.transportpce.topology.rev201019.OtnLinkType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
