7441f42fc5094e068e15818037522d9e44b08099
manuedelf <emmanuelle.delfour@orange.com>
2020-11-30 13:39:47 +0100

    Update naming following flexgrid specification
    - set EFFECTIVE_BITS_VALUE to 768
    - update data file samples, unit tests and functional tests
    with this new value
    - rename NB_CHANNELS to NB_OCTECTS as it's relevant in flexgrid
    - remove FixedFridConstant as there is no more specific value
    
    JIRA: TRNSPRTPCE-230
    Signed-off-by: manuedelf <emmanuelle.delfour@orange.com>
    Change-Id: Ib6ad813fd8d3efbbba5b9c65aca6c1455081c573

--- tests/transportpce_tests/2.2.1/test_otn_end2end.py
+++ tests/transportpce_tests/2.2.1/test_otn_end2end.py
@@ -402,7 +402,7 @@ class TransportPCEtesting(unittest.TestCase):
         for ele in liste_tp:
             if ele['tp-id'] == 'DEG2-CTP-TXRX':
                 self.assertIn({u'map-name': 'cband', u'freq-map-granularity': 6.25, u'start-edge-freq': 191.325,
-                               u'effective-bits': 8, u'freq-map': INDEX_1_USED_FREQ_MAP},
+                               u'effective-bits': 768, u'freq-map': INDEX_1_USED_FREQ_MAP},
                               ele['org-openroadm-network-topology:ctp-attributes']['avail-freq-maps'])
             if ele['tp-id'] == 'DEG2-TTP-TXRX':
                 self.assertIn({u'index': 1, u'frequency': 196.1,
