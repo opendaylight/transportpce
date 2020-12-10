11002869ea44ecc8a49538af8aaf6837daa97d3c
manuedelf <emmanuelle.delfour@orange.com>
2020-12-10 15:46:35 +0100

    Move TopologyDataUtils from tapi to test-common
    Move TopologyDataUtils test class from tapi to test-common in order to
    be able to reuse it in other maven modules. As this class is now in
    src/main/java, fix spotbugs issues.
    
    JIRA: TRNSPRTPCE-230
    Signed-off-by: manuedelf <emmanuelle.delfour@orange.com>
    Change-Id: I29e987bff1c55ce3432c86728b4898f7ce1e38f9

---
+++ tapi/src/test/java/org/opendaylight/transportpce/tapi/utils/TapiTopologyDataUtils.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright © 2020 Orange, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.tapi.utils;
+
+import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.topology.rev181210.GetTopologyDetailsInput;
+import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.topology.rev181210.GetTopologyDetailsInputBuilder;
+
+public final class TapiTopologyDataUtils {
+    public static final String OPENROADM_TOPOLOGY_FILE = "src/test/resources/openroadm-topology.xml";
+    public static final String OTN_TOPOLOGY_FILE = "src/test/resources/otn-topology.xml";
+    public static final String PORTMAPPING_FILE = "src/test/resources/portmapping.xml";
+
+    public static GetTopologyDetailsInput buildGetTopologyDetailsInput(String topoName) {
+        GetTopologyDetailsInputBuilder builtInput = new GetTopologyDetailsInputBuilder();
+        builtInput.setTopologyIdOrName(topoName);
+        return builtInput.build();
+    }
+
+    private TapiTopologyDataUtils() {
+    }
+
+}
