f5cbeb4428771440390d8532eac0f05db024f7f0
Thierry Jiao <thierry.jiao@orange.com>
2020-12-17 18:04:59 +0100

    Add new Maven module to manage NBI Notifications
    - Implement a new Maven module named nbinotifications containing classes
      capable of communicating with Kafka server
    - Add the class Subscriber capable of reading events from topics Kafka
    - Add the class Publisher capable of writing events from topics Kafka
    - Add yang file nbi-notifications to model the service notifications
    - Implement a new RPC API named GetNotificationsService capable of
      returning the notifications stored in a topic Kafka
    - Implement a new RPC API named publishNotificationService in charge of
      publishing a message in Kafka topic
    - Add a listener to the RPC GetNotificationsService
    - Add new blueprint in nbinotifications
    - Add unit tests to check the functionning of nbinotifications classes
    - Update pom.xml to implement the new module nbinotifications
    
    JIRA: TRNSPRTPCE-343
    Signed-off-by: Thierry Jiao <thierry.jiao@orange.com>
    Change-Id: I74298292034e4a158b27dd5db47e63918026684b

---
+++ nbinotifications/src/main/java/org/opendaylight/transportpce/nbinotifications/utils/NbiNotificationsUtils.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2020 Orange, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.transportpce.nbinotifications.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class NbiNotificationsUtils {
+
+    private static final Logger LOG = LoggerFactory.getLogger(NbiNotificationsUtils.class);
+
+    private NbiNotificationsUtils() {
+    }
+
+    public static Properties loadProperties(String propertyFileName) {
+        Properties props = new Properties();
+        InputStream inputStream = NbiNotificationsUtils.class.getClassLoader()
+                .getResourceAsStream(propertyFileName);
+        try {
+            if (inputStream != null) {
+                props.load(inputStream);
+            } else {
+                LOG.warn("Kafka property file '{}' is empty", propertyFileName);
+            }
+        } catch (IOException e) {
+            LOG.warn("Kafka property file '{}' was not found in the classpath", propertyFileName);
+        }
+        return props;
+    }
+}
