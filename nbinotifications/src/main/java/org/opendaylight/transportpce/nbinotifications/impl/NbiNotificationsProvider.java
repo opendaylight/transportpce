de683dd2aabcde4321be02c3c2bbccb5f2178915
Thierry Jiao <thierry.jiao@orange.com>
2020-12-17 18:04:59 +0100

    Add new Maven module to manage NBI Notifications
    - Implement a new Maven module named nbinotifications containing classes
      capable of communicating with Kafka server
    - Add the class Subscriber capable of reading events from topics Kafka
    - Add the class Publisher capable of writing events from topics Kafka
    - Add yang file nbi-notifications to model the service notifications
    - Implement a new RPC API named GetNotificationsService capable of
      returning the notifications stored in a topic Kafka
    - Implement a new RPC API named publishNotificationService in charge of
      publishing a message in Kafka topic
    - Add a listener to the RPC GetNotificationsService
    - Add new blueprint in nbinotifications
    - Update pom.xml to implement the new module nbinotifications
    
    JIRA: TRNSPRTPCE-343
    Signed-off-by: Thierry Jiao <thierry.jiao@orange.com>
    Change-Id: I74298292034e4a158b27dd5db47e63918026684b

---
+++ nbinotifications/src/main/java/org/opendaylight/transportpce/nbinotifications/impl/NbiNotificationsProvider.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright © 2020 Orange, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.transportpce.nbinotifications.impl;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.opendaylight.mdsal.binding.api.NotificationService;
+import org.opendaylight.mdsal.binding.api.RpcProviderService;
+import org.opendaylight.transportpce.nbinotifications.listener.NbiNotificationsListenerImpl;
+import org.opendaylight.transportpce.nbinotifications.producer.Publisher;
+import org.opendaylight.yang.gen.v1.nbi.notifications.rev201130.NbiNotificationsListener;
+import org.opendaylight.yang.gen.v1.nbi.notifications.rev201130.NbiNotificationsService;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.concepts.ObjectRegistration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NbiNotificationsProvider {
+
+    private static final Logger LOG = LoggerFactory.getLogger(NbiNotificationsProvider.class);
+    private static Map<String, Publisher> publishersMap =  new HashMap<>();
+
+    private final RpcProviderService rpcService;
+    private ObjectRegistration<NbiNotificationsService> rpcRegistration;
+    private ListenerRegistration<NbiNotificationsListener> listenerRegistration;
+    private NotificationService notificationService;
+
+
+    public NbiNotificationsProvider(List<String> topics,
+            RpcProviderService rpcProviderService, NotificationService notificationService) {
+        this.rpcService = rpcProviderService;
+        this.notificationService = notificationService;
+        for (String topic: topics) {
+            LOG.info("Creating publisher for topic {}", topic);
+            publishersMap.put(topic, new Publisher(topic));
+        }
+    }
+
+    /**
+     * Method called when the blueprint container is created.
+     */
+    public void init() {
+        LOG.info("NbiNotificationsProvider Session Initiated");
+        rpcRegistration = rpcService.registerRpcImplementation(NbiNotificationsService.class,
+                new NbiNotificationsImpl());
+        listenerRegistration = notificationService.registerNotificationListener(
+                new NbiNotificationsListenerImpl(publishersMap));
+    }
+
+    /**
+     * Method called when the blueprint container is destroyed.
+     */
+    public void close() {
+        for (Publisher publisher : publishersMap.values()) {
+            publisher.close();
+        }
+        rpcRegistration.close();
+        listenerRegistration.close();
+        LOG.info("NbiNotificationsProvider Closed");
+    }
+
+}
