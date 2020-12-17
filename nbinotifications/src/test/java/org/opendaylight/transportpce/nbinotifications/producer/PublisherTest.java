dae75a19f69ea844314f091b75206667beaaab29
Thierry Jiao <thierry.jiao@orange.com>
2020-12-17 18:39:49 +0100

    Add tests for service Notifications
    - Add functional test test_nbinotifications.py to check the sending of
      service notifications when service-create and service-delete
      are invoked
    - Add unit tests to check the functionning of nbinotifications classes
    - Update nbinotifications/pom.xml to add tests dependencies
    - Update test_utils.py by adding a new function capable of getting the
      service notifications
    - Add docker-compose.yml necessary for Kafka and ZooKeeper dockers
    - Update tox.ini to include the new functional test and run
      Kafka/ZooKeeper dockers
    
    JIRA: TRNSPRTPCE-343
    Signed-off-by: Thierry Jiao <thierry.jiao@orange.com>
    Change-Id: Ice4d8a2980317ee1dca6533752466305ed525981

---
+++ nbinotifications/src/test/java/org/opendaylight/transportpce/nbinotifications/producer/PublisherTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright © 2020 Orange, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.transportpce.nbinotifications.producer;
+
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.KafkaAdminClient;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.opendaylight.transportpce.common.converter.JsonStringConverter;
+import org.opendaylight.transportpce.nbinotifications.utils.NbiNotificationsUtils;
+import org.opendaylight.transportpce.test.AbstractTest;
+import org.opendaylight.yang.gen.v1.nbi.notifications.rev201130.NotificationService;
+import org.opendaylight.yang.gen.v1.nbi.notifications.rev201130.NotificationServiceBuilder;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+
+public class PublisherTest extends AbstractTest {
+
+    @Mock
+    private KafkaProducer<String, String> producer;
+
+    @Mock
+    private AdminClient client;
+
+    private ListeningExecutorService executorService;
+    private CountDownLatch endSignal;
+    private static final int NUM_THREADS = 5;
+    private boolean callbackRan;
+    private JsonStringConverter<NotificationService> converter;
+
+    @Before
+    public void setUp() {
+        executorService = MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(NUM_THREADS));
+        endSignal = new CountDownLatch(1);
+        callbackRan = false;
+        MockitoAnnotations.openMocks(this);
+        Properties propsProducer = NbiNotificationsUtils.loadProperties("publisher.properties");
+        propsProducer.put(ProducerConfig.CLIENT_ID_CONFIG, "test");
+        producer = new KafkaProducer<>(propsProducer);
+        Properties propsClient = new Properties();
+        propsClient.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                propsProducer.getProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG));
+        client = KafkaAdminClient.create(propsClient);
+        converter = new JsonStringConverter<NotificationService>(
+                getDataStoreContextUtil().getBindingDOMCodecServices());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void sendEventShouldBeFailedWithEmptyInput() {
+        Publisher publisher = new Publisher("test", producer, client, converter);
+        publisher.sendEvent(new NotificationServiceBuilder().build());
+    }
+
+    @Test
+    public void sendEventShouldBeSuccessful() throws IOException {
+        String json = Files.readString(Paths.get("src/test/resources/event.json"));
+        NotificationService notificationService = converter
+                .createDataObjectFromJsonString(YangInstanceIdentifier.of(NotificationService.QNAME), json);
+        Publisher publisher = new Publisher("test", producer, client, converter);
+        publisher.sendEvent(notificationService);
+    }
+
+}
