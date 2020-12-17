b7abdbc3f30c4ca9fa890dcb6ca781d05b7a1f34
Thierry Jiao <thierry.jiao@orange.com>
2020-12-17 18:04:59 +0100

    Add new Maven module to manage NBI Notifications
    - Implement a new Maven module named nbinotifications containing classes
      capable of communicating with Kafka server
    - Add the class Subscriber capable of reading events from topics Kafka
    - Add the class Publisher capable of writing events from topics Kafka
    - Add yang file nbi-notifications to model the service notifications
    - Implement a new RPC API named GetNotificationsService capable of
      returning the notifications stored in a topic Kafka
    - Implement a new RPC API named publishNotificationService in charge of
      publishing a message in Kafka topic
    - Add a listener to the RPC GetNotificationsService
    - Add new blueprint in nbinotifications
    - Add unit tests to check the functionning of nbinotifications classes
    - Update pom.xml to implement the new module nbinotifications
    
    JIRA: TRNSPRTPCE-343
    Signed-off-by: Thierry Jiao <thierry.jiao@orange.com>
    Change-Id: I74298292034e4a158b27dd5db47e63918026684b

---
+++ nbinotifications/src/test/java/org/opendaylight/transportpce/nbinotifications/listener/NbiNotificationsListenerImplTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright © 2021 Orange, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.transportpce.nbinotifications.listener;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.util.Map;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.opendaylight.transportpce.nbinotifications.producer.Publisher;
+import org.opendaylight.transportpce.test.AbstractTest;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.common.service.types.rev190531.ConnectionType;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.common.state.types.rev181130.State;
+import org.opendaylight.yang.gen.v1.nbi.notifications.rev201130.PublishNotificationService;
+import org.opendaylight.yang.gen.v1.nbi.notifications.rev201130.PublishNotificationServiceBuilder;
+
+public class NbiNotificationsListenerImplTest extends AbstractTest {
+    @Mock
+    private Publisher publisher;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void onPublishNotificationServiceTest() {
+        NbiNotificationsListenerImpl listener = new NbiNotificationsListenerImpl(Map.of("test", publisher));
+        PublishNotificationService notification = new PublishNotificationServiceBuilder().setTopic("test")
+                .setCommonId("commonId").setConnectionType(ConnectionType.Service).setMessage("Service deleted")
+                .setOperationalState(State.OutOfService).setServiceName("service name").build();
+        listener.onPublishNotificationService(notification);
+        verify(publisher, times(1)).sendEvent(any());
+    }
+
+    @Test
+    public void onPublishNotificationServiceWrongTopicTest() {
+        NbiNotificationsListenerImpl listener = new NbiNotificationsListenerImpl(Map.of("test", publisher));
+        PublishNotificationService notification = new PublishNotificationServiceBuilder().setTopic("wrongtopic")
+                .setCommonId("commonId").setConnectionType(ConnectionType.Service).setMessage("Service deleted")
+                .setOperationalState(State.OutOfService).setServiceName("service name").build();
+        listener.onPublishNotificationService(notification);
+        verify(publisher, times(0)).sendEvent(any());
+    }
+}
---
+++ nbinotifications/src/test/java/org/opendaylight/transportpce/nbinotifications/serialization/NotificationServiceDeserializerTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright © 2021 Orange, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.transportpce.nbinotifications.serialization;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import org.junit.Test;
+import org.opendaylight.transportpce.common.converter.JsonStringConverter;
+import org.opendaylight.transportpce.test.AbstractTest;
+import org.opendaylight.yang.gen.v1.nbi.notifications.rev201130.get.notifications.service.output.NotificationService;
+
+public class NotificationServiceDeserializerTest extends AbstractTest {
+
+    @Test
+    public void deserializeTest() throws IOException {
+        JsonStringConverter<org.opendaylight.yang.gen.v1.nbi.notifications.rev201130.NotificationService> converter =
+                new JsonStringConverter<>(getDataStoreContextUtil().getBindingDOMCodecServices());
+        NotificationServiceDeserializer deserializer = new NotificationServiceDeserializer();
+        Map<String, Object> configs = Map.of(ConfigConstants.CONVERTER, converter);
+        deserializer.configure(configs, false);
+        NotificationService readEvent = deserializer.deserialize("Test",
+                Files.readAllBytes(Paths.get("src/test/resources/event.json")));
+        deserializer.close();
+        assertEquals("Service name should be service1", "service1", readEvent.getServiceName());
+    }
+}
