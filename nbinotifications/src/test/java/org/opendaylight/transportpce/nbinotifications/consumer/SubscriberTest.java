4891887bfd89b9180c2a7966d16caeaca00e2fe6
Thierry Jiao <thierry.jiao@orange.com>
2020-12-17 18:04:59 +0100

    Add new Maven module to manage NBI Notifications
    - Implement a new Maven module named nbinotifications containing classes
      capable of communicating with Kafka server
    - Add the class Subscriber capable of reading events from topics Kafka
    - Add the class Publisher capable of writing events from topics Kafka
    - Add yang file nbi-notifications to model the service notifications
    - Implement a new RPC API named GetNotificationsService capable of
      returning the notifications stored in a topic Kafka
    - Implement a new RPC API named publishNotificationService in charge of
      publishing a message in Kafka topic
    - Add a listener to the RPC GetNotificationsService
    - Add new blueprint in nbinotifications
    - Add unit tests to check the functionning of nbinotifications classes
    - Update pom.xml to implement the new module nbinotifications
    
    JIRA: TRNSPRTPCE-343
    Signed-off-by: Thierry Jiao <thierry.jiao@orange.com>
    Change-Id: I74298292034e4a158b27dd5db47e63918026684b

---
+++ nbinotifications/src/test/java/org/opendaylight/transportpce/nbinotifications/consumer/SubscriberTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright © 2020 Orange, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.transportpce.nbinotifications.consumer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.MockConsumer;
+import org.apache.kafka.clients.consumer.OffsetResetStrategy;
+import org.apache.kafka.common.TopicPartition;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.transportpce.nbinotifications.utils.NotificationServiceDataUtils;
+import org.opendaylight.transportpce.test.AbstractTest;
+import org.opendaylight.yang.gen.v1.nbi.notifications.rev201130.get.notifications.service.output.NotificationService;
+
+public class SubscriberTest extends AbstractTest {
+    private static final String TOPIC = "topic";
+    private static final int PARTITION = 0;
+    private MockConsumer<String, NotificationService> mockConsumer;
+    private Subscriber subscriber;
+
+    @Before
+    public void setUp() {
+        mockConsumer = new MockConsumer<>(OffsetResetStrategy.EARLIEST);
+        subscriber = new Subscriber(mockConsumer);
+    }
+
+    @Test
+    public void subscribeServiceShouldBeSuccessful() {
+        // from https://www.baeldung.com/kafka-mockconsumer
+        ConsumerRecord<String, NotificationService> record = new ConsumerRecord<String, NotificationService>(
+                TOPIC, PARTITION, 0L, "key", NotificationServiceDataUtils.buildReceivedEvent());
+        mockConsumer.schedulePollTask(() -> {
+            mockConsumer.rebalance(Collections.singletonList(new TopicPartition(TOPIC, PARTITION)));
+            mockConsumer.addRecord(record);
+        });
+
+        Map<TopicPartition, Long> startOffsets = new HashMap<>();
+        TopicPartition tp = new TopicPartition(TOPIC, PARTITION);
+        startOffsets.put(tp, 0L);
+        mockConsumer.updateBeginningOffsets(startOffsets);
+        List<NotificationService> result = subscriber.subscribeService(TOPIC);
+        assertEquals("There should be 1 record", 1, result.size());
+        assertTrue("Consumer should be closed", mockConsumer.closed());
+    }
+}
---
+++ nbinotifications/src/test/java/org/opendaylight/transportpce/nbinotifications/producer/PublisherTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright © 2020 Orange, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.transportpce.nbinotifications.producer;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import org.apache.kafka.clients.producer.MockProducer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.transportpce.common.converter.JsonStringConverter;
+import org.opendaylight.transportpce.nbinotifications.serialization.ConfigConstants;
+import org.opendaylight.transportpce.nbinotifications.serialization.NotificationServiceSerializer;
+import org.opendaylight.transportpce.test.AbstractTest;
+import org.opendaylight.yang.gen.v1.nbi.notifications.rev201130.NotificationService;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.codec.gson.JSONCodecFactorySupplier;
+
+public class PublisherTest extends AbstractTest {
+    private JsonStringConverter<NotificationService> converter;
+    private Publisher publisher;
+    private MockProducer<String, NotificationService> mockProducer;
+
+    @Before
+    public void setUp() {
+        NotificationServiceSerializer serializer = new NotificationServiceSerializer();
+        Map<String, Object> properties = Map.of(ConfigConstants.CONVERTER , serializer);
+        serializer.configure(properties, false);
+        mockProducer =  new MockProducer<>(true, new StringSerializer(), serializer);
+        converter = new JsonStringConverter<NotificationService>(
+                getDataStoreContextUtil().getBindingDOMCodecServices());
+        publisher = new Publisher("test",mockProducer);
+    }
+
+    @Test
+    public void sendEventShouldBeSuccessful() throws IOException {
+        String json = Files.readString(Paths.get("src/test/resources/event.json"));
+        NotificationService notificationService = converter
+                .createDataObjectFromJsonString(YangInstanceIdentifier.of(NotificationService.QNAME),
+                        json, JSONCodecFactorySupplier.RFC7951);
+        publisher.sendEvent(notificationService);
+        assertEquals("We should have one message", 1, mockProducer.history().size());
+        assertEquals("Key should be test", "test",mockProducer.history().get(0).key());
+    }
+
+}
