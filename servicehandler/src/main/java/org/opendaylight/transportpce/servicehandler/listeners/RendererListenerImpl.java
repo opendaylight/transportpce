72b4442b1f42c59ff65f06d2ec76f968e3f47f4c
Javier <errea@eurecom.fr>
2020-12-18 14:45:03 +0100

    NetworkModelListener: handling TopologyUpdates
    - Update of service state affected by topology update
    - Added state to PathDescription elements
    - Update PathDescription according to topology update notification
    - Fixed test_pce.py by adding state to link in tests 21 & 23
    - New revision in path description model
    
    JIRA: TRNSPRTPCE-363
    Change-Id: Ic7e6a2f2abdfb70361ac251e3d44dc8505910409
    Signed-off-by: Javier <errea@eurecom.fr>

--- servicehandler/src/main/java/org/opendaylight/transportpce/servicehandler/listeners/RendererListenerImpl.java
+++ servicehandler/src/main/java/org/opendaylight/transportpce/servicehandler/listeners/RendererListenerImpl.java
@@ -19,8 +19,8 @@ import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.renderer.
 import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.servicehandler.rev201125.ServiceRpcResultSh;
 import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.servicehandler.rev201125.ServiceRpcResultShBuilder;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.common.service.types.rev190531.ServiceNotificationTypes;
-import org.opendaylight.yang.gen.v1.http.org.openroadm.common.state.types.rev181130.State;
-import org.opendaylight.yang.gen.v1.http.org.openroadm.equipment.states.types.rev181130.AdminStates;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.common.state.types.rev191129.State;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.equipment.states.types.rev191129.AdminStates;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
