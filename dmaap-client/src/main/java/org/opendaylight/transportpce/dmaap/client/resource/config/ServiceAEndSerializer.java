be1c16a42eac808c5e22a0d3416c61a5e2a67ede
manuedelf <emmanuelle.delfour@orange.com>
2021-02-02 23:08:17 +0100

    Dmaap client
    - REST client in order to send TPCE notification
    on ONAP Dmaap Message router.
    - Add a new maven module dmaap-client.
    - Use customized jackson serializer as a temporary workaround
    while waiting jackson support in yang tools
    https://git.opendaylight.org/gerrit/c/yangtools/+/94852
    
    JIRA: TRNSPRTPCE-394
    Signed-off-by: manuedelf <emmanuelle.delfour@orange.com>
    Change-Id: If31ee8befccba0b7e0e34b7cdadb27cc87858fed

---
+++ dmaap-client/src/main/java/org/opendaylight/transportpce/dmaap/client/resource/config/ServiceAEndSerializer.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright © 2021 Orange, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.transportpce.dmaap.client.resource.config;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import java.io.IOException;
+import org.opendaylight.yang.gen.v1.nbi.notifications.rev201130.notification.service.ServiceAEnd;
+
+// This class is a temporary workaround while waiting jackson
+// support in yang tools https://git.opendaylight.org/gerrit/c/yangtools/+/94852
+public class ServiceAEndSerializer extends StdSerializer<ServiceAEnd> {
+    private static final long serialVersionUID = 1L;
+
+    public ServiceAEndSerializer() {
+        super(ServiceAEnd.class);
+    }
+
+    @Override
+    public void serialize(ServiceAEnd value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        if (value != null) {
+            gen.writeStartObject();
+            gen.writeStringField("clli", value.getClli());
+            if (value.getServiceFormat() != null) {
+                gen.writeStringField("service-format", value.getServiceFormat().getName());
+            }
+            if (value.getNodeId() != null) {
+                gen.writeStringField("node-id", value.getNodeId().getValue());
+            }
+            if (value.getServiceRate() != null) {
+                gen.writeNumberField("service-rate", value.getServiceRate().intValue());
+            }
+            if (value.getOpticType() != null) {
+                gen.writeStringField("optic-type", value.getOpticType().getName());
+            }
+            if (value.getTxDirection() != null) {
+                gen.writeObjectField("tx-direction", value.getTxDirection());
+            }
+            if (value.getRxDirection() != null) {
+                gen.writeObjectField("rx-direction", value.getRxDirection());
+            }
+            gen.writeEndObject();
+        }
+    }
+}
