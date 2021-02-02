a60e8d4ce466f89059e63ac5bb27fb44e6fb4969
manuedelf <emmanuelle.delfour@orange.com>
2021-02-02 23:08:17 +0100

    Dmaap client
    - REST client in order to send TPCE notification
    on ONAP Dmaap Message router.
    - Add a new maven module dmaap-client.
    - Use customized jackson serializer as a temporary workaround
    while waiting jackson support in yang tools
    https://git.opendaylight.org/gerrit/c/yangtools/+/94852
    
    JIRA: TRNSPRTPCE-394
    Signed-off-by: manuedelf <emmanuelle.delfour@orange.com>
    Change-Id: If31ee8befccba0b7e0e34b7cdadb27cc87858fed

---
+++ dmaap-client/src/main/java/org/opendaylight/transportpce/dmaap/client/listener/NbiNotificationsListenerImpl.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright © 2021 Orange, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.transportpce.dmaap.client.listener;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
+import org.glassfish.jersey.client.proxy.WebResourceFactory;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.logging.LoggingFeature;
+import org.opendaylight.transportpce.dmaap.client.resource.EventsApi;
+import org.opendaylight.transportpce.dmaap.client.resource.config.JsonConfigurator;
+import org.opendaylight.transportpce.dmaap.client.resource.model.CreatedEvent;
+import org.opendaylight.yang.gen.v1.nbi.notifications.rev201130.NbiNotificationsListener;
+import org.opendaylight.yang.gen.v1.nbi.notifications.rev201130.PublishNotificationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NbiNotificationsListenerImpl implements NbiNotificationsListener {
+    private static final Logger LOG = LoggerFactory.getLogger(NbiNotificationsListenerImpl.class);
+    private String topic = "unauthenticated.TPCE";
+    private EventsApi api;
+
+    public NbiNotificationsListenerImpl(String baseUrl, String username, String password) {
+        LOG.info("Dmaap server {} for user {}", baseUrl, username);
+        Client client = ClientBuilder.newClient();
+        if (username != null && username.isBlank() && password != null && !password.isBlank()) {
+            HttpAuthenticationFeature authFeature = HttpAuthenticationFeature.basic(username, password);
+            client.register(authFeature);
+            topic = "authenticated.TPCE";
+        }
+        client.register(new LoggingFeature(java.util.logging.Logger.getLogger(this.getClass().getName())))
+        .register(JacksonFeature.class).register(JsonConfigurator.class);
+        api = WebResourceFactory.newResource(EventsApi.class, client.target(baseUrl));
+
+    }
+
+    @Override
+    public void onPublishNotificationService(PublishNotificationService notification) {
+        try {
+            CreatedEvent response = api.sendEvent(topic, notification);
+            LOG.info("Response received {}", response);
+        } catch (WebApplicationException e) {
+            LOG.warn("Cannot send event {}", notification, e);
+        }
+
+    }
+
+}
---
+++ dmaap-client/src/main/java/org/opendaylight/transportpce/dmaap/client/resource/config/TxDirectionSerializer.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2021 Orange, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.transportpce.dmaap.client.resource.config;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import java.io.IOException;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.common.service.types.rev190531.service.endpoint.TxDirection;
+
+// This class is a temporary workaround while waiting jackson
+// support in yang tools https://git.opendaylight.org/gerrit/c/yangtools/+/94852
+public class TxDirectionSerializer extends StdSerializer<TxDirection> {
+    private static final long serialVersionUID = 1L;
+
+    public TxDirectionSerializer() {
+        super(TxDirection.class);
+    }
+
+    @Override
+    public void serialize(TxDirection value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        if (value != null) {
+            gen.writeStartObject();
+            if (value.getPort() != null) {
+                gen.writeObjectField("port", value.getPort());
+            }
+            if (value.getLgx() != null) {
+                gen.writeObjectField("lgx", value.getLgx());
+            }
+            gen.writeEndObject();
+        }
+    }
+}
