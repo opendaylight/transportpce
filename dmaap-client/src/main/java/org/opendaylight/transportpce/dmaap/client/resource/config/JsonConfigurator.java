32fa4e5379636641b16eef5e725fe0146037440a
manuedelf <emmanuelle.delfour@orange.com>
2021-02-02 23:08:17 +0100

    Dmaap client
    - REST client in order to send TPCE notification
    on ONAP Dmaap Message router.
    - Add a new maven module dmaap-client.
    - Use customized jackson serializer as a temporary workaround
    while waiting jackson support in yang tools
    https://git.opendaylight.org/gerrit/c/yangtools/+/94852
    
    JIRA: TRNSPRTPCE-394
    Signed-off-by: manuedelf <emmanuelle.delfour@orange.com>
    Change-Id: If31ee8befccba0b7e0e34b7cdadb27cc87858fed

---
+++ dmaap-client/src/main/java/org/opendaylight/transportpce/dmaap/client/resource/config/JsonConfigurator.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright © 2021 Orange, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.transportpce.dmaap.client.resource.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import javax.ws.rs.ext.ContextResolver;
+
+public class JsonConfigurator implements ContextResolver<ObjectMapper> {
+
+    private final ObjectMapper mapper;
+
+    public JsonConfigurator() {
+        mapper = new ObjectMapper();
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        mapper.registerModule(new JavaTimeModule());
+        mapper.registerModule(new PublishNotificationServiceModule());
+    }
+
+    @Override
+    public ObjectMapper getContext(Class<?> type) {
+        return mapper;
+    }
+
+}
