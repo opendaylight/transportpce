87d6ec6c45102e19b56c87527bb587252cf41598
manuedelf <emmanuelle.delfour@orange.com>
2021-02-02 23:08:17 +0100

    Dmaap client
    - REST client in order to send TPCE notification
    on ONAP Dmaap Message router.
    - Add a new maven module dmaap-client.
    - Use customized jackson serializer as a temporary workaround
    while waiting jackson support in yang tools
    https://git.opendaylight.org/gerrit/c/yangtools/+/94852
    
    JIRA: TRNSPRTPCE-394
    Signed-off-by: manuedelf <emmanuelle.delfour@orange.com>
    Change-Id: If31ee8befccba0b7e0e34b7cdadb27cc87858fed

---
+++ dmaap-client/src/main/java/org/opendaylight/transportpce/dmaap/client/resource/EventsApi.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright © 2021 Orange, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.transportpce.dmaap.client.resource;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.opendaylight.transportpce.dmaap.client.resource.model.CreatedEvent;
+import org.opendaylight.yang.gen.v1.nbi.notifications.rev201130.PublishNotificationService;
+
+@Path("/events")
+public interface EventsApi {
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{topic}")
+    CreatedEvent sendEvent(@PathParam("topic") String topic, PublishNotificationService event);
+
+}
