d6e6433e27ba0d12fff27c17113bb54ee617125b
manuedelf <emmanuelle.delfour@orange.com>
2021-02-02 23:08:17 +0100

    Dmaap client
    REST client in order to send TPCE notification
    on ONAP Dmaap Message router.
    Add a new maven module dmaap-client.
    
    Signed-off-by: manuedelf <emmanuelle.delfour@orange.com>
    Change-Id: If31ee8befccba0b7e0e34b7cdadb27cc87858fed

---
+++ dmaap-client/src/main/java/org/opendaylight/transportpce/dmaap/client/resource/config/PublishNotificationServiceModule.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright © 2020 Orange, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.transportpce.dmaap.client.resource.config;
+
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.PackageVersion;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.common.service.types.rev190531.service.endpoint.RxDirection;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.common.service.types.rev190531.service.endpoint.TxDirection;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.common.service.types.rev190531.service.lgx.Lgx;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.common.service.types.rev190531.service.port.Port;
+import org.opendaylight.yang.gen.v1.nbi.notifications.rev201130.PublishNotificationService;
+import org.opendaylight.yang.gen.v1.nbi.notifications.rev201130.notification.service.ServiceAEnd;
+import org.opendaylight.yang.gen.v1.nbi.notifications.rev201130.notification.service.ServiceZEnd;
+
+//This class is a temporary workaround while waiting jackson
+//support in yang tools https://git.opendaylight.org/gerrit/c/yangtools/+/94852
+public class PublishNotificationServiceModule extends SimpleModule {
+
+    private static final long serialVersionUID = 1L;
+
+    public PublishNotificationServiceModule() {
+        super(PackageVersion.VERSION);
+        addSerializer(PublishNotificationService.class, new PublishNotificationServiceSerializer());
+        addSerializer(Lgx.class, new LgxSerializer());
+        addSerializer(Port.class, new PortSerializer());
+        addSerializer(RxDirection.class, new RxDirectionSerializer());
+        addSerializer(TxDirection.class, new TxDirectionSerializer());
+        addSerializer(ServiceAEnd.class, new ServiceAEndSerializer());
+        addSerializer(ServiceZEnd.class, new ServiceZEndSerializer());
+    }
+
+}
---
+++ dmaap-client/src/main/java/org/opendaylight/transportpce/dmaap/client/resource/model/CreatedEvent.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright © 2020 Orange, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.transportpce.dmaap.client.resource.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CreatedEvent {
+
+    @JsonProperty("serverTimeMs")
+    private Integer serverTimeMs;
+
+    @JsonProperty("count")
+    private Integer count;
+
+    public Integer getServerTimeMs() {
+        return serverTimeMs;
+    }
+
+    public void setServerTimeMs(Integer serverTimeMs) {
+        this.serverTimeMs = serverTimeMs;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("CreatedEvent [serverTimeMs=").append(serverTimeMs).append(", count=").append(count).append("]");
+        return builder.toString();
+    }
+}
