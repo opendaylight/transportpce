0e5d938be5c539b42cdee3b4a0a5facd2ea68987
Javier Errea <errea@eurecom.fr>
2021-02-03 16:39:30 +0100

    TAPI Netconf Topology Listener
    - TAPI network model service equiv. to Network Model module
    - TAPI blueprint modification.
    - Lighty module initializes TAPI accordingly
    - TAPI PortMappingListener implementation
    - TAPI topology context population: nodes and links
    - TAPI sip context population
    - TAPI rodm2rdm link creating with LLDP
    
    JIRA: TRNSPRTPCE-385
    Change-Id: I70a68c0cd6156da53cb09ec84d25f600e4d31c87
    Signed-off-by: errea <errea@eurecom.fr>

--- lighty/src/main/java/io/lighty/controllers/tpce/module/TransportPCEImpl.java
+++ lighty/src/main/java/io/lighty/controllers/tpce/module/TransportPCEImpl.java
@@ -75,7 +75,12 @@ import org.opendaylight.transportpce.servicehandler.listeners.PceListenerImpl;
 import org.opendaylight.transportpce.servicehandler.listeners.RendererListenerImpl;
 import org.opendaylight.transportpce.servicehandler.service.ServiceDataStoreOperations;
 import org.opendaylight.transportpce.servicehandler.service.ServiceDataStoreOperationsImpl;
+import org.opendaylight.transportpce.tapi.R2RTapiLinkDiscovery;
 import org.opendaylight.transportpce.tapi.impl.TapiProvider;
+import org.opendaylight.transportpce.tapi.topology.TapiNetconfTopologyListener;
+import org.opendaylight.transportpce.tapi.topology.TapiNetworkModelService;
+import org.opendaylight.transportpce.tapi.topology.TapiNetworkModelServiceImpl;
+import org.opendaylight.transportpce.tapi.topology.TapiPortMappingListener;
 import org.opendaylight.transportpce.tapi.utils.TapiListener;
 import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.networkutils.rev170818.TransportpceNetworkutilsService;
 import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.olm.rev170418.TransportpceOlmService;
@@ -189,7 +194,17 @@ public class TransportPCEImpl extends AbstractLightyModule implements TransportP
                 serviceDataStoreOperations, pceListenerImpl, rendererListenerImpl, networkModelListenerImpl,
                 servicehandler);
 
-        tapiProvider = initTapi(lightyServices, servicehandler, serviceDataStoreOperations);
+        LOG.info("Creating tapi beans ...");
+        R2RTapiLinkDiscovery tapilinkDiscoveryImpl = new R2RTapiLinkDiscovery(lightyServices.getBindingDataBroker(),
+            deviceTransactionManager);
+        TapiNetworkModelService tapiNetworkModelService = new TapiNetworkModelServiceImpl(
+                lightyServices.getBindingDataBroker(), tapilinkDiscoveryImpl);
+        TapiNetconfTopologyListener tapiNetConfTopologyListener =
+                new TapiNetconfTopologyListener(tapiNetworkModelService);
+        TapiPortMappingListener tapiPortMappingListener =
+            new TapiPortMappingListener(tapiNetworkModelService);
+        tapiProvider = initTapi(lightyServices, servicehandler, serviceDataStoreOperations,
+                tapiNetConfTopologyListener, tapiPortMappingListener);
         if(activateNbiNotification) {
             LOG.info("Creating nbi-notifications beans ...");
             nbiNotificationsProvider = new NbiNotificationsProvider(
@@ -248,9 +263,12 @@ public class TransportPCEImpl extends AbstractLightyModule implements TransportP
      * @return TapiProvider instance
      */
     private TapiProvider initTapi(LightyServices lightyServices, OrgOpenroadmServiceService servicehandler,
-                                  ServiceDataStoreOperations serviceDataStoreOperations) {
+                                  ServiceDataStoreOperations serviceDataStoreOperations,
+                                  TapiNetconfTopologyListener tapiNetConfTopologyListener,
+                                  TapiPortMappingListener tapiPortMappingListener) {
         return new TapiProvider(lightyServices.getBindingDataBroker(), lightyServices.getRpcProviderService(),
-                servicehandler, serviceDataStoreOperations, new TapiListener());
+            servicehandler, serviceDataStoreOperations, new TapiListener(), tapiNetConfTopologyListener,
+            tapiPortMappingListener);
     }
 
     /**
