7798a586d4098482bbbedcf0c4b8757648236cfb
Gilles Thouenon <gilles.thouenon@orange.com>
2021-02-05 13:46:14 +0100

    Fix usage of deprecated method in portmapping
    Replace List by Map for McCapabilities treatment to avoid compilation
    warnings.
    
    JIRA: TRNSPRTPCE-396
    Signed-off-by: Gilles Thouenon <gilles.thouenon@orange.com>
    Change-Id: I159835f75ded4ca3952c569ab892fbe430799f85

--- common/src/main/java/org/opendaylight/transportpce/common/mapping/PortMappingVersion221.java
+++ common/src/main/java/org/opendaylight/transportpce/common/mapping/PortMappingVersion221.java
@@ -123,7 +123,7 @@ public class PortMappingVersion221 {
     public boolean createMappingData(String nodeId) {
         LOG.info("{} : OpenROADM version 2.2.1 node - Creating Mapping Data", nodeId);
         List<Mapping> portMapList = new ArrayList<>();
-        List<McCapabilities> mcCapabilitiesList = new ArrayList<>();
+        Map<McCapabilitiesKey, McCapabilities> mcCapabilities = new HashMap<>();
         InstanceIdentifier<Info> infoIID = InstanceIdentifier.create(OrgOpenroadmDevice.class).child(Info.class);
         Optional<Info> deviceInfoOptional = this.deviceTransactionManager.getDataFromDevice(
                 nodeId, LogicalDatastoreType.OPERATIONAL, infoIID,
@@ -156,7 +156,7 @@ public class PortMappingVersion221 {
                     return false;
                 }
                 // Get MC capabilities
-                if (!createMcCapabilitiesList(nodeId, deviceInfo, mcCapabilitiesList)) {
+                if (!createMcCapabilitiesList(nodeId, deviceInfo, mcCapabilities)) {
                     // return false if MC capabilites failed
                     LOG.warn("{} : Unable to create MC capabilities", nodeId);
                     return false;
@@ -173,7 +173,7 @@ public class PortMappingVersion221 {
                 break;
 
         }
-        return postPortMapping(nodeId, nodeInfo, portMapList, null, null, mcCapabilitiesList);
+        return postPortMapping(nodeId, nodeInfo, portMapList, null, null, mcCapabilities);
     }
 
     public boolean updateMapping(String nodeId, Mapping oldMapping) {
@@ -758,20 +758,22 @@ public class PortMappingVersion221 {
         return cpToDegreeList;
     }
 
-    private List<McCapabilities> getMcCapabilitiesList(Map<Integer, Degree> degrees, List<SharedRiskGroup> srgs,
-            String nodeId) {
+    private Map<McCapabilitiesKey, McCapabilities> getMcCapabilities(Map<Integer, Degree> degrees,
+            List<SharedRiskGroup> srgs, String nodeId) {
         LOG.info("{} : Getting the MC capabilities for degrees", nodeId);
-        List<McCapabilities> mcCapabilitiesList = degrees.values().stream()
-            .map(degree -> createMcCapDegreeObject(degree, nodeId)).collect(Collectors.toList());
+        Map<McCapabilitiesKey, McCapabilities> mcCapabilities = degrees.values().stream()
+            .map(degree -> createMcCapDegreeObject(degree, nodeId))
+            .collect(Collectors.toMap(McCapabilities::key, mcc -> mcc));
         // Add the SRG mc-capabilities
         LOG.info("{} : Getting the MC capabilities for SRGs", nodeId);
-        mcCapabilitiesList.addAll(srgs.stream().map(srg -> createMcCapSrgObject(srg, nodeId))
-            .collect(Collectors.toList()));
-        return mcCapabilitiesList;
+        mcCapabilities.putAll(srgs.stream().map(srg -> createMcCapSrgObject(srg, nodeId))
+            .collect(Collectors.toMap(McCapabilities::key, mcCapabilities2 -> mcCapabilities2)));
+        return mcCapabilities;
     }
 
     private boolean postPortMapping(String nodeId, NodeInfo nodeInfo, List<Mapping> portMapList,
-            List<CpToDegree> cp2DegreeList, List<SwitchingPoolLcp> splList, List<McCapabilities> mcCapList) {
+            List<CpToDegree> cp2DegreeList, List<SwitchingPoolLcp> splList,
+            Map<McCapabilitiesKey, McCapabilities> mcCapabilities) {
         NodesBuilder nodesBldr = new NodesBuilder().withKey(new NodesKey(nodeId)).setNodeId(nodeId);
         if (nodeInfo != null) {
             nodesBldr.setNodeInfo(nodeInfo);
@@ -801,8 +803,8 @@ public class PortMappingVersion221 {
             }
             nodesBldr.setSwitchingPoolLcp(splMap);
         }
-        if (mcCapList != null) {
-            nodesBldr.setMcCapabilities(mcCapList);
+        if (mcCapabilities != null) {
+            nodesBldr.setMcCapabilities(mcCapabilities);
         }
         Map<NodesKey,Nodes> nodesList = new HashMap<>();
         Nodes nodes = nodesBldr.build();
@@ -1021,11 +1023,12 @@ public class PortMappingVersion221 {
         return;
     }
 
+    private boolean createMcCapabilitiesList(String nodeId, Info deviceInfo,
+        Map<McCapabilitiesKey, McCapabilities> mcCapabilitiesMap) {
 
-    private boolean createMcCapabilitiesList(String nodeId, Info deviceInfo, List<McCapabilities> mcCapabilitiesList) {
         Map<Integer, Degree> degrees = getDegreesMap(nodeId, deviceInfo);
         List<SharedRiskGroup> srgs = getSrgs(nodeId, deviceInfo);
-        mcCapabilitiesList.addAll(getMcCapabilitiesList(degrees, srgs, nodeId));
+        mcCapabilitiesMap.putAll(getMcCapabilities(degrees, srgs, nodeId));
         return true;
     }
 
