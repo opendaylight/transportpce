952583aa9f0de2de04f230291a5989413591a7b7
manuedelf <emmanuelle.delfour@orange.com>
2021-02-08 22:35:38 +0100

    GNPy client refactor
    - Put GNPy api consumer in a specific package
    - Remove hard-coded base url, username and password by putting
    paramteres in blueprint configuration file (but still hardcoded in
    lighty)
    - Use jersey client package for requesting GNPy
    - Use customized jackson serializer/deserialize as a temporary
    workaround while waiting jackson support in yang tools
    https://git.opendaylight.org/gerrit/c/yangtools/+/94852
    - Update unit tests
    
    Signed-off-by: manuedelf <emmanuelle.delfour@orange.com>
    Change-Id: I28cba22c7df7ffa419d205c024e8e5e5bb909650

---
+++ pce/src/main/java/org/opendaylight/transportpce/pce/gnpy/consumer/GnpyConsumerImpl.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright © 2021 Orange, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.transportpce.pce.gnpy.consumer;
+
+import javax.ws.rs.ProcessingException;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
+import org.glassfish.jersey.client.proxy.WebResourceFactory;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.logging.LoggingFeature;
+import org.opendaylight.mdsal.binding.dom.codec.spi.BindingDOMCodecServices;
+import org.opendaylight.transportpce.common.converter.JsonStringConverter;
+import org.opendaylight.yang.gen.v1.gnpy.gnpy.api.rev190103.GnpyApi;
+import org.opendaylight.yang.gen.v1.gnpy.path.rev200909.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GnpyConsumerImpl implements GnpyConsumer {
+    private static final Logger LOG = LoggerFactory.getLogger(GnpyConsumerImpl.class);
+
+    private GnpyResource api;
+    private JsonStringConverter<GnpyApi> gnpyApiConverter;
+    private JsonStringConverter<Result> resultConverter;
+
+    public GnpyConsumerImpl(String baseUrl, String username, String password,
+            BindingDOMCodecServices bindingDOMCodecServices) {
+        gnpyApiConverter = new JsonStringConverter<>(bindingDOMCodecServices);
+        resultConverter = new JsonStringConverter<>(bindingDOMCodecServices);
+        JsonConfigurator jsonConfigurator = new JsonConfigurator(gnpyApiConverter, resultConverter);
+        Client client = ClientBuilder.newClient();
+        HttpAuthenticationFeature authFeature = HttpAuthenticationFeature.basic(username, password);
+        client.register(authFeature);
+        client.register(new LoggingFeature(java.util.logging.Logger.getLogger(this.getClass().getName())))
+            .register(JacksonFeature.class).register(jsonConfigurator);
+        api = WebResourceFactory.newResource(GnpyResource.class, client.target(baseUrl));
+    }
+
+    @Override
+    public boolean isAvailable() {
+        try {
+            api.checkStatus();
+            LOG.info("GNPy is available");
+            return true;
+        } catch (WebApplicationException | ProcessingException e) {
+            LOG.info("GNPy is not available ", e);
+            return false;
+        }
+    }
+
+    @Override
+    public Result computePaths(GnpyApi request) {
+        try {
+            return api.computePathRequest(request);
+        } catch (WebApplicationException | ProcessingException e) {
+            LOG.info("Something went wrong while requesting GNPy ", e);
+            return null;
+        }
+    }
+}
