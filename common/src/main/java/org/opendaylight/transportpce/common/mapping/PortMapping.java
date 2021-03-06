c53ee6d1f05bddd6fbd0df31f4fbc26bf36c6a5a
Gilles Thouenon <gilles.thouenon@orange.com>
2021-03-06 15:34:27 +0100

    Update portmapping on port state notification
    - review javadoc for PortMapping interface
    - implements new getMapping method to retreive mapping from circuit-pack
    name and port name
    - add new updateMappingObject method
    - review updateMapping() method
    - review createMappingObject() method
    
    JIRA: TRNSPRTPCE-423
    Signed-off-by: Gilles Thouenon <gilles.thouenon@orange.com>
    Change-Id: I72fe43f17b395ba67f8ac24a41e65b294f06d171

--- common/src/main/java/org/opendaylight/transportpce/common/mapping/PortMapping.java
+++ common/src/main/java/org/opendaylight/transportpce/common/mapping/PortMapping.java
@@ -16,7 +16,7 @@ public interface PortMapping {
 
     /**
      * This method creates logical to physical port mapping for a given device.
-     * Instead of parsing all the circuit packs/ports in the device this methods
+     * Instead of parsing all the circuit packs/ports in the device, this methods
      * does a selective read operation on degree/srg subtree to get circuit
      * packs/ports that map to :
      *
@@ -48,8 +48,8 @@ public interface PortMapping {
     boolean createMappingData(String nodeId, String nodeVersion);
 
     /**
-     * This method removes mapping data from the datastore after disconnecting
-     * ODL from a Netconf device.
+     * This method removes mapping data from the datastore after disconnecting ODL
+     * from a Netconf device.
      *
      * @param nodeId
      *            node ID
@@ -57,10 +57,10 @@ public interface PortMapping {
     void deleteMappingData(String nodeId);
 
     /**
-     * This method for a given node's termination point returns the Mapping
-     * object based on portmapping.yang model stored in the MD-SAL data store
-     * which is created when the node is connected for the first time. The
-     * mapping object basically contains the following attributes of interest:
+     * This method for a given node's termination point returns the Mapping object
+     * based on portmapping.yang model stored in the MD-SAL data store which is
+     * created when the node is connected for the first time. The mapping object
+     * basically contains the following attributes of interest:
      *
      * <p>
      * 1. Supporting circuit pack
@@ -69,8 +69,7 @@ public interface PortMapping {
      * 2. Supporting port
      *
      * <p>
-     * 3. Supporting OMS interface (if port on ROADM) 4. Supporting OTS
-     * interface (if port on ROADM)
+     * 3. Supporting OTS/OMS interface (if port on ROADM)
      *
      * @param nodeId
      *            Unique Identifier for the node of interest.
@@ -82,10 +81,27 @@ public interface PortMapping {
     Mapping getMapping(String nodeId, String logicalConnPoint);
 
     /**
-     * This method for a given node's media channel-capabilities returns the
-     * object based on portmapping.yang model stored in the MD-SAL data store
-     * which is created when the node is connected for the first time. The
-     * mapping object basically contains the following attributes of interest:
+     * This method allows retrieving a Mapping object from the mapping list stored in
+     * the MD-SAL data store. The main interest is to retrieve the
+     * logical-connection-point associated with a given port on a supporting
+     * circuit-pack
+     *
+     * @param nodeId
+     *            Unique Identifier for the node of interest.
+     * @param circuitPackName
+     *            Name of the supporting circuit-pack
+     * @param circuitPackName
+     *            Name of the supporting port
+     *
+     * @return Result Mapping object if success otherwise null.
+     */
+    Mapping getMapping(String nodeId, String circuitPackName, String portName);
+
+    /**
+     * This method, for a given node media channel-capabilities, returns the object
+     * based on portmapping.yang model stored in the MD-SAL data store which is
+     * created when the node is connected for the first time. The mapping object
+     * basically contains the following attributes of interest:
      *
      * <p>
      * 1. slot width granularity
@@ -94,8 +110,10 @@ public interface PortMapping {
      * 2. center frequency granularity
      *
      * <p>
-     * 3. Supporting OMS interface (if port on ROADM) 4. Supporting OTS
-     * interface (if port on ROADM)
+     * 3. Supporting OMS interface (if port on ROADM)
+     *
+     * <p>
+     * 4. Supporting OTS interface (if port on ROADM)
      *
      * @param nodeId
      *            Unique Identifier for the node of interest.
@@ -106,14 +124,26 @@ public interface PortMapping {
      */
     McCapabilities getMcCapbilities(String nodeId, String mcLcp);
 
+    /**
+     * This method for a given node, allows to update a specific mapping based on
+     * portmapping.yang model already stored in the MD-SAL data store, following
+     * some changes on the device port (creation of interface supported on this
+     * port, change of port admin state, etc).
+     *
+    * @param nodeId
+     *            Unique Identifier for the node of interest.
+     * @param mapping
+     *            Old mapping to be updated.
+     *
+     * @return Result true/false based on status of operation.
+     */
     boolean updateMapping(String nodeId, Mapping mapping);
 
     /**
-     * Returns all Mapping informations for a given ordm device.
-     * This method returns all Mapping informations already stored in the MD-SAL
-     * data store for a given openroadm device. Beyound the list of mappings, it
-     * gives access to general node information as its version or its node type,
-     * etc.
+     * Returns all Mapping informations for a given ordm device. This method returns
+     * all Mapping informations already stored in the MD-SAL data store for a given
+     * openroadm device. Beyound the list of mappings, it gives access to general
+     * node information as its version or its node type, etc.
      *
      * @param nodeId
      *            Unique Identifier for the node of interest.
