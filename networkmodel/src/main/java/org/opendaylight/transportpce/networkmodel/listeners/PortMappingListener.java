24c7c180494d5bd52a7dc35dc02b20954984b981
Gilles Thouenon <gilles.thouenon@orange.com>
2021-03-16 11:35:58 +0100

    Update node-tp and link states on topologies
    When a mapping is updated after reception of an administrative state
    or operational state change from a device port, update associated node
    TP and links in openroadm-topology and otn-topology.
    - add in networkmodel a portmapping listener that listens to mapping
    updates
    - add a new updateOpenRoadmTopologies method in NetworkModelService to
    manage topology updates from a given mapping
    - remove updateOpenRoadmNetworkTopology that handled a change on a
    circuit-pack (not present as it in port-mapping)
    - implement the new updateTopologyShard method in TopologyUtils to
    update tp and if need be links (usable for both openroadm and otn
    topologies)
    
    JIRA: TRNSPRTPCE-420
    Signed-off-by: Gilles Thouenon <gilles.thouenon@orange.com>
    Co-authored-by: Christophe Betoule <christophe.betoule@orange.com>
    Change-Id: I8de09921e5a938676348e83e4883d953c47c13cb

---
+++ networkmodel/src/main/java/org/opendaylight/transportpce/networkmodel/listeners/PortMappingListener.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright © 2021 Orange.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.transportpce.networkmodel.listeners;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.mdsal.binding.api.DataTreeChangeListener;
+import org.opendaylight.mdsal.binding.api.DataTreeModification;
+import org.opendaylight.transportpce.networkmodel.service.NetworkModelService;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev210315.mapping.Mapping;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev210315.network.Nodes;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier.PathArgument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PortMappingListener implements DataTreeChangeListener<Mapping> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PortMappingListener.class);
+
+    private final NetworkModelService networkModelService;
+
+    public PortMappingListener(NetworkModelService networkModelService) {
+        this.networkModelService = networkModelService;
+    }
+
+    @Override
+    public void onDataTreeChanged(@NonNull Collection<DataTreeModification<Mapping>> changes) {
+        for (DataTreeModification<Mapping> change : changes) {
+            if (change.getRootNode().getDataBefore() != null && change.getRootNode().getDataAfter() != null) {
+                Mapping oldMapping = change.getRootNode().getDataBefore();
+                Mapping newMapping = change.getRootNode().getDataAfter();
+                if (oldMapping.getPortAdminState().equals(newMapping.getPortAdminState())
+                    || oldMapping.getPortOperState().equals(newMapping.getPortOperState())) {
+                    return;
+                } else {
+                    LinkedList<PathArgument> path = new LinkedList<>();
+                    path.addAll((Collection<? extends PathArgument>) change.getRootPath().getRootIdentifier()
+                        .getPathArguments());
+                    path.removeLast();
+                    InstanceIdentifier<Nodes> portmappintNodeID = (InstanceIdentifier<Nodes>) InstanceIdentifier
+                        .create(path);
+                    String nodeId = InstanceIdentifier.keyOf(portmappintNodeID).getNodeId();
+                    networkModelService.updateOpenRoadmTopologies(nodeId, newMapping);
+                }
+            }
+        }
+    }
+}
