a128c710c79e7a7dc281accd3f32334fc31a9e77
Shweta Vachhani <sv111y@att.com>
2021-04-21 11:08:18 -0400

    OTN-topology creation fix
    Please refer to following JIRA for more details:
    JIRA: TRNSPRTPCE-443
    Fix in portmapping the type conversion issue.
    
    Signed-off-by: Shweta Vachhani <sv111y@att.com>
    Co-authored-by: Gilles Thouenon <gilles.thouenon@orange.com>
    Change-Id: I3eb51b64d986ee6ab620a7142e1e3e83a67f7eaa

--- common/src/main/java/org/opendaylight/transportpce/common/mapping/PortMappingVersion221.java
+++ common/src/main/java/org/opendaylight/transportpce/common/mapping/PortMappingVersion221.java
@@ -874,11 +874,7 @@ public class PortMappingVersion221 {
             List<Class<? extends org.opendaylight.yang.gen.v1.http.org.openroadm.port.types.rev200327
                 .SupportedIfCapability>> supportedIntf = new ArrayList<>();
             for (Class<? extends SupportedIfCapability> sup: port.getSupportedInterfaceCapability()) {
-                @SuppressWarnings("unchecked") Class<? extends
-                    org.opendaylight.yang.gen.v1.http.org.openroadm.port.types.rev200327.SupportedIfCapability> sup1 =
-                    (Class<? extends org.opendaylight.yang.gen.v1.http.org.openroadm.port.types.rev200327
-                        .SupportedIfCapability>) sup;
-                supportedIntf.add(sup1);
+                supportedIntf.add(MappingUtilsImpl.convertSupIfCapa(sup.getSimpleName()));
             }
             mpBldr.setSupportedInterfaceCapability(supportedIntf);
         }
