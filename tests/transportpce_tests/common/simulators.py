54d8ced7c4bfcf860c603f9377b13081961c063d
Balagangadhar Bathula <bb4341@att.com>
2021-04-21 10:40:08 -0400

    Port-mapping functional tests for 7.1.0 transponder
     - Verify port-mapping data for xpdr port quals
     - Verifies node data in the topology (addition/deletion)
    
    JIRA: TRNSPRTPCE-347
    Change-Id: I80863cdda4f54eaad246d532d8bc3d2c70c68977

--- tests/transportpce_tests/common/simulators.py
+++ tests/transportpce_tests/common/simulators.py
@@ -26,6 +26,7 @@ SIMS = {
     ('spdra', '2.2.1'): {'port': '17845', 'configfile': 'oper-SPDRA.xml', 'logfile': 'spdra221.log'},
     ('spdrc', '2.2.1'): {'port': '17846', 'configfile': 'oper-SPDRC.xml', 'logfile': 'spdrc221.log'},
     ('xpdra', '7.1.0'): {'port': '17850', 'configfile': 'oper-XPDRA.xml', 'logfile': 'xpdra71.log'},
+    ('xpdra2', '7.1.0'): {'port': '17859', 'configfile': 'oper-XPDRA2.xml', 'logfile': 'xpdra2_71.log'},
     ('roadma', '7.1.0'): {'port': '17851', 'configfile': 'oper-ROADMA.xml', 'logfile': 'roadma71.log'},
     ('roadmb', '7.1.0'): {'port': '17852', 'configfile': 'oper-ROADMB.xml', 'logfile': 'roadmb71.log'},
     ('roadmc', '7.1.0'): {'port': '17853', 'configfile': 'oper-ROADMC.xml', 'logfile': 'roadmc71.log'},
