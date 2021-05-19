160825d274409794dce52062435cf45c066be8ec
guillaume.lambert <guillaume.lambert@orange.com>
2021-05-19 22:08:42 +0200

    Mute snapshots download status during lighty build
    JIRA: TRNSPRTPCE-442
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    Change-Id: I4bd399059ba8c22728c4d27b93bc6e2204979a09

--- lighty/build.sh
+++ lighty/build.sh
@@ -3,6 +3,5 @@
 cd $(dirname "$0")
 export JDK_JAVA_OPTIONS="--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED"
 #./build_lighty_core.sh
-#mvn clean install -s tests/odl_settings.xml -DskipTests -Dmaven.javadoc.skip=true -Dodlparent.spotbugs.skip -Dodlparent.checkstyle.skip
-mvn clean install -Dmaven.javadoc.skip=true
+mvn clean install -B -U -Dmaven.javadoc.skip=true -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
 unzip -q target/tpce-bin.zip -d target
