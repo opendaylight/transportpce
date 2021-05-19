172a074a62ca7a0405a7d6d7d975ce474b5681b2
guillaume.lambert <guillaume.lambert@orange.com>
2021-05-19 21:49:37 +0200

    Mute Lighty reflective access warnings
    JIRA: TRNSPRTPCE-442
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    Change-Id: Icd684c09309fe6f7061c246f713f8e73e4ea82f4

--- lighty/build_lighty_core.sh
+++ lighty/build_lighty_core.sh
@@ -6,4 +6,5 @@ git clone https://github.com/PantheonTechnologies/lighty-core.git
 cd lighty-core
 #git checkout master
 git checkout 14.0.x
+export JDK_JAVA_OPTIONS="--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED"
 mvn clean install -DskipTests -Dmaven.javadoc.skip=true
