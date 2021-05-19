72058477a6c73307467889f77c454432bf8e2fda
guillaume.lambert <guillaume.lambert@orange.com>
2021-05-19 21:49:37 +0200

    Mute Lighty reflective access warnings
    JIRA: TRNSPRTPCE-442
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    Change-Id: Icd684c09309fe6f7061c246f713f8e73e4ea82f4

--- lighty/build.sh
+++ lighty/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cd $(dirname "$0")
+export JDK_JAVA_OPTIONS="--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED"
 #./build_lighty_core.sh
 #mvn clean install -s tests/odl_settings.xml -DskipTests -Dmaven.javadoc.skip=true -Dodlparent.spotbugs.skip -Dodlparent.checkstyle.skip
 mvn clean install -Dmaven.javadoc.skip=true
