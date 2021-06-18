a4a03c55e7319bf26db10abd4adf26d33a35ed25
Guillaume Lambert <guillaume.lambert@orange.com>
2021-06-18 22:18:39 +0200

    Refactor tests launching procedure
    The possibility to run test separately was lost during preliminary work
    to prepare jobs parallelization.
    This refactoring restores that feature but with a different approach.
    For example, the equivalents of the old commands
        $ tox -e topoPortmapping
    and
        $ tox -e portmapping,pce
    are now
        $ tox -e tests121 topo_portmapping
    and
        $ tox -e tests121 "portmapping pce"
    (preceded by $ tox -e buildcontroller)
    
    The possibility to choose the script launcher command from a global
    variable have been added.
    For example,
        $ export LAUNCHER='python3'
    allows to replace 'nosetests --with-xunit' by a straight call to python3
    to have more debug output during tests.
    
    Tests order can also be changed by directly modifying script filenames
    without touching tox.ini. Filenames have been modified here to add a
    numbering and keep the tests current order.
    
    JIRA: TRNSPRTPCE-482
    Signed-off-by: Guillaume Lambert <guillaume.lambert@orange.com>
    Change-Id: I725d1dfac42bd309d18fbf87ff1c85bc1bce5be9

---
+++ tests/launch_tests.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+for arg in $@; do
+    if [ -z "$test_suite" ]; then
+        test_suite=$1
+    else
+# Tox reinterprets "olm" and "pce" by "../olm" "../pce" because of the
+# changedir directive and the presence of modules folders "olm" and pce"
+# at project root.
+        if [ "$arg" = "../olm" ]; then
+            arglist=$arglist" olm"
+        elif [ "$arg" = "../pce" ]; then
+            arglist=$arglist" pce"
+        else
+            arglist=$arglist" "$arg
+        fi
+    fi
+done
+
+scriptlist=""
+if [ -z "$arglist" ]; then
+    scriptlist="transportpce_tests/$test_suite/test[0-9][0-9]_*.py"
+else
+    for test in $arglist; do
+        scriptlist=$scriptlist" transportpce_tests/$test_suite/test[0-9][0-9]_$test.py"
+    done
+fi
+if [ -z "$LAUNCHER" ]; then
+    LAUNCHER="nosetests --with-xunit";
+fi
+
+for script in $scriptlist; do
+    echo $LAUNCHER $(ls $script)
+    $LAUNCHER $script || exit 1
+done
