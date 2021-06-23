a3ca3f1f43df74848b89cdd043069aba9df0c74d
Gilles Thouenon <gilles.thouenon@orange.com>
2021-06-23 11:49:41 +0200

    Move otn link update from renderer to SH
    - align service-path delete on service-path create
    - use DeviceRenderingTask to both create and delete optical service
    - uses action to create service-path inputs
    - use new link-tp object to notify otn-link termination-points to be
    updated
    - change updateOtnLink signature method to use new link-tp objects
    notified by renderer
    - use service-type to chose between service-path or otn-service-path
    - adapt RendererListener of service handler to handle otn-link update
    notifications received according to the service-type
    
    Signed-off-by: Gilles Thouenon <gilles.thouenon@orange.com>
    Change-Id: Ic277dd716676053b62ca59c562e4077a8e45f92e

--- renderer/src/main/java/org/opendaylight/transportpce/renderer/provisiondevice/OtnDeviceRenderingResult.java
+++ renderer/src/main/java/org/opendaylight/transportpce/renderer/provisiondevice/OtnDeviceRenderingResult.java
@@ -11,17 +11,26 @@ package org.opendaylight.transportpce.renderer.provisiondevice;
 import java.util.Collections;
 import java.util.List;
 import org.opendaylight.transportpce.common.OperationResult;
+import org.opendaylight.yang.gen.v1.http.org.transportpce.common.types.rev210618.link.tp.LinkTp;
 import org.opendaylight.yang.gen.v1.http.org.transportpce.common.types.rev210618.node.interfaces.NodeInterface;
 
 public final class OtnDeviceRenderingResult extends OperationResult {
     private final List<NodeInterface> renderedNodeInterfaces;
+    private final List<LinkTp> otnLinkTps;
 
-    private OtnDeviceRenderingResult(boolean success, String message, List<NodeInterface> renderedNodeInterfaces) {
+
+    private OtnDeviceRenderingResult(boolean success, String message, List<NodeInterface> renderedNodeInterfaces,
+        List<LinkTp> otnLinkTps) {
         super(success, message);
         if (renderedNodeInterfaces != null) {
             this.renderedNodeInterfaces = Collections.unmodifiableList(renderedNodeInterfaces);
         } else {
             this.renderedNodeInterfaces = Collections.emptyList();
+        }
+        if (otnLinkTps != null) {
+            this.otnLinkTps = Collections.unmodifiableList(otnLinkTps);
+        } else {
+            this.otnLinkTps = Collections.emptyList();
         }
     }
 
@@ -29,12 +38,17 @@ public final class OtnDeviceRenderingResult extends OperationResult {
         return this.renderedNodeInterfaces;
     }
 
-    public static OtnDeviceRenderingResult failed(String message) {
-        return new OtnDeviceRenderingResult(false, message, null);
+    public List<LinkTp> getOtnLinkTps() {
+        return this.otnLinkTps;
     }
 
-    public static OtnDeviceRenderingResult ok(List<NodeInterface> renderedNodeInterfaces) {
-        return new OtnDeviceRenderingResult(true, "", renderedNodeInterfaces);
+    public static OtnDeviceRenderingResult failed(String message) {
+        return new OtnDeviceRenderingResult(false, message, null, null);
+    }
+
+    public static OtnDeviceRenderingResult ok(List<NodeInterface> renderedNodeInterfaces,
+            List<LinkTp> otnLinkTps) {
+        return new OtnDeviceRenderingResult(true, "", renderedNodeInterfaces, otnLinkTps);
     }
 
 }
