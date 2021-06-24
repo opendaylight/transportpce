c61556a6417ceca3b70d9c35d7757167c95b4eb6
Guillaume Lambert <guillaume.lambert@orange.com>
2021-06-24 11:24:05 +0200

    Allow Lighty build to listen to alternative ports
    - Add lighty configuration files templates for akka and RESTCONF
    - Update lighty clean start script to generate from environment
      variables its configuration files (based on the new templates)
    
    Allowing the controller configuration to listen to alternative ports
    is a commodity to run functional tests in parallel.
    
    JIRA: TRNSPRTPCE-482
    Signed-off-by: Guillaume Lambert <guillaume.lambert@orange.com>
    Change-Id: Ib49dcad14e464bdf60c70dbebf6a22c5eba06699

--- lighty/src/main/assembly/resources/clean-start-controller.sh
+++ lighty/src/main/assembly/resources/clean-start-controller.sh
@@ -1,11 +1,36 @@
 #!/bin/sh
 
 BASEDIR=$(dirname "$0")
-#echo "${BASEDIR}"
 cd ${BASEDIR}
 
 rm -rf cache
 rm -rf target
 
+# check if default ports must be overriden
+if [ -z "$USE_ODL_ALT_RESTCONF_PORT" ]; then
+    RESTCONF_PORT=8181
+else
+    RESTCONF_PORT=$USE_ODL_ALT_RESTCONF_PORT
+fi
+if [ -z "$USE_ODL_ALT_WEBSOCKET_PORT" ]; then
+    WEBSOCKET_PORT=8185
+else
+    WEBSOCKET_PORT=$USE_ODL_ALT_WEBSOCKET_PORT
+fi
+if [ -z "$USE_ODL_ALT_AKKA_PORT" ]; then
+    AKKA_PORT=2550
+else
+    AKKA_PORT=$USE_ODL_ALT_AKKA_PORT
+fi
+if [ -z "$USE_ODL_ALT_AKKA_MGT_PORT" ]; then
+    AKKA_MGT_PORT=8558
+else
+    AKKA_MGT_PORT=$USE_ODL_ALT_AKKA_MGT_PORT
+fi
+
+# generate appropriate configuration files
+cat config_template.json | sed -e "s/ODL_RESTCONF_PORT/$RESTCONF_PORT/" -e "s/ODL_WEBSOCKET_PORT/$WEBSOCKET_PORT/" >config.json
+cat akka-default_template.conf | sed -e "s/ODL_AKKA_PORT/$AKKA_PORT/" -e "s/ODL_AKKA_MGT_PORT/$AKKA_MGT_PORT/" >singlenode/akka-default.conf
+
 #start controller
-java -ms128m -mx512m -XX:MaxMetaspaceSize=128m -jar tpce.jar
+java -ms128m -mx512m -XX:MaxMetaspaceSize=128m -jar tpce.jar -restconf config.json
