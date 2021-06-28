11199bbb70f2731dbb8ebf95fc9de5f8fbd96b24
guillaume.lambert <guillaume.lambert@orange.com>
2021-06-28 14:53:28 +0200

    Allow Karaf to listen to alternative RMI ports
    In Karaf post-install script for tests
    - generate configuration files templates for Karaf Management
    - patch Karaf exec to generate configuration files from environment
      variables and templates
    
    Allowing the controller configuration to listen to alternative ports
    is a commodity to run functional tests in parallel.
    
    JIRA: TRNSPRTPCE-482
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    Change-Id: Ic2f0db92b9da14fcd65fc9840aa4365b8f9469f8

--- karaf/src/main/assembly/ressources/post_install_for_tests.sh
+++ karaf/src/main/assembly/ressources/post_install_for_tests.sh
@@ -24,9 +24,21 @@ else\
     SHELL_PORT=$USE_ODL_ALT_SHELL_PORT\
 fi\
 sed -e "s/ODL_SHELL_PORT/$SHELL_PORT/" \$(dirname \$0)/\.\./ressources/org.apache.karaf.shell._template.cfg  >\$(dirname \$0)/\.\./etc/org.apache.karaf.shell.cfg\
+if [ -z "$USE_ODL_ALT_RMI_REGISTRY_PORT" ]; then\
+    RMI_REGISTRY_PORT=1099\
+else\
+    RMI_REGISTRY_PORT=$USE_ODL_ALT_RMI_REGISTRY_PORT\
+fi\
+if [ -z "$USE_ODL_ALT_RMI_SERVER_PORT" ]; then\
+    RMI_SERVER_PORT=44444\
+else\
+    RMI_SERVER_PORT=$USE_ODL_ALT_RMI_SERVER_PORT\
+fi\
+sed -e "s/ODL_RMI_REGISTRY_PORT/$RMI_REGISTRY_PORT/" -e "s/ODL_RMI_SERVER_PORT/$RMI_SERVER_PORT/" \$(dirname \$0)/\.\./ressources/org.apache.karaf.management._template.cfg  >\$(dirname \$0)/\.\./etc/org.apache.karaf.management.cfg\
 ' ../bin/karaf
 
 sed 's/2550/ODL_AKKA_PORT/' ../system/org/opendaylight/controller/sal-clustering-config/*/sal-clustering-config-*-akkaconf.xml >akka-default_template.conf
 sed 's/8181/ODL_RESTCONF_PORT/' ../etc/org.ops4j.pax.web.cfg > org.ops4j.pax.web._template.cfg
 sed 's/8181/ODL_RESTCONF_PORT/' ../etc/jetty.xml > jetty_template.xml
 sed 's/8101/ODL_SHELL_PORT/' ../etc/org.apache.karaf.shell.cfg > org.apache.karaf.shell._template.cfg
+sed -e 's/1099/ODL_RMI_REGISTRY_PORT/' -e 's/44444/ODL_RMI_SERVER_PORT/' ../etc/org.apache.karaf.management.cfg > org.apache.karaf.management._template.cfg
