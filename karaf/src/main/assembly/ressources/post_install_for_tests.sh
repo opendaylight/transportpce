0a71e2842fd9477fd6648722b4b1758dd972f7d6
guillaume.lambert <guillaume.lambert@orange.com>
2021-06-28 14:41:50 +0200

    Allow Karaf to listen to an alternative SSH port
    In Karaf post-install script for tests
    - generate a configuration file template for Karaf Shell
    - patch Karaf exec to generate configuration file from environment
      variable and template
    
    In tox.ini
    - declare the related variable in SETENV parameters
    
    Allowing the controller configuration to listen to alternative ports
    is a commodity to run functional tests in parallel.
    
    JIRA: TRNSPRTPCE-482
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    Change-Id: Id1b367aaaaccae4bcf74d1b6ac0aa4997f3083a6

--- karaf/src/main/assembly/ressources/post_install_for_tests.sh
+++ karaf/src/main/assembly/ressources/post_install_for_tests.sh
@@ -18,8 +18,15 @@ else\
     AKKA_PORT=$USE_ODL_ALT_AKKA_PORT\
 fi\
 sed -e "s/ODL_AKKA_PORT/$AKKA_PORT/" \$(dirname \$0)/\.\./ressources/akka-default_template.conf  >\$(dirname \$0)/\.\./configuration/initial/akka.conf\
+if [ -z "$USE_ODL_ALT_SHELL_PORT" ]; then\
+    SHELL_PORT=8101\
+else\
+    SHELL_PORT=$USE_ODL_ALT_SHELL_PORT\
+fi\
+sed -e "s/ODL_SHELL_PORT/$SHELL_PORT/" \$(dirname \$0)/\.\./ressources/org.apache.karaf.shell._template.cfg  >\$(dirname \$0)/\.\./etc/org.apache.karaf.shell.cfg\
 ' ../bin/karaf
 
 sed 's/2550/ODL_AKKA_PORT/' ../system/org/opendaylight/controller/sal-clustering-config/*/sal-clustering-config-*-akkaconf.xml >akka-default_template.conf
 sed 's/8181/ODL_RESTCONF_PORT/' ../etc/org.ops4j.pax.web.cfg > org.ops4j.pax.web._template.cfg
 sed 's/8181/ODL_RESTCONF_PORT/' ../etc/jetty.xml > jetty_template.xml
+sed 's/8101/ODL_SHELL_PORT/' ../etc/org.apache.karaf.shell.cfg > org.apache.karaf.shell._template.cfg
