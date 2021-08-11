8cea4c4bdef1bdc48a331363aa6cb3c01b3164f1
Peter Heringer <peter.heringer@nokia.com>
2021-08-11 15:06:54 +0200

    Send TAPI notification to Kafka
    - Send tapi notification if state of connectivity service changes
    - Add possibility to create Kafka Topics dynamically
    - Fix nbi-notification yang model to avoid yantools error
    
    JIRA: TRNSPRTPCE-466
    Change-Id: I32ebbf62570474b5f18787ad0c1c422f3e5e8d68
    Signed-off-by: errea <errea@eurecom.fr>

---
+++ nbinotifications/src/main/java/org/opendaylight/transportpce/nbinotifications/serialization/TapiNotificationSerializer.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright © 2021 Orange, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.transportpce.nbinotifications.serialization;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import org.apache.kafka.common.serialization.Serializer;
+import org.opendaylight.transportpce.common.converter.JsonStringConverter;
+import org.opendaylight.yang.gen.v1.nbi.notifications.rev210813.Notification;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.codec.gson.JSONCodecFactorySupplier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TapiNotificationSerializer implements Serializer<Notification> {
+    private static final Logger LOG = LoggerFactory.getLogger(TapiNotificationSerializer.class);
+    private JsonStringConverter<Notification> converter;
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+        LOG.info("Deserializer configuration {}", configs);
+        if (configs.containsKey(ConfigConstants.CONVERTER)
+                && configs.get(ConfigConstants.CONVERTER) instanceof JsonStringConverter<?>) {
+            converter = (JsonStringConverter<Notification>) configs.get(ConfigConstants.CONVERTER);
+        }
+    }
+
+    @Override
+    public byte[] serialize(String topic, Notification data) {
+        if (converter == null) {
+            throw new IllegalArgumentException(
+                    "Converter should be" + "configured through configure method of serializer");
+        }
+        if (data == null) {
+            return new byte[0];
+        }
+        try {
+            InstanceIdentifier<Notification> iid = InstanceIdentifier.builder(Notification.class).build();
+            String serialized = converter.createJsonStringFromDataObject(iid, data, JSONCodecFactorySupplier.RFC7951);
+            LOG.info("Serialized event {}", serialized);
+            return serialized.getBytes(StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            return new byte[0];
+        }
+    }
+}
