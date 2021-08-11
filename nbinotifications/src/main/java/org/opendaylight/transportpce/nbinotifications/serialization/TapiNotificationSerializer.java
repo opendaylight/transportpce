b99a942d2f13b4edf0e763240b467899ec3c46d9
Javier Errea <errea@eurecom.fr>
2021-08-11 15:06:54 +0200

    Initial tapi notification implementation
    - Tapi notification to nbi module
    - Create notification subscription service rpc
    - Create tapi topic in kafka server and send tapi notifications
    - Service state change use case
    - Extension of nbi module to support tapi notification service
    - Update nbinotification revision date
    - TapiRendererListener sends a notification when TAPI service
      is implemented
    
    JIRA: TRNSPRTPCE-387
    Change-Id: I32ebbf62570474b5f18787ad0c1c422f3e5e8d68
    Signed-off-by: errea <errea@eurecom.fr>

--- nbinotifications/src/main/java/org/opendaylight/transportpce/nbinotifications/serialization/TapiNotificationSerializer.java
+++ nbinotifications/src/main/java/org/opendaylight/transportpce/nbinotifications/serialization/TapiNotificationSerializer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2021 Orange, Inc. and others.  All rights reserved.
+ * Copyright © 2021 Nokia, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -12,15 +12,15 @@ import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import org.apache.kafka.common.serialization.Serializer;
 import org.opendaylight.transportpce.common.converter.JsonStringConverter;
-import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.NotificationProcessService;
+import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.NotificationTapiService;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.codec.gson.JSONCodecFactorySupplier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class NotificationServiceSerializer implements Serializer<NotificationProcessService> {
-    private static final Logger LOG = LoggerFactory.getLogger(NotificationServiceSerializer.class);
-    private JsonStringConverter<NotificationProcessService> converter;
+public class TapiNotificationSerializer implements Serializer<NotificationTapiService> {
+    private static final Logger LOG = LoggerFactory.getLogger(TapiNotificationSerializer.class);
+    private JsonStringConverter<NotificationTapiService> converter;
 
     @SuppressWarnings("unchecked")
     @Override
@@ -28,26 +28,28 @@ public class NotificationServiceSerializer implements Serializer<NotificationPro
         LOG.info("Deserializer configuration {}", configs);
         if (configs.containsKey(ConfigConstants.CONVERTER)
                 && configs.get(ConfigConstants.CONVERTER) instanceof JsonStringConverter<?>) {
-            converter = (JsonStringConverter<NotificationProcessService>) configs.get(ConfigConstants.CONVERTER);
+            converter = (JsonStringConverter<NotificationTapiService>) configs.get(ConfigConstants.CONVERTER);
         }
     }
 
     @Override
-    public byte[] serialize(String topic, NotificationProcessService data) {
+    public byte[] serialize(String topic, NotificationTapiService data) {
         if (converter == null) {
-            throw new IllegalArgumentException("Converter should be configured through configure method of serializer");
+            throw new IllegalArgumentException(
+                    "Converter should be configured through configure method of serializer");
         }
         if (data == null) {
+            LOG.error("Notification data is empty");
             return new byte[0];
         }
         try {
-            InstanceIdentifier<NotificationProcessService> iid = InstanceIdentifier
-                    .builder(NotificationProcessService.class).build();
+            InstanceIdentifier<NotificationTapiService> iid = InstanceIdentifier.builder(NotificationTapiService.class)
+                .build();
             String serialized = converter.createJsonStringFromDataObject(iid, data, JSONCodecFactorySupplier.RFC7951);
             LOG.info("Serialized event {}", serialized);
             return serialized.getBytes(StandardCharsets.UTF_8);
         } catch (IOException e) {
-            LOG.error("exception raised during serialization", e);
+            LOG.error("Event couldnt be serialized", e);
             return new byte[0];
         }
     }
