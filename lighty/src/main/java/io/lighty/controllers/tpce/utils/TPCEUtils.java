2dd499b433c7d3b4e89dd73dee063c1d9616d97b
Gilles Thouenon <gilles.thouenon@orange.com>
2021-09-02 11:10:12 +0200

    Change absolute path of leafref in GNPy model
    JIRA: TRNSPRTPCE-497
    Signed-off-by: Gilles Thouenon <gilles.thouenon@orange.com>
    Change-Id: Ief01620be3f2842664dcba6aa7c4851c92e327d5

--- lighty/src/main/java/io/lighty/controllers/tpce/utils/TPCEUtils.java
+++ lighty/src/main/java/io/lighty/controllers/tpce/utils/TPCEUtils.java
@@ -310,7 +310,7 @@ public final class TPCEUtils {
             org.opendaylight.yang.gen.v1.http.org.transportpce.common.types.rev210618.$YangModuleInfoImpl.getInstance(),
             org.opendaylight.yang.gen.v1.gnpy.gnpy.api.rev190103.$YangModuleInfoImpl.getInstance(),
             org.opendaylight.yang.gen.v1.gnpy.gnpy.eqpt.config.rev181119.$YangModuleInfoImpl.getInstance(),
-            org.opendaylight.yang.gen.v1.gnpy.gnpy.network.topology.rev181214.$YangModuleInfoImpl.getInstance(),
+            org.opendaylight.yang.gen.v1.gnpy.gnpy.network.topology.rev210831.$YangModuleInfoImpl.getInstance(),
             org.opendaylight.yang.gen.v1.gnpy.path.rev200909.$YangModuleInfoImpl.getInstance(),
             org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev210426
                     .$YangModuleInfoImpl.getInstance(),
