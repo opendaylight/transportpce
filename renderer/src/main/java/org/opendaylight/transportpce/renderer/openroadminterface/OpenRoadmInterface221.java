00be35161ba8e97958ed9451d1207e1c6b8affde
Gilles Thouenon <gilles.thouenon@orange.com>
2021-09-09 15:41:39 +0200

    Adapt renderer to manage 100GE service on Switch
    - Add new method in OpenRoadmInterface221 to create ODU4 interface as
    an HighOrder interface (to support 100GE flow)
    - manage the creation/deletion of Ethernet, ODU4-Client, ODU4-Network
    and odu-connection to support the 100GE service over an OTN switch
    - manage the new 100GE_S service-type in OtnDeviceRendererService
    - propagate the service-type inside the renderer in order to have to
    determine it only once at the highest renderer level
    (RendererServiceOperations)
    
    JIRA: TRNSPRTPCE-499
    Signed-off-by: Gilles Thouenon <gilles.thouenon@orange.com>
    Change-Id: I23f4df43a66f3d4de8f75687219d3ffa0c148ba5

--- renderer/src/main/java/org/opendaylight/transportpce/renderer/openroadminterface/OpenRoadmInterface221.java
+++ renderer/src/main/java/org/opendaylight/transportpce/renderer/openroadminterface/OpenRoadmInterface221.java
@@ -53,8 +53,11 @@ import org.opendaylight.yang.gen.v1.http.org.openroadm.optical.channel.interface
 import org.opendaylight.yang.gen.v1.http.org.openroadm.optical.transport.interfaces.rev181019.OtsAttributes;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.optical.transport.interfaces.rev181019.ots.container.OtsBuilder;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.otn.common.types.rev171215.ODU4;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.otn.common.types.rev171215.ODUCTP;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.otn.common.types.rev171215.ODUTTP;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.otn.common.types.rev171215.ODUTTPCTP;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.otn.common.types.rev171215.OTU4;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.otn.common.types.rev171215.OduFunctionIdentity;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.otn.common.types.rev171215.PayloadTypeDef;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.otn.odu.interfaces.rev181019.OduAttributes;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.otn.odu.interfaces.rev181019.odu.container.OduBuilder;
@@ -237,6 +240,39 @@ public class OpenRoadmInterface221 {
         }
 
         return ochInterfaceBldr.getName();
+    }
+
+    public String createOpenRoadmOdu4HOInterface(String nodeId, String logicalConnPoint, boolean isNetworkPort,
+            String supportingInterface) throws OpenRoadmInterfaceException {
+        Mapping mapping = portMapping.getMapping(nodeId, logicalConnPoint);
+        if (mapping == null) {
+            throw new OpenRoadmInterfaceException(
+                    String.format(MAPPING_ERROR_EXCEPTION_MESSAGE, nodeId, logicalConnPoint));
+        }
+        InterfaceBuilder oduInterfaceBldr = createGenericInterfaceBuilder(mapping, OtnOdu.class,
+            logicalConnPoint + "-ODU4");
+        if (supportingInterface != null) {
+            oduInterfaceBldr.setSupportingInterface(supportingInterface);
+        }
+
+        // ODU interface specific data
+        Class<? extends OduFunctionIdentity> oduFunction = isNetworkPort ? ODUCTP.class : ODUTTPCTP.class;
+        // TODO look at imports of different versions of class
+        oduInterfaceBldr.addAugmentation(
+            new org.opendaylight.yang.gen.v1.http.org.openroadm.otn.odu.interfaces.rev181019.Interface1Builder()
+                .setOdu(new OduBuilder()
+                    .setRate(ODU4.class)
+                    .setOduFunction(oduFunction)
+                    .setMonitoringMode(OduAttributes.MonitoringMode.Terminated)
+                    .setOpu(
+                        new OpuBuilder()
+                        .setPayloadType(PayloadTypeDef.getDefaultInstance("07"))
+                        .setExpPayloadType(PayloadTypeDef.getDefaultInstance("07")).build()).build())
+                .build());
+
+        // Post interface on the device
+        openRoadmInterfaces.postInterface(nodeId, oduInterfaceBldr);
+        return oduInterfaceBldr.getName();
     }
 
     public String createOpenRoadmOdu4Interface(String nodeId, String logicalConnPoint, String supportingOtuInterface)
