22e65ff7abe80bdc92ca20240a642e95265c50b5
guillaume.lambert <guillaume.lambert@orange.com>
2021-10-25 12:15:02 +0200

    Refactor Karaf builds for tests and tox.ini
    - use dedicated env files for Karaf listening ports settings
    - create tox profiles to (re)build Karaf folders dedicated to tests
    - adapt tests launcher and build scripts accordingly
    
    This should avoid weird race conditions that can happen when performing
    several tests locally.
    
    JIRA: TRNSPRTPCE-482
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    Change-Id: Icfa0e09e65ed054155002b9e4b07370ca1ad0aaf

--- tests/build_karaf_for_tests.sh
+++ tests/build_karaf_for_tests.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
+if [ "$USE_LIGHTY" = "True" ]; then
+    echo "USE_LIGHTY set to True - no need to build karaf"
+    exit
+fi
+
 if [ -z "$USE_ODL_ALT_KARAF_INSTALL_DIR" ]; then
     exit
 fi
 
 cd $(dirname $0)
 
-if [ -f ../"$USE_ODL_ALT_KARAF_INSTALL_DIR"/target/assembly/bin/karaf_ ]; then
+if [ -z "$USE_ODL_ALT_KARAF_ENV" ]; then
     exit
 fi
 
+. $USE_ODL_ALT_KARAF_ENV
 . ./reflectwarn.sh
 cd  ../$USE_ODL_ALT_KARAF_INSTALL_DIR
 mvn clean install -B -q -s ../tests/odl_settings.xml -DskipTests -Dmaven.javadoc.skip=true
