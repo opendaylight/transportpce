0a1b19e5e0291f0707b85fddaa20a4a706de44ae
guillaume.lambert <guillaume.lambert@orange.com>
2021-10-28 09:48:24 +0200

    Migrate portmapping functional tests to RFC8040
    JIRA: TRNSPRTPCE-567
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    Change-Id: Ie6150a68a4a588c06979b27a1753ee5cb9bc283c

--- tests/transportpce_tests/2.2.1/test01_portmapping.py
+++ tests/transportpce_tests/2.2.1/test01_portmapping.py
@@ -17,7 +17,7 @@ import time
 import requests
 import sys
 sys.path.append('transportpce_tests/common/')
-import test_utils  # nopep8
+import test_utils_rfc8040  # nopep8
 
 
 class TransportPCEPortMappingTesting(unittest.TestCase):
@@ -27,14 +27,14 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.processes = test_utils.start_tpce()
-        cls.processes = test_utils.start_sims([('xpdra', cls.NODE_VERSION), ('roadma', cls.NODE_VERSION)])
+        cls.processes = test_utils_rfc8040.start_tpce()
+        cls.processes = test_utils_rfc8040.start_sims([('xpdra', cls.NODE_VERSION), ('roadma', cls.NODE_VERSION)])
 
     @classmethod
     def tearDownClass(cls):
         # pylint: disable=not-an-iterable
         for process in cls.processes:
-            test_utils.shutdown_process(process)
+            test_utils_rfc8040.shutdown_process(process)
         print("all processes killed")
 
     def setUp(self):
@@ -42,105 +42,92 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
         time.sleep(10)
 
     def test_01_rdm_device_connection(self):
-        response = test_utils.mount_device("ROADM-A1", ('roadma', self.NODE_VERSION))
-        self.assertEqual(response.status_code, requests.codes.created, test_utils.CODE_SHOULD_BE_201)
+        response = test_utils_rfc8040.mount_device("ROADM-A1", ('roadma', self.NODE_VERSION))
+        self.assertEqual(response.status_code, requests.codes.created, test_utils_rfc8040.CODE_SHOULD_BE_201)
 
     def test_02_rdm_device_connected(self):
-        response = test_utils.get_netconf_oper_request("ROADM-A1")
-        self.assertEqual(response.status_code, requests.codes.ok)
-        res = response.json()
-        self.assertEqual(
-            res['node'][0]['netconf-node-topology:connection-status'],
-            'connected')
+        response = test_utils_rfc8040.check_device_connection("ROADM-A1")
+        self.assertEqual(response['status_code'], requests.codes.ok)
+        self.assertEqual(response['connection-status'], 'connected')
         time.sleep(10)
 
     def test_03_rdm_portmapping_info(self):
-        response = test_utils.portmapping_request("ROADM-A1/node-info")
-        self.assertEqual(response.status_code, requests.codes.ok)
-        res = response.json()
+        response = test_utils_rfc8040.get_portmapping_node_info("ROADM-A1")
+        self.assertEqual(response['status_code'], requests.codes.ok)
         self.assertEqual(
-            {'node-info': {'node-type': 'rdm',
-                           'node-ip-address': '127.0.0.11',
-                           'node-clli': 'NodeA',
-                           'openroadm-version': '2.2.1',
-                           'node-vendor': 'vendorA',
-                           'node-model': 'model2'}},
-            res)
+            {'node-type': 'rdm',
+             'node-ip-address': '127.0.0.11',
+             'node-clli': 'NodeA',
+             'openroadm-version': '2.2.1',
+             'node-vendor': 'vendorA',
+             'node-model': 'model2'},
+            response['node-info'])
         time.sleep(3)
 
     def test_04_rdm_portmapping_DEG1_TTP_TXRX(self):
-        response = test_utils.portmapping_request("ROADM-A1/mapping/DEG1-TTP-TXRX")
-        self.assertEqual(response.status_code, requests.codes.ok)
-        res = response.json()
+        response = test_utils_rfc8040.portmapping_request("ROADM-A1", "DEG1-TTP-TXRX")
+        self.assertEqual(response['status_code'], requests.codes.ok)
         self.assertIn(
             {'supporting-port': 'L1', 'supporting-circuit-pack-name': '1/0',
              'logical-connection-point': 'DEG1-TTP-TXRX', 'port-direction': 'bidirectional',
              'port-admin-state': 'InService', 'port-oper-state': 'InService'},
-            res['mapping'])
+            response['mapping'])
 
     def test_05_rdm_portmapping_DEG2_TTP_TXRX_with_ots_oms(self):
-        response = test_utils.portmapping_request("ROADM-A1/mapping/DEG2-TTP-TXRX")
-        self.assertEqual(response.status_code, requests.codes.ok)
-        res = response.json()
+        response = test_utils_rfc8040.portmapping_request("ROADM-A1", "DEG2-TTP-TXRX")
+        self.assertEqual(response['status_code'], requests.codes.ok)
         self.assertIn(
             {'supporting-port': 'L1', 'supporting-circuit-pack-name': '2/0',
              'logical-connection-point': 'DEG2-TTP-TXRX',
              'supporting-oms': 'OMS-DEG2-TTP-TXRX', 'supporting-ots': 'OTS-DEG2-TTP-TXRX',
              'port-direction': 'bidirectional',
              'port-admin-state': 'InService', 'port-oper-state': 'InService'},
-            res['mapping'])
+            response['mapping'])
 
     def test_06_rdm_portmapping_SRG1_PP3_TXRX(self):
-        response = test_utils.portmapping_request("ROADM-A1/mapping/SRG1-PP3-TXRX")
-        self.assertEqual(response.status_code, requests.codes.ok)
-        res = response.json()
+        response = test_utils_rfc8040.portmapping_request("ROADM-A1", "SRG1-PP3-TXRX")
+        self.assertEqual(response['status_code'], requests.codes.ok)
         self.assertIn(
             {'supporting-port': 'C3', 'supporting-circuit-pack-name': '3/0',
              'logical-connection-point': 'SRG1-PP3-TXRX', 'port-direction': 'bidirectional',
              'port-admin-state': 'InService', 'port-oper-state': 'InService'},
-            res['mapping'])
+            response['mapping'])
 
     def test_07_rdm_portmapping_SRG3_PP1_TXRX(self):
-        response = test_utils.portmapping_request("ROADM-A1/mapping/SRG3-PP1-TXRX")
-        self.assertEqual(response.status_code, requests.codes.ok)
-        res = response.json()
+        response = test_utils_rfc8040.portmapping_request("ROADM-A1", "SRG3-PP1-TXRX")
+        self.assertEqual(response['status_code'], requests.codes.ok)
         self.assertIn(
             {'supporting-port': 'C1', 'supporting-circuit-pack-name': '5/0',
              'logical-connection-point': 'SRG3-PP1-TXRX', 'port-direction': 'bidirectional',
              'port-admin-state': 'InService', 'port-oper-state': 'InService'},
-            res['mapping'])
+            response['mapping'])
 
     def test_08_xpdr_device_connection(self):
-        response = test_utils.mount_device("XPDR-A1", ('xpdra', self.NODE_VERSION))
-        self.assertEqual(response.status_code, requests.codes.created, test_utils.CODE_SHOULD_BE_201)
+        response = test_utils_rfc8040.mount_device("XPDR-A1", ('xpdra', self.NODE_VERSION))
+        self.assertEqual(response.status_code, requests.codes.created, test_utils_rfc8040.CODE_SHOULD_BE_201)
 
     def test_09_xpdr_device_connected(self):
-        response = test_utils.get_netconf_oper_request("XPDR-A1")
-        self.assertEqual(response.status_code, requests.codes.ok)
-        res = response.json()
-        self.assertEqual(
-            res['node'][0]['netconf-node-topology:connection-status'],
-            'connected')
+        response = test_utils_rfc8040.check_device_connection("XPDR-A1")
+        self.assertEqual(response['status_code'], requests.codes.ok)
+        self.assertEqual(response['connection-status'], 'connected')
         time.sleep(10)
 
     def test_10_xpdr_portmapping_info(self):
-        response = test_utils.portmapping_request("XPDR-A1/node-info")
-        self.assertEqual(response.status_code, requests.codes.ok)
-        res = response.json()
+        response = test_utils_rfc8040.get_portmapping_node_info("XPDR-A1")
+        self.assertEqual(response['status_code'], requests.codes.ok)
         self.assertEqual(
-            {'node-info': {'node-type': 'xpdr',
-                           'node-ip-address': '1.2.3.4',
-                           'node-clli': 'NodeA',
-                           'openroadm-version': '2.2.1',
-                           'node-vendor': 'vendorA',
-                           'node-model': 'model2'}},
-            res)
+            {'node-type': 'xpdr',
+             'node-ip-address': '1.2.3.4',
+             'node-clli': 'NodeA',
+             'openroadm-version': '2.2.1',
+             'node-vendor': 'vendorA',
+             'node-model': 'model2'},
+            response['node-info'])
         time.sleep(3)
 
     def test_11_xpdr_portmapping_NETWORK1(self):
-        response = test_utils.portmapping_request("XPDR-A1/mapping/XPDR1-NETWORK1")
-        self.assertEqual(response.status_code, requests.codes.ok)
-        res = response.json()
+        response = test_utils_rfc8040.portmapping_request("XPDR-A1", "XPDR1-NETWORK1")
+        self.assertEqual(response['status_code'], requests.codes.ok)
         self.assertIn(
             {'supported-interface-capability': ['org-openroadm-port-types:if-OCH'],
              'supporting-port': '1', 'supporting-circuit-pack-name': '1/0/1-PLUG-NET',
@@ -148,12 +135,11 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
              'port-direction': 'bidirectional', 'connection-map-lcp': 'XPDR1-CLIENT1',
              'lcp-hash-val': 'AMkDwQ7xTmRI',
              'port-admin-state': 'InService', 'port-oper-state': 'InService'},
-            res['mapping'])
+            response['mapping'])
 
     def test_12_xpdr_portmapping_NETWORK2(self):
-        response = test_utils.portmapping_request("XPDR-A1/mapping/XPDR1-NETWORK2")
-        self.assertEqual(response.status_code, requests.codes.ok)
-        res = response.json()
+        response = test_utils_rfc8040.portmapping_request("XPDR-A1", "XPDR1-NETWORK2")
+        self.assertEqual(response['status_code'], requests.codes.ok)
         self.assertIn(
             {'supported-interface-capability': ['org-openroadm-port-types:if-OCH'],
              'supporting-port': '1', 'supporting-circuit-pack-name': '1/0/2-PLUG-NET',
@@ -161,12 +147,11 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
              'connection-map-lcp': 'XPDR1-CLIENT2', 'port-qual': 'xpdr-network',
              'lcp-hash-val': 'AMkDwQ7xTmRL',
              'port-admin-state': 'InService', 'port-oper-state': 'InService'},
-            res['mapping'])
+            response['mapping'])
 
     def test_13_xpdr_portmapping_CLIENT1(self):
-        response = test_utils.portmapping_request("XPDR-A1/mapping/XPDR1-CLIENT1")
-        self.assertEqual(response.status_code, requests.codes.ok)
-        res = response.json()
+        response = test_utils_rfc8040.portmapping_request("XPDR-A1", "XPDR1-CLIENT1")
+        self.assertEqual(response['status_code'], requests.codes.ok)
         self.assertIn(
             {'supported-interface-capability': ['org-openroadm-port-types:if-100GE'],
              'supporting-port': 'C1',
@@ -175,12 +160,11 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
              'connection-map-lcp': 'XPDR1-NETWORK1', 'port-qual': 'xpdr-client',
              'lcp-hash-val': 'AJUUr6I5fALj',
              'port-admin-state': 'InService', 'port-oper-state': 'InService'},
-            res['mapping'])
+            response['mapping'])
 
     def test_14_xpdr_portmapping_CLIENT2(self):
-        response = test_utils.portmapping_request("XPDR-A1/mapping/XPDR1-CLIENT2")
-        self.assertEqual(response.status_code, requests.codes.ok)
-        res = response.json()
+        response = test_utils_rfc8040.portmapping_request("XPDR-A1", "XPDR1-CLIENT2")
+        self.assertEqual(response['status_code'], requests.codes.ok)
         self.assertIn(
             {'supported-interface-capability': ['org-openroadm-port-types:if-100GE'],
              'supporting-port': 'C1',
@@ -189,51 +173,51 @@ class TransportPCEPortMappingTesting(unittest.TestCase):
              'connection-map-lcp': 'XPDR1-NETWORK2', 'port-qual': 'xpdr-client',
              'lcp-hash-val': 'AJUUr6I5fALg',
              'port-admin-state': 'InService', 'port-oper-state': 'InService'},
-            res['mapping'])
+            response['mapping'])
 
     def test_15_xpdr_device_disconnection(self):
-        response = test_utils.unmount_device("XPDR-A1")
-        self.assertEqual(response.status_code, requests.codes.ok, test_utils.CODE_SHOULD_BE_200)
+        response = test_utils_rfc8040.unmount_device("XPDR-A1")
+        self.assertIn(response.status_code, (requests.codes.ok, requests.codes.no_content))
 
     def test_16_xpdr_device_disconnected(self):
-        response = test_utils.get_netconf_oper_request("XPDR-A1")
-        self.assertEqual(response.status_code, requests.codes.conflict)
-        res = response.json()
+        response = test_utils_rfc8040.check_device_connection("XPDR-A1")
+        self.assertEqual(response['status_code'], requests.codes.conflict)
         self.assertIn(
-            {"error-type": "application", "error-tag": "data-missing",
-             "error-message": "Request could not be completed because the relevant data model content does not exist"},
-            res['errors']['error'])
+            {"error-tag": "data-missing",
+             "error-message": "Request could not be completed because the relevant data model content does not exist",
+             "error-type": "protocol"},
+            response['connection-status'])
 
     def test_17_xpdr_device_not_connected(self):
-        response = test_utils.portmapping_request("XPDR-A1")
-        self.assertEqual(response.status_code, requests.codes.conflict)
-        res = response.json()
+        response = test_utils_rfc8040.get_portmapping_node_info("XPDR-A1")
+        self.assertEqual(response['status_code'], requests.codes.conflict)
         self.assertIn(
-            {"error-type": "application", "error-tag": "data-missing",
-             "error-message": "Request could not be completed because the relevant data model content does not exist"},
-            res['errors']['error'])
+            {"error-tag": "data-missing",
+             "error-message": "Request could not be completed because the relevant data model content does not exist",
+             "error-type": "protocol"},
+            response['node-info'])
 
     def test_18_rdm_device_disconnection(self):
-        response = test_utils.unmount_device("ROADM-A1")
-        self.assertEqual(response.status_code, requests.codes.ok, test_utils.CODE_SHOULD_BE_200)
+        response = test_utils_rfc8040.unmount_device("ROADM-A1")
+        self.assertIn(response.status_code, (requests.codes.ok, requests.codes.no_content))
 
     def test_19_rdm_device_disconnected(self):
-        response = test_utils.get_netconf_oper_request("ROADM-A1")
-        self.assertEqual(response.status_code, requests.codes.conflict)
-        res = response.json()
+        response = test_utils_rfc8040.check_device_connection("ROADM-A1")
+        self.assertEqual(response['status_code'], requests.codes.conflict)
         self.assertIn(
-            {"error-type": "application", "error-tag": "data-missing",
-             "error-message": "Request could not be completed because the relevant data model content does not exist"},
-            res['errors']['error'])
+            {"error-tag": "data-missing",
+             "error-message": "Request could not be completed because the relevant data model content does not exist",
+             "error-type": "protocol"},
+            response['connection-status'])
 
     def test_20_rdm_device_not_connected(self):
-        response = test_utils.portmapping_request("ROADM-A1")
-        self.assertEqual(response.status_code, requests.codes.conflict)
-        res = response.json()
+        response = test_utils_rfc8040.get_portmapping_node_info("ROADM-A1")
+        self.assertEqual(response['status_code'], requests.codes.conflict)
         self.assertIn(
-            {"error-type": "application", "error-tag": "data-missing",
-             "error-message": "Request could not be completed because the relevant data model content does not exist"},
-            res['errors']['error'])
+            {"error-tag": "data-missing",
+             "error-message": "Request could not be completed because the relevant data model content does not exist",
+             "error-type": "protocol"},
+            response['node-info'])
 
 
 if __name__ == "__main__":
