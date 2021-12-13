130188e07926d4e1e7609d86f224b41f46ea9d63
guillaume.lambert <guillaume.lambert@orange.com>
2021-12-13 11:33:45 +0100

    Add and fix Phosphorus lighty.io support
    - fix tapiLink uninitialized variable
    - fix initPortMapping calls
    - modify PortMapping and PortMappingImpl to expose PortMappingVersion221
    - fix initOpenRoadmInterfaces calls by using it
    - add to pom the org.eclipse.jetty dependency removed from lighty-core
    - limit its scope to tests only
    
    JIRA: TRNSPRTPCE-579
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    Change-Id: I31e8a0a58d7f7e342a7119f364975eb0096f5e55

--- lighty/build_lighty_core.sh
+++ lighty/build_lighty_core.sh
@@ -5,6 +5,6 @@ cd ..
 git clone https://github.com/PantheonTechnologies/lighty-core.git
 cd lighty-core
 #git checkout master
-git checkout 14.0.x
+git checkout 15.1.x
 export JDK_JAVA_OPTIONS="--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED"
 mvn clean install -B -U -DskipTests -Dmaven.javadoc.skip=true -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
