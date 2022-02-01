86ece83b42cabac18bf33b7ff7ed546cc4b17f75
Gilles Thouenon <gilles.thouenon@orange.com>
2022-02-01 17:50:20 +0100

    Change log level in MappingUtilsImpl
    Change part of log messages from info to debug.
    
    JIRA: TRNSPRTPCE-570
    Signed-off-by: Gilles Thouenon <gilles.thouenon@orange.com>
    Change-Id: Ia93d80cdd738a87353dfa39ca357935581f85bd8

--- common/src/main/java/org/opendaylight/transportpce/common/mapping/MappingUtilsImpl.java
+++ common/src/main/java/org/opendaylight/transportpce/common/mapping/MappingUtilsImpl.java
@@ -118,7 +118,7 @@ public class MappingUtilsImpl implements MappingUtils {
             Optional<Nodes> nodePortMapObject = readTx.read(LogicalDatastoreType.CONFIGURATION, nodePortMappingIID)
                     .get();
             if (nodePortMapObject.isPresent()) {
-                LOG.info("Found node {}", nodeId);
+                LOG.debug("Found node {}", nodeId);
                 Nodes node = nodePortMapObject.get();
                 mcCapabilities.addAll(node.nonnullMcCapabilities().values());
             }
@@ -128,7 +128,7 @@ public class MappingUtilsImpl implements MappingUtils {
             LOG.error("Request interrupted for node {} interrupted", nodeId, e);
             Thread.currentThread().interrupt();
         }
-        LOG.info("Capabilitities for node {}: {}", nodeId, mcCapabilities);
+        LOG.debug("Capabilitities for node {}: {}", nodeId, mcCapabilities);
         return mcCapabilities;
     }
 
