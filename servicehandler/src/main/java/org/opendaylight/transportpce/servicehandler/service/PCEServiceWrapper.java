8eda9f775f6ab5d4b22723928a47e8150066837e
Nathan Ellsworth <nathan@ellsworthzoo.com>
2022-02-14 17:16:19 -0600

    Adding additional kafka messages
    - Add kafka messages in various places.
    - Add kafka.properties file to configure kafka broker details.
    - Made import ordering and other changes to satisfy StyleChecker.
    - Add kafka messaging bypass when bootstrap.servers=SKIP in kafka.properties file.
    
    Change-Id: Idfa3cf857c637cc557499a68dae1182aaa1223c5
    Signed-off-by: Nathan Ellsworth <nathan@ellsworthzoo.com>

--- servicehandler/src/main/java/org/opendaylight/transportpce/servicehandler/service/PCEServiceWrapper.java
+++ servicehandler/src/main/java/org/opendaylight/transportpce/servicehandler/service/PCEServiceWrapper.java
@@ -15,6 +15,8 @@ import com.google.common.util.concurrent.MoreExecutors;
 import java.util.concurrent.Executors;
 import org.opendaylight.mdsal.binding.api.NotificationPublishService;
 import org.opendaylight.transportpce.common.ResponseCodes;
+import org.opendaylight.transportpce.common.kafka.KafkaPublisher;
+import org.opendaylight.transportpce.common.kafka.KafkaPublisherImpl;
 import org.opendaylight.transportpce.pce.service.PathComputationService;
 import org.opendaylight.transportpce.servicehandler.ModelMappingUtils;
 import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.pce.rev220118.CancelResourceReserveInput;
@@ -57,6 +59,7 @@ public class PCEServiceWrapper {
     private final NotificationPublishService notificationPublishService;
     private ServiceRpcResultSh notification = null;
     private final ListeningExecutorService executor;
+    private final KafkaPublisher kafkaPublisher = KafkaPublisherImpl.getPublisher();
 
     public PCEServiceWrapper(PathComputationService pathComputationService,
             NotificationPublishService notificationPublishService) {
@@ -109,6 +112,9 @@ public class PCEServiceWrapper {
             String serviceName, SdncRequestHeader sdncRequestHeader, ServiceEndpoint serviceAEnd,
             ServiceEndpoint serviceZEnd, ServiceNotificationTypes notifType, boolean reserveResource) {
         LOG.info("Calling path computation.");
+        //UTD kafkaPublisher
+        kafkaPublisher.publishNotification("service", this.getClass().getSimpleName(),
+                "Calling path computation...");
         notification = new ServiceRpcResultShBuilder().setNotificationType(notifType).setServiceName(serviceName)
                 .setStatus(RpcStatusEx.Pending)
                 .setStatusMessage("Service compliant, submitting PathComputation Request ...").build();
@@ -335,6 +341,9 @@ public class PCEServiceWrapper {
         @Override
         public void onFailure(Throwable arg0) {
             LOG.error("Path not calculated..");
+            // UTD
+            kafkaPublisher.publishNotification("service", this.getClass().getSimpleName(),
+                    "Path not calculated..");
             notification = new ServiceRpcResultShBuilder().setNotificationType(notifType)
                     .setServiceName(serviceName)
                     .setStatus(RpcStatusEx.Failed).setStatusMessage("PCR Request failed  : " + arg0.getMessage())
