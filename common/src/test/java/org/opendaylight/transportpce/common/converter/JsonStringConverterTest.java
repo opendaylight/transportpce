2c3562b559a961bddf78818f910fe93606c6f1c9
guillaume.lambert <guillaume.lambert@orange.com>
2022-02-24 17:41:10 +0100

    Refactor common JsonStringConverterTest
    JIRA: TRNSPRTPCE-513
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    Change-Id: Ica347d7e21aedad5fb7982b9a72b465e14b348b4

--- common/src/test/java/org/opendaylight/transportpce/common/converter/JsonStringConverterTest.java
+++ common/src/test/java/org/opendaylight/transportpce/common/converter/JsonStringConverterTest.java
@@ -19,32 +19,34 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import org.junit.Test;
 import org.opendaylight.transportpce.test.AbstractTest;
-import org.opendaylight.transportpce.test.converter.DataObjectConverter;
 import org.opendaylight.transportpce.test.converter.JSONDataObjectConverter;
 import org.opendaylight.yang.gen.v1.gnpy.gnpy.api.rev220221.Request;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
-import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.codec.gson.JSONCodecFactorySupplier;
 
 public class JsonStringConverterTest extends AbstractTest {
 
     @Test
     public void createJsonStringFromDataObjectTest() {
-        DataObjectConverter dataObjectConverter = JSONDataObjectConverter
-                .createWithDataStoreUtil(getDataStoreContextUtil());
         try (Reader reader = new FileReader("src/test/resources/gnpy_request.json", StandardCharsets.UTF_8)) {
-            NormalizedNode normalizedNode = dataObjectConverter
-                    .transformIntoNormalizedNode(reader).get();
-            Request gnpyRequest = (Request) getDataStoreContextUtil().getBindingDOMCodecServices()
-                    .fromNormalizedNode(YangInstanceIdentifier.of(Request.QNAME), normalizedNode).getValue();
-            JsonStringConverter<Request> gnpyJsonConverter = new JsonStringConverter<Request>(
-                    getDataStoreContextUtil().getBindingDOMCodecServices());
-            String jsonString = gnpyJsonConverter
-                    .createJsonStringFromDataObject(InstanceIdentifier.builder(Request.class).build(),
-                            gnpyRequest, JSONCodecFactorySupplier.DRAFT_LHOTKA_NETMOD_YANG_JSON_02);
-            assertEquals("Should be a valid request",
-                    Files.readString(Paths.get("src/test/resources/expected_string.json")), jsonString);
+            assertEquals(
+                "Should be a valid request",
+                Files.readString(Paths.get("src/test/resources/expected_string.json")),
+                new JsonStringConverter<Request>(getDataStoreContextUtil().getBindingDOMCodecServices())
+                    .createJsonStringFromDataObject(
+                        InstanceIdentifier.builder(Request.class).build(),
+                        //gnpyRequest
+                        (Request) getDataStoreContextUtil()
+                            .getBindingDOMCodecServices()
+                            .fromNormalizedNode(
+                                YangInstanceIdentifier.of(Request.QNAME),
+                                JSONDataObjectConverter
+                                    .createWithDataStoreUtil(getDataStoreContextUtil())
+                                    .transformIntoNormalizedNode(reader)
+                                    .get())
+                            .getValue(),
+                        JSONCodecFactorySupplier.DRAFT_LHOTKA_NETMOD_YANG_JSON_02));
         } catch (IOException e) {
             fail("Cannot load path description ");
         }
@@ -52,12 +54,12 @@ public class JsonStringConverterTest extends AbstractTest {
 
     @Test
     public void createDataObjectFromJsonStringTest() throws IOException {
-        String json = Files.readString(Paths.get("src/test/resources/expected_string.json"));
-        JsonStringConverter<Request> gnpyJsonCOnverter = new JsonStringConverter<Request>(
-                getDataStoreContextUtil().getBindingDOMCodecServices());
-        Request request = gnpyJsonCOnverter
-                .createDataObjectFromJsonString(YangInstanceIdentifier.of(Request.QNAME), json,
-                        JSONCodecFactorySupplier.DRAFT_LHOTKA_NETMOD_YANG_JSON_02);
-        assertNotNull("Should not be null", request);
+        assertNotNull(
+            "Should not be null",
+            new JsonStringConverter<Request>(getDataStoreContextUtil().getBindingDOMCodecServices())
+                .createDataObjectFromJsonString(
+                    YangInstanceIdentifier.of(Request.QNAME),
+                    Files.readString(Paths.get("src/test/resources/expected_string.json")),
+                    JSONCodecFactorySupplier.DRAFT_LHOTKA_NETMOD_YANG_JSON_02));
     }
 }
