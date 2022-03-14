a968ffe4cf922975792d085653d76d9b4394a974
Christophe Betoule <christophe.betoule@orange.com>
2022-03-14 11:17:54 +0100

    Update Servicehandler checks
    Update Tx/Rx service handler checks to be compliant with service models
    of openROADM MSA
    
    JIRA: TRNSPRTPCE-646
    Signed-off-by: Christophe Betoule <christophe.betoule@orange.com>
    Change-Id: I0f3f7f7c25fc4988bcf433382a47de5175768a5c

--- servicehandler/src/main/java/org/opendaylight/transportpce/servicehandler/validation/checks/ServicehandlerTxRxCheck.java
+++ servicehandler/src/main/java/org/opendaylight/transportpce/servicehandler/validation/checks/ServicehandlerTxRxCheck.java
@@ -10,10 +10,6 @@ package org.opendaylight.transportpce.servicehandler.validation.checks;
 
 import org.opendaylight.transportpce.servicehandler.ServiceEndpointType;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.common.service.types.rev190531.ServiceEndpoint;
-import org.opendaylight.yang.gen.v1.http.org.openroadm.common.service.types.rev190531.service.endpoint.RxDirection;
-import org.opendaylight.yang.gen.v1.http.org.openroadm.common.service.types.rev190531.service.endpoint.TxDirection;
-import org.opendaylight.yang.gen.v1.http.org.openroadm.common.service.types.rev190531.service.lgx.Lgx;
-import org.opendaylight.yang.gen.v1.http.org.openroadm.common.service.types.rev190531.service.port.Port;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.service.format.rev190531.ServiceFormat;
 
 /**
@@ -47,6 +43,10 @@ public final class ServicehandlerTxRxCheck {
             return SERVICE + endpointType + " is not set";
         }
 
+        public static String rateNull(ServiceEndpointType endpointType) {
+            return "Something wrong when accessing " + SERVICE + endpointType + " rate, format or clli";
+        }
+
         public static String rateNotSet(ServiceEndpointType endpointType) {
             return SERVICE + endpointType + " rate is not set";
         }
@@ -63,7 +63,6 @@ public final class ServicehandlerTxRxCheck {
         }
     }
 
-
     /**
      * Check if a String is not null and not equal to ''.
      *
@@ -75,85 +74,6 @@ public final class ServicehandlerTxRxCheck {
         return (value != null && !value.isEmpty());
     }
 
-    /**
-     * check if Port info is compliant.
-     *
-     * @param port
-     *            port info
-     * @return true if String ok false if not
-     */
-    @SuppressWarnings("java:S1067")
-    //sonar issue Reduce the number of conditional operators (4) used in the expression (maximum allowed 3)
-    //won't be fixed because of functional checks needed
-    public static boolean checkPort(Port port) {
-        if (port == null) {
-            return false;
-        }
-        String portDeviceName = port.getPortDeviceName();
-        String portType = port.getPortType();
-        String portName = port.getPortName();
-        String portRack = port.getPortRack();
-        String portShelf = port.getPortShelf();
-
-        return checkString(portDeviceName)
-                && checkString(portType)
-                && checkString(portName)
-                && checkString(portRack)
-                && checkString(portShelf);
-    }
-
-    /**
-     * Check if lgx info is compliant.
-     *
-     * @param lgx
-     *            lgx info
-     * @return true if String ok false if not
-     */
-    public static boolean checkLgx(Lgx lgx) {
-        if (lgx == null) {
-            return false;
-        }
-        String lgxDeviceName = lgx.getLgxDeviceName();
-        String lgxPortName = lgx.getLgxPortName();
-        String lgxPortRack = lgx.getLgxPortRack();
-        String lgxPortShelf = lgx.getLgxPortShelf();
-        return checkString(lgxDeviceName)
-                && checkString(lgxPortName)
-                && checkString(lgxPortRack)
-                && checkString(lgxPortShelf);
-    }
-
-    /**
-     * Check if Tx/Rx Direction complaincy info.
-     *
-     * @param txDirection
-     *            TxDirection
-     * @param rxDirection
-     *            RxDirection
-     *
-     * @return <code>true</code> if check is ok <code>false</code> else
-     */
-    public static ComplianceCheckResult checkTxOrRxInfo(TxDirection txDirection, RxDirection rxDirection) {
-        if (txDirection == null) {
-            return new ComplianceCheckResult(false, LogMessages.TXDIR_NOT_SET);
-        }
-        if (rxDirection == null) {
-            return new ComplianceCheckResult(false, LogMessages.RXDIR_NOT_SET);
-        }
-        if (!checkPort(txDirection.getPort())) {
-            return new ComplianceCheckResult(false, LogMessages.TXDIR_PORT_NOT_SET);
-        }
-        if (!checkLgx(txDirection.getLgx())) {
-            return new ComplianceCheckResult(false, LogMessages.TXDIR_LGX_NOT_SET);
-        }
-        if (!checkPort(rxDirection.getPort())) {
-            return new ComplianceCheckResult(false, LogMessages.RXDIR_PORT_NOT_SET);
-        }
-        if (!checkLgx(rxDirection.getLgx())) {
-            return new ComplianceCheckResult(false, LogMessages.RXDIR_LGX_NOT_SET);
-        }
-        return new ComplianceCheckResult(true, "");
-    }
 
     /**
      * Check Compliance of Service TxRx info.
@@ -182,12 +102,6 @@ public final class ServicehandlerTxRxCheck {
         }
         if (!checkString(clli)) {
             return new ComplianceCheckResult(false, LogMessages.clliNotSet(endpointType));
-        }
-
-        ComplianceCheckResult complianceCheckResult
-                = checkTxOrRxInfo(serviceEnd.getTxDirection(), serviceEnd.getRxDirection());
-        if (!complianceCheckResult.hasPassed()) {
-            return new ComplianceCheckResult(false, complianceCheckResult.getMessage());
         }
 
         return new ComplianceCheckResult(true, "");
