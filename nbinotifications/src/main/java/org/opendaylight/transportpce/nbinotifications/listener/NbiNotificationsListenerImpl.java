0e994f8a4dfc46acf6fc832cbcb7a7b507172ad5
Javier Errea <errea@eurecom.fr>
2022-04-06 15:32:30 +0200

    T-API notification JUnit test
    JIRA: TRNSPRTPCE-649
    Signed-off-by: errea <errea@eurecom.fr>
    Change-Id: Icc1bd2a4815e3d6df6ab8c22bcba2466d3b1ee49

--- nbinotifications/src/main/java/org/opendaylight/transportpce/nbinotifications/listener/NbiNotificationsListenerImpl.java
+++ nbinotifications/src/main/java/org/opendaylight/transportpce/nbinotifications/listener/NbiNotificationsListenerImpl.java
@@ -7,9 +7,9 @@
  */
 package org.opendaylight.transportpce.nbinotifications.listener;
 
-import java.util.HashMap;
 import java.util.Map;
 import org.opendaylight.transportpce.nbinotifications.producer.Publisher;
+import org.opendaylight.transportpce.nbinotifications.utils.NbiNotificationsUtils;
 import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.NbiNotificationsListener;
 import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.NotificationAlarmService;
 import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.NotificationAlarmServiceBuilder;
@@ -20,20 +20,6 @@ import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.NotificationTapi
 import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.PublishNotificationAlarmService;
 import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.PublishNotificationProcessService;
 import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.PublishTapiNotificationService;
-import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.notification.tapi.service.AdditionalInfo;
-import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.notification.tapi.service.AdditionalInfoBuilder;
-import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.notification.tapi.service.AdditionalInfoKey;
-import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.notification.tapi.service.AlarmInfoBuilder;
-import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.notification.tapi.service.ChangedAttributes;
-import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.notification.tapi.service.ChangedAttributesBuilder;
-import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.notification.tapi.service.ChangedAttributesKey;
-import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.notification.tapi.service.TargetObjectName;
-import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.notification.tapi.service.TargetObjectNameBuilder;
-import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.notification.tapi.service.TargetObjectNameKey;
-import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.notification.tapi.service.TcaInfoBuilder;
-import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev181210.global._class.Name;
-import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev181210.global._class.NameBuilder;
-import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev181210.global._class.NameKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -98,64 +84,8 @@ public class NbiNotificationsListenerImpl implements NbiNotificationsListener {
             return;
         }
         Publisher<NotificationTapiService> publisher = tapiPublisherMap.get(topic);
-        publisher.sendEvent(new NotificationTapiServiceBuilder(transformTapiNotification(notification))
-                .build(), topic);
-    }
-
-    private NotificationTapiService transformTapiNotification(PublishTapiNotificationService notification) {
-        Map<AdditionalInfoKey, AdditionalInfo> addInfoMap = new HashMap<>();
-        if (notification.getAdditionalInfo() != null) {
-            for (org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.notification.rev181210.notification.AdditionalInfo
-                addInfo:notification.getAdditionalInfo().values()) {
-                AdditionalInfo transAddInfo = new AdditionalInfoBuilder(addInfo).build();
-                addInfoMap.put(transAddInfo.key(), transAddInfo);
-            }
-        }
-        Map<ChangedAttributesKey, ChangedAttributes> changedAttMap = new HashMap<>();
-        if (notification.getChangedAttributes() != null) {
-            for (org.opendaylight.yang.gen.v1
-                .urn.onf.otcc.yang.tapi.notification.rev181210.notification.ChangedAttributes
-                    changedAtt:notification.getChangedAttributes().values()) {
-                ChangedAttributes transChangedAtt = new ChangedAttributesBuilder(changedAtt).build();
-                changedAttMap.put(transChangedAtt.key(), transChangedAtt);
-            }
-        }
-        Map<NameKey, Name> nameMap = new HashMap<>();
-        if (notification.getName() != null) {
-            for (Name name:notification.getName().values()) {
-                Name transName = new NameBuilder(name).build();
-                nameMap.put(transName.key(), transName);
-            }
-        }
-        Map<TargetObjectNameKey, TargetObjectName> targetObjNameMap = new HashMap<>();
-        if (notification.getTargetObjectName() != null) {
-            for (org.opendaylight.yang.gen.v1
-                .urn.onf.otcc.yang.tapi.notification.rev181210.notification.TargetObjectName
-                    targetObjectName:notification.getTargetObjectName().values()) {
-                TargetObjectName transTargetObjName = new TargetObjectNameBuilder(targetObjectName).build();
-                targetObjNameMap.put(transTargetObjName.key(), transTargetObjName);
-            }
-        }
-        LOG.info("Notification uuid = {}", notification.getUuid());
-        return new NotificationTapiServiceBuilder()
-            .setAlarmInfo(notification.getAlarmInfo() == null ? null
-                : new AlarmInfoBuilder(notification.getAlarmInfo()).build())
-            .setAdditionalText(notification.getAdditionalText())
-            .setAdditionalInfo(addInfoMap)
-            .setNotificationType(notification.getNotificationType())
-            .setChangedAttributes(changedAttMap)
-            .setEventTimeStamp(notification.getEventTimeStamp())
-            .setLayerProtocolName(notification.getLayerProtocolName())
-            .setName(nameMap)
-            .setSequenceNumber(notification.getSequenceNumber())
-            .setSourceIndicator(notification.getSourceIndicator())
-            .setTargetObjectIdentifier(notification.getTargetObjectIdentifier())
-            .setTargetObjectName(targetObjNameMap)
-            .setTargetObjectType(notification.getTargetObjectType())
-            .setTcaInfo(notification.getTcaInfo() == null ? null
-                : new TcaInfoBuilder(notification.getTcaInfo()).build())
-            .setUuid(notification.getUuid())
-            .build();
+        publisher.sendEvent(new NotificationTapiServiceBuilder(
+                NbiNotificationsUtils.transformTapiNotification(notification)).build(), topic);
     }
 
     public void setPublishersServiceMap(Map<String, Publisher<NotificationProcessService>> publishersServiceMap) {
@@ -168,5 +98,9 @@ public class NbiNotificationsListenerImpl implements NbiNotificationsListener {
 
     public void setTapiPublishersMap(Map<String, Publisher<NotificationTapiService>> tapiPublishersMap) {
         this.tapiPublisherMap = tapiPublishersMap;
+    }
+
+    public Publisher<NotificationTapiService> getTapiPublisherFromTopic(String topic) {
+        return this.tapiPublisherMap.get(topic);
     }
 }
--- nbinotifications/src/test/java/org/opendaylight/transportpce/nbinotifications/consumer/SubscriberTest.java
+++ nbinotifications/src/test/java/org/opendaylight/transportpce/nbinotifications/consumer/SubscriberTest.java
@@ -24,23 +24,29 @@ import org.opendaylight.transportpce.nbinotifications.utils.NotificationServiceD
 import org.opendaylight.transportpce.test.AbstractTest;
 import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.NotificationAlarmService;
 import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.NotificationProcessService;
+import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.NotificationTapiService;
 import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.get.notifications.alarm.service.output.NotificationsAlarmService;
 import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.get.notifications.process.service.output.NotificationsProcessService;
+import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.notification.rev181210.get.notification.list.output.Notification;
 
 public class SubscriberTest extends AbstractTest {
     private static final String TOPIC = "topic";
     private static final int PARTITION = 0;
     private MockConsumer<String, NotificationsProcessService> mockConsumer;
     private MockConsumer<String, NotificationsAlarmService> mockConsumerAlarm;
+    private MockConsumer<String, Notification> mockConsumerTapi;
     private Subscriber<NotificationProcessService, NotificationsProcessService> subscriberService;
     private Subscriber<NotificationAlarmService, NotificationsAlarmService> subscriberAlarmService;
+    private Subscriber<NotificationTapiService, Notification> subscriberTapiService;
 
     @Before
     public void setUp() {
         mockConsumer = new MockConsumer<>(OffsetResetStrategy.EARLIEST);
         mockConsumerAlarm = new MockConsumer<>(OffsetResetStrategy.EARLIEST);
+        mockConsumerTapi = new MockConsumer<>(OffsetResetStrategy.EARLIEST);
         subscriberService = new Subscriber<>(mockConsumer);
         subscriberAlarmService = new Subscriber<>(mockConsumerAlarm);
+        subscriberTapiService = new Subscriber<>(mockConsumerTapi);
     }
 
     @Test
@@ -81,5 +87,25 @@ public class SubscriberTest extends AbstractTest {
                 NotificationsAlarmService.QNAME);
         assertEquals("There should be 1 record", 1, result.size());
         assertTrue("Consumer should be closed", mockConsumerAlarm.closed());
+    }
+
+    @Test
+    public void subscribeTapiAlarmShouldBeSuccessful() {
+        // from https://www.baeldung.com/kafka-mockconsumer
+        ConsumerRecord<String, Notification> record = new ConsumerRecord<>(
+            TOPIC, PARTITION, 0L, "key", NotificationServiceDataUtils.buildReceivedTapiAlarmEvent());
+        mockConsumerTapi.schedulePollTask(() -> {
+            mockConsumerTapi.rebalance(Collections.singletonList(new TopicPartition(TOPIC, PARTITION)));
+            mockConsumerTapi.addRecord(record);
+        });
+
+        Map<TopicPartition, Long> startOffsets = new HashMap<>();
+        TopicPartition tp = new TopicPartition(TOPIC, PARTITION);
+        startOffsets.put(tp, 0L);
+        mockConsumerTapi.updateBeginningOffsets(startOffsets);
+        List<Notification> result = subscriberTapiService.subscribe(TOPIC,
+            NotificationTapiService.QNAME);
+        assertEquals("There should be 1 record", 1, result.size());
+        assertTrue("Consumer should be closed", mockConsumerTapi.closed());
     }
 }
