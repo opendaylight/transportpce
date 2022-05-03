7ab1afc1374c8de73cbca6e64f3006ee7fa2fe25
Gilles Thouenon <gilles.thouenon@orange.com>
2022-05-03 17:41:13 +0200

    Bump lighty to P-SR2
    JIRA: TRNSPRTPCE-579
    Signed-off-by: Gilles Thouenon <gilles.thouenon@orange.com>
    Change-Id: I2267dbbe0fb90b1c43574c829dcbbf9bda16a55c

--- lighty/build.sh
+++ lighty/build.sh
@@ -2,6 +2,6 @@
 
 cd $(dirname "$0")
 export JDK_JAVA_OPTIONS="--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED"
-./build_lighty_core.sh
+#./build_lighty_core.sh
 mvn clean install -B -U -Dmaven.javadoc.skip=true -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
 unzip -q target/tpce-bin.zip -d target
