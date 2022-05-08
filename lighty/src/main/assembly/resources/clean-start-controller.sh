2dbd8a17c1546e3a123c0c16765f97599100e10c
Gilles Thouenon <gilles.thouenon@orange.com>
2022-05-08 18:45:28 +0200

    Adapt lighty to run with optional karaf features
    - put tapi as an optional feature to install in lighty, as it is the
      case with karaf
    - add use of two new environment variables (INSTALL_NBINOTIFICATIONS and
      INSTALL_TAPI) to start lighty with nbinotifications and tapi features
    - adapt lighty launcher shell script to take into acount these
      environment variables, as well as the OLM_TIMER ones
    - adapt tox consequently
    
    JIRA: TRNSPRTPCE-663
    Signed-off-by: Gilles Thouenon <gilles.thouenon@orange.com>
    Change-Id: I85c9a27f38de01343e4f13ce548440a9171502b1

--- lighty/src/main/assembly/resources/clean-start-controller.sh
+++ lighty/src/main/assembly/resources/clean-start-controller.sh
@@ -27,10 +27,26 @@ if [ -z "$USE_ODL_ALT_AKKA_MGT_PORT" ]; then
 else
     AKKA_MGT_PORT=$USE_ODL_ALT_AKKA_MGT_PORT
 fi
+if [ -z "$OLM_TIMER1" ]; then
+    olmtimer1=3000
+else
+    olmtimer1=$OLM_TIMER1
+fi
+if [ -z "$OLM_TIMER2" ]; then
+    olmtimer2=2000
+else
+    olmtimer2=$OLM_TIMER2
+fi
+if [ -n "$INSTALL_NBINOTIFICATIONS" ]  && [ "$INSTALL_NBINOTIFICATIONS" = "True" ]; then
+    install_nbinotifications="-nbinotification"
+fi
+if [ -n "$INSTALL_TAPI" ]; then
+    install_tapi="-tapi"
+fi
 
 # generate appropriate configuration files
 cat config_template.json | sed -e "s/ODL_RESTCONF_PORT/$RESTCONF_PORT/" -e "s/ODL_WEBSOCKET_PORT/$WEBSOCKET_PORT/" >config.json
 cat akka-default_template.conf | sed -e "s/ODL_AKKA_PORT/$AKKA_PORT/" -e "s/ODL_AKKA_MGT_PORT/$AKKA_MGT_PORT/" >singlenode/akka-default.conf
 
 #start controller
-java -ms128m -mx512m -XX:MaxMetaspaceSize=128m -jar tpce.jar -restconf config.json -olmtimer1 3000 -olmtimer2 2000
+java -ms128m -mx512m -XX:MaxMetaspaceSize=128m -jar tpce.jar -restconf config.json $install_nbinotifications $install_tapi -olmtimer1 $olmtimer1 -olmtimer2 $olmtimer2
