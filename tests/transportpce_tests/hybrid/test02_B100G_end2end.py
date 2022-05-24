eb9c879bf8f2a332b212578b6d1546e9c13a9464
Balagangadhar Bathula <bb4341@att.com>
2022-05-24 13:03:39 -0400

    Functional tests for 100 OFEC 31.6 GBaud
    - Update the configuration files with the new
      capabilities for 100G OFEC.
    - Add functional tests to render the 100G 31.6 Gbaud
      (50 GHz) interfaces.
    
    JIRA: TRNSPRTPCE-637
    
    Change-Id: I5e0be59be3a2dcc41f276078020ed9e2b66b588a
    Signed-off-by: Balagangadhar Bathula <bb4341@att.com>

--- tests/transportpce_tests/hybrid/test02_B100G_end2end.py
+++ tests/transportpce_tests/hybrid/test02_B100G_end2end.py
@@ -239,7 +239,7 @@ class TransportPCEtesting(unittest.TestCase):
         self.assertEqual(response.status_code, requests.codes.ok)
         res = response.json()
         nbNode = len(res['network'][0]['node'])
-        self.assertEqual(nbNode, 4, 'There should be 4 nodes')
+        self.assertEqual(nbNode, 6, 'There should be 6 nodes')
         self.assertNotIn('ietf-network-topology:link', res['network'][0])
 
     def test_12_create_OTUC4_service(self):
