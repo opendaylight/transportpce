4cac95cc4498d1f2cfbd7180bb8b434088e9f110
guillaume.lambert <guillaume.lambert@orange.com>
2022-10-27 20:13:29 +0200

    Force additionnal Kafka server use
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    Change-Id: I7162d970f2526672992d1db990be9ff2196228e4

--- common/src/main/java/org/opendaylight/transportpce/common/kafka/KafkaPublisherImpl.java
+++ common/src/main/java/org/opendaylight/transportpce/common/kafka/KafkaPublisherImpl.java
@@ -37,7 +37,14 @@ public class KafkaPublisherImpl implements KafkaPublisher {
         } catch (IOException e) {
             LOG.error("Kafka property file '{}' was not found in the classpath", propertyFileName, e);
         }
-
+        props.put("bootstrap.servers", "127.0.0.1:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         Thread.currentThread().setContextClassLoader(null);
         if (props.getProperty("bootstrap.servers").equals("SKIP")) {
             skipKafka = true;
