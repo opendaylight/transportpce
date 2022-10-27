86c0302b4bbd16808fa2d79706d40c539dc99836
nathan.ellsworth <nathan@ellsworthzoo.com>
2022-10-27 02:26:49 -0500

    Adding additional kafka messages
    - Add kafka messages in various places
    - Add kafka.properties file to configure kafka broker details
    - Add kafka messaging bypass when bootstrap.servers=SKIP
    - Resolved some merge conflicts
    - Added null check to all publishNotification calls
    
    Change-Id: Idfa3cf857c637cc557499a68dae1182aaa1223c5
    Signed-off-by: Nathan Ellsworth <nathan@ellsworthzoo.com>

---
+++ common/src/main/java/org/opendaylight/transportpce/common/kafka/KafkaPublisherImpl.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright © 2017 ATT and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.common.kafka;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KafkaPublisherImpl implements KafkaPublisher {
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaPublisherImpl.class);
+    private static KafkaProducer<String, String> producer;
+    private static volatile KafkaPublisher sInstance = null;
+    private static boolean skipKafka = false;
+
+    static {
+        Properties props = new Properties();
+        String propertyFileName = "kafka.properties";
+        InputStream inputStream = KafkaPublisherImpl.class.getClassLoader().getResourceAsStream(propertyFileName);
+        LOG.info("Trying to read kafka properties file {}", propertyFileName);
+
+        try {
+            if (inputStream != null) {
+                props.load(inputStream);
+            } else {
+                LOG.warn("Kafka property file '{}' is empty", propertyFileName);
+            }
+        } catch (IOException e) {
+            LOG.error("Kafka property file '{}' was not found in the classpath", propertyFileName, e);
+        }
+
+        Thread.currentThread().setContextClassLoader(null);
+        if (props.getProperty("bootstrap.servers").equals("SKIP")) {
+            skipKafka = true;
+            producer = null;
+            LOG.info("Bypssing Kafka messages since bootstrap.servers = SKIP");
+        } else {
+            producer = new KafkaProducer<>(props);
+            LOG.info("Instantiate Kafka publisher with properties {}", props);
+        }
+    }
+
+    //singleton Kafkapublisher for other services usage
+    //return null if we are skipping Kafka messaging
+    public static KafkaPublisher getPublisher() {
+        if (!skipKafka && sInstance == null) {
+            synchronized (KafkaPublisher.class) {
+                if (sInstance == null) {
+                    sInstance = new KafkaPublisherImpl();
+                }
+            }
+        }
+        return sInstance;
+    }
+
+    public String publishNotification(String topicName, String key, String message) {
+        if (!skipKafka) {
+            LOG.info("kakfaPublisher called: Topic {} Key {} Message {}", topicName, key, message);
+            if (producer != null) {
+                producer.send(new ProducerRecord<>(topicName, key, message));
+            }
+        } else {
+            LOG.info("kakfaPublisher SKIPPED: Topic {} Key {} Message {}", topicName, key, message);
+        }
+        return "success";
+    }
+
+}
