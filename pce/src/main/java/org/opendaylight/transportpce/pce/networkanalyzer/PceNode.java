f164051716fdf76eb620bef8a282863e01792d36
orenais <olivier.renais@orange.com>
2023-01-09 11:30:00 +0100

    Initial T-API 2.4 PCE link & Nodes Classes
    JIRA: TRNSPRTPCE-724
    Signed-off-by: orenais <olivier.renais@orange.com>
    Change-Id: Id2244c4f63c3dd24284ef69cbd6e5398a172937e

--- pce/src/main/java/org/opendaylight/transportpce/pce/networkanalyzer/PceNode.java
+++ pce/src/main/java/org/opendaylight/transportpce/pce/networkanalyzer/PceNode.java
@@ -17,6 +17,9 @@ import org.opendaylight.yang.gen.v1.http.org.openroadm.equipment.states.types.re
 import org.opendaylight.yang.gen.v1.http.org.openroadm.network.topology.rev230526.networks.network.node.termination.point.XpdrNetworkAttributes;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.network.types.rev230526.OpenroadmNodeType;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.rev180226.NodeId;
+import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev221121.AdministrativeState;
+import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev221121.OperationalState;
+import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev221121.Uuid;
 import org.opendaylight.yangtools.yang.common.Uint16;
 
 public interface PceNode {
@@ -38,7 +41,11 @@ public interface PceNode {
 
     AdminStates getAdminStates();
 
+    AdministrativeState getAdminState();
+
     State getState();
+
+    OperationalState getOperationalState();
 
     NodeId getNodeId();
 
@@ -48,9 +55,15 @@ public interface PceNode {
 
     String getXponderOperationalMode(XpdrNetworkAttributes tp);
 
+    String getXpdrOperationalMode(Uuid nepUuid);
+
+    Uuid getNodeUuid();
+
     Map<String, List<Uint16>> getAvailableTribPorts();
 
     Map<String, List<Uint16>> getAvailableTribSlots();
+
+    List<BasePceNep> getListOfNep();
 
     /**
      * Get the version of node.
