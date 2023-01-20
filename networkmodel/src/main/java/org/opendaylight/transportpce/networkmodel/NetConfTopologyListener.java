78fcc168a499982cc819312b12f36749fa43a8fd
Gilles Thouenon <gilles.thouenon@orange.com>
2023-01-20 09:37:24 +0100

    Bump upstream dependencies to Argon
    Adopt:
    - odlparent-12.0.2
    - yangtools-10.0.3
    - mdsal-11.0.6
    - netconf-5.0.2
    - transportpce-models-18.0.0-SNAPSHOT
    
    Also
    - add netconf-topology dependency and adapt some netconf imports
    - adapt GNPy UT
    - adapt karaf message in functional tests
    
    TODO: need to upgrade lighty to 18.0.0 when available
    
    JIRA: TRNSPRTPCE-719
    Signed-off-by: Gilles Thouenon <gilles.thouenon@orange.com>
    Change-Id: Icecd1b21a25c1fc7f66b31f090a4a614ba741c6f

--- networkmodel/src/main/java/org/opendaylight/transportpce/networkmodel/NetConfTopologyListener.java
+++ networkmodel/src/main/java/org/opendaylight/transportpce/networkmodel/NetConfTopologyListener.java
@@ -38,9 +38,9 @@ import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.netconf.notification.
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.netmod.notification.rev080714.Netconf;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.netmod.notification.rev080714.netconf.Streams;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.netmod.notification.rev080714.netconf.streams.Stream;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.NetconfNode;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.NetconfNodeConnectionStatus.ConnectionStatus;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.netconf.node.connection.status.available.capabilities.AvailableCapability;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.device.rev221225.ConnectionOper.ConnectionStatus;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.device.rev221225.connection.oper.available.capabilities.AvailableCapability;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev221225.NetconfNode;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.common.RpcResult;
