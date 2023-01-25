0fe18efd47300e7f13031f86dfaa9d16333401d1
guillaume.lambert <guillaume.lambert@orange.com>
2023-01-25 11:27:49 +0100

    Remove websocket config from post-install script
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    Change-Id: Ib8fe1b5c8bd6d990f14310782405fc79e691d84d

--- karaf/src/main/assembly/ressources/post_install_for_tests.sh
+++ karaf/src/main/assembly/ressources/post_install_for_tests.sh
@@ -7,7 +7,6 @@ sed 's/8181/ODL_RESTCONF_PORT/' ../etc/org.ops4j.pax.web.cfg > org.ops4j.pax.web
 sed 's/8181/ODL_RESTCONF_PORT/' ../etc/jetty.xml > jetty_template.xml
 sed 's/8101/ODL_SHELL_PORT/' ../etc/org.apache.karaf.shell.cfg > org.apache.karaf.shell._template.cfg
 sed -e 's/1099/ODL_RMI_REGISTRY_PORT/' -e 's/44444/ODL_RMI_SERVER_PORT/' ../etc/org.apache.karaf.management.cfg > org.apache.karaf.management._template.cfg
-sed 's/^[#|]websocket-port=8185/websocket-port=ODL_WEBSOCKET_PORT/' ../system/org/opendaylight/netconf/sal-rest-connector-config/[0-9.]*/sal-rest-connector-config-[0-9.]*-restconf.cfg >org.opendaylight.restconf._template.cfg
 
 sed -i'_' -e '1 a\
 \
