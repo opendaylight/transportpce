253651ee685c07d342c96adb18fee852b5e02b67
guillaume.lambert <guillaume.lambert@orange.com>
2023-01-26 20:16:52 +0100

    Add test_utils ODL startup opt-out support
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    Change-Id: I28b94e6ff6cf94c4bc9643992aef6c0ff68ba41a

--- tests/transportpce_tests/tapi/test02_full_topology.py
+++ tests/transportpce_tests/tapi/test02_full_topology.py
@@ -104,7 +104,7 @@ class TransportPCEtesting(unittest.TestCase):
         os.environ['JAVA_MAX_MEM'] = '4096M'
         cls.processes = test_utils.start_tpce()
         # TAPI feature is not installed by default in Karaf
-        if "USE_LIGHTY" not in os.environ or os.environ['USE_LIGHTY'] != 'True':
+        if "NO_ODL_STARTUP" not in os.environ or "USE_LIGHTY" not in os.environ or os.environ['USE_LIGHTY'] != 'True':
             print("installing tapi feature...")
             result = test_utils.install_karaf_feature("odl-transportpce-tapi")
             if result.returncode != 0:
