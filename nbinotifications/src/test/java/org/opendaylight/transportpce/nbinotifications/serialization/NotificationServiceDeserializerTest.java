6198e5c160336d6fa87b51e8c731692cbc6d5ffb
Gilles Thouenon <gilles.thouenon@orange.com>
2023-02-25 21:19:13 +0100

    Migrate nbinotifications module to JUnit5
    JIRA: TRNSPRTPCE-730
    Signed-off-by: Gilles Thouenon <gilles.thouenon@orange.com>
    Change-Id: Id6ec23473646a899bb28e152ba941109aad16cde

--- nbinotifications/src/test/java/org/opendaylight/transportpce/nbinotifications/serialization/NotificationServiceDeserializerTest.java
+++ nbinotifications/src/test/java/org/opendaylight/transportpce/nbinotifications/serialization/NotificationServiceDeserializerTest.java
@@ -7,13 +7,14 @@
  */
 package org.opendaylight.transportpce.nbinotifications.serialization;
 
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Map;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.opendaylight.transportpce.common.converter.JsonStringConverter;
 import org.opendaylight.transportpce.test.AbstractTest;
 import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.NotificationProcessService;
@@ -22,7 +23,7 @@ import org.opendaylight.yang.gen.v1.nbi.notifications.rev211013.get.notification
 public class NotificationServiceDeserializerTest extends AbstractTest {
 
     @Test
-    public void deserializeTest() throws IOException {
+    void deserializeTest() throws IOException {
         JsonStringConverter<NotificationProcessService> converter = new JsonStringConverter<>(
                 getDataStoreContextUtil().getBindingDOMCodecServices());
         NotificationServiceDeserializer deserializer = new NotificationServiceDeserializer();
@@ -31,6 +32,6 @@ public class NotificationServiceDeserializerTest extends AbstractTest {
         NotificationsProcessService readEvent = deserializer.deserialize("Test",
                 Files.readAllBytes(Paths.get("src/test/resources/event.json")));
         deserializer.close();
-        assertEquals("Service name should be service1", "service1", readEvent.getServiceName());
+        assertEquals("service1", readEvent.getServiceName(), "Service name should be service1");
     }
 }
