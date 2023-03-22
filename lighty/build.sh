90ba5e17d4b9f67089412604da311037f21a15ce
guillaume.lambert <guillaume.lambert@orange.com>
2023-03-22 14:24:10 +0100

    Quiet and check lighty.io build in the CI
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    Change-Id: I06444f05e01004537405552c313aeb47c1709b5b

--- lighty/build.sh
+++ lighty/build.sh
@@ -3,5 +3,5 @@
 cd $(dirname "$0")
 export JDK_JAVA_OPTIONS="--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED"
 ./build_lighty_core.sh
-mvn clean install -B -U -Dmaven.javadoc.skip=true -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -s ../tests/odl_settings.xml
+mvn clean install -B -U -q -Dmaven.javadoc.skip=true -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -s ../tests/odl_settings.xml
 unzip -q target/tpce-bin.zip -d target
