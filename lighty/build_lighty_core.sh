f09118caf224c2ac9f3d202eaa55dd83f698ef54
Gilles Thouenon <gilles.thouenon@orange.com>
2023-04-04 08:34:17 +0200

    Bump lighty-core to 18.0.0 released version
    Signed-off-by: Gilles Thouenon <gilles.thouenon@orange.com>
    Change-Id: I6e584618f0d25bc34f4c2364bb2c3d39454bc765

--- lighty/build_lighty_core.sh
+++ lighty/build_lighty_core.sh
@@ -4,7 +4,7 @@ cd $(dirname "$0")
 cd ..
 git clone https://github.com/PantheonTechnologies/lighty-core.git
 cd lighty-core
-git checkout main
-#git checkout 18.0.x
+#git checkout main
+git checkout 18.0.0
 export JDK_JAVA_OPTIONS="--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED"
 mvn clean install -B -U -q -DskipTests -Dmaven.javadoc.skip=true -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
