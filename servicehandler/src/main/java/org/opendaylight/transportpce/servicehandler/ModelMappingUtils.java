aab3f1157afeae5e97d011a5692c3cfaf071ba43
Robert Varga <robert.varga@pantheon.tech>
2023-07-05 23:03:17 +0200

    WIP: Bump upstream dependencies to Potassium GA
    Adopt:
    - odlparent-13.0.3
    - yangtools-11.0.0
    - mdsal-12.0.0
    - netconf-6.0.1-SNAPSHOT
    - transportpce-models-19.0.0-SNAPSHOT
    
    Also, adapt the expected serialized result for JsonStringConverterTest
    and SuppressFBWarnings annotations
    
    // FIXME: use netconf-6.0.1 when released
    
    Change-Id: Ic0c7f6de5fa16e778c0d3104ed27be76bb56378e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- servicehandler/src/main/java/org/opendaylight/transportpce/servicehandler/ModelMappingUtils.java
+++ servicehandler/src/main/java/org/opendaylight/transportpce/servicehandler/ModelMappingUtils.java
@@ -9,6 +9,7 @@ package org.opendaylight.transportpce.servicehandler;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.util.concurrent.ListenableFuture;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.HashMap;
 import java.util.Map;
 import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.pce.rev220808.PathComputationRequestOutput;
@@ -660,7 +661,7 @@ public final class ModelMappingUtils {
     }
 
 
-    @edu.umd.cs.findbugs.annotations.SuppressFBWarnings(
+    @SuppressFBWarnings(
         value = "PZLA_PREFER_ZERO_LENGTH_ARRAYS",
         justification = "not relevant to return and zero length array as we need real pos")
     public static int[] findTheLongestSubstring(String s1, String s2) {
