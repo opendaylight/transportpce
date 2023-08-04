c0f3c7d6a91eb1efea1d70d40068f6fd5d354c02
orenais <olivier.renais@orange.com>
2023-08-04 16:56:01 +0200

    Migration to TAPI 2.4 Step2
    _Migration of nbi-notification to T-API 2.4
    
    JIRA: TRNSPRTPCE:734, TRNSPRTPCE:735
    
    Signed-off-by: orenais <olivier.renais@orange.com>
    Change-Id: I868d82258cdb1c7b2ee3cb16653dbb89a5ca13cc

--- nbinotifications/src/test/java/org/opendaylight/transportpce/nbinotifications/serialization/NotificationTapiServiceDeserializerTest.java
+++ nbinotifications/src/test/java/org/opendaylight/transportpce/nbinotifications/serialization/NotificationTapiServiceDeserializerTest.java
@@ -17,8 +17,8 @@ import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.opendaylight.transportpce.common.converter.JsonStringConverter;
 import org.opendaylight.transportpce.test.AbstractTest;
-import org.opendaylight.yang.gen.v1.nbi.notifications.rev230726.NotificationTapiService;
-import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.notification.rev181210.get.notification.list.output.Notification;
+import org.opendaylight.yang.gen.v1.nbi.notifications.rev230728.NotificationTapiService;
+import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.notification.rev221121.get.notification.list.output.Notification;
 
 public class NotificationTapiServiceDeserializerTest extends AbstractTest {
 
