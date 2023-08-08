e8e5e8a9efb6619384c21e4479d3d293713d6f20
Christophe Betoule <christophe.betoule@orange.com>
2023-08-08 15:34:28 +0200

    Use lightynode sims in tox
    - Migration of functional tests from honeynode to lighynode simulators.
    - activate lighty instead of karaf.
    - use netconf instead of restconf connector in hybrid functional tests
    to change the admin-state of ports as soon as lightynode does not
    implement natively a restconf connector.
    - update 1.2.1 roadm device configuration to be compliant with 1.2.1
    device models (multi-wavelegnth instead of multi-wavelength which
    is solved with 2.2.1 device models).
    
    JIRA: TRNSPRTPCE-754
    Signed-off-by: Christophe Betoule <christophe.betoule@orange.com>
    Change-Id: Ia3a9219ffcdaafc34c422f7a3ce62b2a2db43cae

---
+++ tests/install_lightynode.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+#set -x
+
+#check if curl exists
+if ! [ -x "$(command -v curl)" ];then
+    echo "curl is not installed." >&2
+    exit 1
+fi
+#check if unzip exists
+if ! [ -x "$(command -v unzip)" ];then
+    echo "unzip is not installed." >&2
+    exit 1
+fi
+
+PLUGIN_VERSION=18.1.0.7
+TARGET_DIR=$(dirname $0)/lightynode
+INSTALL_DIR=$TARGET_DIR/lighty-openroadm-device-$PLUGIN_VERSION
+ARTIFACT_ZIPFILE=$TARGET_DIR/artifact.zip
+TARGET_URL="https://gitlab.com/api/v4/projects/36076125/packages/maven/io/lighty/transportpce/netconf/device/lighty-openroadm-device/$PLUGIN_VERSION/lighty-openroadm-device-$PLUGIN_VERSION-bin.zip"
+
+#clean lightynode install directory
+
+if [ -d "$INSTALL_DIR" ];then
+    echo "Removing $INSTALL_DIR directory"
+    rm -rf $INSTALL_DIR
+fi
+
+#download lithynode and install it
+#complete source code can be found at https://gitlab.com/Orange-OpenSource/lfn/odl/Lightynode-simulator.git
+
+echo "Installing ligthynode version $PLUGIN_VERSION to $TARGET_DIR/lightynode-simulator"
+curl --retry-delay 10 --retry 3 -sS --location --request GET $TARGET_URL -o $ARTIFACT_ZIPFILE || exit 2
+unzip -q $ARTIFACT_ZIPFILE -d $TARGET_DIR
+mv $INSTALL_DIR $TARGET_DIR/lightynode-simulator
+rm -f $ARTIFACT_ZIPFILE
+
+exit
--- tests/transportpce_tests/common/test_utils.py
+++ tests/transportpce_tests/common/test_utils.py
@@ -29,6 +29,7 @@ import simulators
 SIMS = simulators.SIMS
 
 HONEYNODE_OK_START_MSG = 'Netconf SSH endpoint started successfully at 0.0.0.0'
+LIGHTYNODE_OK_START_MSG = 'Data tree change listeners registered'
 KARAF_OK_START_MSG = "Transportpce controller started"
 LIGHTY_OK_START_MSG = re.escape("lighty.io and RESTCONF-NETCONF started")
 
@@ -137,8 +138,8 @@ def start_sims(sims_list):
     for sim in sims_list:
         print('starting simulator ' + sim[0] + ' in OpenROADM device version ' + sim[1] + '...')
         log_file = os.path.join(SIM_LOG_DIRECTORY, SIMS[sim]['logfile'])
-        process = start_honeynode(log_file, sim)
-        if wait_until_log_contains(log_file, HONEYNODE_OK_START_MSG, 100):
+        process = start_lightynode(log_file, sim)
+        if wait_until_log_contains(log_file, LIGHTYNODE_OK_START_MSG, 100):
             print('simulator for ' + sim[0] + ' started')
         else:
             print('simulator for ' + sim[0] + ' failed to start')
@@ -224,6 +225,23 @@ def start_honeynode(log_file: str, sim):
         with open(log_file, 'w', encoding='utf-8') as outfile:
             return subprocess.Popen(
                 [executable, SIMS[sim]['port'], os.path.join(sample_directory, SIMS[sim]['configfile'])],
+                stdout=outfile, stderr=outfile)
+    return None
+
+
+def start_lightynode(log_file: str, sim):
+    executable = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                              '..', '..', 'lightynode', 'lightynode-simulator', 'start-device.sh')
+    print("executable :")
+    print(executable)
+    sample_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                    '..', '..', 'sample_configs', 'openroadm', sim[1])
+
+    if os.path.isfile(executable):
+        with open(log_file, 'w', encoding='utf-8') as outfile:
+            return subprocess.Popen(
+                [executable, "-v" + sim[1], "-p" + SIMS[sim]['port'], "-f" + os.path.join(sample_directory,
+                                                                                          SIMS[sim]['configfile'])],
                 stdout=outfile, stderr=outfile)
     return None
 
