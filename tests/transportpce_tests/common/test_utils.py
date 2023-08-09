3f5a1660ef4eaf514346de6378a8b2f78591a27a
Christophe Betoule <christophe.betoule@orange.com>
2023-08-09 12:02:51 +0200

    Use of karaf instead of Lighty
    JIRA: TRNSPRTPCE-754
    Signed-off-by: Christophe Betoule <christophe.betoule@orange.com>
    Change-Id: Ibaaf378f04f6744fae9e6d4d551c9e1e70dafcca

--- tests/transportpce_tests/common/test_utils.py
+++ tests/transportpce_tests/common/test_utils.py
@@ -231,7 +231,7 @@ def start_honeynode(log_file: str, sim):
 
 def start_lightynode(log_file: str, sim):
     executable = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                              '..', '..', 'lightynode', 'lightynode-simulator', 'start-device.sh')
+                              '..', '..', 'lightynode', sim[1], 'lighty-openroadm-device', 'start-device.sh')
     print("executable :")
     print(executable)
     sample_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)),
