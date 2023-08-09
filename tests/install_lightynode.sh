edb32db6c64f8b578d39c0ccdfd24a92a047b6a2
Christophe Betoule <christophe.betoule@orange.com>
2023-08-09 12:02:51 +0200

    Use of karaf instead of Lighty
    JIRA: TRNSPRTPCE-754
    Signed-off-by: Christophe Betoule <christophe.betoule@orange.com>
    Change-Id: Ibaaf378f04f6744fae9e6d4d551c9e1e70dafcca

--- tests/install_lightynode.sh
+++ tests/install_lightynode.sh
@@ -15,7 +15,7 @@ fi
 
 PLUGIN_VERSION=18.1.0.7
 TARGET_DIR=$(dirname $0)/lightynode
-INSTALL_DIR=$TARGET_DIR/lighty-openroadm-device-$PLUGIN_VERSION
+INSTALL_DIR=$TARGET_DIR/lighty-openroadm-device
 ARTIFACT_ZIPFILE=$TARGET_DIR/artifact.zip
 TARGET_URL="https://gitlab.com/api/v4/projects/36076125/packages/maven/io/lighty/transportpce/netconf/device/lighty-openroadm-device/$PLUGIN_VERSION/lighty-openroadm-device-$PLUGIN_VERSION-bin.zip"
 
@@ -29,10 +29,10 @@ fi
 #download lithynode and install it
 #complete source code can be found at https://gitlab.com/Orange-OpenSource/lfn/odl/Lightynode-simulator.git
 
-echo "Installing ligthynode version $PLUGIN_VERSION to $TARGET_DIR/lightynode-simulator"
+echo "Installing ligthynode version $PLUGIN_VERSION to $INSTALL_DIR directory"
 curl --retry-delay 10 --retry 3 -sS --location --request GET $TARGET_URL -o $ARTIFACT_ZIPFILE || exit 2
 unzip -q $ARTIFACT_ZIPFILE -d $TARGET_DIR
-mv $INSTALL_DIR $TARGET_DIR/lightynode-simulator
 rm -f $ARTIFACT_ZIPFILE
+mv $TARGET_DIR/lighty-openroadm-device-$PLUGIN_VERSION $INSTALL_DIR
 
 exit
