9209b97ba82e8179dba1b779951d6053fd8eba96
Gilles Thouenon <gilles.thouenon@orange.com>
2023-09-21 18:53:46 +0200

    Reactivate disabled functional tests
    Reactivate few functional tests disabled due to the restconf bug
    (https://jira.opendaylight.org/browse/AAA-265)
    
    Signed-off-by: Gilles Thouenon <gilles.thouenon@orange.com>
    Change-Id: I2963cf8084faba60d0199dd9f681698d9be1c18b

--- tests/transportpce_tests/1.2.1/test04_renderer_service_path_nominal.py
+++ tests/transportpce_tests/1.2.1/test04_renderer_service_path_nominal.py
@@ -230,7 +230,6 @@ class TransportPCERendererTesting(unittest.TestCase):
              'fec': 'off'},
             response['interface'][0]['org-openroadm-ethernet-interfaces:ethernet'])
 
-    @unittest.skip("attributes that contains slashes seems to give some troubles to the controller")
     def test_13_service_path_create_xpdr_check(self):
         response = test_utils.check_node_attribute_request("XPDRA01", "circuit-packs", "1%2F0%2F1-PLUG-NET")
         # FIXME: https://jira.opendaylight.org/browse/TRNSPRTPCE-591
@@ -286,7 +285,6 @@ class TransportPCERendererTesting(unittest.TestCase):
         response = test_utils.check_node_attribute_request("XPDRA01", "interface", "XPDR1-CLIENT1-ETHERNET")
         self.assertEqual(response['status_code'], requests.codes.conflict)
 
-    @unittest.skip("attributes that contains slashes seems to give some troubles to the controller")
     def test_22_service_path_delete_xpdr_check(self):
         response = test_utils.check_node_attribute_request("XPDRA01", "circuit-packs", "1%2F0%2F1-PLUG-NET")
         self.assertEqual(response['status_code'], requests.codes.ok)
