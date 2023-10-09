b5a2b3b707820a3d7d7cb7677237e1107daeaf2e
guillaume.lambert <guillaume.lambert@orange.com>
2023-10-09 15:30:15 +0200

    test build lighty
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    Change-Id: I53b4e2eab890ed63ecd4c5e48ad2a3bf0055fd71

--- lighty/build_lighty_core.sh
+++ lighty/build_lighty_core.sh
@@ -7,4 +7,4 @@ cd lighty-repo
 git checkout main
 #git checkout 18.0.x
 export JDK_JAVA_OPTIONS="--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED"
-mvn clean install -B -U -q -DskipTests -Dmaven.javadoc.skip=true -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
+mvn clean install -B -U -q -DskipTests -s ../tests/odl_settings.xml -Dmaven.javadoc.skip=true -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
