c6fe107ea778ba7905b56d8d2b25352c97397789
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2023-12-07 14:13:13 +0000

    Library determining preferred node/port from PCRI
    Package intended to be used to determine preferred SRG port
    usage when setting up optical services between ROADMs.
    
    Uses an instance of PathComputationRequestInput, e.g. API
    input and provides a method for checking if a specific SRG port
    is among the ports in the client request.
    
    Package org.opendaylight.transportpce.pce.networkanalyzer.port:
    
    * Preference        - Interface defining one method capable
                          of determining if a node/port is 'preferred'
                          by the client.
    * ClientPreference  - Implements Preference. Contains information
                          about actual client port preference.
    * NoPreference      - Implements Preference. Typically used when
                          the client has no preference regarding port.
    
    * Factory           - Interface defining methods needed in order
                          to convert a PathComputationRequestInput
                          to an implementation of Preferenc.
    * PreferenceFactory - Implements Factory. The method
                          'portPreference' should typically be used
                          implementing the feature in the application.
    
                          The method portPreference either returns
                          an instance of 'ClientPreference' or
                          'NoPreference'.
    
    Example:
    
    public void someMethod(PathComputationRequestInput input) {
    
        Factory portPreferenceFactory = new PreferenceFactory();
    
        Preference preference = portPreferenceFactory.portPreference(input);
    
        if (preference.preferredPort("ROADM-A-SRG1", "SRG1-PP1-TXRX")) {
            System.out.println("The port 'SRG1-PP1-TXRX' on 'ROADM-A-SRG1' "
                              +"is preferred by the client.");
        }
    
    }
    
    JIRA: TRNSPRTPCE-176
    Change-Id: Iba7e7886678deb6d965391633f819995635caa06
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

---
+++ pce/src/main/java/org/opendaylight/transportpce/pce/networkanalyzer/port/Factory.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2023 Smartoptics, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.pce.networkanalyzer.port;
+
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.pce.rev230925.PathComputationRequestInput;
+
+public interface Factory {
+
+    /**
+     * Extracting preferred ports from pathComputationRequestInput.
+     *
+     * <p>
+     * This is the recommended method of determining if a node/port combination
+     * is preferred by the client.
+     *
+     * <p>
+     * Pseudocode example:
+     * <pre>
+     *     Factory.portPreference(PCRI).preferredPort("ROADM-B-SRG1", "SRG1-PP1-TXRX");
+     * </pre>
+     *
+     * @return Client port preference
+     */
+    Preference portPreference(PathComputationRequestInput pathComputationRequestInput);
+
+}
