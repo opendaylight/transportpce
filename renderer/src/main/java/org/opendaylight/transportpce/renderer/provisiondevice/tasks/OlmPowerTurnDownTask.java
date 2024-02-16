2bcfc471310c70dfa8ca213773c306a434ae6911
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2024-02-16 12:15:13 +0000

    Olm Power Down Task
    A task capable of running in parallel powering down OLM power.
    The package also contains an Interface left open to be implemented
    at a later time.
    
    Pseudo code example usage:
    
    ListenableFuture<OLMRenderingResult> futureAtoZ =
        this.executor.submit(
            new OlmPowerTurnDownTask(...)
        );
    
    ListenableFuture<OLMRenderingResult> futureZtoA =
        this.executor.submit(
            new OlmPowerTurnDownTask(...)
        );
    
    ListenableFuture<List<OLMRenderingResult>> futures =
        Futures.allAsList(futureAtoZ, futureZtoA);
    
    List<OLMRenderingResult> results;
    
    try{
        results = futures.get(Timeouts.OLM_TIMEOUT, TimeUnit.MILLISECONDS);
    } catch (Exception e) {
        LOG.error("Error while turning down power!");
    }
    
    if (results.get(0).isSuccess() && results.get(1).isSuccess()) {
        LOG.info("OLM power successfully turned down");
    }
    
    JIRA: TRNSPRTPCE-616
    Change-Id: I6839ad8bdd5feeec5bcd63700efe55c8aed01085
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

---
+++ renderer/src/main/java/org/opendaylight/transportpce/renderer/provisiondevice/tasks/OlmPowerTurnDownTask.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright © 2024 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.renderer.provisiondevice.tasks;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+import org.opendaylight.transportpce.common.ResponseCodes;
+import org.opendaylight.transportpce.renderer.ServicePathInputData;
+import org.opendaylight.transportpce.renderer.provisiondevice.OLMRenderingResult;
+import org.opendaylight.transportpce.renderer.provisiondevice.notification.Notification;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.olm.rev210618.ServicePowerTurndownInputBuilder;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.olm.rev210618.ServicePowerTurndownOutput;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.olm.rev210618.TransportpceOlmService;
+import org.opendaylight.yang.gen.v1.http.org.transportpce.b.c._interface.service.types.rev220118.RpcStatusEx;
+import org.opendaylight.yang.gen.v1.http.org.transportpce.b.c._interface.service.types.rev220118.ServicePathNotificationTypes;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OlmPowerTurnDownTask implements Callable<OLMRenderingResult> {
+
+    private final String serviceName;
+
+    private final String path;
+
+    private final TransportpceOlmService olmService;
+
+    private final ServicePathInputData servicePathInputData;
+
+    private final Notification notification;
+
+    private static final Logger LOG = LoggerFactory.getLogger(OlmPowerTurnDownTask.class);
+
+    /**
+     * Task used to power down OLM.
+     *
+     * <p>
+     * Intended to be used for parallel execution.
+     */
+    public OlmPowerTurnDownTask(
+        String serviceName,
+        String path,
+        TransportpceOlmService olmService,
+        ServicePathInputData servicePathInputData,
+        Notification notification
+    ) {
+        this.serviceName = serviceName;
+        this.path = path;
+        this.olmService = olmService;
+        this.servicePathInputData = servicePathInputData;
+        this.notification = notification;
+    }
+
+    @Override
+    public OLMRenderingResult call() throws Exception {
+
+        LOG.debug("Turning down power on {} path for service {}", path, serviceName);
+
+        Future<RpcResult<ServicePowerTurndownOutput>> fr = this.olmService.servicePowerTurndown(
+            new ServicePowerTurndownInputBuilder(
+                servicePathInputData.getServicePathInput()
+            ).build()
+        );
+
+        notification.send(
+            ServicePathNotificationTypes.ServiceDelete,
+            serviceName,
+            RpcStatusEx.Pending,
+            String.format("Turning down power on %s path for service %s", path, serviceName)
+        );
+
+        RpcResult<ServicePowerTurndownOutput> result = fr.get();
+
+        if (result == null || !ResponseCodes.SUCCESS_RESULT.equals(result.getResult().getResult())) {
+            notification.send(
+                ServicePathNotificationTypes.ServiceDelete,
+                serviceName,
+                RpcStatusEx.Failed,
+                String.format("Service power turn down failed on %s path for service %s", path, serviceName)
+            );
+            return OLMRenderingResult.failed(
+                String.format("Service power turn down failed on %s path for service %s", path, serviceName)
+            );
+        } else {
+            LOG.debug("OLM power turn down finished successfully on {} for service {}", path, serviceName);
+            return OLMRenderingResult.ok();
+        }
+    }
+}
