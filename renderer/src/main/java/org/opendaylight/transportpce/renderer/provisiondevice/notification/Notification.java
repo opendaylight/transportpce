9a7152352478b6345f4ef11e18f1d1fefe84f4ff
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2024-02-16 12:15:13 +0000

    Olm Power Down Task
    A task capable of running in parallel powering down OLM power.
    The package also contains an Interface left open to be implemented
    at a later time.
    
    Pseudo code example usage:
    
    ListenableFuture<OLMRenderingResult> futureAtoZ =
        this.executor.submit(
            new OlmPowerTurnDownTask(...)
        );
    
    ListenableFuture<OLMRenderingResult> futureZtoA =
        this.executor.submit(
            new OlmPowerTurnDownTask(...)
        );
    
    ListenableFuture<List<OLMRenderingResult>> futures =
        Futures.allAsList(futureAtoZ, futureZtoA);
    
    List<OLMRenderingResult> results;
    
    try{
        results = futures.get(Timeouts.OLM_TIMEOUT, TimeUnit.MILLISECONDS);
    } catch (Exception e) {
        LOG.error("Error while turning down power!");
    }
    
    if (results.get(0).isSuccess() && results.get(1).isSuccess()) {
        LOG.info("OLM power successfully turned down");
    }
    
    JIRA: TRNSPRTPCE-616
    Change-Id: I6839ad8bdd5feeec5bcd63700efe55c8aed01085
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

---
+++ renderer/src/main/java/org/opendaylight/transportpce/renderer/provisiondevice/notification/Notification.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright © 2024 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.renderer.provisiondevice.notification;
+
+import java.util.Set;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.renderer.rev210915.RendererRpcResultSp;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.renderer.rev210915.renderer.rpc.result.sp.Link;
+import org.opendaylight.yang.gen.v1.http.org.transportpce.b.c._interface.pathdescription.rev230501.PathDescription;
+import org.opendaylight.yang.gen.v1.http.org.transportpce.b.c._interface.service.types.rev220118.RpcStatusEx;
+import org.opendaylight.yang.gen.v1.http.org.transportpce.b.c._interface.service.types.rev220118.ServicePathNotificationTypes;
+
+public interface Notification {
+
+    /**
+     * Send renderer notification.
+     */
+    void send(ServicePathNotificationTypes servicePathNotificationTypes,
+              String serviceName,
+              RpcStatusEx rpcStatusEx,
+              String message);
+
+    /**
+     * Send renderer notification.
+     */
+    void send(org.opendaylight.yangtools.yang.binding.Notification notification);
+
+    /**
+     * Build notification containing path description information.
+     *
+     * @param servicePathNotificationTypes ServicePathNotificationTypes
+     * @param serviceName String
+     * @param rpcStatusEx RpcStatusEx
+     * @param message String
+     * @param pathDescription PathDescription
+     * @return notification with RendererRpcResultSp type.
+     */
+    RendererRpcResultSp buildNotification(ServicePathNotificationTypes servicePathNotificationTypes,
+                                          String serviceName,
+                                          RpcStatusEx rpcStatusEx,
+                                          String message,
+                                          PathDescription pathDescription,
+                                          Link notifLink,
+                                          Set<String> supportedLinks,
+                                          String serviceType);
+
+}
