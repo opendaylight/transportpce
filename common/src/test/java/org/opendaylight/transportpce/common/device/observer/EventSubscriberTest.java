206dfa2c03fa308881d88ac6ecd0532f7646e68d
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2024-03-27 13:39:57 +0000

    New observer package subscribing for error msg
    The class EventSubscriber can be used to track events of various
    severity. The class is intended as a way to collect error messages later
    for later use. The class contains convenient methods extracting the
    first or last message from a given type (error, warning etc).
    
    The class is particularly useful in the case of an iteration where it's
    not uncommon for errors to occur during the iteration, although at some
    point in the iteration at least one item should complete without error.
    
    Change-Id: I9210afca69f516a7ef3788e854b1af4d8bbcc450
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

---
+++ common/src/test/java/org/opendaylight/transportpce/common/device/observer/EventSubscriberTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright © 2024 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.common.device.observer;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.slf4j.event.Level;
+
+class EventSubscriberTest {
+
+    @Test
+    void last() {
+        Subscriber subscriber = new EventSubscriber();
+        subscriber.event(Level.ERROR, "First");
+        subscriber.event(Level.ERROR, "Last");
+
+        Assert.assertEquals("Last", subscriber.last(Level.ERROR));
+    }
+
+    @Test
+    void firstNumber() {
+        Subscriber subscriber = new EventSubscriber();
+        subscriber.event(Level.ERROR, "First error");
+        subscriber.event(Level.WARN, "First warn");
+        subscriber.event(Level.ERROR, "Second error");
+        subscriber.event(Level.ERROR, "Third error");
+
+
+        Assert.assertEquals("First error, Second error, Third error", subscriber.first(Level.ERROR, "", 3));
+    }
+
+    @Test
+    void firstNumberContainsLessThanThree() {
+        Subscriber subscriber = new EventSubscriber();
+        subscriber.event(Level.ERROR, "First error");
+        subscriber.event(Level.WARN, "First warn");
+        subscriber.event(Level.ERROR, "Second error");
+
+
+        Assert.assertEquals("First error, Second error", subscriber.first(Level.ERROR, "", 3));
+    }
+
+    @Test
+    void firstNumberEmpty() {
+        Subscriber subscriber = new EventSubscriber();
+
+        Assert.assertEquals("Asdf", subscriber.first(Level.ERROR, "Asdf", 3));
+    }
+}
