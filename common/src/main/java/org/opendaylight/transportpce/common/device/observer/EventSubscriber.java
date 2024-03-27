2544a9685a2fd8991d66d23211045322b0e94065
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2024-03-27 13:39:57 +0000

    New observer package subscribing for error msg
    The class EventSubscriber can be used to track events of various
    severity. The class is intended as a way to collect error messages
    for later use. The class contains convenient methods extracting the
    first or last message from a given type (error, warning etc).
    
    The class is particularly useful in the case of an iteration where it's
    not uncommon for errors to occur during the iteration, although at some
    point in the iteration at least one item should complete without error.
    
    Change-Id: I9210afca69f516a7ef3788e854b1af4d8bbcc450
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

---
+++ common/src/main/java/org/opendaylight/transportpce/common/device/observer/EventSubscriber.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright © 2024 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.common.device.observer;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+import org.slf4j.event.Level;
+
+public class EventSubscriber implements Subscriber {
+
+    private final Map<Level, Set<String>> messages = new ConcurrentHashMap<>();
+
+    @Override
+    public void event(Level level, String message) {
+        if (message == null || message.isEmpty()) {
+            return;
+        }
+
+        if (!messages.containsKey(level)) {
+            messages.put(level, new LinkedHashSet<>());
+        }
+
+        messages.get(level).add(message);
+    }
+
+    @Override
+    public void error(String message) {
+        event(Level.ERROR, message);
+    }
+
+    @Override
+    public void warn(String message) {
+        event(Level.WARN, message);
+    }
+
+    @Override
+    public String first(Level level) {
+        return first(level, "");
+    }
+
+    @Override
+    public String first(Level level, String defaultMessage) {
+        if (!messages.containsKey(level)) {
+            return defaultMessage;
+        }
+
+        return messages.get(level).iterator().next();
+    }
+
+    @Override
+    public String first(Level level, String defaultMessage, int count) {
+        if (!messages.containsKey(level)) {
+            return defaultMessage;
+        }
+
+        return messages.get(level)
+            .stream().limit(Math.max(count, 1))
+            .collect(Collectors.joining(", "));
+    }
+
+    @Override
+    public String last(Level level) {
+        return last(level, "");
+    }
+
+    @Override
+    public String last(Level level, String defaultMessage) {
+        if (!messages.containsKey(level)) {
+            return defaultMessage;
+        }
+
+        Set<String> strings = messages.get(level);
+
+        return strings.stream().skip(strings.size() - 1).findFirst().orElse(defaultMessage);
+
+    }
+
+    @Override
+    public String[] messages(Level level) {
+
+        if (!messages.containsKey(level)) {
+            return new String[0];
+        }
+
+        Object[] arr = messages.get(level).toArray();
+
+        return Arrays.copyOf(arr, arr.length, String[].class);
+
+    }
+}
