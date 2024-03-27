caab5c041fbb7a9ed026bb46be2a603723d268a4
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2024-03-27 13:46:46 +0000

    New result message builder (WeightedResultMessage)
    The new class WeightedResultMessage simplifies creating a resultmessage
    based on a list of DeviceRenderingResult or OLMRenderingResult objects.
    
    Useful when there is two parallell tasks running and none, one or both
    result in an error. The WeightedResultMessage turns them into one
    message (string) prioritizing error messages before successful messages.
    
    JIRA: TRNSPRTPCE-852
    Change-Id: I52415c1b1f6e1de632e4cc3313210ca2e15905c2
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

---
+++ renderer/src/main/java/org/opendaylight/transportpce/renderer/provisiondevice/result/Message.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright © 2024 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.renderer.provisiondevice.result;
+
+import java.util.List;
+import org.opendaylight.transportpce.renderer.provisiondevice.DeviceRenderingResult;
+import org.opendaylight.transportpce.renderer.provisiondevice.OLMRenderingResult;
+
+public interface Message {
+
+    /**
+     * Turn a list of rendering results into one 'weighted' response.
+     */
+    String deviceRenderingResultMessage(List<DeviceRenderingResult> renderingResults, String defaultErrorMessage,
+            String defaultSuccessMessage);
+
+    /**
+     * Turn a list of rendering results into one 'weighted' response.
+     */
+    String olmRenderingResultMessage(List<OLMRenderingResult> renderingResults, String defaultErrorMessage,
+            String defaultSuccessMessage);
+
+}
