e1d29cf789e485e00973f4d59ba15019a4498ec4
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2024-03-27 14:48:44 +0000

    Implement error message subscriber
    APIImpact
    
    Adds a more descriptive error message for the create service API.
    
    Refactorings
    
    PceResult is refactored allowing more options
    specifying error message:
    
    * The method setRC is removed together with an if-statement checking
      for success or failure. The hard coded error message is also removed.
    * The method success() is replacing one of the if-branches found
      in the method setRC.
    * Three new error methods are added with different ways of adding
      an error message. The hard previously hard coded error message
      is used in case a null or empty value is provided as error message.
      One of the methods accepts an instance of Subscriber found in the
      package org.opendaylight.transportpce.common.device.observer.
    
    PceCalculation is refactored to pass on error messages using PceResult.
    
    RendererServiceOperationsImpl.java is refactored:
    The class WeightedResultMessage is used to create an error message
    by processing the results from AtoZ and ZtoA rendering.
    
    Change-Id: Ibae306efc8002a17caf61d3555eafa2985098ce2
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

--- pce/src/main/java/org/opendaylight/transportpce/pce/networkanalyzer/PceCalculation.java
+++ pce/src/main/java/org/opendaylight/transportpce/pce/networkanalyzer/PceCalculation.java
@@ -19,8 +19,9 @@ import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.transportpce.common.NetworkUtils;
-import org.opendaylight.transportpce.common.ResponseCodes;
 import org.opendaylight.transportpce.common.StringConstants;
+import org.opendaylight.transportpce.common.device.observer.EventSubscriber;
+import org.opendaylight.transportpce.common.device.observer.Subscriber;
 import org.opendaylight.transportpce.common.fixedflex.GridConstant;
 import org.opendaylight.transportpce.common.mapping.MappingUtils;
 import org.opendaylight.transportpce.common.mapping.MappingUtilsImpl;
@@ -54,6 +55,7 @@ import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.common.Uint32;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
 
 public class PceCalculation {
     /* Logging. */
@@ -129,19 +131,21 @@ public class PceCalculation {
 
         LOG.debug("In PceCalculation retrieveNetwork");
 
-        if (!readMdSal()) {
-            returnStructure.setRC(ResponseCodes.RESPONSE_FAILED);
+        Subscriber subscriber = new EventSubscriber();
+
+        if (!readMdSal(subscriber)) {
+            returnStructure.error(subscriber);
             return;
         }
         MapUtils.mapDiversityConstraints(allNodes, allLinks, pceHardConstraints);
 
-        if (!analyzeNw()) {
-            returnStructure.setRC(ResponseCodes.RESPONSE_FAILED);
+        if (!analyzeNw(subscriber)) {
+            returnStructure.error(subscriber);
             return;
         }
         printNodesInfo(allPceNodes);
 
-        returnStructure.setRC(ResponseCodes.RESPONSE_OK);
+        returnStructure.success();
     }
 
     private boolean parseInput() {
@@ -207,7 +211,7 @@ public class PceCalculation {
                 : input.getServiceZEnd().getNodeId();
     }
 
-    private boolean readMdSal() {
+    private boolean readMdSal(Subscriber subscriber) {
         InstanceIdentifier<Network> nwInstanceIdentifier = null;
         switch (serviceType) {
             case StringConstants.SERVICE_TYPE_100GE_T:
@@ -241,6 +245,7 @@ public class PceCalculation {
 
         if (readTopology(nwInstanceIdentifier) == null) {
             LOG.error("readMdSal: network is null: {}", nwInstanceIdentifier);
+            subscriber.event(Level.ERROR, String. format("Network is null: %s", nwInstanceIdentifier));
             return false;
         }
 
@@ -256,6 +261,7 @@ public class PceCalculation {
         }
         if (allNodes == null || allNodes.isEmpty()) {
             LOG.error("readMdSal: no nodes ");
+            subscriber.event(Level.ERROR, "No nodes found in network");
             return false;
         }
         LOG.info("readMdSal: network nodes: {} nodes added", allNodes.size());
@@ -263,6 +269,7 @@ public class PceCalculation {
 
         if (allLinks == null || allLinks.isEmpty()) {
             LOG.error("readMdSal: no links ");
+            subscriber.event(Level.ERROR, "No links found in network");
             return false;
         }
         LOG.info("readMdSal: network links: {} links added", allLinks.size());
@@ -282,12 +289,12 @@ public class PceCalculation {
             }
         } catch (InterruptedException | ExecutionException e) {
             LOG.error("readMdSal: Error reading topology {}", nwInstanceIdentifier);
-            returnStructure.setRC(ResponseCodes.RESPONSE_FAILED);
+            returnStructure.error("Unexpected error occurred while reading topology from internal data store.");
         }
         return nw;
     }
 
-    private boolean analyzeNw() {
+    private boolean analyzeNw(Subscriber subscriber) {
 
         LOG.debug("analyzeNw: allNodes size {}, allLinks size {}", allNodes.size(), allLinks.size());
         switch (serviceType) {
@@ -308,7 +315,13 @@ public class PceCalculation {
                 LOG.debug("analyzeNw: allPceNodes size {}", allPceNodes.size());
 
                 if (aendPceNode == null || zendPceNode == null) {
-                    LOG.error("analyzeNw: Error in reading nodes: A or Z do not present in the network");
+                    if (aendPceNode == null) {
+                        LOG.error("analyzeNw: Error in reading nodes: A not present in the network");
+                        subscriber.event(Level.ERROR, "Error during reading nodes: A is not present in the network");
+                    } else {
+                        LOG.error("analyzeNw: Error in reading nodes: Z not present in the network");
+                        subscriber.event(Level.ERROR, "Error during reading nodes: Z is not present in the network");
+                    }
                     return false;
                 }
                 for (Link link : allLinks) {
@@ -341,6 +354,13 @@ public class PceCalculation {
 
                 if (aendPceNode == null || zendPceNode == null) {
                     LOG.error("analyzeNw: Error in reading nodes: A or Z do not present in the network");
+                    if (aendPceNode == null) {
+                        subscriber.event(
+                            Level.ERROR, "OTN node validation failed. A-node was not found in the network.");
+                    } else {
+                        subscriber.event(
+                            Level.ERROR, "OTN node validation failed. Z-node was not found in the network.");
+                    }
                     return false;
                 }
                 for (Link link : allLinks) {
@@ -352,6 +372,11 @@ public class PceCalculation {
         LOG.info("analyzeNw: allPceNodes size {}, allPceLinks size {}", allPceNodes.size(), allPceLinks.size());
 
         if ((allPceNodes.size() == 0) || (allPceLinks.size() == 0)) {
+            if (allPceNodes.size() == 0) {
+                subscriber.event(Level.ERROR, "No nodes found in network.");
+            } else {
+                subscriber.event(Level.ERROR, "No links found in network.");
+            }
             return false;
         }
 
@@ -419,7 +444,7 @@ public class PceCalculation {
 
         if (State.OutOfService.equals(state)) {
             LOG.debug("validateLink: Link is ignored due operational state - {}",
-                    state.getName());
+                state.getName());
             return false;
         }
 
@@ -457,7 +482,7 @@ public class PceCalculation {
         }
         if (State.OutOfService.equals(node1.getOperationalState())) {
             LOG.error("getNodeType: node is ignored due to operational state - {}", node1.getOperationalState()
-                    .getName());
+                .getName());
             return;
         }
         OpenroadmNodeType nodeType = node1.getNodeType();
@@ -525,18 +550,18 @@ public class PceCalculation {
                 if (pceNode.getSupNetworkNodeId().equals(azNodeId)) {
                     return true;
                 }
-            //fallthrough
+                //fallthrough
             case "OTU":
             case "other":
                 switch (azEndPoint) {
                     case "A":
                         return checkAendInputTxPortDeviceName()
                             && pceNode.getNodeId().getValue()
-                                .equals(this.input.getServiceAEnd().getTxDirection().getPort().getPortDeviceName());
+                            .equals(this.input.getServiceAEnd().getTxDirection().getPort().getPortDeviceName());
                     case "Z":
                         return checkZendInputTxPortDeviceName()
                             && pceNode.getNodeId().getValue()
-                                .equals(this.input.getServiceZEnd().getTxDirection().getPort().getPortDeviceName());
+                            .equals(this.input.getServiceZEnd().getTxDirection().getPort().getPortDeviceName());
                     default:
                         return false;
                 }
@@ -549,9 +574,6 @@ public class PceCalculation {
     private void validateOtnNode(Node node) {
         LOG.info("validateOtnNode: {} ", node.getNodeId().getValue());
         // PceOtnNode will be used in Graph algorithm
-        if (node.getNodeId().getValue().equals("TAPI-SBI-ABS-NODE")) {
-            return;
-        }
         if (node.augmentation(Node1.class) == null) {
             LOG.error("ValidateOtnNode: no node-type augmentation. Node {} is ignored", node.getNodeId().getValue());
             return;
