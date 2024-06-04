aa21631fa3f80212af23ea1e0ca484fe7f370e01
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2024-06-04 12:15:01 +0000

    Copy OpenROADM service to TAPI
    A listener is registered to listen for successful service creation
    messages in TPCE. Once a message is received, the OpenROADM service is
    copied to TAPI.
    
    NOTE: The topology is not copied.
    
    Change-Id: I03e0c8d82a713087d4392f0836512b1bfb8f9007
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

---
+++ tapi/src/main/java/org/opendaylight/transportpce/tapi/openroadm/service/OpenRoadmService.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright © 2025 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.tapi.openroadm.service;
+
+import java.util.HashMap;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.transportpce.common.network.NetworkTransactionService;
+import org.opendaylight.transportpce.servicehandler.service.ServiceDataStoreOperations;
+import org.opendaylight.transportpce.tapi.connectivity.ConnectivityUtils;
+import org.opendaylight.transportpce.tapi.impl.TapiProvider;
+import org.opendaylight.transportpce.tapi.topology.TopologyUtils;
+import org.opendaylight.transportpce.tapi.utils.TapiContext;
+import org.opendaylight.transportpce.tapi.utils.TapiInitialORMapping;
+import org.opendaylight.transportpce.tapi.utils.TapiLink;
+import org.opendaylight.transportpce.tapi.utils.TapiLinkImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OpenRoadmService implements Service {
+
+    private static final Logger LOG = LoggerFactory.getLogger(OpenRoadmService.class);
+
+    private final DataBroker dataBroker;
+    private final NetworkTransactionService networkTransactionService;
+    private final ServiceDataStoreOperations serviceDataStoreOperations;
+    private final TapiContext tapiContext;
+
+    public OpenRoadmService(
+            DataBroker dataBroker,
+            NetworkTransactionService networkTransactionService,
+            ServiceDataStoreOperations serviceDataStoreOperations,
+            TapiContext tapiContext) {
+
+        this.dataBroker = dataBroker;
+        this.networkTransactionService = networkTransactionService;
+        this.serviceDataStoreOperations = serviceDataStoreOperations;
+        this.tapiContext = tapiContext;
+    }
+
+    @Override
+    public boolean copyServiceToTAPI(String openRoadmServiceName) {
+        LOG.info("Starting to copy OpenROADM service {} to TAPI", openRoadmServiceName);
+
+        TapiLink tapiLink = new TapiLinkImpl(this.networkTransactionService, tapiContext);
+
+        TopologyUtils topologyUtils = new TopologyUtils(this.networkTransactionService, this.dataBroker, tapiLink);
+
+        ConnectivityUtils connectivityUtils = new ConnectivityUtils(
+                this.serviceDataStoreOperations,
+                new HashMap<>(),
+                tapiContext,
+                this.networkTransactionService,
+                TapiProvider.TAPI_TOPO_UUID
+                );
+
+        TapiInitialORMapping tapiInitialORMapping = new TapiInitialORMapping(
+                topologyUtils,
+                connectivityUtils,
+                tapiContext,
+                this.serviceDataStoreOperations);
+
+        if (tapiInitialORMapping.performServInitialMapping(openRoadmServiceName)) {
+            LOG.info("Done, OpenROADM service {} copied to TAPI!", openRoadmServiceName);
+            return true;
+        }
+
+        LOG.error("Copy OpenROADM service to TAPI failed!");
+        return false;
+    }
+
+}
