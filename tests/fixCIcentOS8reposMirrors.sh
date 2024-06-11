941dd1f2ea2517c58b441a365339c9ae31aa6146
Gilles Thouenon <gilles.thouenon@orange.com>
2024-06-11 15:10:42 +0200

    Fix CI checkbashisms issue BIS
    Signed-off-by: Gilles Thouenon <gilles.thouenon@orange.com>
    Change-Id: I87831c5fe4cb81031f75d42762e1ad16e296a3e5

---
+++ tests/fixCIcentOS8reposMirrors.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+which yum || exit 0
+grep 'CentOS Stream release 8' /etc/redhat-release || exit 0
+cat /etc/yum.repos.d/CentOS-Stream-AppStream.repo
+sed -i_ 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-* || exit 1
+sed -i_ 's@#baseurl=http://mirror.centos.org@baseurl=http://vault.centos.org@g' /etc/yum.repos.d/CentOS-* || exit 1
+cat /etc/yum.repos.d/CentOS-Stream-AppStream.repo
