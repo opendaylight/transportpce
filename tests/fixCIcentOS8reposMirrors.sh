8a923bdceb95ea869cfafad2666ee91a0d814a96
guillaume.lambert <guillaume.lambert@orange.com>
2024-06-12 11:46:18 +0200

    Fix CI Cent OS 8 configuration issue (workaround)
    - reverts commit 94578ec62abf23679af8557bde48e26e1b7bc7d7
      to reenable voting for checkbashisms and pre-commit
    
    Mirrorlist URLs have been archived and moved to Cent OS vault.
    https://forums.centos.org/viewtopic.php?t=78708&start=30
    This impacts Jenkins minions configuration.
    
    - force vault use in yum repos config till this is fixed upstream
    
    Change-Id: I87831c5fe4cb81031f75d42762e1ad16e296a3e5
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>

---
+++ tests/fixCIcentOS8reposMirrors.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+which yum || exit 0
+grep 'CentOS Stream release 8' /etc/redhat-release || exit 0
+cat /etc/yum.repos.d/CentOS-Stream-AppStream.repo
+sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-* || exit 1
+sudo sed -i 's@#baseurl=http://mirror.centos.org@baseurl=http://vault.centos.org@g' /etc/yum.repos.d/CentOS-* || exit 1
+cat /etc/yum.repos.d/CentOS-Stream-AppStream.repo
