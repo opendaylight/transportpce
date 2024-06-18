28d81b0a98a8c9a7973318665fd737a1ac8aea62
Gilles Thouenon <gilles.thouenon@orange.com>
2024-06-18 10:34:23 +0200

    Bump CI scripts to Ubuntu 20.04
    Build node on the CI have migrated from CentOS to Ubuntu-2004 which
    already supports different versions of java.
    - remove old installMavenCentOS.sh script which is now obsolete
    - replace it with installMavenUbuntu.sh which configures java in java-17
    
    Change-Id: I50c29126cb2797d1ba4c386284c4015f8079f6df
    Signed-off-by: Gilles Thouenon <gilles.thouenon@orange.com>

---
+++ tests/installMavenUbuntu.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -x
+
+#list Java versions available
+update-java-alternatives -l
+
+#select JAVA-17
+sudo update-java-alternatives -s java-1.17.0-openjdk-amd64
+JAVA_VER=$(java -version 2>&1 | sed -n ';s/.* version "\(.*\)\.\(.*\)\..*".*$/\1/p;')
+echo $JAVA_VER
+JAVAC_VER=$(javac -version 2>&1 |  sed -n ';s/javac \(.*\)\.\(.*\)\..*.*$/\1/p;')
+echo $JAVAC_VER
+if [ "$JAVA_VER" -ge 17 -a "$JAVAC_VER" -ge 17 ];then
+    echo "ok, java is 17 or newer"
+else
+   echo "No java17 or newer available"
+   exit 1
+fi
+
+#download maven image 3.8.8 and install it
+wget -nv https://dlcdn.apache.org/maven/maven-3/3.8.8/binaries/apache-maven-3.8.8-bin.tar.gz -P /tmp
+sudo mkdir -p /opt
+sudo tar xf /tmp/apache-maven-3.8.8-bin.tar.gz -C /opt
+sudo ln -s /opt/apache-maven-3.8.8 /opt/maven
+sudo ln -s /opt/maven/bin/mvn /usr/bin/mvn
+
+mvn --version
