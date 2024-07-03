10a1922ed9e769e565f270df262e1b1566822438
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2024-07-03 12:25:45 +0200

    New Tapi package dealing with frequency BitSets
    Refactored code creating classes capable of converting
    byte to BitSet to numeric frequency ranges.
    
    e.g. byte[] = {-1} -> 191.325:191.375 THz
    
    Each byte in an array is treated as 8 bits. Meaning an array
    of 96 bytes is treated as a BitSet of 768 bits. The bits may
    subsequently be converted to a map where the key is the
    lower frequency and the value is the upper frequency in a range.
    
    The package treats each signed byte at "face value". FlexGrid
    or FixGrid data are treated equally.
    
    Examples
    byte[] = {1}       -> BitSet: {0}               -> 191.325:191.33125
    byte[] = {15}      -> Bitset: {0,1,2,3}         -> 191.325:191.35
    byte[] = {-16}     -> BitSet: {4,5,6,7}         -> 191.35:191.375
    byte[] = {-1}      -> BitSet: {0,1,2,3,4,5,6,7} -> 191.325:191.375
    byte[] = {-128}    -> BitSet: {7}               -> 191.36875:191.375
    byte[] = {-128, 1} -> BitSet: {7,8}             -> 191.36875:191.38125
    
    Package Overview
    
    The main parts of this package are the classes AvailableGrid and
    NumericFrequency. AvailableGrid is used to ease converting
    a byte array to either assigned (used) or available frequencies
    represented by a BitSet.
    
    AvailableGridFactory provides a way to instantiate an AvailableGrid
    object using either available or used frequency grid byte data.
    
    The method getFreqMapFromBitSet in ConvertORToTapiTopology has
    been modified and copied to NumericFrequency and is used to
    convert a bitset to a Map<Double, Double> (numeric frequency range).
    
    The interface Math is intended to make it easier to unit test the code.
    The implementation of Math (i.e. FrequencyMath) simply wraps calls to
    GridUtils.getStartFrequencyFromIndex(...) and
    GridUtils.getStopFrequencyFromIndex(...).
    Since GridUtils depends on global constants, any changes to those
    constants may result in unit tests failing. The interface 'Math'
    provides a way to test the code using predictable data.
    
    Example usage:
    
    /**
     * This method converts a byte array (e.g. 96 elements) representing
     * frequency ranges into a BitSet (i.e. 768 bits), and finally into
     * a Map<Double, Double> where the key represents the start frequency
     * and the value the end frequency in a range.
     *
     * In short, this example will convert...
     *     byte[] = {-1}
     * ...into...
     *      Map<Double, Double> = 191.325:191.375
     */
    public Map<Double, Double> availableMap(byte[] availableByteMap) {
    
        Available available = new AvailableGrid(availableByteMap);
    
        // This object will help us convert an instance of 'Available'
        // into a numeric frequency range.
        Numeric numericFrequency = new NumericFrequency(
                GridConstant.START_EDGE_FREQUENCY,
                GridConstant.EFFECTIVE_BITS,
                new FrequencyMath()
        );
    
        // Convert BitSet data contained in the bitMap object
        // into a frequency range map.
        // Note: If instead we want the assigned frequencies
        //       we could execute:
        //       numericFrequency.assignedFrequency(available):
        return numericFrequency.availableFrequency(available);
    }
    
    JIRA: TRNSPRTPCE-802
    Change-Id: I578abad37351eb4ead5e8907207f1384df27f09a
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

---
+++ tapi/src/main/java/org/opendaylight/transportpce/tapi/frequency/AvailableGrid.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright © 2024 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.tapi.frequency;
+
+import java.util.BitSet;
+import java.util.Map;
+
+public class AvailableGrid implements Available {
+
+    private final byte[] availableFrequencyGrid;
+
+    /**
+     * The available frequency slots are represented by a (signed) byte array.
+     *
+     * <p>
+     * The size of the byte array multiplied by byte size is equal to the
+     * total number of frequency slots.
+     * e.g. 96 bytes = 768 bits = 768 frequency slots.
+     *
+     * <p>
+     * The available frequency slots...
+     *     byte[] frequencies = {-58, -1} => {0b11000110, 0b11111111}
+     * ...can be reversed using the method assignedFrequencies():
+     *     availableFrequencies() => {0b00111001, 0b00000000}
+     */
+    public AvailableGrid(byte[] availableFrequencyGrid) {
+        this.availableFrequencyGrid = new byte[availableFrequencyGrid.length];
+        System.arraycopy(availableFrequencyGrid, 0, this.availableFrequencyGrid, 0, availableFrequencyGrid.length);
+    }
+
+    @Override
+    public byte[] availableFrequencyRanges() {
+
+        byte[] freqBitSetCopy = new byte[availableFrequencyGrid.length];
+
+        System.arraycopy(availableFrequencyGrid, 0, freqBitSetCopy, 0, availableFrequencyGrid.length);
+
+        return freqBitSetCopy;
+    }
+
+    @Override
+    public BitSet assignedFrequencies() {
+
+        BitSet bitSet = new BitSet(availableFrequencyGrid.length * Byte.SIZE);
+
+        bitSet.or(BitSet.valueOf(availableFrequencyGrid));
+
+        bitSet.flip(0, availableFrequencyGrid.length * Byte.SIZE);
+
+        return bitSet;
+    }
+
+    @Override
+    public BitSet availableFrequencies() {
+
+        BitSet bitSet = new BitSet(availableFrequencyGrid.length * Byte.SIZE);
+
+        bitSet.or(BitSet.valueOf(availableFrequencyGrid));
+
+        return bitSet;
+
+    }
+
+    @Override
+    public Map<Double, Double> assignedFrequency(Numeric numeric) {
+        return numeric.assignedFrequency(this);
+    }
+
+    @Override
+    public Map<Double, Double> availableFrequency(Numeric numeric) {
+        return numeric.availableFrequency(this);
+    }
+}
---
+++ tapi/src/test/java/org/opendaylight/transportpce/tapi/frequency/NumericFrequencyTest.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright © 2024 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.tapi.frequency;
+
+import java.util.BitSet;
+import java.util.Map;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class NumericFrequencyTest {
+
+    @Test
+    void assignedFrequency() {
+        BitSet assigned = new BitSet(768);
+        assigned.set(760, 768);
+
+        Available used = Mockito.mock(Available.class);
+        Mockito.when(used.assignedFrequencies()).thenReturn(assigned);
+
+        Math math = Mockito.mock(Math.class);
+        Mockito.when(math.getStartFrequencyFromIndex(760)).thenReturn(196.075);
+        Mockito.when(math.getStopFrequencyFromIndex(767)).thenReturn(196.125);
+
+        Map<Double, Double> expected = Map.of(196.075, 196.125);
+
+        Numeric frequencyService = new NumericFrequency(191.325, 768, math);
+        Assert.assertEquals(expected, frequencyService.assignedFrequency(used));
+
+    }
+
+    @Test
+    void assignedStartAndEndFrequencyRanges() {
+        Math math = Mockito.mock(Math.class);
+        Mockito.when(math.getStartFrequencyFromIndex(8)).thenReturn(191.375);
+        Mockito.when(math.getStartFrequencyFromIndex(760)).thenReturn(196.075);
+        Mockito.when(math.getStopFrequencyFromIndex(767)).thenReturn(196.125);
+
+        BitSet assigned = new BitSet(768);
+        assigned.set(0, 8);
+        assigned.set(760, 768);
+
+        Available used = Mockito.mock(Available.class);
+        Mockito.when(used.assignedFrequencies()).thenReturn(assigned);
+
+        Map<Double, Double> expected = Map.of(191.325, 191.375, 196.075, 196.125);
+
+        Numeric frequencyService = new NumericFrequency(191.325, 768, math);
+        Assert.assertEquals(expected, frequencyService.assignedFrequency(used));
+
+    }
+
+    @Test
+    void assignedMiddleFrequencyRanges() {
+        Math math = Mockito.mock(Math.class);
+        Mockito.when(math.getStartFrequencyFromIndex(32)).thenReturn(191.525);
+        Mockito.when(math.getStartFrequencyFromIndex(40)).thenReturn(191.575);
+
+        BitSet assigned = new BitSet(768);
+        assigned.set(32, 40);
+
+        Available used = Mockito.mock(Available.class);
+        Mockito.when(used.assignedFrequencies()).thenReturn(assigned);
+
+        Map<Double, Double> expected = Map.of(191.525, 191.575);
+
+        Numeric frequencyService = new NumericFrequency(191.325, 768, math);
+        Assert.assertEquals(expected, frequencyService.assignedFrequency(used));
+
+    }
+
+    @Test
+    void assignedStartMiddleAndEndFrequencyRanges() {
+        Math math = Mockito.mock(Math.class);
+        Mockito.when(math.getStartFrequencyFromIndex(8)).thenReturn(191.375);
+        Mockito.when(math.getStartFrequencyFromIndex(32)).thenReturn(191.525);
+        Mockito.when(math.getStartFrequencyFromIndex(40)).thenReturn(191.575);
+        Mockito.when(math.getStartFrequencyFromIndex(760)).thenReturn(196.075);
+        Mockito.when(math.getStopFrequencyFromIndex(767)).thenReturn(196.125);
+
+        BitSet assigned = new BitSet(768);
+        assigned.set(0, 8);
+        assigned.set(32, 40);
+        assigned.set(760, 768);
+
+        Available used = Mockito.mock(Available.class);
+        Mockito.when(used.assignedFrequencies()).thenReturn(assigned);
+
+        Map<Double, Double> expected = Map.of(191.325, 191.375, 191.525, 191.575, 196.075, 196.125);
+
+        Numeric frequencyService = new NumericFrequency(191.325, 768, math);
+        Assert.assertEquals(expected, frequencyService.assignedFrequency(used));
+
+    }
+
+    @Test
+    void availableFrequency() {
+        Math math = Mockito.mock(Math.class);
+        Mockito.when(math.getStartFrequencyFromIndex(760)).thenReturn(196.075);
+
+        Numeric frequencyService = new NumericFrequency(191.325, 768, math);
+
+        BitSet available = new BitSet(768);
+        available.set(0, 760);
+
+        Available bitMap = Mockito.mock(Available.class);
+        Mockito.when(bitMap.availableFrequencies()).thenReturn(available);
+
+        Map<Double, Double> expected = Map.of(191.325, 196.075);
+        Assert.assertEquals(expected, frequencyService.availableFrequency(bitMap));
+    }
+
+    @Test
+    void availableSplitByAssignedMiddleFrequencyRanges() {
+        Math math = Mockito.mock(Math.class);
+        Mockito.when(math.getStartFrequencyFromIndex(32)).thenReturn(191.525);
+        Mockito.when(math.getStartFrequencyFromIndex(40)).thenReturn(191.575);
+        Mockito.when(math.getStopFrequencyFromIndex(767)).thenReturn(196.125);
+
+        BitSet available = new BitSet(768);
+        available.set(0, 32);
+        available.set(40, 768);
+
+        Available used = Mockito.mock(Available.class);
+        Mockito.when(used.availableFrequencies()).thenReturn(available);
+
+        Map<Double, Double> expected = Map.of(191.325, 191.525, 191.575, 196.125);
+
+        Numeric frequencyService = new NumericFrequency(191.325, 768, math);
+        Assert.assertEquals(expected, frequencyService.availableFrequency(used));
+
+    }
+
+    @Test
+    void flexGrid() {
+        Math math = Mockito.mock(Math.class);
+        Mockito.when(math.getStartFrequencyFromIndex(5)).thenReturn(191.35625);
+        Mockito.when(math.getStartFrequencyFromIndex(11)).thenReturn(191.39374999999998);
+        Mockito.when(math.getStartFrequencyFromIndex(19)).thenReturn(191.44375);
+        Mockito.when(math.getStopFrequencyFromIndex(767)).thenReturn(196.125);
+
+        BitSet available = new BitSet(768);
+        available.set(5, 11);
+        available.set(19, 768);
+
+        Available used = Mockito.mock(Available.class);
+        Mockito.when(used.availableFrequencies()).thenReturn(available);
+
+        Map<Double, Double> expected = Map.of(191.35625, 191.39374999999998, 191.44375, 196.125);
+
+        Numeric frequencyService = new NumericFrequency(191.325, 768, math);
+        Assert.assertEquals(expected, frequencyService.availableFrequency(used));
+
+    }
+
+}
