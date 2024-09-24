96a67db93c9d18f7882b6dd9cf10a4adc2983679
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2024-09-24 10:07:38 +0200

    Fix grid spectrum computation (reversed logic)
    Corrects an issue where flexgrid spectrum assignment
    searches for available frequencies in 50GHz steps and
    fixgrid searches in 6.25GHz steps. The reverse makes more sense.
    
    Consider the for-loop in the method computeBestSpectrumAssignment
    in the class PostAlgoPathValidator.
    
        for (int i=spectrumOccupation.size(); i >= spectralWidthSlotNumber;
                i -= isFlexGrid ? spectralWidthSlotNumber : 1) {
            ...
        }
    
    * If isFlexGrid = true and spectralWidthSlotNumber = 8, then this loop
      subtracts 8 from i each iteration.
    * If isFlexGrid = false, this loop subtracts 1 from i each iteration.
    
    Changes
    ===============
    
    In essence, fixGrid behaves as flexGrid and vice versa. This commit
    inverts the logic. As an example, the bug affects scenarios where
    part of a 50GHz range is available. See the examples below.
    
    The method computeBestSpectrumAssignment is changed from private
    to public access to allow unit testing. Two unit tests are also
    added in the new class PostAlgoPathValidatorTest.
    
    Examples
    ===============
    
    Assuming the available frequency range can be represented
    by this BitSet (1=available, 0=used):
    
                          |         50GHz         |         50GHz         |
    SLOT:       0 1 ... 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
    AVAILABLE:  0 0      0  1  1  1  1  1  1  1  1  1  1  1  1  0  0  0  0
    
    Example FixGrid
    ===============
    
    Compute spectrumassignment:
    
        BitSet available = new BitSet(768);
        available.set(16, 28);
        postAlgoPathValidator.computeBestSpectrumAssignment(
                available,
                8,
                false
        )
    
    The above example will produce this result:
    
        SpectrumAssignment{beginIndex=20, flexGrid=false, stopIndex=27}
    
    This may be illustrated as:
                          |         50GHz         |         50GHz         |
    SLOT:       0 1 ... 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
    ASSIGNED:   0 0      0  0  0  0  0  1  1  1  1  1  1  1  1  0  0  0  0
    
    FIX
    ------------
    
    After this commit, the above example will produce this result:
    
        SpectrumAssignment{beginIndex=16, flexGrid=false, stopIndex=23}
    
    This may be illustrated as:
                          |         50GHz         |         50GHz         |
    SLOT:       0 1 ... 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
    ASSIGNED:   0 0      0  1  1  1  1  1  1  1  1  0  0  0  0  0  0  0  0
    
    EXAMPLE FlexGrid
    ================
    
    Compute spectrumassignment:
    
        BitSet available = new BitSet(768);
        available.set(16, 28);
        postAlgoPathValidator.computeBestSpectrumAssignment(
                available,
                8,
                true
        )
    
    The above example will produce this result:
    
        SpectrumAssignment{beginIndex=16, flexGrid=true, stopIndex=23}
    
    This may be illustrated as:
                          |         50GHz         |         50GHz         |
    SLOT:       0 1 ... 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
    ASSIGNED:   0 0      0  1  1  1  1  1  1  1  1  0  0  0  0  0  0  0  0
    
    FIX
    ------------
    
    After this commit, the above example will produce this result:
    
        SpectrumAssignment{beginIndex=20, flexGrid=false, stopIndex=27}
    
    This may be illustrated as:
                          |         50GHz         |         50GHz         |
    SLOT:       0 1 ... 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
    ASSIGNED:   0 0      0  0  0  0  0  1  1  1  1  1  1  1  1  0  0  0  0
    
    JIRA: TRNSPRTPCE-821
    Change-Id: Idb4e7509a9575775846f6d3f0dab63a9ba2ea927
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

---
+++ pce/src/test/java/org/opendaylight/transportpce/pce/graph/PostAlgoPathValidatorTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright © 2024 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.pce.graph;
+
+import java.util.BitSet;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.opendaylight.transportpce.common.network.NetworkTransactionService;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.pce.rev240205.SpectrumAssignment;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.pce.rev240205.SpectrumAssignmentBuilder;
+import org.opendaylight.yangtools.yang.common.Uint16;
+
+class PostAlgoPathValidatorTest {
+
+    @Test
+    void computeBestSpectrumAssignmentFixGrid() {
+
+        NetworkTransactionService networkTransactionService = Mockito.mock(NetworkTransactionService.class);
+        PostAlgoPathValidator postAlgoPathValidator = new PostAlgoPathValidator(
+                networkTransactionService,
+                new BitSet()
+        );
+
+        BitSet available = new BitSet(768);
+        available.set(16, 28);
+
+        boolean isFlexGrid = false;
+        SpectrumAssignment expectedFixGrid = new SpectrumAssignmentBuilder()
+                .setBeginIndex(Uint16.valueOf(16))
+                .setStopIndex(Uint16.valueOf(23))
+                .setFlexGrid(isFlexGrid)
+                .build();
+
+        SpectrumAssignment fixGrid = postAlgoPathValidator.computeBestSpectrumAssignment(available, 8, isFlexGrid);
+
+        Assertions.assertEquals(expectedFixGrid, fixGrid);
+
+
+    }
+
+    @Test
+    void computeBestSpectrumAssignmentFlexGrid() {
+
+        NetworkTransactionService networkTransactionService = Mockito.mock(NetworkTransactionService.class);
+        PostAlgoPathValidator postAlgoPathValidator = new PostAlgoPathValidator(
+                networkTransactionService,
+                new BitSet()
+        );
+
+        BitSet available = new BitSet(768);
+        available.set(16, 28);
+
+        boolean isFlexGrid = true;
+        SpectrumAssignment expectedFlexGrid = new SpectrumAssignmentBuilder()
+                .setBeginIndex(Uint16.valueOf(20))
+                .setStopIndex(Uint16.valueOf(27))
+                .setFlexGrid(isFlexGrid)
+                .build();
+
+        SpectrumAssignment flexGrid = postAlgoPathValidator.computeBestSpectrumAssignment(available, 8, isFlexGrid);
+
+        Assertions.assertEquals(expectedFlexGrid, flexGrid);
+
+
+    }
+}
