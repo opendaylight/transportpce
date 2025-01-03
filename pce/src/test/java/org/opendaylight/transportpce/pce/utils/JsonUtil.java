d00c4e3440f56c1a23aac6da6164d863e4460940
Gilles Thouenon <gilles.thouenon@orange.com>
2025-01-03 17:53:34 +0100

    Migrate BindingCodecContext
    Use DefaultBindingDOMCodecFactory instead of BindingCodexContext to
    instantiate BindingDOMCodecServices.
    
    Change-Id: Iff3213fe59947bde5163e6da340739b877230aa6
    Signed-off-by: Gilles Thouenon <gilles.thouenon@orange.com>

--- pce/src/test/java/org/opendaylight/transportpce/pce/utils/JsonUtil.java
+++ pce/src/test/java/org/opendaylight/transportpce/pce/utils/JsonUtil.java
@@ -15,11 +15,10 @@ import java.util.Map.Entry;
 import java.util.ServiceLoader;
 import org.opendaylight.yangtools.binding.DataObject;
 import org.opendaylight.yangtools.binding.DataObjectReference;
-import org.opendaylight.yangtools.binding.data.codec.impl.BindingCodecContext;
+import org.opendaylight.yangtools.binding.data.codec.impl.di.DefaultBindingDOMCodecFactory;
 import org.opendaylight.yangtools.binding.data.codec.spi.BindingDOMCodecServices;
 import org.opendaylight.yangtools.binding.meta.YangModelBindingProvider;
 import org.opendaylight.yangtools.binding.meta.YangModuleInfo;
-import org.opendaylight.yangtools.binding.runtime.api.BindingRuntimeContext;
 import org.opendaylight.yangtools.binding.runtime.spi.BindingRuntimeHelpers;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
@@ -48,8 +47,8 @@ public final class JsonUtil {
         }
         /* Create the schema context for loaded models */
         this.schemaCtx = BindingRuntimeHelpers.createEffectiveModel(moduleInfos);
-        BindingRuntimeContext bindingContext = BindingRuntimeHelpers.createRuntimeContext();
-        bindingDOMCodecServices = new BindingCodecContext(bindingContext);
+        bindingDOMCodecServices = new DefaultBindingDOMCodecFactory()
+                .createBindingDOMCodec(BindingRuntimeHelpers.createRuntimeContext());
     }
 
     public static JsonUtil getInstance() {
