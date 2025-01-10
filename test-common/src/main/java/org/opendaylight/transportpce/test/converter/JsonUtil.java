540d98703129d5cfb81519e5a24e49625f2da771
Gilles Thouenon <gilles.thouenon@orange.com>
2025-01-10 15:06:02 +0100

    Move JsonUtil to test-common
    JsonUtil is a utility test class that is duplicated in several modules
    and used to deserialize json string into DataObject.
    Move it to test-common which is dedicated for this.
    
    Change-Id: Ia233daf88cf055d4074dbaa547afdbff7975cbe0
    Signed-off-by: Gilles Thouenon <gilles.thouenon@orange.com>

--- test-common/src/main/java/org/opendaylight/transportpce/test/converter/JsonUtil.java
+++ test-common/src/main/java/org/opendaylight/transportpce/test/converter/JsonUtil.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.transportpce.networkmodel.util.test;
+package org.opendaylight.transportpce.test.converter;
 
 import com.google.gson.stream.JsonReader;
 import java.io.IOException;
@@ -56,7 +56,7 @@ public final class JsonUtil {
                 .createBindingDOMCodec(BindingRuntimeHelpers.createRuntimeContext()));
     }
 
-    public static JsonUtil getInstance() {
+    public static synchronized JsonUtil getInstance() {
         if (instance == null) {
             instance = new JsonUtil();
         }
