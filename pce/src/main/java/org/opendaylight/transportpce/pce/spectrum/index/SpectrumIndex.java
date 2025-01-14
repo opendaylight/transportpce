635186c6bc3df717dc37cc2e5f92af85c33cdaa6
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2025-01-14 14:30:51 +0100

    New packages aiding finding available frequencies
    OVERVIEW
    
    The classes in this commit finds an available frequency range by
    iterating through a BitSet representing available frequencies
    
    The iteration uses the base frequency (e.g. 193.1THz) index location
    in the BitSet as the reference point (as opposed to the start or
    end of the BitSet). In essence, an adaption to the
    center-freq-granularity as specified in mc-capability-profile in
    org-openroadm-device.yang.
    
    Also, the iteration moves in "center frequency granularity" steps
    through the BitSet. The intention is to allow the client to use the
    frequency granularity settings of an SRG (for example) to find
    suitable available frequency ranges in a service path.
    
    BaseFrequency
        Contains a method finding the reference frequency index nr.
        i.e. the index nr for the anchor frequency on a spectrum grid.
    
    SpectrumIndex
        Finds the first or last usable center frequency index in the
        BitSet.
        Depending on the center frequency granularity width
        (i.e nr of slots relative to 193.1) and the service slot width,
        the first/last possible center frequency index may be at different
        locations.
    
    AssignSpectrumHighToLow/AssignSpectrumLowToHigh
        Uses the reference frequency index nr and first/last center
        frequency index to iterate through a BitSet representing available
        frequencies and tries to locate an available range suitable for
        the service. The classes either starts from the highest or lowest
        frequency in the range.
    
    SpectrumAssignmentState
        May be used with together with a system variable to return either
        AssignSpectrumHighToLow or AssignSpectrumLowToHigh, thus allowing
        from which end of the spectrum the application should start when
        finding available frequencies.
    
    EXAMPLE
    
    /**
     * Find a frequency range for a new service
     *
     * @param pceNodes The nodes in the service
     * @param serviceFrequencySlotCount The nr of slots required for
     *                                  the new service
     * @param assignableBitset A bitset representing which slots
     *                         are available
     * @return A start and stop index nr forming a range of frequencies
     *         available for the service.
     */
    private SpectrumAssignment availableFrequencyRange(
            Set<PceNode> pceNodes,
            int serviceFrequencySlotCount,
            BitSet assignableBitset) {
    
        //Create an empty collection of center frequency granularities.
        //An empty collection will use 50GHz as the default granularity.
        Collection centerFrequencyGranularityCollection =
            new CenterFrequencyGranularityCollection(50);
    
        //Iterate through the nodes in the path and add each
        //center frequency granularity to the collection.
        for (PceNode pceNode : pceNodes) {
            centerFrequencyGranularityCollection.add(
                pceNode.getCentralFreqGranularity()
            );
        }
    
        Base baseFrequency = new BaseFrequency();
    
        //Use the strategy of going from high frequencies to low
        //when finding an available range.
        Assign assignSpectrum =
                new AssignSpectrumHighToLow(new SpectrumIndex());
    
        //Find an available frequency range
        Range range = assignSpectrum.range(
                768,
                baseFrequency.referenceFrequencySpectrumIndex(
                        193.1,
                        191.325,
                        6.25
                ),
                assignableBitset,
                centerFrequencyGranularityCollection.slots(6.25),
                serviceFrequencySlotCount
        );
    
        //Return the available frequency range as a begin and stop index.
        return new SpectrumAssignmentBuilder()
                .setBeginIndex(Uint16.valueOf(range.lower()))
                .setStopIndex(Uint16.valueOf(range.upper()))
                .setFlexGrid(true)
                .build();
    
    }
    
    JIRA: TRNSPRTPCE-849
    Change-Id: I822093783ff36518078cdfe735d804dc91df7930
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

---
+++ pce/src/main/java/org/opendaylight/transportpce/pce/spectrum/index/SpectrumIndex.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright © 2024 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.pce.spectrum.index;
+
+public class SpectrumIndex implements Index {
+
+    @Override
+    public int firstCenterFrequencyIndex(
+        int centerFrequencySlotWidth, int baseFrequencySlotIndex, int serviceSlotWidth) {
+
+        if (serviceSlotWidth % 2 != 0) {
+            throw new RuntimeException("Cannot process an odd serviceSlotWidth");
+        }
+
+        int slotIndexCandidate = (baseFrequencySlotIndex % centerFrequencySlotWidth);
+
+        while (slotIndexCandidate < serviceSlotWidth / 2) {
+            slotIndexCandidate += centerFrequencySlotWidth;
+        }
+
+        return slotIndexCandidate;
+    }
+
+    @Override
+    public int lastCenterFrequencyIndex(int centerFrequencySlotWidth, int baseFrequencySlotIndex, int serviceSlotWidth,
+            int effectiveBits) {
+
+        if (serviceSlotWidth % 2 != 0) {
+            throw new RuntimeException("Cannot process an odd serviceSlotWidth");
+        }
+
+        int slotIndexCandidate =
+                effectiveBits - (((effectiveBits - baseFrequencySlotIndex) % centerFrequencySlotWidth));
+
+        while ((effectiveBits - slotIndexCandidate) < serviceSlotWidth / 2) {
+            slotIndexCandidate -= centerFrequencySlotWidth;
+        }
+
+        return slotIndexCandidate;
+    }
+}
