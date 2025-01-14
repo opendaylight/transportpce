4ab8a93c2df044c24c55d0973c6712971c4479c7
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2025-01-14 14:30:51 +0100

    New packages aiding finding available frequencies
    OVERVIEW
    
    The classes in this commit finds an available frequency range by
    iterating through a BitSet representing available frequencies
    
    The iteration uses the base frequency (e.g. 193.1THz) index location
    in the BitSet as the reference point (as opposed to the start or
    end of the BitSet). In essence, an adaption to the
    center-freq-granularity as specified in mc-capability-profile in
    org-openroadm-device.yang.
    
    Also, the iteration moves in "center frequency granularity" steps
    through the BitSet. The intention is to allow the client to use the
    frequency granularity settings of an SRG (for example) to find
    suitable available frequency ranges in a service path.
    
    BaseFrequency
        Contains a method finding the reference frequency index nr.
        i.e. the index nr for the anchor frequency on a spectrum grid.
    
    SpectrumIndex
        Finds the first or last usable center frequency index in the
        BitSet.
        Depending on the center frequency granularity width
        (i.e nr of slots relative to 193.1) and the service slot width,
        the first/last possible center frequency index may be at different
        locations.
    
    AssignSpectrumHighToLow/AssignSpectrumLowToHigh
        Uses the reference frequency index nr and first/last center
        frequency index to iterate through a BitSet representing available
        frequencies and tries to locate an available range suitable for
        the service. The classes either starts from the highest or lowest
        frequency in the range.
    
    SpectrumAssignmentState
        May be used with together with a system variable to return either
        AssignSpectrumHighToLow or AssignSpectrumLowToHigh, thus allowing
        from which end of the spectrum the application should start when
        finding available frequencies.
    
    EXAMPLE
    
    /**
     * Find a frequency range for a new service
     *
     * @param pceNodes The nodes in the service
     * @param serviceFrequencySlotCount The nr of slots required for
     *                                  the new service
     * @param assignableBitset A bitset representing which slots
     *                         are available
     * @return A start and stop index nr forming a range of frequencies
     *         available for the service.
     */
    private SpectrumAssignment availableFrequencyRange(
            Set<PceNode> pceNodes,
            int serviceFrequencySlotCount,
            BitSet assignableBitset) {
    
        //Create an empty collection of center frequency granularities.
        //An empty collection will use 50GHz as the default granularity.
        Collection centerFrequencyGranularityCollection =
            new CenterFrequencyGranularityCollection(50);
    
        //Iterate through the nodes in the path and add each
        //center frequency granularity to the collection.
        for (PceNode pceNode : pceNodes) {
            centerFrequencyGranularityCollection.add(
                pceNode.getCentralFreqGranularity()
            );
        }
    
        Base baseFrequency = new BaseFrequency();
    
        //Use the strategy of going from high frequencies to low
        //when finding an available range.
        Assign assignSpectrum =
                new AssignSpectrumHighToLow(new SpectrumIndex());
    
        //Find an available frequency range
        Range range = assignSpectrum.range(
                768,
                baseFrequency.referenceFrequencySpectrumIndex(
                        193.1,
                        191.325,
                        6.25
                ),
                assignableBitset,
                centerFrequencyGranularityCollection.slots(6.25),
                serviceFrequencySlotCount
        );
    
        //Return the available frequency range as a begin and stop index.
        return new SpectrumAssignmentBuilder()
                .setBeginIndex(Uint16.valueOf(range.lower()))
                .setStopIndex(Uint16.valueOf(range.upper()))
                .setFlexGrid(true)
                .build();
    
    }
    
    JIRA: TRNSPRTPCE-849
    Change-Id: I822093783ff36518078cdfe735d804dc91df7930
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

---
+++ pce/src/test/java/org/opendaylight/transportpce/pce/spectrum/assignment/SpectrumIndexTest.java
@@ -0,0 +1,192 @@
+/*
+ * Copyright © 2024 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.pce.spectrum.assignment;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.opendaylight.transportpce.pce.spectrum.index.Index;
+import org.opendaylight.transportpce.pce.spectrum.index.SpectrumIndex;
+
+// These tests will verify the first/last possible center frequency spectrum index.
+class SpectrumIndexTest {
+
+    @Test
+    void highighToLowowCenterFrequencyGranularityEqualToServiceSlotWidth() {
+
+        Index spectrumIndex = new SpectrumIndex();
+
+        //Given that...
+        // 1) ... the base frequency slot index (e.g. 193.1) is found at index 284
+        // 2) ... and the center frequency granularity slot width is 8
+        // 3) ... and the service frequency slot width is 8 (e.g 8 x 6.25 = 50GHz)
+        // we expect the LAST potential center frequency slot at index 764
+        // (i.e. possible center frequencies are at ..., 284, 292, 300, ..., 756, 764)
+        Assertions.assertEquals(764, spectrumIndex.lastCenterFrequencyIndex(8, 284, 8, 768));
+
+    }
+
+    @Test
+    void highToLowCenterFrequencyGranularityLessThanServiceSlotWidth() {
+
+        Index spectrumIndex = new SpectrumIndex();
+
+        //Given that...
+        // 1) ... the base frequency slot index (e.g. 193.1) is found at index 284
+        // 2) ... and the center frequency granularity slot width is 2
+        // 3) ... and the service frequency slot width is 8 (e.g 8 x 6.25 = 50GHz)
+        // we expect the LAST potential center frequency slot at index 764
+        // (i.e. possible center frequencies are at ..., 284, 286, 288, ..., 762, 764, 766).
+        // A service of 8 slots need a center frequency ± 4 slots. Choosing center frequency slot index = 766
+        // will therefore not work.
+        Assertions.assertEquals(764, spectrumIndex.lastCenterFrequencyIndex(2, 284, 8, 768));
+
+    }
+
+    @Test
+    void highToLowMinimumCenterFrequencyGranularityLessThanServiceSlotWidth() {
+
+        Index spectrumIndex = new SpectrumIndex();
+
+        Assertions.assertEquals(764, spectrumIndex.lastCenterFrequencyIndex(1, 284, 8, 768));
+
+    }
+
+    @Test
+    void highToLowCenterFrequencyGranularityMoreThanServiceSlotWidth() {
+
+        Index spectrumIndex = new SpectrumIndex();
+
+        //Given that...
+        // 1) ... the base frequency slot index (e.g. 193.1) is found at index 284
+        // 2) ... and the center frequency granularity slot width is 2
+        // 3) ... and the service frequency slot width is 8 (e.g 8 x 6.25 = 50GHz)
+        // we expect the first center frequency slot at index 764
+        // (i.e. possible center frequencies are at ..., 284, 300, 316, ..., 732, 748, 764)
+        Assertions.assertEquals(764, spectrumIndex.lastCenterFrequencyIndex(16, 284, 8, 768));
+
+    }
+
+    @Test
+    void highToLowCenterFrequencyGranularityLessThanServiceSlotWidths() {
+
+        Index spectrumIndex = new SpectrumIndex();
+
+        //Given that...
+        // 1) ... the base frequency slot index (e.g. 193.1) is found at index 284
+        // 2) ... and the center frequency granularity slot width is 2
+        // 3) ... and the service frequency slot width is 38 (e.g 26 x 6.25 = 162.5GHz)
+        // we expect the first center frequency slot at index 748
+        // (i.e. possible center frequencies are at ..., 284, 300, 316, ..., 732, 748, 764)
+        Assertions.assertEquals(748, spectrumIndex.lastCenterFrequencyIndex(16, 284, 26, 768));
+
+    }
+
+    @Test
+    void highToLowCenterFrequencyGranularityMoreThanServiceSlotWidthDifferentReferenceIndex() {
+
+        Index spectrumIndex = new SpectrumIndex();
+
+        //Given that...
+        // 1) ... the base frequency slot index (e.g. 193.1) is found at index 280 (as opposed to 284)
+        // 2) ... and the center frequency granularity slot width is 16
+        // 3) ... and the service frequency slot width is 8 (e.g 8 x 6.25 = 50GHz)
+        // we expect the first center frequency slot at index 760
+        // (i.e. possible center frequencies are at ..., 280, 296, 312, ..., 728, 744, 760)
+        Assertions.assertEquals(760, spectrumIndex.lastCenterFrequencyIndex(16, 280, 8, 768));
+
+    }
+
+    //Low To High
+
+    @Test
+    void lowToHighCenterFrequencyGranularityEqualToServiceSlotWidth() {
+
+        Index spectrumIndex = new SpectrumIndex();
+
+        //Given that...
+        // 1) ... the base frequency slot index (e.g. 193.1) is found at index 284
+        // 2) ... and the center frequency granularity slot width is 8
+        // 3) ... and the service frequency slot width is 8 (e.g 8 x 6.25 = 50GHz)
+        // we expect the first center frequency slot at index 4
+        // (i.e. possible center frequencies are at 4, 12, 20, ...,268, 276, 284, ...)
+        Assertions.assertEquals(4, spectrumIndex.firstCenterFrequencyIndex(8, 284, 8));
+
+    }
+
+    @Test
+    void lowToHighCenterFrequencyGranularityLessThanServiceSlotWidth() {
+
+        Index spectrumIndex = new SpectrumIndex();
+
+        //Given that...
+        // 1) ... the base frequency slot index (e.g. 193.1) is found at index 284
+        // 2) ... and the center frequency granularity slot width is 2
+        // 3) ... and the service frequency slot width is 8 (e.g 8 x 6.25 = 50GHz)
+        // we expect the first center frequency slot at index 4.
+        // A service of 8 slots need a center frequency ± 4 slots. Choosing center frequency slot index = 2
+        // will not work since all indexes >= 0.
+        Assertions.assertEquals(4, spectrumIndex.firstCenterFrequencyIndex(2, 284, 8));
+
+    }
+
+    @Test
+    void lowToHighMinimumCenterFrequencyGranularityLessThanServiceSlotWidth() {
+
+        Index spectrumIndex = new SpectrumIndex();
+
+        Assertions.assertEquals(4, spectrumIndex.firstCenterFrequencyIndex(1, 284, 8));
+
+    }
+
+    @Test
+    void lowToHighCenterFrequencyGranularityMoreThanServiceSlotWidth() {
+
+        Index spectrumIndex = new SpectrumIndex();
+
+        //Given that...
+        // 1) ... the base frequency slot index (e.g. 193.1) is found at index 284
+        // 2) ... and the center frequency granularity slot width is 2
+        // 3) ... and the service frequency slot width is 8 (e.g 8 x 6.25 = 50GHz)
+        // we expect the first center frequency slot at index 12
+        // (i.e. possible center frequencies are at 12, 28, 44, ...,252, 268, 284, ...)
+        Assertions.assertEquals(12, spectrumIndex.firstCenterFrequencyIndex(16, 284, 8));
+
+    }
+
+    @Test
+    void lowToHighCenterFrequencyGranularityLessThanServiceSlotWidths() {
+
+        Index spectrumIndex = new SpectrumIndex();
+
+        //Given that...
+        // 1) ... the base frequency slot index (e.g. 193.1) is found at index 284
+        // 2) ... and the center frequency granularity slot width is 2
+        // 3) ... and the service frequency slot width is 38 (e.g 26 x 6.25 = 162.5GHz)
+        // we expect the first center frequency slot at index 12
+        // (i.e. possible center frequencies are at 28, 44, ...,252, 268, 284, ...)
+        Assertions.assertEquals(28, spectrumIndex.firstCenterFrequencyIndex(16, 284, 26));
+
+    }
+
+    @Test
+    void lowToHighCenterFrequencyGranularityMoreThanServiceSlotWidthDifferentReferenceIndex() {
+
+        Index spectrumIndex = new SpectrumIndex();
+
+        //Given that...
+        // 1) ... the base frequency slot index (e.g. 193.1) is found at index 280 (as opposed to 284)
+        // 2) ... and the center frequency granularity slot width is 16
+        // 3) ... and the service frequency slot width is 8 (e.g 8 x 6.25 = 50GHz)
+        // we expect the first center frequency slot at index 8
+        //(i.e. possible center frequencies are at 8, 24, 40, ...,248, 264, 280, ...)
+        Assertions.assertEquals(8, spectrumIndex.firstCenterFrequencyIndex(16, 280, 8));
+
+    }
+
+}
