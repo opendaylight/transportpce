1f61a3eed7cc45a4ea664592611800129a8ad3bf
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2025-01-14 14:30:51 +0100

    New packages aiding finding available frequencies
    OVERVIEW
    
    The classes in this commit finds an available frequency range by
    iterating through a BitSet representing available frequencies.
    
    The iteration uses the base frequency (e.g. 193.1THz) index location
    in the BitSet as the reference point (as opposed to the start or
    end of the BitSet).
    
    Also, the iteration moves in "center frequency granularity" steps
    through the BitSet. The intention is to allow the client to use the
    frequency granularity settings of an SRg, for example to find suitable
    available frequency ranges.
    
    BaseFrequency
        Contains a method finding the reference frequency index nr.
    
    SpectrumIndex
        Finds the first center frequency index in the BitSet. Depending on
        the center frequency granularity width (i.e nr of slots) the
        first possible center frequency index may be at different
        locations.
    
    AssignSpectrum
        Uses the reference frequency index nr and first center frequency
        index to iterate through a BitSet representing available
        frequencies and tries to locate an available range suitable for
        the service.
    
    EXAMPLE
    
    /**
     * Find a frequency range for a new service
     *
     * @param pceNodes The nodes in the service
     * @param serviceFrequencySlotCount The nr of slots required for
     *                                  the new service
     * @param assignableBitset A bitset representing which slots
     *                         are available
     * @return A start and stop index nr forming a range of frequencies
     *         available for the service.
     */
    private SpectrumAssignment availableFrequencyRange(
            Set<PceNode> pceNodes,
            int serviceFrequencySlotCount,
            BitSet assignableBitset) {
    
        Collection centerFrequencyGranularityCollection =
            new CenterFrequencyGranularityCollection(50);
    
        for (PceNode pceNode : pceNodes) {
            centerFrequencyGranularityCollection.add(
                pceNode.getCentralFreqGranularity()
            );
        }
    
        Base baseFrequency = new BaseFrequency();
    
        Assign assignSpectrum = new AssignSpectrum(new SpectrumIndex());
    
        Range range = assignSpectrum.range(
                768,
                baseFrequency.referenceFrequencySpectrumIndex(
                        193.1,
                        191.325,
                        6.25
                ),
                assignableBitset,
                centerFrequencyGranularityCollection.slots(6.25),
                serviceFrequencySlotCount
        );
    
        return new SpectrumAssignmentBuilder()
                .setBeginIndex(Uint16.valueOf(range.lower()))
                .setStopIndex(Uint16.valueOf(range.upper()))
                .setFlexGrid(true)
                .build();
    
    }
    
    JIRA: TRNSPRTPCE-849
    Change-Id: I822093783ff36518078cdfe735d804dc91df7930
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

---
+++ pce/src/main/java/org/opendaylight/transportpce/pce/spectrum/index/BaseFrequency.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2024 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.pce.spectrum.index;
+
+import java.math.BigDecimal;
+
+public class BaseFrequency implements Base {
+
+    @Override
+    public int referenceFrequencySpectrumIndex(
+        double referenceFrequencyTHz, double edgeFrequencyTHz, double frequencyGranularityGHz) {
+
+        BigDecimal referenceFrequency = BigDecimal.valueOf(referenceFrequencyTHz);
+        BigDecimal edgeFrequency = BigDecimal.valueOf(edgeFrequencyTHz);
+        BigDecimal granularity = (BigDecimal.valueOf(frequencyGranularityGHz)).multiply(BigDecimal.valueOf(0.001));
+
+        if (referenceFrequency.compareTo(edgeFrequency) < 0) {
+            throw new NoIndexFoundException(String.format(
+                    "Cannot find an index for %s (THz) since it's lower than the edge frequency %s (THz).",
+                    referenceFrequencyTHz, edgeFrequencyTHz));
+        }
+
+        if (referenceFrequency.subtract(edgeFrequency).remainder(granularity).compareTo(BigDecimal.ZERO) != 0) {
+            throw new NoIndexFoundException(
+                String.format("Cannot find an index for %s (THz) since it's not on a grid"
+                                + " separated by %s (GHz) starting with the edge frequency %s (THz).",
+                        referenceFrequencyTHz, frequencyGranularityGHz, edgeFrequencyTHz));
+        }
+
+        return referenceFrequency.subtract(edgeFrequency).divide(granularity).intValue();
+    }
+}
