3ccbfd3a8e37aacf5157b62d6c12c850d63af531
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2025-01-14 14:02:38 +0100

    New package for center frequency granularity LCM
    Introduces a new collection used for storing center frequency
    granularity values. The collection makes it easier to calculate
    Least Common Multiple values for multiple SRGs and determine by
    how many slots each center frequency is separated (e.g. on a 768 grid
    where each slot is 6.25GHz wide).
    
    Example
    
    Collection collection = new CenterFrequencyGranularityCollection(50);
    collection.add(50.0);
    collection.add(6.25);
    collection.add(75);
    
    System.out.println(collection.leastCommonMultiplierInGHz());
    \> 150
    
    System.out.println(collection.slots(6.25));
    \> 24
    
    JIRA: TRNSPRTPCE-849
    Change-Id: I8529a77b7f5aed814e9bb8a85faa2d834ad66acd
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

---
+++ pce/src/main/java/org/opendaylight/transportpce/pce/spectrum/centerfrequency/CenterFrequencyGranularityCollection.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright © 2024 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.pce.spectrum.centerfrequency;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class CenterFrequencyGranularityCollection implements Collection {
+
+    private final Set<BigDecimal> centerFrequencyGranularitiesHz;
+
+    private final double defaultFrequencyGranularity;
+
+    public CenterFrequencyGranularityCollection(double defaultFrequencyGranularity) {
+        this.defaultFrequencyGranularity = defaultFrequencyGranularity;
+        centerFrequencyGranularitiesHz = new LinkedHashSet<>();
+    }
+
+    @Override
+    public boolean add(BigDecimal centerFrequencyGranularityGHz) {
+        if (centerFrequencyGranularityGHz != null) {
+            return this.centerFrequencyGranularitiesHz.add(
+                new BigDecimal(centerFrequencyGranularityGHz
+                    .multiply(BigDecimal.valueOf(1000000000)).stripTrailingZeros().toPlainString()
+                )
+            );
+        }
+        return false;
+    }
+
+    @Override
+    public boolean add(float centerFrequencyGranularityGHz) {
+        return add(new BigDecimal(centerFrequencyGranularityGHz));
+    }
+
+    @Override
+    public boolean add(double centerFrequencyGranularityGHz) {
+        return add(new BigDecimal(centerFrequencyGranularityGHz));
+    }
+
+    @Override
+    public Set<BigDecimal> set() {
+        return new HashSet<>(centerFrequencyGranularitiesHz)
+            .stream().map(frequency -> frequency.divide(BigDecimal.valueOf(1000000000)))
+            .collect(Collectors.toSet());
+    }
+
+    @Override
+    public BigDecimal leastCommonMultipleInGHz() {
+
+        if (centerFrequencyGranularitiesHz.isEmpty()) {
+            return new BigDecimal(BigDecimal.valueOf(defaultFrequencyGranularity).stripTrailingZeros().toPlainString());
+        }
+
+        Iterator<BigDecimal> iterator = centerFrequencyGranularitiesHz.iterator();
+        if (centerFrequencyGranularitiesHz.size() == 1) {
+            return iterator.next().divide(BigDecimal.valueOf(1000000000));
+        }
+
+        BigInteger lcm = new BigInteger(String.valueOf(iterator.next()));
+
+        while (iterator.hasNext()) {
+            lcm = this.leastCommonMultiple(lcm, new BigInteger(String.valueOf(iterator.next())));
+        }
+
+        return new BigDecimal(lcm.toString()).divide(BigDecimal.valueOf(1000000000));
+    }
+
+    private BigInteger leastCommonMultiple(BigInteger numberOne, BigInteger numberTwo) {
+        BigInteger greatestCommonDivisor = numberOne.gcd(numberTwo);
+        BigInteger absProduct = numberOne.multiply(numberTwo).abs();
+
+        return absProduct.divide(greatestCommonDivisor);
+    }
+
+    @Override
+    public int slots(BigDecimal frequencyGranularityGHz) {
+
+        BigDecimal lcm = leastCommonMultipleInGHz();
+
+        if (lcm.remainder(frequencyGranularityGHz).compareTo(BigDecimal.ZERO) != 0) {
+            throw new LeastCommonMultipleException(
+                String.format(
+                    "The LCM center frequency %s is not evenly dividable by frequency granularity %s",
+                    lcm,
+                    frequencyGranularityGHz
+                )
+            );
+        }
+
+        return lcm.divide(frequencyGranularityGHz).intValue();
+
+    }
+
+    @Override
+    public int slots(double frequencyGranularityGHz) {
+        return slots(new BigDecimal(frequencyGranularityGHz));
+    }
+}
