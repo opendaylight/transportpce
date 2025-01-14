ae01a66d7a26cdc9bcf152340a9606333957b683
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2025-01-14 14:02:38 +0100

    New package for center frequency granularity LCM
    Introduces a new collection used for storing center frequency
    granularity values. The collection makes it easier to calculate
    Least Common Multiple values for multiple SRGs and determine by
    how many slots each center frequency is separated (e.g. on a 768 grid
    where each slot is 6.25GHz wide).
    
    Example
    
    Collection collection = new CenterFrequencyGranularityCollection(50);
    collection.add(50.0);
    collection.add(6.25);
    collection.add(75);
    
    System.out.println(collection.leastCommonMultiplierInGHz());
    \> 150
    
    System.out.println(collection.slots(6.25));
    \> 24
    
    JIRA: TRNSPRTPCE-849
    Change-Id: I8529a77b7f5aed814e9bb8a85faa2d834ad66acd
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

---
+++ pce/src/main/java/org/opendaylight/transportpce/pce/spectrum/centerfrequency/Collection.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright © 2024 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.pce.spectrum.centerfrequency;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+public interface Collection {
+
+    /**
+     * Add a center frequency to this collection.
+     * @param centerFrequencyGranularityGHz A center frequency, e.g. for a node.
+     */
+    boolean add(BigDecimal centerFrequencyGranularityGHz);
+
+    /**
+     * Add a center frequency to this collection.
+     * @param centerFrequencyGranularityGHz A center frequency, e.g. for a node.
+     */
+    boolean add(float centerFrequencyGranularityGHz);
+
+    /**
+     * Add a center frequency to this collection.
+     * @param centerFrequencyGranularityGHz A center frequency, e.g. for a node.
+     */
+    boolean add(double centerFrequencyGranularityGHz);
+
+    /**
+     * A unique list of frequency granularities in this collection.
+     */
+    Set<BigDecimal> set();
+
+    /**
+     * Finds the least common multiple of all center frequency granularities in this collection.
+     * @throws LeastCommonMultipleException if no least common multiple is possible.
+     */
+    BigDecimal leastCommonMultipleInGHz();
+
+    /**
+     * The nr of slots with a width of frequencyGranularityGHz this collection will occupy.
+     *
+     * <p>Calculates the least common multiplier for this collection of frequency granularities
+     * and then calculates how many slots it will occupy.</p>
+     * @throws LeastCommonMultipleException In case this collection is empty or if the lcm is not evenly dividable
+     *                                        by frequencyGranularityGHz.
+     */
+    int slots(BigDecimal frequencyGranularityGHz);
+
+    /**
+     * The nr of slots with a width of frequencyGranularityGHz this collection will occupy.
+     *
+     * @see Collection#slots(BigDecimal)
+     */
+    int slots(double frequencyGranularityGHz);
+
+}
