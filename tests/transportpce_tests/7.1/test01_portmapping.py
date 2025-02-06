2da81768c83efb4ffb5ed2fe19bd6d0d3643bd1a
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2025-02-06 07:34:08 +0100

    Implement mc min/max slots in portmapping
    Portmapping for v2.2.1 and 7.1 is updated with mc capabilities
    min/max slots.
    
    JIRA: TRNSPRTPCE-849
    Change-Id: If08319dc3915e38cee089e47cbf239d18492cee5
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

--- tests/transportpce_tests/7.1/test01_portmapping.py
+++ tests/transportpce_tests/7.1/test01_portmapping.py
@@ -156,6 +156,8 @@ class TransportPCE400GPortMappingTesting(unittest.TestCase):
         self.assertEqual(res['mc-capabilities'][0]['mc-node-name'], 'XPDR-mcprofile')
         self.assertEqual(float(res['mc-capabilities'][0]['center-freq-granularity']), 3.125)
         self.assertEqual(float(res['mc-capabilities'][0]['slot-width-granularity']), 6.25)
+        self.assertEqual(float(res['mc-capabilities'][0]['min-slots']), 1)
+        self.assertEqual(float(res['mc-capabilities'][0]['max-slots']), 14)
 
     def test_09_mpdr_switching_pool(self):
         response = test_utils.get_portmapping_node_attr("XPDR-A2", "switching-pool-lcp", "1")
