903f78ca80c07a0819ad11c43dfc004e036cee37
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2025-02-21 13:12:56 +0100

    New packages aiding finding available frequencies
    OVERVIEW
    
    The classes in this commit finds an available frequency range by
    iterating through a BitSet representing available frequencies.
    
    The iteration uses the base frequency (i.e. 193.1THz, G.694.1) index
    location in the BitSet as the reference point (as opposed to the start
    or end of the BitSet). In essence, an adaption to the
    center-freq-granularity as specified in mc-capability-profile in
    org-openroadm-device.yang.
    
    Also, the iteration moves in "center frequency granularity" steps
    through the BitSet. The intention is to use the frequency granularity
    settings of all service path nodes to find a suitable available
    frequency range.
    
    EXAMPLE
    
    /**
     * Find a frequency range for a new service
     *
     * @param pceNodes The nodes in the service
     * @param serviceFrequencySlotCount The nr of slots required for
     *                                  the new service
     * @param assignableBitset A bitset representing which slots
     *                         are available
     * @return A start and stop index nr forming a range of frequencies
     *         available for the service.
     */
    private SpectrumAssignment availableFrequencyRange(
            Set<PceNode> pceNodes,
            int serviceFrequencySlotCount,
            BitSet assignableBitset) {
    
        //Create an empty collection of center frequency granularities.
        //An empty collection will use 50GHz as the default granularity.
        Collection centerFrequencyGranularityCollection =
            new CenterFrequencyGranularityCollection(50);
    
        //Iterate through the nodes in the path and add each
        //center frequency granularity to the collection.
        for (PceNode pceNode : pceNodes) {
            centerFrequencyGranularityCollection.add(
                pceNode.getCentralFreqGranularity()
            );
        }
    
        Base baseFrequency = new BaseFrequency();
    
        //Use the strategy of going from high frequencies to low
        //when finding an available range.
        Assign assignSpectrum =
                new AssignSpectrumHighToLow(new SpectrumIndex());
    
        //Find an available frequency range
        Range range = assignSpectrum.range(
                768,
                baseFrequency.referenceFrequencySpectrumIndex(
                        193.1,
                        191.325,
                        6.25
                ),
                assignableBitset,
                centerFrequencyGranularityCollection.slots(6.25),
                serviceFrequencySlotCount
        );
    
        //Return the available frequency range as a begin and stop index.
        return new SpectrumAssignmentBuilder()
                .setBeginIndex(Uint16.valueOf(range.lower()))
                .setStopIndex(Uint16.valueOf(range.upper()))
                .setFlexGrid(true)
                .build();
    
    }
    
    JIRA: TRNSPRTPCE-849
    Change-Id: I822093783ff36518078cdfe735d804dc91df7930
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

---
+++ pce/src/test/java/org/opendaylight/transportpce/pce/spectrum/assignment/AssignSpectrumHighToLowTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright © 2025 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.pce.spectrum.assignment;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.BitSet;
+import org.junit.jupiter.api.Test;
+import org.opendaylight.transportpce.pce.spectrum.index.SpectrumIndex;
+
+class AssignSpectrumHighToLowTest {
+
+    @Test
+    void testEmptyRange() {
+        Assign assignSpectrum = new AssignSpectrumHighToLow(new SpectrumIndex());
+        assertEquals(new IndexRange(0, 0), assignSpectrum.range(768,284,
+                new BitSet(),8,8
+        ));
+    }
+
+    @Test
+    void testLastBitsAreAvailable() {
+        Assign assignSpectrum = new AssignSpectrumHighToLow(new SpectrumIndex());
+        BitSet available = new BitSet();
+        available.set(750, 768);
+
+        assertEquals(new IndexRange(760, 767), assignSpectrum.range(768, 284, available, 8, 8));
+    }
+
+    @Test
+    void testSecondToLastBitsAreAvailable() {
+        Assign assignSpectrum = new AssignSpectrumHighToLow(new SpectrumIndex());
+        BitSet available = new BitSet();
+        available.set(740, 760);
+
+        assertEquals(new IndexRange(752, 759), assignSpectrum.range(768, 284, available, 8, 8));
+    }
+
+    @Test
+    void testFirstBitsAreAvailable() {
+        Assign assignSpectrum = new AssignSpectrumHighToLow(new SpectrumIndex());
+        BitSet available = new BitSet();
+        available.set(0, 8);
+
+        assertEquals(new IndexRange(0, 7), assignSpectrum.range(768, 284, available, 8, 8));
+    }
+
+    @Test
+    void testFirstBitsAreAvailableTwiceCenterFrequencyGranularity() {
+        Assign assignSpectrum = new AssignSpectrumHighToLow(new SpectrumIndex());
+        BitSet available = new BitSet();
+        available.set(0, 32);
+
+        assertEquals(new IndexRange(4, 19), assignSpectrum.range(768, 284, available, 16, 16));
+    }
+}
