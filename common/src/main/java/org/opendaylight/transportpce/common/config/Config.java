5e240740d2270ff5afedd01e5f34cf4cc21769cb
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2025-03-07 10:01:49 +0100

    Introduce env variables controlling timeout
    Variables controlling device read/write timout are added.
    
    Change-Id: If944ffc823965a16b9825c03cbf273545b21b6f1
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

---
+++ common/src/main/java/org/opendaylight/transportpce/common/config/Config.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright © 2025 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.common.config;
+
+import org.opendaylight.transportpce.common.time.Time;
+
+
+public interface Config {
+
+    /**
+     * While reading from the device, TPCE waits this amount of time for the device to respond.
+     *
+     * @return the device read timeout
+     */
+    Time deviceReadTimeout();
+
+    /**
+     * While writing to the device, TPCE waits this amount of time for the transaction to complete.
+     *
+     * @return the device write timeout
+     */
+    Time deviceWriteTimeout();
+
+}
