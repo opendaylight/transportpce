a7ea759521b1f50a2df6d591406b0185c38bd795
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2025-05-27 13:53:49 +0200

    Copy OpenROADM topology to tapi
    - TPCE throws a new event after a toplogy update (after a successful
      service create).
    - TAPI listens for the event and copies the toplogy updates to from
      OpenROADm to TAPI.
    
    JIRA: TRNSPRTPCE-865
    Change-Id: I45b482099c6fb9af0c0c0272bcc6b429e61126c9
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

---
+++ tapi/src/main/java/org/opendaylight/transportpce/tapi/openroadm/topology/Storage.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright © 2025 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.tapi.openroadm.topology;
+
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.rev180226.networks.Network;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.rev180226.networks.NetworkKey;
+import org.opendaylight.yangtools.binding.DataObjectIdentifier;
+
+public interface Storage {
+
+    Network openRoadmNetwork(DataObjectIdentifier.WithKey<Network, NetworkKey> openroadmTopologyIdentifier);
+
+}
--- tapi/src/test/java/org/opendaylight/transportpce/tapi/topology/TapiTopologyImplExceptionTest.java
+++ tapi/src/test/java/org/opendaylight/transportpce/tapi/topology/TapiTopologyImplExceptionTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2019 Orange, Inc. and others.  All rights reserved.
+ * Copyright © 2025 Smartoptics and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
