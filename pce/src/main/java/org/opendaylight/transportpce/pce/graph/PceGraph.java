96f8049d412942ab6570b66eea72ea7aeb28bfd1
orenais <olivier.renais@orange.com>
2025-06-26 19:02:53 +0200

    Rationalize use of PceNode/Links simplifying migration
    JIRA: TRNSPRTPCE-874
    Signed-off-by: orenais <olivier.renais@orange.com>
    Change-Id: If081d52543ceb65dbda2558dfcb055a5cc9cdaf9

--- pce/src/main/java/org/opendaylight/transportpce/pce/graph/PceGraph.java
+++ pce/src/main/java/org/opendaylight/transportpce/pce/graph/PceGraph.java
@@ -32,7 +32,7 @@ import org.opendaylight.transportpce.pce.constraints.PceConstraints;
 import org.opendaylight.transportpce.pce.input.ClientInput;
 import org.opendaylight.transportpce.pce.networkanalyzer.PceLink;
 import org.opendaylight.transportpce.pce.networkanalyzer.PceNode;
-import org.opendaylight.transportpce.pce.networkanalyzer.PceORLink;
+//import org.opendaylight.transportpce.pce.networkanalyzer.PceORLink;
 import org.opendaylight.transportpce.pce.networkanalyzer.PceResult;
 import org.opendaylight.transportpce.pce.networkanalyzer.PceResult.LocalCause;
 import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.pce.rev240205.PceConstraintMode;
@@ -52,7 +52,7 @@ public class PceGraph {
 
     // input
     private Map<NodeId, PceNode> allPceNodes = new HashMap<>();
-    private Map<LinkId, PceORLink> allPceLinks = new HashMap<>();
+    private Map<LinkId, PceLink> allPceLinks = new HashMap<>();
     private PceNode apceNode = null;
     private PceNode zpceNode = null;
     private String serviceType = "";
@@ -64,17 +64,17 @@ public class PceGraph {
 
     // results
     private PceResult pceResult = null;
-    private List<PceORLink> shortestPathAtoZ = null;
+    private List<PceLink> shortestPathAtoZ = null;
 
     // for path calculation
     Map<Integer, GraphPath<String, PceGraphEdge>> allWPaths = null;
 
-    private List<PceORLink> pathAtoZ = new ArrayList<>();
+    private List<PceLink> pathAtoZ = new ArrayList<>();
 
     private final NetworkTransactionService networkTransactionService;
 
     public PceGraph(PceNode aendNode, PceNode zendNode, Map<NodeId, PceNode> allPceNodes,
-            Map<LinkId, PceORLink> allPceLinks, PceConstraints pceHardConstraints,PceResult pceResult,
+            Map<LinkId, PceLink> allPceLinks, PceConstraints pceHardConstraints,PceResult pceResult,
             String serviceType, NetworkTransactionService networkTransactionService, PceConstraintMode mode,
             BitSet spectrumConstraint, ClientInput clientInput) {
         super();
@@ -204,7 +204,7 @@ public class PceGraph {
         return true;
     }
 
-    private boolean validateLinkforGraph(PceORLink pcelink) {
+    private boolean validateLinkforGraph(PceLink pcelink) {
 
         PceNode source = allPceNodes.get(pcelink.getSourceId());
         PceNode dest = allPceNodes.get(pcelink.getDestId());
@@ -240,15 +240,15 @@ public class PceGraph {
             Map.Entry<NodeId, PceNode> node = nodes.next();
 
             PceNode pcenode = node.getValue();
-            List<PceORLink> links = new ArrayList<>();
+            List<PceLink> links = new ArrayList<>();
             for (PceLink pcelink : pcenode.getOutgoingLinks()) {
-                links.add((PceORLink) pcelink);
+                links.add((PceLink) pcelink);
             }
             //List<PceLink> links = pcenode.getOutgoingLinks();
 
             LOG.debug("In populateGraph: use node for graph {}", pcenode);
 
-            for (PceORLink link : links) {
+            for (PceLink link : links) {
                 LOG.debug("In populateGraph node {} : add edge to graph {}", pcenode, link);
 
                 if (!validateLinkforGraph(link)) {
@@ -265,7 +265,7 @@ public class PceGraph {
         return true;
     }
 
-    private double chooseWeight(PceORLink link) {
+    private double chooseWeight(PceLink link) {
         // HopCount is default
         double weight = 1;
         switch (pceHardConstraints.getPceMetrics()) {
@@ -301,7 +301,7 @@ public class PceGraph {
         this.kpathsToBring = kpathsToBring;
     }
 
-    public List<PceORLink> getPathAtoZ() {
+    public List<PceLink> getPathAtoZ() {
         return shortestPathAtoZ;
     }
 
