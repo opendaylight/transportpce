9d4830cd906be614dfa54ce71422ccd5cc71f343
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2025-07-07 13:30:11 +0200

    Package for contentionless SRG information
    New package:    org.opendaylight.transportpce.common.srg
    The intention of the package is to...
    1. Provide a convenient way of reading SRG information from
       the datastore
    2. Determine whether or not a node (SRG) is contentionless
    
    Example
    
    /**
     * Read information/determine if node is contentionless.
     */
    public boolean isContentionLessSrg(
            Node node,
            NetworkNode networkNode,
            Storage storage) {
    
        if (!nodeType.equals(OpenroadmNodeType.SRG)) {
            return false;
        }
    
        return networkNode.contentionLess(
                new NetworkId(StringConstants.OPENROADM_NETWORK),
                node,
                storage
        );
    }
    
    JIRA: TRNSPRTPCE-252
    Change-Id: I373c412b363671bba9a7c73cfbb677180131ccd3
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

---
+++ common/src/main/java/org/opendaylight/transportpce/common/srg/storage/Storage.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright © 2025 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.common.srg.storage;
+
+import java.util.Map;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev250714.network.NodesKey;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev250714.shared.risk.group.SharedRiskGroup;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev250714.shared.risk.group.SharedRiskGroupKey;
+
+public interface Storage {
+
+    /**
+     * Read the Shared Risk Groups for a given node.
+     *
+     * @param nodeId the ID of the node
+     * @return a map of SharedRiskGroupKey to SharedRiskGroup
+     */
+    Map<SharedRiskGroupKey, SharedRiskGroup> read(String nodeId);
+
+    /**
+     * Read the Shared Risk Groups for a given node.
+     *
+     * @param nodeId the key of the node
+     * @return a map of SharedRiskGroupKey to SharedRiskGroup
+     */
+    Map<SharedRiskGroupKey, SharedRiskGroup> read(NodesKey nodeId);
+
+}
