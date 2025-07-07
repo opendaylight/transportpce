e0235a1bf40a577ade61a861bb80868918ae4ec6
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2025-07-07 12:45:44 +0200

    Add wavelength duplication to TPCE portmapping
    The yang model update is intended to be used by TransportPCE during
    device mount to store SRG wavelength duplication.
    
    The information may later be used during service setup.
    
    JIRA: TRNSPRTPCE-252
    Change-Id: I0961e51043f0e61cb430a205602efd66e36157ab
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

--- tapi/src/main/java/org/opendaylight/transportpce/tapi/topology/TapiNetworkModelService.java
+++ tapi/src/main/java/org/opendaylight/transportpce/tapi/topology/TapiNetworkModelService.java
@@ -7,8 +7,8 @@
  */
 package org.opendaylight.transportpce.tapi.topology;
 
-import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev250325.mapping.Mapping;
-import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev250325.network.Nodes;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev250714.mapping.Mapping;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev250714.network.Nodes;
 
 public interface TapiNetworkModelService {
 
