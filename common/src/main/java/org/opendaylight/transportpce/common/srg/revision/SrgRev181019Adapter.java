bf7d201c737714302c8e8a406b8d4d653c56d675
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2025-07-07 13:30:11 +0200

    Package for contentionless SRG information
    New package:    org.opendaylight.transportpce.common.srg
    
    The intention of the package is to...
    1. Provide a convenient way of reading SRG information from
       the datastore
    2. Determine whether or not a node (SRG) is contentionless
    
    Example
    
    /**
     * Read information from datastore and determine if node
     * is contentionless.
     */
    public boolean isContentionLessSrg(
            Node node,
            NetworkNode networkNode,
            Storage storage) {
    
        if (!nodeType.equals(OpenroadmNodeType.SRG)) {
            return false;
        }
    
        return networkNode.contentionLess(
                new NetworkId(StringConstants.OPENROADM_NETWORK),
                node,
                storage
        );
    }
    
    JIRA: TRNSPRTPCE-252
    Change-Id: I373c412b363671bba9a7c73cfbb677180131ccd3
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

---
+++ common/src/main/java/org/opendaylight/transportpce/common/srg/revision/SrgRev181019Adapter.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright © 2025 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.common.srg.revision;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev250905.shared.risk.group.SharedRiskGroupBuilder;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.device.rev181019.org.openroadm.device.container.org.openroadm.device.SharedRiskGroup;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.device.rev181019.org.openroadm.device.container.org.openroadm.device.SharedRiskGroupKey;
+
+/**
+ * The primary intent of this class is to adapt the Shared Risk Group (SRG) data model from
+ * one revision to another, specifically from Rev181019 to rev250714.
+ */
+public class SrgRev181019Adapter implements Rev250905 {
+
+    private final Map<SharedRiskGroupKey, SharedRiskGroup> sharedRiskGroups;
+
+    public SrgRev181019Adapter(Map<SharedRiskGroupKey, SharedRiskGroup> sharedRiskGroups) {
+        this.sharedRiskGroups = sharedRiskGroups;
+    }
+
+    @Override
+    public Map<org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping
+            .rev250905.shared.risk.group.SharedRiskGroupKey,
+            org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping
+                    .rev250905.shared.risk.group.SharedRiskGroup> srg() {
+
+        Map<org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping
+                .rev250905.shared.risk.group.SharedRiskGroupKey,
+                org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping
+                        .rev250905.shared.risk.group.SharedRiskGroup> rev250714ShareRiskGroups = new HashMap<>();
+
+        for (Map.Entry<SharedRiskGroupKey, SharedRiskGroup> entries : sharedRiskGroups.entrySet()) {
+            SharedRiskGroup srgRev181019 = entries.getValue();
+
+            SharedRiskGroupBuilder rev250714SharedRiskGroupBuilder = new SharedRiskGroupBuilder();
+            org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping
+                    .rev250905.shared.risk.group.SharedRiskGroup rev250714Srg = rev250714SharedRiskGroupBuilder
+                    .setSrgNumber(srgRev181019.getSrgNumber())
+                    .setWavelengthDuplication(
+                            org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping
+                                    .rev250905.shared.risk.group.SharedRiskGroup.WavelengthDuplication.forValue(
+                                            srgRev181019.getWavelengthDuplication().getIntValue())
+                    ).setSrgNumber(srgRev181019.getSrgNumber())
+                    .build();
+
+            rev250714ShareRiskGroups.put(rev250714Srg.key(), rev250714Srg);
+        }
+
+        return rev250714ShareRiskGroups;
+    }
+}
---
+++ common/src/test/java/org/opendaylight/transportpce/common/srg/storage/CacheTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright © 2025 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.common.srg.storage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev250905.network.NodesKey;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev250905.shared.risk.group.SharedRiskGroup;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev250905.shared.risk.group.SharedRiskGroupBuilder;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev250905.shared.risk.group.SharedRiskGroupKey;
+import org.opendaylight.yangtools.yang.common.Uint16;
+
+class CacheTest {
+
+    @Test
+    void readFromStorage() {
+        Map<SharedRiskGroupKey, SharedRiskGroup> sharedRiskGroupMap = new HashMap<>();
+        SharedRiskGroup sharedRiskGroup = new SharedRiskGroupBuilder().setSrgNumber(Uint16.ONE).build();
+        sharedRiskGroupMap.put(sharedRiskGroup.key(), sharedRiskGroup);
+
+        Storage fakeStore = mock(Storage.class);
+        when(fakeStore.read(new NodesKey("ROADM-A-SRG1"))).thenReturn(sharedRiskGroupMap);
+
+        Storage cache = new Cache(fakeStore);
+
+        assertEquals(sharedRiskGroupMap, cache.read("ROADM-A-SRG1"));
+
+        verify(fakeStore, times(1)).read(new NodesKey("ROADM-A-SRG1"));
+    }
+
+    @Test
+    void multipleReadRequestsOnlyReadOnceFromDatastore() {
+        Map<SharedRiskGroupKey, SharedRiskGroup> sharedRiskGroupMap = new HashMap<>();
+        SharedRiskGroup sharedRiskGroup = new SharedRiskGroupBuilder().setSrgNumber(Uint16.ONE).build();
+        sharedRiskGroupMap.put(sharedRiskGroup.key(), sharedRiskGroup);
+
+        Storage fakeStore = mock(Storage.class);
+        when(fakeStore.read(new NodesKey("ROADM-A-SRG1"))).thenReturn(sharedRiskGroupMap);
+
+        Storage cache = new Cache(fakeStore);
+
+        assertEquals(sharedRiskGroupMap, cache.read("ROADM-A-SRG1"));
+
+        assertEquals(sharedRiskGroupMap, cache.read("ROADM-A-SRG1"));
+
+        verify(fakeStore, times(1)).read(new NodesKey("ROADM-A-SRG1"));
+    }
+
+    @Test
+    void assertReadDontReturnNull() {
+        Map<SharedRiskGroupKey, SharedRiskGroup> sharedRiskGroupMap = new HashMap<>();
+        SharedRiskGroup sharedRiskGroup = new SharedRiskGroupBuilder().setSrgNumber(Uint16.ONE).build();
+        sharedRiskGroupMap.put(sharedRiskGroup.key(), sharedRiskGroup);
+
+        Storage fakeStore = mock(Storage.class);
+
+        Storage cache = new Cache(fakeStore);
+
+        assertNotNull(cache.read("ROADM-B-SRG1"));
+    }
+}
