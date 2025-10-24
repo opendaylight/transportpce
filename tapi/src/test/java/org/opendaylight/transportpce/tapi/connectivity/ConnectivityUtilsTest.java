bd7d27b456ac23a964795c724a9873cff2a2f113
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2025-10-24 18:18:27 +0200

    Refactor ConnectivityUtils add unit tests for TP
    BACKGROUND
    
    In ConnectivityUtils there is a method called
    createConnectionsFromService(...). The method essentially process a
    pathDescription, collecting ROADM, and Xponder, and degrees etc node
    names/termination points and saving those into these lists:
    
      List<String> xpdrClientTplist
      List<String> xpdrNetworkTplist
      List<String> rdmAddDropTplist
      List<String> rdmDegTplist
      List<String> rdmNodelist
      List<String> xpdrNodelist
    
    CHANGES OVERVIEW
    
    - Refactoring the method createConnectionsFromService(...) in the class
      ConnectivityUtils. The rest of the changes are mostly a ripple effect
      as a direct result of the refactoring.
    
    - Introducing a new class capable of storing the collection (i.e. Lists)
      of IDs: TPAndNodeCollection. A unit test class is also added.
      The class TPAndNodeCollection ensures no duplicate values are stored
      and provides methods for returning various ID Lists.
    
    - Extract collection of IDs to its own method in ConnectivityUtils:
        IDCollection extractTPandNodeIds(PathDescription pathDescription).
    
      In essence, createConnectionsFromService(...) is now using the method
      extractTPandNodeIds(...) as a "helper" method to collect the IDs, as
      opposed to doing so directly.
    
    - Introducing a new test class ConnectivityUtilsTest, unit testing
      the new method extractTPandNodeIds(...) in the class
      ConnectivityUtils. This ensures further refactoring of
      extractTPandNodeIds(...), at a later time, can be verified.
    
    JIRA: TRNSPRTPCE-889
    Change-Id: Iad6ecb46fc77572de14ebbce82e22016da9f15eb
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

---
+++ tapi/src/test/java/org/opendaylight/transportpce/tapi/connectivity/ConnectivityUtilsTest.java
@@ -0,0 +1,295 @@
+/*
+ * Copyright © 2025 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.tapi.connectivity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.opendaylight.transportpce.common.network.NetworkTransactionService;
+import org.opendaylight.transportpce.servicehandler.service.ServiceDataStoreOperations;
+import org.opendaylight.transportpce.tapi.TapiConstants;
+import org.opendaylight.transportpce.tapi.utils.TapiContext;
+import org.opendaylight.transportpce.test.AbstractTest;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.pce.rev240205.service.path.rpc.result.PathDescriptionBuilder;
+import org.opendaylight.yang.gen.v1.http.org.openroadm.common.state.types.rev191129.State;
+import org.opendaylight.yang.gen.v1.http.org.transportpce.b.c._interface.pathdescription.rev230501.PathDescription;
+import org.opendaylight.yang.gen.v1.http.org.transportpce.b.c._interface.pathdescription.rev230501.path.description.AToZDirectionBuilder;
+import org.opendaylight.yang.gen.v1.http.org.transportpce.b.c._interface.pathdescription.rev230501.path.description.atoz.direction.AToZ;
+import org.opendaylight.yang.gen.v1.http.org.transportpce.b.c._interface.pathdescription.rev230501.path.description.atoz.direction.AToZBuilder;
+import org.opendaylight.yang.gen.v1.http.org.transportpce.b.c._interface.pathdescription.rev230501.path.description.atoz.direction.AToZKey;
+import org.opendaylight.yang.gen.v1.http.org.transportpce.b.c._interface.pathdescription.rev230501.pce.resource.ResourceBuilder;
+import org.opendaylight.yang.gen.v1.http.org.transportpce.b.c._interface.pathdescription.rev230501.pce.resource.resource.resource.LinkBuilder;
+import org.opendaylight.yang.gen.v1.http.org.transportpce.b.c._interface.pathdescription.rev230501.pce.resource.resource.resource.NodeBuilder;
+import org.opendaylight.yang.gen.v1.http.org.transportpce.b.c._interface.pathdescription.rev230501.pce.resource.resource.resource.TerminationPointBuilder;
+import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev221121.Uuid;
+
+
+class ConnectivityUtilsTest extends AbstractTest {
+
+    @Mock
+    private NetworkTransactionService networkTransactionService;
+
+    @Mock
+    private ServiceDataStoreOperations serviceDataStoreOperations;
+
+    @Mock
+    private TapiContext tapiContext;
+
+    @Test
+    void testRoadmToRoadmConnectivity() {
+        Map<AToZKey, AToZ> atoZMap = getAToZRoadmKeyAToZMap();
+
+        // Build the AToZDirection and PathDescription
+        AToZDirectionBuilder atoZDirectionBuilder = new AToZDirectionBuilder()
+                .setAToZ(atoZMap);
+
+        PathDescription pathDescription = new PathDescriptionBuilder()
+                .setAToZDirection(atoZDirectionBuilder.build())
+                .build();
+
+        ConnectivityUtils connectivityUtils = new ConnectivityUtils(
+                serviceDataStoreOperations,
+                new HashMap<>(),
+                tapiContext,
+                networkTransactionService,
+                new Uuid(TapiConstants.T0_FULL_MULTILAYER_UUID)
+        );
+        IDCollection idCollection = connectivityUtils.extractTPandNodeIds(pathDescription);
+
+        List<String> expectedRoadmNodeList = List.of("ROADM-A1", "ROADM-C1");
+        assertEquals(expectedRoadmNodeList, idCollection.rdmNodelist(), "ROADM node list mismatch");
+
+        List<String> expectedRoadmDegreeList = List.of(
+                "ROADM-A1+DEG2-TTP-TXRX",
+                "ROADM-C1+DEG1-TTP-TXRX"
+        );
+        assertEquals(expectedRoadmDegreeList, idCollection.rdmDegTplist(), "ROADM degree list mismatch");
+
+        List<String> expectedRoadmAddDropList = List.of(
+                "ROADM-A1+SRG1-PP1-TXRX",
+                "ROADM-C1+SRG1-PP1-TXRX"
+        );
+        assertEquals(expectedRoadmAddDropList, idCollection.rdmAddDropTplist(), "ROADM add/drop list mismatch");
+
+        List<String> expectedXpdrNodeList = List.of(
+                "SPDR-SA1-XPDR1",
+                "SPDR-SC1-XPDR1"
+        );
+        assertEquals(expectedXpdrNodeList, idCollection.xpdrNodelist(), "XPDR node list mismatch");
+
+        List<String> expectedXpdrNetworkList = List.of(
+                "SPDR-SA1-XPDR1+XPDR1-NETWORK1",
+                "SPDR-SC1-XPDR1+XPDR1-NETWORK1"
+        );
+        assertEquals(expectedXpdrNetworkList, idCollection.xpdrNetworkTplist(), "XPDR network list mismatch");
+
+        List<String> expectedXpdrClientList = List.of(); // empty
+        assertEquals(expectedXpdrClientList, idCollection.xpdrClientTplist(), "XPDR client list mismatch");
+    }
+
+    @Test
+    void testXpdrToXpdrConnectivity() {
+        // --- Build the PathDescription for this scenario ---
+        Map<AToZKey, AToZ> atoZMap = getXpdrToXpdrAtoZMap();
+
+        // Build PathDescription
+        AToZDirectionBuilder atoZDirectionBuilder = new AToZDirectionBuilder().setAToZ(atoZMap);
+        PathDescription pathDescription = new PathDescriptionBuilder()
+                .setAToZDirection(atoZDirectionBuilder.build())
+                .build();
+
+        ConnectivityUtils connectivityUtils = new ConnectivityUtils(
+                serviceDataStoreOperations,
+                new HashMap<>(),
+                tapiContext,
+                networkTransactionService,
+                new Uuid(TapiConstants.T0_FULL_MULTILAYER_UUID)
+        );
+
+        IDCollection idCollection = connectivityUtils.extractTPandNodeIds(pathDescription);
+
+        // Assertions
+        List<String> expectedRoadmNodeList = List.of();
+        assertEquals(expectedRoadmNodeList, idCollection.rdmNodelist(), "ROADM node list mismatch");
+
+        List<String> expectedRoadmDegreeList = List.of();
+        assertEquals(expectedRoadmDegreeList, idCollection.rdmDegTplist(), "ROADM degree list mismatch");
+
+        List<String> expectedRoadmAddDropList = List.of();
+        assertEquals(expectedRoadmAddDropList, idCollection.rdmAddDropTplist(), "ROADM add/drop list mismatch");
+
+        List<String> expectedXpdrNodeList = List.of("SPDR-SA1-XPDR1", "SPDR-SC1-XPDR1");
+        assertEquals(expectedXpdrNodeList, idCollection.xpdrNodelist(), "XPDR node list mismatch");
+
+        List<String> expectedXpdrNetworkList = List.of(
+                "SPDR-SA1-XPDR1+XPDR1-NETWORK1",
+                "SPDR-SC1-XPDR1+XPDR1-NETWORK1"
+        );
+        assertEquals(expectedXpdrNetworkList, idCollection.xpdrNetworkTplist(), "XPDR network list mismatch");
+
+        List<String> expectedXpdrClientList = List.of();
+        assertEquals(expectedXpdrClientList, idCollection.xpdrClientTplist(), "XPDR client list mismatch");
+    }
+
+    @Test
+    void testXpdrToXpdrWithClientConnectivity() {
+        Map<AToZKey, AToZ> atoZMap = getXpdrToXpdrClienmtAtoZMap();
+
+        // Build the AToZDirection and PathDescription
+        AToZDirectionBuilder atoZDirectionBuilder = new AToZDirectionBuilder()
+                .setAToZ(atoZMap);
+
+        PathDescription pathDescription = new PathDescriptionBuilder()
+                .setAToZDirection(atoZDirectionBuilder.build())
+                .build();
+
+        // --- Create ConnectivityUtils instance ---
+        ConnectivityUtils connectivityUtils = new ConnectivityUtils(
+                serviceDataStoreOperations,
+                new HashMap<>(),
+                tapiContext,
+                networkTransactionService,
+                new Uuid(TapiConstants.T0_FULL_MULTILAYER_UUID)
+        );
+
+        IDCollection idCollection = connectivityUtils.extractTPandNodeIds(pathDescription);
+
+        List<String> expectedRoadmNodeList = List.of();
+        assertEquals(expectedRoadmNodeList, idCollection.rdmNodelist(), "ROADM node list mismatch");
+
+        List<String> expectedRoadmDegreeList = List.of();
+        assertEquals(expectedRoadmDegreeList, idCollection.rdmDegTplist(), "ROADM degree list mismatch");
+
+        List<String> expectedRoadmAddDropList = List.of();
+        assertEquals(expectedRoadmAddDropList, idCollection.rdmAddDropTplist(), "ROADM add/drop list mismatch");
+
+        List<String> expectedXpdrNodeList = List.of("SPDR-SA1-XPDR1", "SPDR-SC1-XPDR1");
+        assertEquals(expectedXpdrNodeList, idCollection.xpdrNodelist(), "XPDR node list mismatch");
+
+        List<String> expectedXpdrNetworkList = List.of(
+                "SPDR-SA1-XPDR1+XPDR1-NETWORK1",
+                "SPDR-SC1-XPDR1+XPDR1-NETWORK1");
+        assertEquals(expectedXpdrNetworkList, idCollection.xpdrNetworkTplist(), "XPDR network list mismatch");
+
+        List<String> expectedXpdrClientList = List.of(
+                "SPDR-SA1-XPDR1+XPDR1-CLIENT1",
+                "SPDR-SC1-XPDR1+XPDR1-CLIENT1");
+        assertEquals(expectedXpdrClientList, idCollection.xpdrClientTplist(), "XPDR client list mismatch");
+    }
+
+    private Map<AToZKey, AToZ> getAToZRoadmKeyAToZMap() {
+        return buildAtoZMap(roadmPathElements());
+    }
+
+    private Map<AToZKey, AToZ> getXpdrToXpdrAtoZMap() {
+        List<PathElement> elements = List.of(
+                tp("0", "", "SPDR-SA1-XPDR1"),
+                node("1", "SPDR-SA1-XPDR1"),
+                tp("2", "XPDR1-NETWORK1", "SPDR-SA1-XPDR1"),
+                link("3", "OTU4-SPDR-SA1-XPDR1-XPDR1-NETWORK1toSPDR-SC1-XPDR1-XPDR1-NETWORK1"),
+                tp("4", "XPDR1-NETWORK1", "SPDR-SC1-XPDR1"),
+                node("5", "SPDR-SC1-XPDR1"),
+                tp("6", "", "SPDR-SC1-XPDR1")
+        );
+
+        Map<AToZKey, AToZ> map = new HashMap<>();
+        for (PathElement e : elements) {
+            AToZ item = new AToZBuilder()
+                    .setId(e.id)
+                    .setResource(e.resource)
+                    .build();
+            map.put(item.key(), item);
+        }
+        return map;
+    }
+
+    private Map<AToZKey, AToZ> getXpdrToXpdrClienmtAtoZMap() {
+        List<PathElement> elements = List.of(
+                tp("0", "XPDR1-CLIENT1", "SPDR-SA1-XPDR1"),
+                node("1", "SPDR-SA1-XPDR1"),
+                tp("2", "XPDR1-NETWORK1", "SPDR-SA1-XPDR1"),
+                link("3", "ODTU4-SPDR-SA1-XPDR1-XPDR1-NETWORK1toSPDR-SC1-XPDR1-XPDR1-NETWORK1"),
+                tp("4", "XPDR1-NETWORK1", "SPDR-SC1-XPDR1"),
+                node("5", "SPDR-SC1-XPDR1"),
+                tp("6", "XPDR1-CLIENT1", "SPDR-SC1-XPDR1")
+        );
+
+        return buildAtoZMap(elements);
+    }
+
+    private Map<AToZKey, AToZ> buildAtoZMap(List<PathElement> elements) {
+        Map<AToZKey, AToZ> map = new HashMap<>();
+        for (PathElement e : elements) {
+            AToZ item = new AToZBuilder().setId(e.id).setResource(e.resource).build();
+            map.put(item.key(), item);
+        }
+        return map;
+    }
+
+    private record PathElement(
+            String id,
+            org.opendaylight.yang.gen.v1.http.org.transportpce.b.c._interface.pathdescription.rev230501
+                    .pce.resource.Resource resource) {}
+
+    private List<PathElement> roadmPathElements() {
+        return List.of(
+                tp("0", "", "SPDR-SA1-XPDR1"),
+                node("1", "SPDR-SA1-XPDR1"),
+                tp("2", "XPDR1-NETWORK1", "SPDR-SA1-XPDR1"),
+                link("3", "SPDR-SA1-XPDR1-XPDR1-NETWORK1toROADM-A1-SRG1-SRG1-PP1-TXRX"),
+                tp("4", "SRG1-PP1-TXRX", "ROADM-A1-SRG1"),
+                node("5", "ROADM-A1-SRG1"),
+                tp("6", "SRG1-CP-TXRX", "ROADM-A1-SRG1"),
+                link("7", "ROADM-A1-SRG1-SRG1-CP-TXRXtoROADM-A1-DEG2-DEG2-CTP-TXRX"),
+                tp("8", "DEG2-CTP-TXRX", "ROADM-A1-DEG2"),
+                node("9", "ROADM-A1-DEG2"),
+                tp("10", "DEG2-TTP-TXRX", "ROADM-A1-DEG2"),
+                link("11", "ROADM-A1-DEG2-DEG2-TTP-TXRXtoROADM-C1-DEG1-DEG1-TTP-TXRX"),
+                tp("12", "DEG1-TTP-TXRX", "ROADM-C1-DEG1"),
+                node("13", "ROADM-C1-DEG1"),
+                tp("14", "DEG1-CTP-TXRX", "ROADM-C1-DEG1"),
+                link("15", "ROADM-C1-DEG1-DEG1-CTP-TXRXtoROADM-C1-SRG1-SRG1-CP-TXRX"),
+                tp("16", "SRG1-CP-TXRX", "ROADM-C1-SRG1"),
+                node("17", "ROADM-C1-SRG1"),
+                tp("18", "SRG1-PP1-TXRX", "ROADM-C1-SRG1"),
+                link("19", "ROADM-C1-SRG1-SRG1-PP1-TXRXtoSPDR-SC1-XPDR1-XPDR1-NETWORK1"),
+                tp("20", "XPDR1-NETWORK1", "SPDR-SC1-XPDR1"),
+                node("21", "SPDR-SC1-XPDR1"),
+                tp("22", "", "SPDR-SC1-XPDR1")
+        );
+    }
+
+    private PathElement node(String id, String nodeId) {
+        return new PathElement(id, new ResourceBuilder()
+                .setResource(new NodeBuilder().setNodeId(nodeId).build())
+                .setState(State.InService)
+                .build());
+    }
+
+    private PathElement tp(String id, String tpId, String tpNodeId) {
+        return new PathElement(id, new ResourceBuilder()
+                .setResource(new TerminationPointBuilder()
+                        .setTpId(tpId)
+                        .setTpNodeId(tpNodeId)
+                        .build())
+                .setState(State.InService)
+                .build());
+    }
+
+    private PathElement link(String id, String linkId) {
+        return new PathElement(id, new ResourceBuilder()
+                .setResource(new LinkBuilder().setLinkId(linkId).build())
+                .setState(State.InService)
+                .build());
+    }
+}
