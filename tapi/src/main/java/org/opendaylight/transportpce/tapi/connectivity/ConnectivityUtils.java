54af1d57cc08060f00b49a414dfe9e9ce3eb0fb8
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2025-10-28 16:07:51 +0100

    Add unit tests for SIP methods
    In ConnectivityUtils class, add unit tests for:
      - getSipIdFromAend(...)
      - getSipIdFromZend(...)
    
    JIRA: TRNSPRTPCE-889
    Change-Id: I7153fbe4ef00de599cdea47030ba02312bdfe67d
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

--- tapi/src/main/java/org/opendaylight/transportpce/tapi/connectivity/ConnectivityUtils.java
+++ tapi/src/main/java/org/opendaylight/transportpce/tapi/connectivity/ConnectivityUtils.java
@@ -2280,7 +2280,7 @@ public final class ConnectivityUtils {
             .build();
     }
 
-    private Uuid getSipIdFromZend(Map<ZToAKey, ZToA> mapztoa, String serviceNodeId, ServiceFormat serviceFormat) {
+    public Uuid getSipIdFromZend(Map<ZToAKey, ZToA> mapztoa, String serviceNodeId, ServiceFormat serviceFormat) {
         if (serviceNodeId.contains("ROADM")) {
             // Service from ROADM to ROADM
             // AddDrop-AddDrop ports --> MC layer SIPs
@@ -2348,7 +2348,7 @@ public final class ConnectivityUtils {
         return null;
     }
 
-    private Uuid getSipIdFromAend(Map<AToZKey, AToZ> mapatoz, String serviceNodeId, ServiceFormat serviceFormat) {
+    public Uuid getSipIdFromAend(Map<AToZKey, AToZ> mapatoz, String serviceNodeId, ServiceFormat serviceFormat) {
         LOG.info("ServiceNode = {} and ServiceFormat = {}", serviceNodeId, serviceFormat.getName());
         LOG.info("Map a to z = {}", mapatoz);
         if (serviceNodeId.contains("ROADM")) {
