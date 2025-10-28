e154ee7d4e66964bb115518d7809b9e3a49a95d0
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2025-10-28 17:36:57 +0100

    Refactor ConnectivityUtils add unit test Phtnc
    Add unit tests for the methods:
      - getConnectionTypePhtnc(...)
      - getServiceFormatPhtnc(...)
    
    JIRA: TRNSPRTPCE-889
    Change-Id: I927dfc33d3be2df07f555ed462651974e10a9cee
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

--- tapi/src/main/java/org/opendaylight/transportpce/tapi/connectivity/ConnectivityUtils.java
+++ tapi/src/main/java/org/opendaylight/transportpce/tapi/connectivity/ConnectivityUtils.java
@@ -2536,7 +2536,7 @@ public final class ConnectivityUtils {
                 .build();
     }
 
-    private ConnectionType getConnectionTypePhtnc(
+    public ConnectionType getConnectionTypePhtnc(
             Collection<org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.connectivity.rev221121
                 .create.connectivity.service.input.EndPoint> endPoints) {
         return endPoints.stream()
@@ -2547,7 +2547,7 @@ public final class ConnectivityUtils {
             : ConnectionType.Infrastructure;
     }
 
-    private ServiceFormat getServiceFormatPhtnc(
+    public ServiceFormat getServiceFormatPhtnc(
             Collection<org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.connectivity.rev221121
                 .create.connectivity.service.input.EndPoint> endPoints) {
         return endPoints.stream()
