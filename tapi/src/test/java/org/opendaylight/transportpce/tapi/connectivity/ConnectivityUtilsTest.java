2bdb5c0443962f26fbc2e8aa2035354697ed619a
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2025-10-28 16:23:37 +0100

    Remove ROADM string check in SIP methods
    In ConnectivityUtils class, updated SIP-related methods to use topology
    lookups instead of checking for the string "ROADM" in node IDs:
      - getSipIdFromAend(...)
      - getSipIdFromZend(...)
    
    JIRA: TRNSPRTPCE-889
    Change-Id: Id18c666ed62e1c2090532b04aea900efeb668d8d
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

--- tapi/src/test/java/org/opendaylight/transportpce/tapi/connectivity/ConnectivityUtilsTest.java
+++ tapi/src/test/java/org/opendaylight/transportpce/tapi/connectivity/ConnectivityUtilsTest.java
@@ -44,6 +44,7 @@ import org.opendaylight.yang.gen.v1.http.org.transportpce.b.c._interface.pathdes
 import org.opendaylight.yang.gen.v1.http.org.transportpce.b.c._interface.pathdescription.rev230501.pce.resource.resource.resource.LinkBuilder;
 import org.opendaylight.yang.gen.v1.http.org.transportpce.b.c._interface.pathdescription.rev230501.pce.resource.resource.resource.NodeBuilder;
 import org.opendaylight.yang.gen.v1.http.org.transportpce.b.c._interface.pathdescription.rev230501.pce.resource.resource.resource.TerminationPointBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.rev180226.networks.Network;
 import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev221121.Uuid;
 import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev221121.tapi.context.ServiceInterfacePoint;
 import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev221121.tapi.context.ServiceInterfacePointBuilder;
@@ -76,7 +77,7 @@ class ConnectivityUtilsTest extends AbstractTest {
     }
 
     @Test
-    void testAtoZXponderType() {
+    void testAtoZXponderType() throws TapiTopologyException {
         ServiceInterfacePoint serviceInterfacePoint = new ServiceInterfacePointBuilder()
                 .setUuid(Uuid.getDefaultInstance("5efda776-f8de-3e0b-9bbd-2c702e210946"))
                 .build();
@@ -90,14 +91,17 @@ class ConnectivityUtilsTest extends AbstractTest {
                 topologyUtils
         );
 
+        Network openroadmTopo = topologyUtils.readTopology(InstanceIdentifiers.OPENROADM_TOPOLOGY_II);
+
         assertNotNull(connectivityUtils.getSipIdFromAend(
                 getAToZRoadmKeyAToZMap(),
                 "SPDR-SA1",
-                ServiceFormat.ODU));
+                ServiceFormat.ODU,
+                openroadmTopo));
     }
 
     @Test
-    void testZtoAXponderType() {
+    void testZtoAXponderType() throws TapiTopologyException {
         ServiceInterfacePoint serviceInterfacePoint = new ServiceInterfacePointBuilder()
                 .setUuid(Uuid.getDefaultInstance("5efda776-f8de-3e0b-9bbd-2c702e210946"))
                 .build();
@@ -111,14 +115,17 @@ class ConnectivityUtilsTest extends AbstractTest {
                 topologyUtils
         );
 
+        Network openroadmTopo = topologyUtils.readTopology(InstanceIdentifiers.OPENROADM_TOPOLOGY_II);
+
         assertNotNull(connectivityUtils.getSipIdFromZend(
                 getZToARoadmKeyZToAMap(),
                 "SPDR-SA1",
-                ServiceFormat.ODU));
+                ServiceFormat.ODU,
+                openroadmTopo));
     }
 
     @Test
-    void testAtoZRoadmType() {
+    void testAtoZRoadmType() throws TapiTopologyException {
         ServiceInterfacePoint serviceInterfacePoint = new ServiceInterfacePointBuilder()
                 .setUuid(Uuid.getDefaultInstance("abbf1503-11aa-3618-8bbd-e33916678dd3"))
                 .build();
@@ -131,15 +138,18 @@ class ConnectivityUtilsTest extends AbstractTest {
                 new Uuid(TapiConstants.T0_FULL_MULTILAYER_UUID),
                 topologyUtils
         );
+
+        Network openroadmTopo = topologyUtils.readTopology(InstanceIdentifiers.OPENROADM_TOPOLOGY_II);
 
         assertNotNull(connectivityUtils.getSipIdFromAend(
                 getAToZRoadmKeyAToZMap(),
                 "ROADM-A1",
-                ServiceFormat.ODU));
+                ServiceFormat.ODU,
+                openroadmTopo));
     }
 
     @Test
-    void testZtoARoadmType() {
+    void testZtoARoadmType() throws TapiTopologyException {
         ServiceInterfacePoint serviceInterfacePoint = new ServiceInterfacePointBuilder()
                 .setUuid(Uuid.getDefaultInstance("abbf1503-11aa-3618-8bbd-e33916678dd3"))
                 .build();
@@ -153,10 +163,13 @@ class ConnectivityUtilsTest extends AbstractTest {
                 topologyUtils
         );
 
+        Network openroadmTopo = topologyUtils.readTopology(InstanceIdentifiers.OPENROADM_TOPOLOGY_II);
+
         assertNotNull(connectivityUtils.getSipIdFromZend(
                 getZToARoadmKeyZToAMap(),
                 "ROADM-A1",
-                ServiceFormat.ODU));
+                ServiceFormat.ODU,
+                openroadmTopo));
     }
 
     @Test
