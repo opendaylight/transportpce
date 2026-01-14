cab5548d4040565f3ebaaa276bfdb5063a6011aa
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2026-01-14 21:12:56 +0100

    TAPI - extract SpectrumCapabilityPac factory
    Follow up on TRNSPRTPCE-921 by further refactoring photonic
    spectrum capability handling. Introduce
    TapiSpectrumCapabilityPacFactory and a default implementation
    to centralize SpectrumCapabilityPac construction.
    
    This moves spectrum capability creation out of
    ORtoTapiTopoConversionTools, completing the separation of
    concerns outlined in the original refactor. Topology conversion
    remains focused on OTS/OMS gating and NEP augmentation.
    
    Behavior is unchanged: a default available spectrum is created
    when no ranges are provided, and supportable spectrum is always
    set.
    
    JIRA: TRNSPRTPCE-921
    Change-Id: I4fb5d3098d9e837290f7ceecf89826348f7434aa
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

--- tapi/src/test/java/org/opendaylight/transportpce/tapi/topology/ORtoTapiTopoConversionToolsTest.java
+++ tapi/src/test/java/org/opendaylight/transportpce/tapi/topology/ORtoTapiTopoConversionToolsTest.java
@@ -24,6 +24,7 @@ import org.opendaylight.transportpce.tapi.frequency.TeraHertzFactory;
 import org.opendaylight.transportpce.tapi.frequency.grid.Math;
 import org.opendaylight.transportpce.tapi.frequency.grid.NumericFrequency;
 import org.opendaylight.transportpce.tapi.frequency.range.FrequencyRangeFactory;
+import org.opendaylight.transportpce.tapi.openroadm.topology.terminationpoint.spectrum.DefaultTapiSpectrumCapabilityPacFactory;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.common.optical.channel.types.rev250110.FrequencyGHz;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.common.optical.channel.types.rev250110.FrequencyTHz;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.network.topology.rev250110.TerminationPoint1;
@@ -65,7 +66,8 @@ class ORtoTapiTopoConversionToolsTest {
                         math
                 ),
                 new FrequencyRangeFactory(),
-                new TeraHertzFactory()
+                new TeraHertzFactory(),
+                new DefaultTapiSpectrumCapabilityPacFactory(new TeraHertzFactory())
         );
 
         byte[] availableFrequencyMap = new byte[96];
