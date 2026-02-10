d34ac05ee197e80b212fab0a5d7c4d98b705d571
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2026-02-10 13:31:17 +0100

    TAPI - Move all listeners into a common package
    Moving all listeners in TAPI to a common package tree. This greatly
    simplifies finding listeners for future developers.
    
    Change-Id: Ia76eb01fcd2dda3a6cb75c982c7fa608479dd5e2
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

--- tapi/src/main/java/org/opendaylight/transportpce/tapi/listeners/topology/TapiOrLinkListener.java
+++ tapi/src/main/java/org/opendaylight/transportpce/tapi/listeners/topology/TapiOrLinkListener.java
@@ -1,11 +1,11 @@
 /*
- * Copyright © 2021 Nokia.  All rights reserved.
+ * Copyright © 2026 Smartoptics and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.transportpce.tapi.topology;
+package org.opendaylight.transportpce.tapi.listeners.topology;
 
 import java.nio.charset.StandardCharsets;
 import java.util.LinkedHashSet;
