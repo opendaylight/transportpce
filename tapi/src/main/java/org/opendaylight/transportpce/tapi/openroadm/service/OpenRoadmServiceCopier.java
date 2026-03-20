6390b3dd27344fd77c97a9a26a43d940ab0496c1
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2026-03-20 15:55:59 +0100

    Improve readability of tapiServiceExists
    Simplify iteration by using map values instead of entrySet and
    avoid inline Objects.requireNonNull by handling null explicitly.
    
    JIRA: TRNSPRTPCE-699
    Change-Id: I29eb508e662d3ca42dca23af7b967a621c9e3419
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

--- tapi/src/main/java/org/opendaylight/transportpce/tapi/openroadm/service/OpenRoadmServiceCopier.java
+++ tapi/src/main/java/org/opendaylight/transportpce/tapi/openroadm/service/OpenRoadmServiceCopier.java
@@ -100,14 +100,20 @@ public class OpenRoadmServiceCopier {
             String openRoadmServiceName,
             Map<ConnectivityServiceKey, ConnectivityService> connectivityServices) {
 
-        for (Map.Entry<ConnectivityServiceKey, ConnectivityService> entry : connectivityServices.entrySet()) {
-            for (Map.Entry<NameKey, Name> nme : Objects.requireNonNull(entry.getValue().getName()).entrySet()) {
-                if (serviceNameEqualsTapiServiceName(openRoadmServiceName, nme.getValue())) {
+        for (ConnectivityService service : connectivityServices.values()) {
+            Map<NameKey, Name> names = service.getName();
+            if (names == null) {
+                continue;
+            }
+
+            for (Name nme : names.values()) {
+                if (serviceNameEqualsTapiServiceName(openRoadmServiceName, nme)) {
                     LOG.info("Service {} already exists in TAPI", openRoadmServiceName);
                     return true;
                 }
             }
         }
+
         return false;
     }
 
