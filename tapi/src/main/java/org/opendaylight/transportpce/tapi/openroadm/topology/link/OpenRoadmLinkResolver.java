644e8a5beeba9d5a684801108c6499b5556745af
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2026-04-09 14:47:54 +0200

    Add OpenROADM link resolver for TAPI link creation
    Introduce a LinkResolver abstraction and provide an
    OpenRoadmLinkResolver implementation to resolve links from the
    openroadm-topology before building TAPI links.
    
    Also add LinkNotFoundException for clearer failure handling,
    extend TapiLink/TapiLinkImpl with a topology-based createTapiLink
    overload, and cover the resolver behavior with unit tests.
    
    JIRA: TRNSPRTPCE-940
    Change-Id: I5166f84253764d408edda1bf804eace5a6a51a1b
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

---
+++ tapi/src/main/java/org/opendaylight/transportpce/tapi/openroadm/topology/link/OpenRoadmLinkResolver.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright © 2026 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.tapi.openroadm.topology.link;
+
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.rev180226.networks.Network;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.topology.rev180226.LinkId;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.topology.rev180226.Network1;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.topology.rev180226.networks.network.Link;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.topology.rev180226.networks.network.LinkKey;
+
+public class OpenRoadmLinkResolver implements LinkResolver {
+
+    @Override
+    public Link resolveLink(
+            String srcOpenRoadmTopologyNodeId,
+            String srcOpenRoadmTopologyTerminationPointId,
+            String destOpenRoadmTopologyNodeId,
+            String destOpenRoadmTopologyTerminationPointId,
+            Network network) {
+
+        if (network == null) {
+            throw new LinkNotFoundException("No network topology supplied");
+        }
+
+        Network1 network1 = network.augmentationOrElseThrow(Network1.class, () -> new LinkNotFoundException(
+                "No topology link data available in network")
+        );
+
+        LinkKey key = linkKey(
+                srcOpenRoadmTopologyNodeId,
+                srcOpenRoadmTopologyTerminationPointId,
+                destOpenRoadmTopologyNodeId,
+                destOpenRoadmTopologyTerminationPointId
+        );
+
+        Link link = network1
+                .nonnullLink()
+                .get(key);
+
+        if (link == null) {
+            throw new LinkNotFoundException(
+                    srcOpenRoadmTopologyNodeId,
+                    srcOpenRoadmTopologyTerminationPointId,
+                    destOpenRoadmTopologyNodeId,
+                    destOpenRoadmTopologyTerminationPointId,
+                    key.getLinkId().getValue());
+        }
+
+        return link;
+    }
+
+    private LinkKey linkKey(
+            String srcOpenRoadmTopologyNodeId,
+            String srcOpenRoadmTopologyTerminationPointId,
+            String destOpenRoadmTopologyNodeId,
+            String destOpenRoadmTopologyTerminationPointId) {
+
+        return new LinkKey(LinkId.getDefaultInstance(linkId(
+                srcOpenRoadmTopologyNodeId,
+                srcOpenRoadmTopologyTerminationPointId,
+                destOpenRoadmTopologyNodeId,
+                destOpenRoadmTopologyTerminationPointId
+        )));
+    }
+
+    private String linkId(
+            String srcOpenRoadmTopologyNodeId,
+            String srcOpenRoadmTopologyTerminationPointId,
+            String destOpenRoadmTopologyNodeId,
+            String destOpenRoadmTopologyTerminationPointId) {
+
+        return "%s-%sto%s-%s".formatted(
+                srcOpenRoadmTopologyNodeId,
+                srcOpenRoadmTopologyTerminationPointId,
+                destOpenRoadmTopologyNodeId,
+                destOpenRoadmTopologyTerminationPointId
+        );
+    }
+}
