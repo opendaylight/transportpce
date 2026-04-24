44e4714853cb46c4fd447dbae5cd7bd64fd70b4c
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2026-04-24 14:50:52 +0200

    Link operational and administrative status
    This commit introduces new classes intended to resolve a links
    administrative and operationa status.
    
    Change-Id: Ic54c8f115eee8f7b9989a70d1392dd437e4ccca4

---
+++ tapi/src/main/java/org/opendaylight/transportpce/tapi/openroadm/topology/link/state/LinkStateAttributes.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright © 2026 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.tapi.openroadm.topology.link.state;
+
+import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev221121.AdministrativeState;
+import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev221121.OperationalState;
+
+public record LinkStateAttributes(AdministrativeState administrativeState, OperationalState operationalState) {
+}
