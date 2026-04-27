fa2c764a9a911ab46fd591bff9468e37fba0a62e
Joakim Törnqvist <joakim.tornqvist@smartoptics.com>
2026-04-27 11:16:02 +0200

    Support prefixed OpenROADM link IDs
    Allow OpenRoadmLinkResolver to use an injected link ID formatter.
    
    Add default and prefix-based formatters so callers can resolve links
    whose topology IDs include protocol prefixes such as OTU4 or ODTU4.
    
    JIRA: TRNSPRTPCE-940
    Change-Id: Icfbfe7b74cc6815734c8efc278e337ef27e05374
    Signed-off-by: Joakim Törnqvist <joakim.tornqvist@smartoptics.com>

---
+++ tapi/src/main/java/org/opendaylight/transportpce/tapi/openroadm/topology/link/format/DefaultLinkIdFormatter.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright © 2026 Smartoptics and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.transportpce.tapi.openroadm.topology.link.format;
+
+public class DefaultLinkIdFormatter implements LinkIdFormatter {
+
+    @Override
+    public String linkId(
+            String srcOpenRoadmTopologyNodeId,
+            String srcOpenRoadmTopologyTerminationPointId,
+            String destOpenRoadmTopologyNodeId,
+            String destOpenRoadmTopologyTerminationPointId) {
+
+        return "%s-%sto%s-%s".formatted(
+                srcOpenRoadmTopologyNodeId,
+                srcOpenRoadmTopologyTerminationPointId,
+                destOpenRoadmTopologyNodeId,
+                destOpenRoadmTopologyTerminationPointId
+        );
+    }
+}
