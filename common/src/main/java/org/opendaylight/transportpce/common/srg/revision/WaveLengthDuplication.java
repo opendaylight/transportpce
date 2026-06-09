798109e6ae1bf34b7f401b0bcd2c9b77a572c0f5
Jimmi Wimmersjö <jimmi.wimmersjo@smartoptics.com>
2026-06-09 07:19:56 +0000

    Adding xpdr-number to the portmapping
    xpdr-number, or nbNumber, should not be parsed from a string id.
    nbNumber is now stored as xpdr-number in the portmapping for further
    use.
    
    Jira: TRNSPRTPCE-962
    Change-Id: Ia753fbddc8321dd878dd362c629a2e5b8a8890a9
    Signed-off-by: Jimmi Wimmersjö <jimmi.wimmersjo@smartoptics.com>

--- common/src/main/java/org/opendaylight/transportpce/common/srg/revision/WaveLengthDuplication.java
+++ common/src/main/java/org/opendaylight/transportpce/common/srg/revision/WaveLengthDuplication.java
@@ -9,8 +9,8 @@
 package org.opendaylight.transportpce.common.srg.revision;
 
 import java.util.Map;
-import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev260529.shared.risk.group.SharedRiskGroup;
-import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev260529.shared.risk.group.SharedRiskGroupKey;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev260608.shared.risk.group.SharedRiskGroup;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev260608.shared.risk.group.SharedRiskGroupKey;
 
 /**
  * This interface is primarily intended to define methods implemented
@@ -21,9 +21,9 @@ import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmappi
 public interface WaveLengthDuplication {
 
     /**
-     * SRG in rev260529.
+     * SRG in rev260608.
      *
-     * @return a map of SharedRiskGroup in rev260529 format
+     * @return a map of SharedRiskGroup in rev260608 format
      */
     Map<SharedRiskGroupKey, SharedRiskGroup> srg();
 
