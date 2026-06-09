c47498ce2d7e2a9bf2c8a57e0264589903856e5e
Jimmi Wimmersjö <jimmi.wimmersjo@smartoptics.com>
2026-06-09 12:10:21 +0000

    Fetch the xpdr-number from the portmapping
    Instead of parse the logicalConnectionPoint string, the xpdr-number
    is now fetched directly from the portmapping.
    
    When a xpdr node is created this number is put into the portmapping.
    
    Jira: TRNSPRTPCE-962
    Change-Id: Ida60bcdfeec5447bb4378318cf559ca8e951817b
    Signed-off-by: Jimmi Wimmersjö <jimmi.wimmersjo@smartoptics.com>

--- tests/transportpce_tests/1.2.1/test04_renderer_service_path_nominal.py
+++ tests/transportpce_tests/1.2.1/test04_renderer_service_path_nominal.py
@@ -77,7 +77,7 @@ class TestTransportPCERenderer(unittest.TestCase):
              'port-admin-state': 'InService', 'supporting-port': '1',
              'port-oper-state': 'InService', 'connection-map-lcp': 'XPDR1-CLIENT1',
              'port-direction': 'bidirectional', 'xpdr-type': 'tpdr',
-             'port-qual': 'xpdr-network', 'lcp-hash-val': 'OSvMgUyP+mE='},
+             'port-qual': 'xpdr-network', 'xpdr-number': 1, 'lcp-hash-val': 'OSvMgUyP+mE='},
             response['nodes'][0]['mapping'])
         self.assertIn(
             {'logical-connection-point': 'XPDR1-CLIENT1',
@@ -85,7 +85,7 @@ class TestTransportPCERenderer(unittest.TestCase):
              'port-admin-state': 'InService', 'supporting-port': 'C1',
              'port-oper-state': 'InService', 'connection-map-lcp': 'XPDR1-NETWORK1',
              'port-direction': 'bidirectional', 'xpdr-type': 'tpdr',
-             'port-qual': 'xpdr-client', 'lcp-hash-val': 'AO9UFkY/TLYw'},
+             'port-qual': 'xpdr-client', 'xpdr-number': 1, 'lcp-hash-val': 'AO9UFkY/TLYw'},
             response['nodes'][0]['mapping'])
 
     def test_05_service_path_create(self):
