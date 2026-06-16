5ca643fd08c85b1fe301b731218c5545fd1544cb
Balagangadhar (Bala) Bathula <bb4341@att.com>
2026-06-16 11:30:46 -0400

    Add support for new OpenROADM models
    - 13.1.1 and 17.1.0
    - Enum 4, 5 respectively in port-mapping API.
    - Update revision statements wherever necessary.
    
    Change-Id: Iac508a0ec2678a0020d3c9e36d2fe14f122526ca
    Signed-off-by: Balagangadhar (Bala) Bathula <bb4341@att.com>

--- common/src/main/java/org/opendaylight/transportpce/common/srg/revision/WaveLengthDuplicationRev200529.java
+++ common/src/main/java/org/opendaylight/transportpce/common/srg/revision/WaveLengthDuplicationRev200529.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev250905.shared.risk.group.SharedRiskGroupBuilder;
+import org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev260612.shared.risk.group.SharedRiskGroupBuilder;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.common.optical.channel.types.rev200529.WavelengthDuplicationType;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.device.rev200529.org.openroadm.device.container.org.openroadm.device.SharedRiskGroup;
 import org.opendaylight.yang.gen.v1.http.org.openroadm.device.rev200529.org.openroadm.device.container.org.openroadm.device.SharedRiskGroupKey;
@@ -27,14 +27,14 @@ public class WaveLengthDuplicationRev200529 implements WaveLengthDuplication {
     }
 
     @Override
-    public Map<org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev250905
+    public Map<org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev260612
                     .shared.risk.group.SharedRiskGroupKey,
-            org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev250905
+            org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev260612
                     .shared.risk.group.SharedRiskGroup> srg() {
 
-        Map<org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev250905
+        Map<org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev260612
                         .shared.risk.group.SharedRiskGroupKey,
-                org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev250905
+                org.opendaylight.yang.gen.v1.http.org.opendaylight.transportpce.portmapping.rev260612
                         .shared.risk.group.SharedRiskGroup> rev250905ShareRiskGroups = new HashMap<>();
 
         for (Map.Entry<SharedRiskGroupKey, SharedRiskGroup> entries : sharedRiskGroupsRev200529.entrySet()) {
